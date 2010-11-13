e6f784aa37b02afa59fca62992168fb2342b52d4
Godin <mandrikov@gmail.com>
2010-11-13 17:10:49 +0000

    Add missing license headers

--- plugins/sonar-squid-java-plugin/src/main/java/org/sonar/java/ast/check/UndocumentedApiCheck.java
+++ plugins/sonar-squid-java-plugin/src/main/java/org/sonar/java/ast/check/UndocumentedApiCheck.java
@@ -1,4 +1,26 @@
+/*
+ * Sonar, open source software quality management tool.
+ * Copyright (C) 2009 SonarSource SA
+ * mailto:contact AT sonarsource DOT com
+ *
+ * Sonar is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Sonar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Sonar; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+
 package org.sonar.java.ast.check;
+
+import java.util.List;
 
 import org.apache.commons.lang.StringUtils;
 import org.sonar.api.utils.WildcardPattern;
@@ -15,13 +37,10 @@ import org.sonar.squid.api.SourceFile;
 
 import com.puppycrawl.tools.checkstyle.api.DetailAST;
 
-import java.util.List;
-
-@Rule(key = "UndocumentedApi", name = "Undocumented API", isoCategory = IsoCategory.Usability, priority = Priority.MAJOR, description = "<p>Check that each public class, interface, method and constructor has a Javadoc comment. " +
-    "The following public methods/constructors are not concerned by this rule :</p>" +
-    "<ul><li>Getter / Setter</li>" +
-    "<li>Method with @Override annotation</li>" +
-    "<li>Empty constructor</li></ul>")
+@Rule(key = "UndocumentedApi", name = "Undocumented API", isoCategory = IsoCategory.Usability, priority = Priority.MAJOR,
+    description = "<p>Check that each public class, interface, method and constructor has a Javadoc comment. "
+        + "The following public methods/constructors are not concerned by this rule :</p>" + "<ul><li>Getter / Setter</li>"
+        + "<li>Method with @Override annotation</li>" + "<li>Empty constructor</li></ul>")
 public class UndocumentedApiCheck extends JavaAstCheck {
 
   @RuleProperty(description = "Optional. If this property is not defined, all classes should adhere to this constraint. Ex : **.api.**")
