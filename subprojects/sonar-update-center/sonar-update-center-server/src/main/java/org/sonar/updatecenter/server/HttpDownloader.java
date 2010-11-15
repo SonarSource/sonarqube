0746d937dfd10730f07b4ac116e228192bea7a9f
simonbrandhof <simon.brandhof@gmail.com>
2010-11-15 16:55:52 +0000

    update center server: fix log

--- subprojects/sonar-update-center/sonar-update-center-server/src/main/java/org/sonar/updatecenter/server/HttpDownloader.java
+++ subprojects/sonar-update-center/sonar-update-center-server/src/main/java/org/sonar/updatecenter/server/HttpDownloader.java
@@ -63,7 +63,7 @@ public class HttpDownloader {
   }
 
   File downloadFile(URI fileURI, File toFile, String login, String password) {
-    LOG.error("Download " + fileURI + " in " + toFile);
+    LOG.info("Download " + fileURI + " in " + toFile);
     DefaultHttpClient client = new DefaultHttpClient();
     try {
       if (StringUtils.isNotBlank(login)) {
