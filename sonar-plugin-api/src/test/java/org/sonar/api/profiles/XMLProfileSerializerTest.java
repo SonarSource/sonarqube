43338cdc403db95555e7f46672729f4c72abb903
Godin <mandrikov@gmail.com>
2010-12-09 09:26:15 +0000

    Minor change

--- sonar-plugin-api/src/test/java/org/sonar/api/profiles/XMLProfileSerializerTest.java
+++ sonar-plugin-api/src/test/java/org/sonar/api/profiles/XMLProfileSerializerTest.java
@@ -80,11 +80,7 @@ public class XMLProfileSerializerTest {
   public void exportAlerts() throws Exception {
     Writer writer = new StringWriter();
     RulesProfile profile = RulesProfile.create("sonar way", "java");
-    Alert alert = new Alert();
-    alert.setMetric(new Metric("coverage"));
-    alert.setOperator(Alert.OPERATOR_SMALLER);
-    alert.setValueError("60");
-    alert.setValueWarning("80");
+    Alert alert = new Alert(profile, new Metric("coverage"), Alert.OPERATOR_SMALLER, "60", "80");
     profile.getAlerts().add(alert);
     new XMLProfileSerializer().write(profile, writer);
 
