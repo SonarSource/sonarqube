08dd37963702aae4122387a37874dbcb6e4f1912
simonbrandhof <simon.brandhof@gmail.com>
2010-12-15 10:59:45 +0000

    fix IT

--- tests/integration/tests/src/it/java/org/sonar/tests/integration/Struts139Test.java
+++ tests/integration/tests/src/it/java/org/sonar/tests/integration/Struts139Test.java
@@ -76,7 +76,7 @@ public class Struts139Test {
     assertThat(getProjectMeasure(CoreMetrics.VIOLATIONS_KEY).getValue(), closeTo(7726.0, 500.0));
     assertThat(getProjectMeasure(CoreMetrics.BLOCKER_VIOLATIONS_KEY).getValue(), closeTo(0.0, 20.0));
     assertThat(getProjectMeasure(CoreMetrics.CRITICAL_VIOLATIONS_KEY).getValue(), closeTo(0.0, 20.0));
-    assertThat(getProjectMeasure(CoreMetrics.MAJOR_VIOLATIONS_KEY).getValue(), closeTo(2569.0, 200.0));
+    assertThat(getProjectMeasure(CoreMetrics.MAJOR_VIOLATIONS_KEY).getValue(), closeTo(2889.0, 200.0));
     assertThat(getProjectMeasure(CoreMetrics.MINOR_VIOLATIONS_KEY).getValue(), closeTo(0.0, 200.0));
     assertThat(getProjectMeasure(CoreMetrics.INFO_VIOLATIONS_KEY).getValue(), closeTo(5157.0, 20.0));
     assertThat(getProjectMeasure(CoreMetrics.VIOLATIONS_DENSITY_KEY).getValue(), closeTo(84.6, 5.0));
