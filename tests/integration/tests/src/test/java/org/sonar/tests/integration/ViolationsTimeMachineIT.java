1a78cd41dcecc85dc35df5066afcac19f6b1c199
Godin <mandrikov@gmail.com>
2010-12-16 02:45:59 +0000

    Add ITs for timemachine web service

--- tests/integration/tests/src/test/java/org/sonar/tests/integration/ViolationsTimeMachineIT.java
+++ tests/integration/tests/src/test/java/org/sonar/tests/integration/ViolationsTimeMachineIT.java
@@ -7,8 +7,10 @@ import org.sonar.wsclient.Sonar;
 import org.sonar.wsclient.services.*;
 
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
+import static org.hamcrest.Matchers.nullValue;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -66,4 +68,38 @@ public class ViolationsTimeMachineIT {
     assertThat(violations.get(2).getCreatedAt().getMonth(), is(10)); // new violation
   }
 
+  // Specific cases for timemachine web service
+
+  @Test
+  public void unknownMetrics() {
+    List<TimeMachineData> snapshots = sonar.findAll(TimeMachineQuery.createForMetrics(PROJECT, "notfound"));
+    assertThat(snapshots.size(), is(2));
+    for (TimeMachineData data : snapshots) {
+      assertThat(data.getValues().get(0), nullValue());
+    }
+
+    snapshots = sonar.findAll(TimeMachineQuery.createForMetrics(PROJECT, CoreMetrics.LINES_KEY, "notfound"));
+    assertThat(snapshots.size(), is(2));
+    for (TimeMachineData data : snapshots) {
+      assertThat(data.getValues().get(1), nullValue());
+    }
+
+    snapshots = sonar.findAll(TimeMachineQuery.createForMetrics(PROJECT));
+    assertThat(snapshots.size(), is(0));
+  }
+
+  @Test
+  public void noDataForInterval() {
+    Date date = new Date();
+    List<TimeMachineData> snapshots = sonar.findAll(TimeMachineQuery.createForMetrics(PROJECT, CoreMetrics.LINES_KEY)
+        .setFrom(date)
+        .setTo(date));
+    assertThat(snapshots.size(), is(0));
+  }
+
+  @Test
+  public void unknownResource() {
+    List<TimeMachineData> snapshots = sonar.findAll(TimeMachineQuery.createForMetrics("notfound:notfound", CoreMetrics.LINES_KEY));
+    assertThat(snapshots.size(), is(0));
+  }
 }
