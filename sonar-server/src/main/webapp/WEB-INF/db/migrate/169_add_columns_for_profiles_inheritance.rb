fdf1dbbcef0ea3b5fb25897bc09bc9267c7c63f9
simonbrandhof <simon.brandhof@gmail.com>
2010-12-23 17:24:24 +0000

    fix migration on oracle

--- sonar-server/src/main/webapp/WEB-INF/db/migrate/169_add_columns_for_profiles_inheritance.rb
+++ sonar-server/src/main/webapp/WEB-INF/db/migrate/169_add_columns_for_profiles_inheritance.rb
@@ -24,7 +24,7 @@
 class AddColumnsForProfilesInheritance < ActiveRecord::Migration
 
   def self.up
-    add_column 'active_rules', 'inheritance', :varchar, :limit => 10, :null => true
+    add_column 'active_rules', 'inheritance', :string, :limit => 10, :null => true
     ActiveRule.reset_column_information
     
     add_column 'rules_profiles', 'parent_name', :string, :limit => 100, :null => true
