2f36cd3a805911fc31650bc1e445b8fc0f06cd7f
simonbrandhof <simon.brandhof@gmail.com>
2011-01-03 16:22:18 +0000

    variation periods: keep period when clicking on a resource from filters + disable treemaps

--- sonar-server/src/main/webapp/WEB-INF/app/helpers/application_helper.rb
+++ sonar-server/src/main/webapp/WEB-INF/app/helpers/application_helper.rb
@@ -306,15 +306,17 @@ module ApplicationHelper
   #
   #
   def link_to_resource(resource, name=nil, options={})
+    period_index=options[:period]
+    period_index=nil if period_index && period_index<=0
     if resource.display_dashboard?
       if options[:dashboard]
-        link_to(name || resource.name, {:overwrite_params => {:controller => 'dashboard', :action => 'index', :id => resource.copy_resource_id || resource.id}}, :title => options[:title])
+        link_to(name || resource.name, {:overwrite_params => {:controller => 'dashboard', :action => 'index', :id => (resource.copy_resource_id||resource.id), :period => period_index}}, :title => options[:title])
       else
         # stay on the same page (for example components)
-        link_to(name || resource.name, {:overwrite_params => {:id => resource.copy_resource_id || resource.id}}, :title => options[:title])
+        link_to(name || resource.name, {:overwrite_params => {:id => (resource.copy_resource_id||resource.id), :period => period_index}}, :title => options[:title])
       end
     else
-      link_to(name || resource.name, {:controller => 'resource', :action => 'index', :id => resource.id}, :popup => ['resource', 'height=800,width=900,scrollbars=1,resizable=1'], :title => options[:title])
+      link_to(name || resource.name, {:controller => 'resource', :action => 'index', :id => resource.id, :period => period_index}, :popup => ['resource', 'height=800,width=900,scrollbars=1,resizable=1'], :title => options[:title])
     end
   end
 
