181202a7bcc7764e16c05f8ad93ef748dcc1985b
simonbrandhof <simon.brandhof@gmail.com>
2011-02-04 11:09:26 +0100

    Fix restoring of backups when rules not found

--- sonar-server/src/main/java/org/sonar/server/configuration/ProfilesBackup.java
+++ sonar-server/src/main/java/org/sonar/server/configuration/ProfilesBackup.java
@@ -113,7 +113,7 @@ public class ProfilesBackup implements Backupable {
   }
 
   private void importActiveRules(RulesDao rulesDao, RulesProfile profile) {
-    for (Iterator<ActiveRule> iar = profile.getActiveRules().iterator(); iar.hasNext();) {
+    for (Iterator<ActiveRule> iar = profile.getActiveRules(true).iterator(); iar.hasNext();) {
       ActiveRule activeRule = iar.next();
       Rule unMarshalledRule = activeRule.getRule();
       Rule matchingRuleInDb = rulesDao.getRuleByKey(unMarshalledRule.getRepositoryKey(), unMarshalledRule.getKey());
