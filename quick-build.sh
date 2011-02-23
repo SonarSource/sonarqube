f1cb208b53b2ca4bf5540d81bb1edbf70c8707da
simonbrandhof <simon.brandhof@gmail.com>
2011-02-23 12:41:19 +0100

    Add a warning when using quick-build.sh

--- quick-build.sh
+++ quick-build.sh
@@ -2,5 +2,11 @@
 
 export MAVEN_OPTS='-Xmx512m'
 
+echo '-------------------------------------------------'
+echo ''
+echo ' WARNING - Sonar will be built for FIREFOX ONLY  '
+echo ''
+echo '-------------------------------------------------'
+
 # it is recommended to use maven 3 for faster builds
 mvn clean install -Dtest=false -DfailIfNoTests=false -Ddev
