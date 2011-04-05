df9f736e0e8c9ad2792a242e03759d59548cd8fc
Evgeny Mandrikov <mandrikov@gmail.com>
2011-04-05 19:54:33 +0400

    Fix typo

--- sonar-plugin-api/src/main/java/org/sonar/api/utils/command/CommandExecutor.java
+++ sonar-plugin-api/src/main/java/org/sonar/api/utils/command/CommandExecutor.java
@@ -107,7 +107,7 @@ public final class CommandExecutor {
           logger.info(line);
         }
       } catch (IOException ioe) {
-        logger.error("Error while reading Obeo analyzer output", ioe);
+        logger.error("Error while reading stream", ioe);
 
       } finally {
         IOUtils.closeQuietly(br);
