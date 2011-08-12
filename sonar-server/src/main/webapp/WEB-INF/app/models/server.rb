9800ed545857ddc349d67ea558aba494fd575bfc
Simon Brandhof <simon.brandhof@gmail.com>
2011-08-12 15:46:47 +0200

    Remove server ID from system info page. Replaced by permanent server key

--- sonar-server/src/main/webapp/WEB-INF/app/models/server.rb
+++ sonar-server/src/main/webapp/WEB-INF/app/models/server.rb
@@ -61,7 +61,6 @@ class Server
     sonar_info=[]
     add_property(sonar_info, 'Server Key') {org.sonar.server.platform.Platform.getServer().getKey()}
     add_property(sonar_info, 'Version') {org.sonar.server.platform.Platform.getServer().getVersion()}
-    add_property(sonar_info, 'ID') {org.sonar.server.platform.Platform.getServer().getId()}
     add_property(sonar_info, 'Database') {"#{jdbc_metadata. getDatabaseProductName()} #{jdbc_metadata. getDatabaseProductVersion()}"}
     add_property(sonar_info, 'Database URL') {sonar_property('sonar.jdbc.url')}
     add_property(sonar_info, 'Database Login') {sonar_property('sonar.jdbc.username')}
