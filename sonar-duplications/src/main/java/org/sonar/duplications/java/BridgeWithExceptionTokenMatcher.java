c7c56a4d5ff5e0721f56c245ef11df52d2cc945c
Evgeny Mandrikov <mandrikov@gmail.com>
2011-09-26 22:16:29 +0400

    SONAR-2837 Fix detection of false-positive duplication in case of initialization of multidimensional array

--- sonar-duplications/src/main/java/org/sonar/duplications/java/BridgeWithExceptionTokenMatcher.java
+++ sonar-duplications/src/main/java/org/sonar/duplications/java/BridgeWithExceptionTokenMatcher.java
@@ -17,28 +17,50 @@
  * License along with Sonar; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
  */
-package org.sonar.duplications.statement.matcher;
+package org.sonar.duplications.java;
 
-import java.util.Collections;
 import java.util.List;
 
+import org.sonar.duplications.statement.matcher.TokenMatcher;
 import org.sonar.duplications.token.Token;
 import org.sonar.duplications.token.TokenQueue;
 
-/**
- * Last token would be returned to the queue.
- */
-public class ForgetLastTokenMatcher extends TokenMatcher {
+public class BridgeWithExceptionTokenMatcher extends TokenMatcher {
 
-  /**
-   * @return always true
-   */
+  private final String lToken;
+  private final String rToken;
+  private final String except;
+
+  public BridgeWithExceptionTokenMatcher(String lToken, String rToken, String except) {
+    if (lToken == null || rToken == null || except == null) {
+      throw new IllegalArgumentException();
+    }
+    this.lToken = lToken;
+    this.rToken = rToken;
+    this.except = except;
+  }
+
   @Override
   public boolean matchToken(TokenQueue tokenQueue, List<Token> matchedTokenList) {
-    int last = matchedTokenList.size() - 1;
-    tokenQueue.pushForward(Collections.singletonList(matchedTokenList.get(last)));
-    matchedTokenList.remove(last);
-    return true;
+    if (!tokenQueue.isNextTokenValue(lToken)) {
+      return false;
+    }
+    int stack = 0;
+    while (tokenQueue.peek() != null) {
+      Token token = tokenQueue.poll();
+      matchedTokenList.add(token);
+      if (lToken.equals(token.getValue())) {
+        stack++;
+      } else if (rToken.equals(token.getValue())) {
+        stack--;
+      } else if (except.equals(token.getValue())) {
+        return false;
+      }
+      if (stack == 0) {
+        return true;
+      }
+    }
+    return false;
   }
 
 }
