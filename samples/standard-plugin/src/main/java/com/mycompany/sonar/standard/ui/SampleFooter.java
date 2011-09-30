dc30e55e56000ad1b44ef6c387e1370885f68c4b
Simon Brandhof <simon.brandhof@gmail.com>
2011-09-30 13:30:22 +0200

    SONAR-2797 end of support of maven archetypes

--- samples/standard-plugin/src/main/java/com/mycompany/sonar/standard/ui/SampleFooter.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.sonar.standard.ui;
-
-import org.sonar.api.web.Footer;
-
-public final class SampleFooter implements Footer {
-  public String getHtml() {
-    return "<p>Sample footer - <em>This is static HTML</em></p>";
-  }
-}
