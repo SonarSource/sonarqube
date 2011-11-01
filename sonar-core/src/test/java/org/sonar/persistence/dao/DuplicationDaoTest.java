b425105dd544757bddf88eb7a17968e69e3ae5ea
Evgeny Mandrikov <mandrikov@gmail.com>
2011-11-01 21:23:29 +0400

    Improve DuplicationDaoTest

--- sonar-core/src/test/java/org/sonar/persistence/dao/DuplicationDaoTest.java
+++ sonar-core/src/test/java/org/sonar/persistence/dao/DuplicationDaoTest.java
@@ -19,28 +19,42 @@
  */
 package org.sonar.persistence.dao;
 
-import com.google.common.collect.Lists;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.commons.io.IOUtils;
+import org.dbunit.Assertion;
 import org.dbunit.DataSourceDatabaseTester;
+import org.dbunit.DatabaseUnitException;
 import org.dbunit.IDatabaseTester;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
+import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.dataset.*;
+import org.dbunit.dataset.filter.DefaultColumnFilter;
+import org.dbunit.dataset.xml.FlatXmlDataSet;
+import org.dbunit.operation.DatabaseOperation;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.sonar.persistence.InMemoryDatabase;
 import org.sonar.persistence.MyBatis;
 import org.sonar.persistence.model.DuplicationUnit;
 
-import java.util.List;
-
-import static org.hamcrest.CoreMatchers.nullValue;
-import static org.junit.Assert.assertThat;
+import com.google.common.collect.Lists;
 
 public class DuplicationDaoTest {
-  protected static IDatabaseTester databaseTester;
+
+  private static IDatabaseTester databaseTester;
   private static InMemoryDatabase database;
   private static DuplicationDao dao;
 
-  @BeforeClass
-  public static void startDatabase() throws Exception {
+  @Before
+  public void startDatabase() throws Exception {
     database = new InMemoryDatabase();
     MyBatis myBatis = new MyBatis(database);
 
@@ -51,12 +65,40 @@ public class DuplicationDaoTest {
     databaseTester = new DataSourceDatabaseTester(database.getDataSource());
   }
 
-  @AfterClass
-  public static void stopDatabase() throws Exception {
+  @After
+  public void stopDatabase() throws Exception {
     if (databaseTester != null) {
       databaseTester.onTearDown();
     }
     database.stop();
+  }
+
+  @Test
+  public void shouldGetByHash() throws Exception {
+    setupData("shouldGetByHash");
+
+    List<DuplicationUnit> blocks = dao.selectCandidates(10, 7);
+    assertThat(blocks.size(), is(1));
+
+    DuplicationUnit block = blocks.get(0);
+    assertThat("block resourceId", block.getResourceKey(), is("bar-last"));
+    assertThat("block hash", block.getHash(), is("aa"));
+    assertThat("block index in file", block.getIndexInFile(), is(0));
+    assertThat("block start line", block.getStartLine(), is(1));
+    assertThat("block end line", block.getEndLine(), is(2));
+
+    // check null for lastSnapshotId
+    blocks = dao.selectCandidates(10, null);
+    assertThat(blocks.size(), is(2));
+  }
+
+  @Test
+  public void shouldInsert() throws Exception {
+    setupData("shouldInsert");
+
+    dao.insert(Arrays.asList(new DuplicationUnit(1, 2, "bb", 0, 1, 2)));
+
+    checkTables("shouldInsert", "duplications_index");
   }
 
   @Test
@@ -71,5 +113,108 @@ public class DuplicationDaoTest {
       // batch insert : faster but generated ids are not returned
       assertThat(duplication.getId(), nullValue());
     }
+  }
+
+  // ============================================================
+  // TODO Godin: a kind of copy-paste from AbstractDbUnitTestCase
+
+  private final void setupData(String... testNames) {
+    InputStream[] streams = new InputStream[testNames.length];
+    try {
+      for (int i = 0; i < testNames.length; i++) {
+        String className = getClass().getName();
+        className = String.format("/%s/%s.xml", className.replace(".", "/"), testNames[i]);
+        streams[i] = getClass().getResourceAsStream(className);
+        if (streams[i] == null) {
+          throw new RuntimeException("Test not found :" + className);
+        }
+      }
+
+      setupData(streams);
+
+    } finally {
+      for (InputStream stream : streams) {
+        IOUtils.closeQuietly(stream);
+      }
+    }
+  }
+
+  private final void setupData(InputStream... dataSetStream) {
+    try {
+      IDataSet[] dataSets = new IDataSet[dataSetStream.length];
+      for (int i = 0; i < dataSetStream.length; i++) {
+        ReplacementDataSet dataSet = new ReplacementDataSet(new FlatXmlDataSet(dataSetStream[i]));
+        dataSet.addReplacementObject("[null]", null);
+        dataSets[i] = dataSet;
+      }
+      CompositeDataSet compositeDataSet = new CompositeDataSet(dataSets);
+
+      databaseTester.setDataSet(compositeDataSet);
+      IDatabaseConnection connection = databaseTester.getConnection();
+
+      DatabaseOperation.CLEAN_INSERT.execute(connection, databaseTester.getDataSet());
+
+      connection.getConnection().commit();
+      connection.close();
+    } catch (Exception e) {
+      throw translateException("Could not setup DBUnit data", e);
+    }
+  }
+
+  private final void checkTables(String testName, String... tables) {
+    checkTables(testName, new String[] {}, tables);
+  }
+
+  private final void checkTables(String testName, String[] excludedColumnNames, String... tables) {
+    // getSession().commit();
+    try {
+      IDataSet dataSet = getCurrentDataSet();
+      IDataSet expectedDataSet = getExpectedData(testName);
+      for (String table : tables) {
+        ITable filteredTable = DefaultColumnFilter.excludedColumnsTable(dataSet.getTable(table), excludedColumnNames);
+        Assertion.assertEquals(expectedDataSet.getTable(table), filteredTable);
+      }
+    } catch (DataSetException e) {
+      throw translateException("Error while checking results", e);
+    } catch (DatabaseUnitException e) {
+      fail(e.getMessage());
+    }
+  }
+
+  private final IDataSet getExpectedData(String testName) {
+    String className = getClass().getName();
+    className = String.format("/%s/%s-result.xml", className.replace(".", "/"), testName);
+
+    InputStream in = getClass().getResourceAsStream(className);
+    try {
+      return getData(in);
+    } finally {
+      IOUtils.closeQuietly(in);
+    }
+  }
+
+  private final IDataSet getData(InputStream stream) {
+    try {
+      ReplacementDataSet dataSet = new ReplacementDataSet(new FlatXmlDataSet(stream));
+      dataSet.addReplacementObject("[null]", null);
+      return dataSet;
+    } catch (Exception e) {
+      throw translateException("Could not read the dataset stream", e);
+    }
+  }
+
+  private final IDataSet getCurrentDataSet() {
+    try {
+      IDatabaseConnection connection = databaseTester.getConnection();
+      return connection.createDataSet();
+    } catch (Exception e) {
+      throw translateException("Could not create the current dataset", e);
+    }
+  }
+
+  private static RuntimeException translateException(String msg, Exception cause) {
+    RuntimeException runtimeException = new RuntimeException(String.format("%s: [%s] %s", msg, cause.getClass().getName(), cause.getMessage()));
+    runtimeException.setStackTrace(cause.getStackTrace());
+    return runtimeException;
   }
 }
