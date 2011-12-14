62a1cb35eae3d226c64220139dd45fad20fe9082
Evgeny Mandrikov <mandrikov@gmail.com>
2011-12-14 18:29:18 +0400

    Fix test

--- sonar-duplications/src/test/java/org/sonar/duplications/detector/suffixtree/SuffixTreeCloneDetectionAlgorithmTest.java
+++ sonar-duplications/src/test/java/org/sonar/duplications/detector/suffixtree/SuffixTreeCloneDetectionAlgorithmTest.java
@@ -69,7 +69,7 @@ public class SuffixTreeCloneDetectionAlgorithmTest extends DetectorTestCase {
     CloneIndex index = createIndex();
     List<Block> fileBlocks = Lists.newArrayList();
     int indexInFile = 0;
-    for (int i = 0; i < 5000; i++) {
+    for (int i = 0; i < 2000; i++) {
       Block block = newBlock("x", new ByteArray("01"), indexInFile);
       fileBlocks.add(block);
       indexInFile++;
