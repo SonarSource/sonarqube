f9939c9840d1d0e6e24eebeb5a9309aa1ebba6a8
Fabrice Bellingard <bellingard@gmail.com>
2011-12-22 19:15:38 +0100

    SONAR-2662 Fix some issues

--- sonar-server/src/main/webapp/WEB-INF/app/controllers/resource_controller.rb
+++ sonar-server/src/main/webapp/WEB-INF/app/controllers/resource_controller.rb
@@ -321,7 +321,7 @@ class ResourceController < ApplicationController
     @global_violations=[]
     @expandable=(@lines!=nil)
     @filtered=!@expanded
-    @action_plans_size=ActionPlan.open_by_project_id(@snapshot.root_project_id).size
+    @action_plans_size=ActionPlan.count_open_for_project_id(@snapshot.root_project_id)
 
     conditions='snapshot_id=?'
     values=[@snapshot.id]
