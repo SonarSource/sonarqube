1f6df54ae158856115e3235d8dfdfacbd30c85f6
Fabrice Bellingard <bellingard@gmail.com>
2012-01-17 12:37:46 +0100

    SONAR-2940 Filters: Unable to sort the "language" column on both ways

--- sonar-server/src/main/webapp/WEB-INF/app/helpers/filters_helper.rb
+++ sonar-server/src/main/webapp/WEB-INF/app/helpers/filters_helper.rb
@@ -83,6 +83,9 @@ module FiltersHelper
 
     elsif filter.sorted_column.on_version?
       java_filter.setSortedByVersion()
+  
+      elsif filter.sorted_column.on_language?
+        java_filter.setSortedByLanguage()
 
     elsif filter.sorted_column.on_metric? && filter.sorted_column.metric
       metric=filter.sorted_column.metric
