e8631ce08c4f566cbe73b5b9542392c727027e79
Simon Brandhof <simon.brandhof@gmail.com>
2012-02-01 14:07:25 +0100

    SONAR-3230 Stolen user session

--- sonar-server/src/main/webapp/WEB-INF/config/environment.rb
+++ sonar-server/src/main/webapp/WEB-INF/config/environment.rb
@@ -38,10 +38,8 @@ Rails::Initializer.run do |config|
   
   config.i18n.default_locale = :en
 
-  # Use the database for sessions instead of the cookie-based default,
-  # which shouldn't be used to store highly confidential information
-  # (create the session table with "rake db:sessions:create")
-  # config.action_controller.session_store = :active_record_store
+  # Provided by JRuby-Rack
+  config.action_controller.session_store = :java_servlet_store
 
   # Use SQL instead of Active Record's schema dumper when creating the test database.
   # This is necessary if your schema can't be completely dumped by the schema dumper,
