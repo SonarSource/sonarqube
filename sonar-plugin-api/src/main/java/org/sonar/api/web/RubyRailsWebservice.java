97d10d24beb3eceae9a53be96518cb30870e1d0c
Simon Brandhof <simon.brandhof@gmail.com>
2012-02-02 09:46:37 +0100

    Update javadoc of RubyRailsWebservice.java

--- sonar-plugin-api/src/main/java/org/sonar/api/web/RubyRailsWebservice.java
+++ sonar-plugin-api/src/main/java/org/sonar/api/web/RubyRailsWebservice.java
@@ -21,14 +21,12 @@ package org.sonar.api.web;
 
 
 /**
- * <b>EXPERIMENTAL</b>
- * <p/>
- * Interface to create a ruby web service extension point using the Ruby On Rails controller API (ActionController)
+ * Interface to create a web service by implementing a Ruby On Rails controller.
  * The method getTemplate() return the ROR controller code, the name of the controller class defined in the template
  * MUST match the following name scheme : Api::$Webservice.getId()Controller I.E : Webservice.getId() = TestWS > Api::TestWSController.
  * The plugin will be deployed with the following URL scheme: http://sonarhost/api/plugins/$Webservice.getId()/:action/:id
  * :action is the name of the controller method to call, :id is a param that will be passed to the controller, these 2 params are not mandatory
- * and will call the index controler method if not specified.
+ * and will call the index controller method if not specified.
  *
  * @since 1.11
  */
