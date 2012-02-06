f5b64feae3f60434143ec36337adbd316b1071f3
simonbrandhof <simon.brandhof@gmail.com>
2012-02-06 23:55:03 +0100

    Do not display the message "Logged in"

--- sonar-server/src/main/webapp/WEB-INF/app/controllers/sessions_controller.rb
+++ sonar-server/src/main/webapp/WEB-INF/app/controllers/sessions_controller.rb
@@ -33,7 +33,6 @@ class SessionsController < ApplicationController
         self.current_user.remember_me
         cookies[:auth_token] = { :value => self.current_user.remember_token , :expires => self.current_user.remember_token_expires_at }
       end
-      flash[:notice] = message('session.flash_notice.logged_in')
       redirect_back_or_default(home_url)
     else
       flash.now[:loginerror] = message('session.flash_notice.authentication_failed')
