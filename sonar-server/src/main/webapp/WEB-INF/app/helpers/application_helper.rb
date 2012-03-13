61219df99b0c4770ccd5a819de52f1f8bb4c4e52
Fabrice Bellingard <bellingard@gmail.com>
2012-03-13 10:51:01 +0100

    SONAR-3243 Rollback popup removal and put it in seperate GitHub repo
    => See https://github.com/bellingard/sonar/tree/popup-removal to 
       test the removal of automatic-popup links

--- sonar-server/src/main/webapp/WEB-INF/app/helpers/application_helper.rb
+++ sonar-server/src/main/webapp/WEB-INF/app/helpers/application_helper.rb
@@ -320,17 +320,26 @@ module ApplicationHelper
 
   #
   #
-  # link to the current page with the given resource.
+  # link to the current page with the given resource. If file, then open a popup to display resource viewers.
   #
   #
   def link_to_resource(resource, name=nil, options={})
     period_index=options[:period]
     period_index=nil if period_index && period_index<=0
-    if options[:line]
-      anchor= 'L' + options[:line].to_s
+    if resource.display_dashboard?
+      if options[:dashboard]
+        link_to(name || resource.name, {:overwrite_params => {:controller => 'dashboard', :action => 'index', :id => resource.id, :period => period_index,
+                                                              :tab => options[:tab], :rule => options[:rule]}}, :title => options[:title])
+      else
+        # stay on the same page (for example components)
+        link_to(name || resource.name, {:overwrite_params => {:id => resource.id, :period => period_index, :tab => options[:tab], :rule => options[:rule]}}, :title => options[:title])
+      end
+    else
+      if options[:line]
+        anchor= 'L' + options[:line].to_s
+      end
+      link_to(name || resource.name, {:controller => 'resource', :action => 'index', :anchor => anchor, :id => resource.id, :period => period_index, :tab => options[:tab], :rule => options[:rule], :metric => options[:metric]}, :popup => ['resource', 'height=800,width=900,scrollbars=1,resizable=1'], :title => options[:title])
     end
-    link_to(name || resource.name, {:controller => 'dashboard', :action => 'index', :id => resource.id, :period => period_index,
-                                    :tab => options[:tab], :rule => options[:rule], :anchor => anchor}, :title => options[:title])
   end
 
 
