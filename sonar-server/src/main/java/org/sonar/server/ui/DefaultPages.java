e530b246811acbaac5771833d1b7f42993bf80e3
David Gageot <david@gageot.net>
2012-05-01 16:49:48 +0200

    FIX Review #1929

--- sonar-server/src/main/java/org/sonar/server/ui/DefaultPages.java
+++ sonar-server/src/main/java/org/sonar/server/ui/DefaultPages.java
@@ -97,7 +97,7 @@ public final class DefaultPages {
       CoreMetrics.NEW_MINOR_VIOLATIONS_KEY, CoreMetrics.NEW_INFO_VIOLATIONS_KEY, CoreMetrics.ACTIVE_REVIEWS_KEY, CoreMetrics.UNASSIGNED_REVIEWS_KEY,
       CoreMetrics.UNPLANNED_REVIEWS_KEY, CoreMetrics.FALSE_POSITIVE_REVIEWS_KEY, CoreMetrics.UNREVIEWED_VIOLATIONS_KEY, CoreMetrics.NEW_UNREVIEWED_VIOLATIONS_KEY})
   @ResourceQualifier({Qualifiers.VIEW, Qualifiers.SUBVIEW, Qualifiers.PROJECT, Qualifiers.MODULE, Qualifiers.PACKAGE, Qualifiers.DIRECTORY, Qualifiers.FILE, Qualifiers.CLASS,
-	  Qualifiers.UNIT_TEST_FILE})
+    Qualifiers.UNIT_TEST_FILE})
   @UserRole(UserRole.CODEVIEWER)
   private static final class ViolationsTab implements RubyRailsPage {
     public String getTemplate() {
