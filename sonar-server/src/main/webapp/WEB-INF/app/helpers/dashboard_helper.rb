4b5fad1f58747fc904b025bfd856738beac863bb
David Gageot <david@gageot.net>
2012-05-03 15:34:35 +0200

    SONAR-1927 Dashboard and Detached controller and views are now almost the same

--- sonar-server/src/main/webapp/WEB-INF/app/helpers/dashboard_helper.rb
+++ sonar-server/src/main/webapp/WEB-INF/app/helpers/dashboard_helper.rb
@@ -21,59 +21,63 @@ module DashboardHelper
   include WidgetPropertiesHelper
   include MetricsHelper
 
+  def dashboard_action(action_name, opts={})
+    { :action => action_name, :did => @dashboard.id, :id => @resource.id }.merge!(opts)
+  end
+
   def formatted_value(measure, default='')
-    measure ? measure.formatted_value : default
+	measure ? measure.formatted_value : default
   end
 
   def measure(metric_key)
-    @snapshot.measure(metric_key)
+	@snapshot.measure(metric_key)
   end
 
   def period_select_options(snapshot, index)
-    label=period_label(snapshot, index)
-    if label
-      selected=(params[:period]==index.to_s ? 'selected' : '')
-      "<option value='#{index}' #{selected}>&Delta; #{label}</option>"
-    else
-      nil
-    end
+	label=period_label(snapshot, index)
+	if label
+	  selected=(params[:period]==index.to_s ? 'selected' : '')
+	  "<option value='#{index}' #{selected}>&Delta; #{label}</option>"
+	else
+	  nil
+	end
   end
 
   def violation_period_select_options(snapshot, index)
-    return nil if snapshot.nil? || snapshot.project_snapshot.nil?
-    mode=snapshot.project_snapshot.send "period#{index}_mode"
-    mode_param=snapshot.project_snapshot.send "period#{index}_param"
-    date=snapshot.project_snapshot.send "period#{index}_date"
+	return nil if snapshot.nil? || snapshot.project_snapshot.nil?
+	mode=snapshot.project_snapshot.send "period#{index}_mode"
+	mode_param=snapshot.project_snapshot.send "period#{index}_param"
+	date=snapshot.project_snapshot.send "period#{index}_date"
 
-    if mode
-      if mode=='days'
-        label = message('added_over_x_days', :params => mode_param.to_s)
-      elsif mode=='version'
-        label = message('added_since_version', :params => mode_param.to_s)
-      elsif mode=='previous_analysis'
-        if !date.nil?
-          label = message('added_since_previous_analysis_detailed', :params => date.strftime("%Y %b. %d").to_s)
-        else
-          label = message('added_since_previous_analysis')
-        end
-      elsif mode=='date'
-        label = message('added_since', :params => date.strftime("%Y %b %d").to_s)
-      end
-      if label
-        selected=(params[:period]==index.to_s ? 'selected' : '')
-        "<option value='#{index}' #{selected}>#{label}</option>"
-      end
-    else
-      nil
-    end
+	if mode
+	  if mode=='days'
+		label = message('added_over_x_days', :params => mode_param.to_s)
+	  elsif mode=='version'
+		label = message('added_since_version', :params => mode_param.to_s)
+	  elsif mode=='previous_analysis'
+		if !date.nil?
+		  label = message('added_since_previous_analysis_detailed', :params => date.strftime("%Y %b. %d").to_s)
+		else
+		  label = message('added_since_previous_analysis')
+		end
+	  elsif mode=='date'
+		label = message('added_since', :params => date.strftime("%Y %b %d").to_s)
+	  end
+	  if label
+		selected=(params[:period]==index.to_s ? 'selected' : '')
+		"<option value='#{index}' #{selected}>#{label}</option>"
+	  end
+	else
+	  nil
+	end
 
   end
 
   def measure_or_variation_value(measure)
-    if measure
-      @period_index ? measure.variation(@period_index) : measure.value
-    else
-      nil
-    end
+	if measure
+	  @period_index ? measure.variation(@period_index) : measure.value
+	else
+	  nil
+	end
   end
 end
