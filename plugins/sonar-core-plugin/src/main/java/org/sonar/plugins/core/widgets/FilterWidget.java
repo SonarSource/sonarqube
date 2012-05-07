81d81bfcaffb1f738a830a7ef007bd1347cd5317
David Gageot <david@gageot.net>
2012-05-07 16:08:30 +0200

    We should be able to select with filter to display in the Filter Widget

--- plugins/sonar-core-plugin/src/main/java/org/sonar/plugins/core/widgets/FilterWidget.java
+++ plugins/sonar-core-plugin/src/main/java/org/sonar/plugins/core/widgets/FilterWidget.java
@@ -19,6 +19,10 @@
  */
 package org.sonar.plugins.core.widgets;
 
+import org.sonar.api.web.WidgetProperties;
+import org.sonar.api.web.WidgetProperty;
+import org.sonar.api.web.WidgetPropertyType;
+
 import org.sonar.api.web.AbstractRubyTemplate;
 import org.sonar.api.web.RubyRailsWidget;
 import org.sonar.api.web.WidgetCategory;
@@ -26,6 +30,9 @@ import org.sonar.api.web.WidgetGlobal;
 
 @WidgetCategory("Beta")
 @WidgetGlobal
+@WidgetProperties({
+  @WidgetProperty(key = "filter", type = WidgetPropertyType.INTEGER, defaultValue = "1")
+})
 public class FilterWidget extends AbstractRubyTemplate implements RubyRailsWidget {
 
   public String getId() {
