bc94e150fe4b4ca98c32e291e802aebae08a20d9
David Gageot <david@gageot.net>
2012-07-16 14:02:20 +0200

    Dead code

--- sonar-core/src/main/java/org/sonar/core/persistence/DatabaseUtils.java
+++ sonar-core/src/main/java/org/sonar/core/persistence/DatabaseUtils.java
@@ -23,11 +23,6 @@ package org.sonar.core.persistence;
  * @since 2.13
  */
 public final class DatabaseUtils {
-  /**
-   * Oracle: number of elements in IN statements is limited.
-   */
-  public static final int MAX_IN_ELEMENTS = 1000;
-
   private DatabaseUtils() {
   }
 
