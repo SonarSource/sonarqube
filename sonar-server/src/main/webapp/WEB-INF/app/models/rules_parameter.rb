SonarSource/sonarqube

commit 672f34433e52f15d851dff4978878887e69c2969

Improve slow profile page

sonar-server/src/main/webapp/WEB-INF/app/models/rules_parameter.rb
@@ -34,6 +34,12 @@ class RulesParameter < ActiveRecord::Base
   end
 
   def description
+    description(rule)
+  end
+
+  # We provide the rule as parameter to avoid reloading this rule_parameter's rule.
+  # This hack would be useless if we could use :inverse_of on :rule
+  def description(rule)
     @l10n_description ||=
       begin
         result = Java::OrgSonarServerUi::JRubyFacade.getInstance().getRuleParamDescription(I18n.locale, rule.repository_key, rule.plugin_rule_key, name())

sonar-server/src/main/webapp/WEB-INF/app/controllers/rules_configuration_controller.rb
@@ -342,7 +342,7 @@ class RulesConfigurationController < ApplicationController
     note.text = params[:note]
     note.user_login = current_user.login
     note.save!
-    render :partial => 'active_rule_note', :locals => {:active_rule => active_rule, :is_admin => true } 
+    render :partial => 'active_rule_note', :locals => {:active_rule => active_rule, :is_admin => true, :profile => active_rule.rules_profile }
   end
 
   
@@ -350,7 +350,7 @@ class RulesConfigurationController < ApplicationController
     active_rule = ActiveRule.find(params[:active_rule_id])
     active_rule.note.destroy if active_rule.note
     active_rule.note = nil
-    render :partial => 'active_rule_note', :locals => {:active_rule => active_rule, :is_admin => true }
+    render :partial => 'active_rule_note', :locals => {:active_rule => active_rule, :is_admin => true, :profile => active_rule.rules_profile }
   end
   
   

sonar-server/src/main/webapp/WEB-INF/app/views/rules_configuration/_active_rule_note.html.erb
@@ -1,4 +1,4 @@
-<% #locals = active_rule, is_admin
+<% #locals = active_rule, is_admin, profile
   note = active_rule.note
   active_note_detail_div_id = "and_" + active_rule.id.to_s
   add_active_note_button_id = "aanb_" + active_rule.id.to_s
@@ -28,7 +28,7 @@
       </cite>
       <p><%= note.html_text -%></p>
     </blockquote>
-  <% elsif is_admin && !active_rule.rules_profile.provided? %>
+  <% elsif is_admin && !profile.provided? %>
     <a href="#" onclick="$('<%= active_note_form_div_id -%>').show();$('<%= active_note_detail_div_id -%>').hide();$('<%= active_note_textarea_id -%>').focus(); return false;"
        class="link-action spacer-right" id="<%= add_active_note_button_id -%>"><%= message('rules_configuration.add_note') -%></a>
   <% end %>

sonar-server/src/main/webapp/WEB-INF/app/views/rules_configuration/_rule.html.erb
@@ -85,7 +85,7 @@
 
     <% if active_rule %>
       <div id="active_rule_note_<%= active_rule.id -%>">
-        <%= render :partial => 'active_rule_note', :locals => {:active_rule => active_rule, :is_admin => is_admin} %>
+        <%= render :partial => 'active_rule_note', :locals => {:active_rule => active_rule, :is_admin => is_admin, :profile => profile} %>
       </div>
     <% end %>
 

sonar-server/src/main/webapp/WEB-INF/app/views/rules_configuration/_rule_param.html.erb
@@ -40,7 +40,7 @@
     <% end %>
   <% end %>
 
-  <div class="form-val-note"><%= h(parameter.description || "") -%> <%= ('(' + readable_type(parameter.param_type) + ')') if !readable_type(parameter.param_type).empty? -%></div>
+  <div class="form-val-note"><%= h(parameter.description(rule) || "") -%> <%= ('(' + readable_type(parameter.param_type) + ')') if !readable_type(parameter.param_type).empty? -%></div>
 
   </form>
 </td>

commit 0bbb1d5fd848c45ab7c11a50d5fb547dc3e54f07

Fix regression introduced in 672f34433e52f15d851dff4978878887e69c2969

sonar-server/src/main/webapp/WEB-INF/app/models/rules_parameter.rb
@@ -33,16 +33,12 @@ class RulesParameter < ActiveRecord::Base
     param_type[2, param_type.length-3].split(",")
   end
 
-  def description
-    description(rule)
-  end
-
-  # We provide the rule as parameter to avoid reloading this rule_parameter's rule.
+  # We can provide the rule as parameter to avoid reloading this rule_parameter's rule.
   # This hack would be useless if we could use :inverse_of on :rule
-  def description(rule)
+  def description(param_rule = rule)
     @l10n_description ||=
       begin
-        result = Java::OrgSonarServerUi::JRubyFacade.getInstance().getRuleParamDescription(I18n.locale, rule.repository_key, rule.plugin_rule_key, name())
+        result = Java::OrgSonarServerUi::JRubyFacade.getInstance().getRuleParamDescription(I18n.locale, param_rule.repository_key, param_rule.plugin_rule_key, name())
         result = read_attribute(:description) unless result
         result
       end