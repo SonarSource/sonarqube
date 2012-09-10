dabffb94a652d0f79e6120bf679271407b9b3fe7
David Gageot <david@gageot.net>
2012-09-10 20:51:24 +0200

    SONAR-3776 Provide a WS Api to validate a user's login/password

--- sonar-server/src/main/webapp/WEB-INF/app/controllers/api/authentication_controller.rb
+++ sonar-server/src/main/webapp/WEB-INF/app/controllers/api/authentication_controller.rb
@@ -30,7 +30,10 @@ class Api::AuthenticationController < Api::ApiController
   def validate
     hash={:valid => valid?}
 
+    # make sure no authentication information is left by
+    # this validation 
     reset_session
+    cookies[:auth_token]
 
     respond_to do |format|
       format.json { render :json => jsonp(hash) }
