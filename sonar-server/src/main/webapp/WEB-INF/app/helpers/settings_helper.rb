3582904207a87f818846fbc7cce7c50d5194771f
David Gageot <david@gageot.net>
2012-10-05 17:11:39 +0200

    Sort properties by name

--- sonar-server/src/main/webapp/WEB-INF/app/helpers/settings_helper.rb
+++ sonar-server/src/main/webapp/WEB-INF/app/helpers/settings_helper.rb
@@ -77,8 +77,12 @@ module SettingsHelper
     property.getType()
   end
 
-  def by_name(categories)
+  def by_category_name(categories)
     Api::Utils.insensitive_sort(categories) { |category| category_name(category) }
+  end
+
+  def by_property_name(properties)
+    Api::Utils.insensitive_sort(properties) { |property| property_name(property) }
   end
 
   def input_name(property)
