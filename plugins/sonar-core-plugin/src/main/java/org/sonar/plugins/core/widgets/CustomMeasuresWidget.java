a4ac34cfc3057e74095b688065950ba7684765f7
Fabrice Bellingard <fabrice.bellingard@sonarsource.com>
2012-10-08 11:48:20 +0200

    SONAR-3651 Make the "Custom Measure" widget support diff. views

--- plugins/sonar-core-plugin/src/main/java/org/sonar/plugins/core/widgets/CustomMeasuresWidget.java
+++ plugins/sonar-core-plugin/src/main/java/org/sonar/plugins/core/widgets/CustomMeasuresWidget.java
@@ -19,22 +19,25 @@
  */
 package org.sonar.plugins.core.widgets;
 
-import org.sonar.api.web.*;
+import org.sonar.api.web.AbstractRubyTemplate;
+import org.sonar.api.web.RubyRailsWidget;
+import org.sonar.api.web.WidgetProperties;
+import org.sonar.api.web.WidgetProperty;
+import org.sonar.api.web.WidgetPropertyType;
 
 @WidgetProperties(
-    {
-        @WidgetProperty(key = "metric1", type = WidgetPropertyType.METRIC),
-        @WidgetProperty(key = "metric2", type = WidgetPropertyType.METRIC),
-        @WidgetProperty(key = "metric3", type = WidgetPropertyType.METRIC),
-        @WidgetProperty(key = "metric4", type = WidgetPropertyType.METRIC),
-        @WidgetProperty(key = "metric5", type = WidgetPropertyType.METRIC),
-        @WidgetProperty(key = "metric6", type = WidgetPropertyType.METRIC),
-        @WidgetProperty(key = "metric7", type = WidgetPropertyType.METRIC),
-        @WidgetProperty(key = "metric8", type = WidgetPropertyType.METRIC),
-        @WidgetProperty(key = "metric9", type = WidgetPropertyType.METRIC),
-        @WidgetProperty(key = "metric10", type = WidgetPropertyType.METRIC)
-    }
-)
+{
+  @WidgetProperty(key = "metric1", type = WidgetPropertyType.METRIC, options = {WidgetsConstants.FILTER_OUT_NEW_METRICS}),
+  @WidgetProperty(key = "metric2", type = WidgetPropertyType.METRIC, options = {WidgetsConstants.FILTER_OUT_NEW_METRICS}),
+  @WidgetProperty(key = "metric3", type = WidgetPropertyType.METRIC, options = {WidgetsConstants.FILTER_OUT_NEW_METRICS}),
+  @WidgetProperty(key = "metric4", type = WidgetPropertyType.METRIC, options = {WidgetsConstants.FILTER_OUT_NEW_METRICS}),
+  @WidgetProperty(key = "metric5", type = WidgetPropertyType.METRIC, options = {WidgetsConstants.FILTER_OUT_NEW_METRICS}),
+  @WidgetProperty(key = "metric6", type = WidgetPropertyType.METRIC, options = {WidgetsConstants.FILTER_OUT_NEW_METRICS}),
+  @WidgetProperty(key = "metric7", type = WidgetPropertyType.METRIC, options = {WidgetsConstants.FILTER_OUT_NEW_METRICS}),
+  @WidgetProperty(key = "metric8", type = WidgetPropertyType.METRIC, options = {WidgetsConstants.FILTER_OUT_NEW_METRICS}),
+  @WidgetProperty(key = "metric9", type = WidgetPropertyType.METRIC, options = {WidgetsConstants.FILTER_OUT_NEW_METRICS}),
+  @WidgetProperty(key = "metric10", type = WidgetPropertyType.METRIC, options = {WidgetsConstants.FILTER_OUT_NEW_METRICS})
+})
 public class CustomMeasuresWidget extends AbstractRubyTemplate implements RubyRailsWidget {
   public String getId() {
     return "custom_measures";
