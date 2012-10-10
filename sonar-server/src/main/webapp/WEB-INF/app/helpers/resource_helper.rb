92da0a8b2b2cad6e4604b646451a2a3ed3a67461
David Gageot <david@gageot.net>
2012-10-10 09:14:07 +0200

    SONAR-2804 Restore metric id for its

--- sonar-server/src/main/webapp/WEB-INF/app/helpers/resource_helper.rb
+++ sonar-server/src/main/webapp/WEB-INF/app/helpers/resource_helper.rb
@@ -20,15 +20,26 @@
 module ResourceHelper
   DUPLICATION_SNIPPET_DEFAULT_NB_OF_LINES = 6
 
-  def format(new, prefix, measure_name)
+  def format(new, prefix, measure_name, options = {})
     if new
-      format_variation(measure('new_' + prefix + measure_name), :period => @period, :default => '-', :style => 'none')
+      key = 'new_' + prefix + measure_name
+      value = format_variation(measure(key), :period => @period, :default => '-', :style => 'none')
     else
-      measure(prefix + measure_name).formatted_value
+      key = prefix + measure_name
+      value = measure(key).formatted_value
+    end
+
+    if options[:span]
+      "<span id=\"m_#{key}\">#{value}</span>"
+    else
+      value
     end
   end
 
   def format_ratio(new, prefix, measure_name1, measure_name2)
-    '(' + (format(new, prefix, measure_name1).to_i - format(new, prefix, measure_name2).to_i).to_s + '/' + format(new, prefix, measure_name1) + ')'
+    value1 = format(new, prefix, measure_name1)
+    value2 = format(new, prefix, measure_name2)
+
+    '(' + (value1.to_i - value2.to_i).to_s + '/' + value1 + ')'
   end
 end
