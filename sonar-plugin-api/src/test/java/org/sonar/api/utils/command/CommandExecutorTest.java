7c3bda2872be16a28df497a3565063d1a97ca929
Simon Brandhof <simon.brandhof@gmail.com>
2012-10-10 22:38:33 +0200

    Fix test on MSWindows

--- sonar-plugin-api/src/test/java/org/sonar/api/utils/command/CommandExecutorTest.java
+++ sonar-plugin-api/src/test/java/org/sonar/api/utils/command/CommandExecutorTest.java
@@ -117,7 +117,7 @@ public class CommandExecutorTest {
     File logFile = new File(workDir, "echo.log");
     assertThat(logFile).exists();
     String log = FileUtils.readFileToString(logFile);
-    assertThat(log).contains(workDir.getCanonicalPath());
+    assertThat(log).contains(workDir.getAbsolutePath());
     assertThat(log).contains("Parameter: 1");
     assertThat(log).contains("Environment variable: 2");
   }
