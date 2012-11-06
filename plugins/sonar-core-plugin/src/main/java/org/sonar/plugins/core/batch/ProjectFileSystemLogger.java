14de16c62104cff7d8cb0a272b1f3abb29458f6e
David Gageot <david@gageot.net>
2012-11-06 13:00:26 +0100

    SONAR-3885 Exclusion patterns should support negation

--- plugins/sonar-core-plugin/src/main/java/org/sonar/plugins/core/batch/ProjectFileSystemLogger.java
+++ plugins/sonar-core-plugin/src/main/java/org/sonar/plugins/core/batch/ProjectFileSystemLogger.java
@@ -33,7 +33,9 @@ public class ProjectFileSystemLogger extends Initializer {
 
   @Override
   public void execute(Project project) {
+    logExclusionPatterns("Included sources: {}", project.getInclusionPatterns());
     logExclusionPatterns("Excluded sources: {}", project.getExclusionPatterns());
+    logExclusionPatterns("Included tests: {}", project.getTestExclusionPatterns());
     logExclusionPatterns("Excluded tests: {}", project.getTestExclusionPatterns());
 
     ProjectFileSystem projectFileSystem = project.getFileSystem();
--- sonar-plugin-api/src/main/java/org/sonar/api/resources/Project.java
+++ sonar-plugin-api/src/main/java/org/sonar/api/resources/Project.java
@@ -362,9 +362,7 @@ public class Project extends Resource {
    * @since 3.3 also applies exclusions in general settings page and global exclusions.
    */
   public String[] getExclusionPatterns() {
-    return trimExclusions(ImmutableList.<String> builder()
-      .add(configuration.getStringArray(CoreProperties.PROJECT_EXCLUSIONS_PROPERTY))
-      .add(configuration.getStringArray(CoreProperties.GLOBAL_EXCLUSIONS_PROPERTY)).build());
+    return removeNegativePatterns(exclusionPatterns());
   }
 
   /**
@@ -374,6 +372,34 @@ public class Project extends Resource {
    * @since 3.3
    */
   public String[] getTestExclusionPatterns() {
+    return removeNegativePatterns(testExclusionPatterns());
+  }
+
+  /**
+   * Patterns of resource inclusion as defined in project settings page.
+   *
+   * @since 3.4
+   */
+  public String[] getInclusionPatterns() {
+    return onlyNegativePatterns(exclusionPatterns());
+  }
+
+  /**
+   * Patterns of test inclusion as defined in project settings page.
+   *
+   * @since 3.4
+   */
+  public String[] getTestInclusionPatterns() {
+    return onlyNegativePatterns(testExclusionPatterns());
+  }
+
+  private List<String> exclusionPatterns() {
+    return trimExclusions(ImmutableList.<String> builder()
+        .add(configuration.getStringArray(CoreProperties.PROJECT_EXCLUSIONS_PROPERTY))
+        .add(configuration.getStringArray(CoreProperties.GLOBAL_EXCLUSIONS_PROPERTY)).build());
+  }
+
+  private List<String> testExclusionPatterns() {
     String[] globalTestExclusions = configuration.getStringArray(CoreProperties.GLOBAL_TEST_EXCLUSIONS_PROPERTY);
     if (globalTestExclusions.length == 0) {
       globalTestExclusions = new String[] {CoreProperties.GLOBAL_TEST_EXCLUSIONS_DEFAULT};
@@ -384,13 +410,33 @@ public class Project extends Resource {
         .add(globalTestExclusions).build());
   }
 
-  // http://jira.codehaus.org/browse/SONAR-2261 - exclusion must be trimmed
-  private static String[] trimExclusions(List<String> exclusions) {
-    List<String> trimmed = Lists.newArrayList();
-    for (String exclusion : exclusions) {
-      trimmed.add(StringUtils.trim(exclusion));
+  private static String[] removeNegativePatterns(List<String> patterns) {
+    List<String> filtered = Lists.newArrayList();
+    for (String pattern : patterns) {
+      if (!pattern.startsWith("!")) {
+        filtered.add(pattern);
+      }
     }
-    return trimmed.toArray(new String[trimmed.size()]);
+    return filtered.toArray(new String[filtered.size()]);
+  }
+
+  private static String[] onlyNegativePatterns(List<String> patterns) {
+    List<String> filtered = Lists.newArrayList();
+    for (String pattern : patterns) {
+      if (pattern.startsWith("!")) {
+        filtered.add(pattern.substring(1));
+      }
+    }
+    return filtered.toArray(new String[filtered.size()]);
+  }
+
+  // http://jira.codehaus.org/browse/SONAR-2261 - exclusion must be trimmed
+  private static List<String> trimExclusions(List<String> patterns) {
+    List<String> trimmed = Lists.newArrayList();
+    for (String pattern : patterns) {
+      trimmed.add(StringUtils.trim(pattern));
+    }
+    return trimmed;
   }
 
   /**
