2daa634f3432ec1ae861cb1ca684a16425f22686
Simon Brandhof <simon.brandhof@gmail.com>
2012-11-07 11:16:32 +0100

    'sonar.profile.<language_key>' should take precedence over 'sonar.profile'

--- sonar-batch/src/test/java/org/sonar/batch/DefaultProfileLoaderTest.java
+++ sonar-batch/src/test/java/org/sonar/batch/DefaultProfileLoaderTest.java
@@ -21,7 +21,6 @@ package org.sonar.batch;
 
 import org.apache.commons.configuration.PropertiesConfiguration;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -64,7 +63,6 @@ public class DefaultProfileLoaderTest {
    * SONAR-3922
    */
   @Test
-  @Ignore
   public void should_check_language_property_before_global_property() {
     Settings settings = new Settings();
     settings.setProperty("sonar.profile.java", "one");
