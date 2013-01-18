6cdb0c5c809a86f69599f2006a6b2d1b29e24499
Simon Brandhof <simon.brandhof@gmail.com>
2013-01-18 12:18:38 +0100

    SONAR-4078 fix performance issue on oracle when verifying user permissions

--- sonar-server/src/main/webapp/WEB-INF/lib/authorization/default_authorizer.rb
+++ sonar-server/src/main/webapp/WEB-INF/lib/authorization/default_authorizer.rb
@@ -32,21 +32,27 @@ class SonarAuthorizer
     group_ids=user.groups.map(&:id)
 
     # Oracle is limited to 1000 elements in clause "IN"
-    page_size=950
+    page_size=999
     page_count=(compacted_resource_ids.size/page_size)
     page_count+=1 if (compacted_resource_ids.size % page_size)>0
 
+    sanitized_role = ActiveRecord::Base.connection.quote_string(role.to_s)
+
     group_roles=[]
     if group_ids.empty?
-      # Derby bug: does not support empty IN
+      # Some databases do not support empty IN
       page_count.times do |page_index|
         page_rids=compacted_resource_ids[page_index*page_size...(page_index+1)*page_size]
-        group_roles.concat(GroupRole.find(:all, :select => 'resource_id', :conditions => ["resource_id in(?) and role=? and group_id is null", page_rids, role.to_s]))
+        group_roles.concat(
+          ActiveRecord::Base.connection.execute("SELECT resource_id FROM group_roles WHERE role='#{sanitized_role}' and group_id is null and resource_id in (#{page_rids.join(',')})")
+        )
       end
     else
       page_count.times do |page_index|
         page_rids=compacted_resource_ids[page_index*page_size...(page_index+1)*page_size]
-        group_roles.concat(GroupRole.find(:all, :select => 'resource_id', :conditions => ["resource_id in (?) and role=? and (group_id is null or group_id in(?))", page_rids, role.to_s, group_ids]))
+        group_roles.concat(
+          ActiveRecord::Base.connection.execute("SELECT resource_id FROM group_roles WHERE role='#{sanitized_role}' and (group_id is null or group_id in(#{group_ids.join(',')})) and resource_id in (#{page_rids.join(',')})")
+        )
       end
     end
 
@@ -54,18 +60,20 @@ class SonarAuthorizer
     if user.id
       page_count.times do |page_index|
         page_rids=compacted_resource_ids[page_index*page_size...(page_index+1)*page_size]
-        user_roles.concat(UserRole.find(:all, :select => 'resource_id', :conditions => ['user_id=? and resource_id in(?) and role=?', user.id, page_rids, role.to_s]))
+        user_roles.concat(
+          ActiveRecord::Base.connection.execute("SELECT resource_id FROM user_roles WHERE role='#{sanitized_role}' and user_id=#{user.id} and resource_id in (#{page_rids.join(',')})")
+        )
       end
     end
 
-    autorized_resource_ids={}
+    authorized_resource_ids={}
     (group_roles.concat(user_roles)).each do |x|
-      autorized_resource_ids[x.resource_id]=true
+      authorized_resource_ids[x['resource_id'].to_i]=true
     end
 
     result=Array.new(resource_ids.size)
     resource_ids.each_with_index do |rid,index|
-      result[index]=((autorized_resource_ids[rid]) || false)
+      result[index]=((authorized_resource_ids[rid]) || false)
     end
     result
   end
@@ -79,7 +87,7 @@ class SonarAuthorizer
   def global_roles(user)
     group_ids=user.groups.map(&:id)
     if group_ids.empty?
-      # Derby bug: does not support empty IN
+      # Some databases do not support empty IN
       global_group_roles=GroupRole.find(:all, :select => 'role', :conditions => ["resource_id is null and group_id is null"]).map{|gr| gr.role.to_sym}
     else
       global_group_roles=GroupRole.find(:all, :select => 'role', :conditions => ["resource_id is null and (group_id is null or group_id in(?))", group_ids]).map{|gr| gr.role.to_sym}
