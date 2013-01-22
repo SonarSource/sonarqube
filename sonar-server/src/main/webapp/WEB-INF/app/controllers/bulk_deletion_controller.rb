a8973254128c92b37ca759e1736024e97e927d36
Fabrice Bellingard <fabrice.bellingard@sonarsource.com>
2013-01-22 16:10:24 +0100

    SONAR-3948 Make it possible to delete "ghost" resources
    => The bulk deletion page should allow to delete a project definition 
       which doesn't have any associated quality snapshots

--- sonar-server/src/main/webapp/WEB-INF/app/controllers/bulk_deletion_controller.rb
+++ sonar-server/src/main/webapp/WEB-INF/app/controllers/bulk_deletion_controller.rb
@@ -25,34 +25,56 @@ class BulkDeletionController < ApplicationController
   verify :method => :post, :only => [:delete_resources], :redirect_to => { :action => :index }
 
   def index
-    deletion_manager = ResourceDeletionManager.instance
-    
-    if deletion_manager.currently_deleting_resources? || 
-      (!deletion_manager.currently_deleting_resources? && deletion_manager.deletion_failures_occured?)
-      # a mass deletion is happening or it has just finished with errors => display the message from the Resource Deletion Manager
-      @deletion_manager = deletion_manager
+    if pending_mass_deletion?
       render :template => 'bulk_deletion/pending_deletions'
-    else      
-      @tabs = Java::OrgSonarServerUi::JRubyFacade.getInstance().getQualifiersWithProperty('deletable')
-      
-      @selected_tab = params[:resource_type]
-      @selected_tab = 'TRK' unless @tabs.include?(@selected_tab)
-      
-      # Search for resources
-      conditions = "resource_index.qualifier=:qualifier"
-      values = {:qualifier => @selected_tab}
-      if params[:name_filter] && !params[:name_filter].blank?
-        conditions += " AND resource_index.kee LIKE :kee"
-        values[:kee] = params[:name_filter].strip.downcase + '%'
-      end
+      return
+    end
+        
+    @tabs = deletable_qualifiers
+    
+    @selected_tab = params[:resource_type]
+    @selected_tab = 'TRK' unless @tabs.include?(@selected_tab)
+    
+    # Search for resources
+    conditions = "resource_index.qualifier=:qualifier"
+    values = {:qualifier => @selected_tab}
+    if params[:name_filter] && !params[:name_filter].blank?
+      conditions += " AND resource_index.kee LIKE :kee"
+      values[:kee] = params[:name_filter].strip.downcase + '%'
+    end
 
-      conditions += " AND projects.enabled=:enabled"
-      values[:enabled] = true
-      @resources = Project.find(:all,
-                                :select => 'distinct(resource_index.resource_id),projects.id,projects.name,projects.kee,projects.long_name',
-                                :conditions => [conditions, values],
-                                :joins => :resource_index)
-      @resources = Api::Utils.insensitive_sort!(@resources){|r| r.name}
+    conditions += " AND projects.enabled=:enabled"
+    values[:enabled] = true
+    @resources = Project.find(:all,
+                              :select => 'distinct(resource_index.resource_id),projects.id,projects.name,projects.kee,projects.long_name',
+                              :conditions => [conditions, values],
+                              :joins => :resource_index)
+    @resources = Api::Utils.insensitive_sort!(@resources){|r| r.name}
+  end
+
+  def ghosts
+    if pending_mass_deletion?
+      render :template => 'bulk_deletion/pending_deletions'
+      return
+    end
+      
+    @tabs = deletable_qualifiers
+    
+    conditions = "scope=:scope AND qualifier IN (:qualifiers) AND status=:status"
+    values = {:scope => 'PRJ', :qualifiers => @tabs}
+    unprocessed_project_ids = Snapshot.find(:all, :select => 'project_id', :conditions => [conditions, values.merge({:status => Snapshot::STATUS_UNPROCESSED})]).map(&:project_id).uniq
+    already_processed_project_ids = Snapshot.find(:all, :select => 'project_id', :conditions => [conditions + " AND project_id IN (:pids)", values.merge({:status => Snapshot::STATUS_PROCESSED, :pids => unprocessed_project_ids})]).map(&:project_id).uniq
+    
+    @ghosts = Project.find(:all, :conditions => ["id IN (?)", unprocessed_project_ids - already_processed_project_ids])
+    
+    @ghosts_by_qualifier = {}
+    @ghosts.each do |p|
+      qualifier = p.qualifier
+      if @ghosts_by_qualifier[qualifier]
+        @ghosts_by_qualifier[qualifier] << p
+      else
+        @ghosts_by_qualifier[qualifier] = [p]
+      end
     end
   end
   
@@ -85,6 +107,18 @@ class BulkDeletionController < ApplicationController
     ResourceDeletionManager.instance.reinit
     
     redirect_to :action => 'index'
+  end
+  
+  private
+  
+  # Tells if a mass deletion is happening or if it has finished with errors
+  def pending_mass_deletion?
+    deletion_manager = ResourceDeletionManager.instance
+    deletion_manager.currently_deleting_resources? || (!deletion_manager.currently_deleting_resources? && deletion_manager.deletion_failures_occured?)
+  end
+  
+  def deletable_qualifiers
+    Java::OrgSonarServerUi::JRubyFacade.getInstance().getQualifiersWithProperty('deletable')
   end
 
 end
