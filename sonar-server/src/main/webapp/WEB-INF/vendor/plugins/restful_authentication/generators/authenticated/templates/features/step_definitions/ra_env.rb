f16506ecbf5bc3f1e5f597a634c85b8a6162c692
Simon Brandhof <simon.brandhof@gmail.com>
2013-01-25 13:53:02 +0100

    Remove unused ruby files (generators)

--- sonar-server/src/main/webapp/WEB-INF/vendor/plugins/restful_authentication/generators/authenticated/templates/features/step_definitions/ra_env.rb
+++
@@ -1,9 +0,0 @@
-
-Before do
-  Fixtures.reset_cache
-  fixtures_folder = File.join(RAILS_ROOT, 'spec', 'fixtures')
-  Fixtures.create_fixtures(fixtures_folder, "users")
-end
-
-# Make visible for testing
-ApplicationController.send(:public, :logged_in?, :current_user, :authorized?)
