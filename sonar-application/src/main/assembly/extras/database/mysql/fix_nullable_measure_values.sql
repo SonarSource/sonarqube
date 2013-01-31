e62baf9b1d29eea00f49729de29513a17ce311f1
Simon Brandhof <simon.brandhof@gmail.com>
2013-01-31 12:56:52 +0100

    Remove extras/mysql/fix_nullable_measure_values.sql from distribution
    This file was required for migrations from versions < 1.8

--- sonar-application/src/main/assembly/extras/database/mysql/fix_nullable_measure_values.sql
+++
@@ -1,5 +0,0 @@
-# this script must be executed when the column PROJECT_MEASURES.VALUE is NOT NULL. 
-# It occurs sometimes when database schema has been created before Sonar 1.8
-
-ALTER TABLE sonar.project_measures MODIFY COLUMN value DECIMAL(30,20) DEFAULT NULL;
-  
