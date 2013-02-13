d8c3e901204013aec5c4fa0d4cc02eebb2928383
Simon Brandhof <simon.brandhof@gmail.com>
2013-02-13 09:09:52 +0100

    Fix header

--- sonar-core/src/main/java/org/sonar/core/graph/graphson/GraphsonTokens.java
+++ sonar-core/src/main/java/org/sonar/core/graph/graphson/GraphsonTokens.java
@@ -11,7 +11,7 @@
  * Sonar is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General License for more details.
+ * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with Sonar; if not, write to the Free Software
