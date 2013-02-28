b11b484ebba17ada33e48755209364e23a5f54bf
Julien Lancelot <julien.lancelot@gmail.com>
2013-02-28 15:22:15 +0100

    Remove duplicate DefaultIndex

--- sonar-batch/src/main/java/org/sonar/batch/bootstrap/ProjectTaskContainer.java
+++ sonar-batch/src/main/java/org/sonar/batch/bootstrap/ProjectTaskContainer.java
@@ -67,7 +67,6 @@ public class ProjectTaskContainer extends Container {
 
   private void registerCoreComponentsRequiringProject() {
     container.addSingleton(ProjectTree.class);
-    container.addSingleton(DefaultIndex.class);
     container.addSingleton(DefaultFileLinesContextFactory.class);
     container.addSingleton(ProjectLock.class);
 
