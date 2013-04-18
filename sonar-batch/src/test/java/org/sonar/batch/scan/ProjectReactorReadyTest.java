4e318cf3a6d736ab44583559bef9b9e73a19a2b0
Julien HENRY <julien.henry@sonarsource.com>
2013-04-18 16:10:54 +0200

    SONAR-4188 Validate branch too

--- sonar-batch/src/test/java/org/sonar/batch/scan/ProjectReactorReadyTest.java
+++ sonar-batch/src/test/java/org/sonar/batch/scan/ProjectReactorReadyTest.java
@@ -22,6 +22,7 @@ package org.sonar.batch.scan;
 import org.junit.Test;
 import org.sonar.api.batch.bootstrap.ProjectBuilder;
 import org.sonar.api.batch.bootstrap.ProjectReactor;
+import org.sonar.api.config.Settings;
 
 import static org.mockito.Mockito.mock;
 
@@ -29,13 +30,14 @@ public class ProjectReactorReadyTest {
   @Test
   public void should_do_nothing() {
     // it's only a barrier
-    ProjectReactorReady barrier = new ProjectReactorReady(mock(ProjectExclusions.class), mock(ProjectReactor.class), new ProjectBuilder[] {mock(ProjectBuilder.class)});
+    ProjectReactorReady barrier = new ProjectReactorReady(mock(ProjectExclusions.class), mock(ProjectReactor.class), new Settings(),
+        new ProjectBuilder[] {mock(ProjectBuilder.class)});
     barrier.start();
   }
 
   @Test
   public void project_builders_should_be_optional() {
-    ProjectReactorReady barrier = new ProjectReactorReady(mock(ProjectExclusions.class), mock(ProjectReactor.class));
+    ProjectReactorReady barrier = new ProjectReactorReady(mock(ProjectExclusions.class), mock(ProjectReactor.class), new Settings());
     barrier.start();
   }
 }
