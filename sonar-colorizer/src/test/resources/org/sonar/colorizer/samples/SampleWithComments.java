73a33ff2e2c8d88b9e75bc584ef513b6420041d6
Jean-Baptiste Vilain <jean-baptiste.vilain@sonarsource.com>
2013-04-23 16:46:26 +0200

    SONAR-4258 Added ampersand escaping in sonar-colorizer

---
+++ sonar-colorizer/src/test/resources/org/sonar/colorizer/samples/SampleWithComments.java
@@ -0,0 +1,14 @@
+package org.sonar.colorizer.samples;
+
+public class SampleWithComments {
+
+  /*
+   * This method does <b>something</b>
+   *
+   * &lt;p&gt;description&lt;/p&gt;
+   */
+
+  public void doSomething() {
+  }
+
+}
