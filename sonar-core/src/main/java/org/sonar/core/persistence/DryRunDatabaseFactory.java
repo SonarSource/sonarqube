89fbca5852b7462d4b6038b0be741407f12c76f1
Julien Lancelot <julien.lancelot@gmail.com>
2013-05-15 15:39:12 +0200

    SONAR-3755 Fix issues returned in dry run mode

--- sonar-core/src/main/java/org/sonar/core/persistence/DryRunDatabaseFactory.java
+++ sonar-core/src/main/java/org/sonar/core/persistence/DryRunDatabaseFactory.java
@@ -22,6 +22,7 @@ package org.sonar.core.persistence;
 import com.google.common.io.Files;
 import org.apache.commons.dbcp.BasicDataSource;
 import org.sonar.api.ServerComponent;
+import org.sonar.api.issue.Issue;
 import org.sonar.api.platform.ServerFileSystem;
 import org.sonar.api.utils.SonarException;
 import org.sonar.core.review.ReviewDto;
@@ -83,7 +84,7 @@ public class DryRunDatabaseFactory implements ServerComponent {
         .copyTable(source, dest, "projects", "(id=" + projectId + " or root_id=" + projectId + ")")
         .copyTable(source, dest, "reviews", "project_id=" + projectId, "status<>'" + ReviewDto.STATUS_CLOSED + "'")
         .copyTable(source, dest, "rule_failures", "snapshot_id in (select id from snapshots where " + snapshotCondition + ")")
-        .copyTable(source, dest, "issues", "resource_id in (" + projectsConditionForIssues + ")", "resolution is null")
+        .copyTable(source, dest, "issues", "resource_id in (" + projectsConditionForIssues + ")", "status<>'"+ Issue.STATUS_CLOSED + "'")
         .copyTable(source, dest, "snapshots", snapshotCondition);
     }
   }
