c82e8cbcf642d844dec206ed2b12d3fed383a098
Julien Lancelot <julien.lancelot@gmail.com>
2013-05-22 15:17:42 +0200

    SONAR-3755 Remove index on issues.project_id

--- sonar-server/src/main/webapp/WEB-INF/db/migrate/398_create_issues.rb
+++ sonar-server/src/main/webapp/WEB-INF/db/migrate/398_create_issues.rb
@@ -55,7 +55,6 @@ class CreateIssues < ActiveRecord::Migration
 
     add_index :issues,  :kee,                 :name => 'issues_kee',         :unique => true
     add_index :issues,  :resource_id,         :name => 'issues_resource_id'
-    add_index :issues,  :project_id,          :name => 'issues_project_id'
     add_index :issues,  :rule_id,             :name => 'issues_rule_id'
     add_index :issues,  :severity,            :name => 'issues_severity'
     add_index :issues,  :status,              :name => 'issues_status'
