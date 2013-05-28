c3556dd892de034af47839c1003312734d5b61b2
Simon Brandhof <simon.brandhof@gmail.com>
2013-05-28 17:26:42 +0200

    SONAR-3755 add some javadoc

--- sonar-plugin-api/src/main/java/org/sonar/api/issue/RubyIssueService.java
+++ sonar-plugin-api/src/main/java/org/sonar/api/issue/RubyIssueService.java
@@ -38,10 +38,28 @@ public interface RubyIssueService extends ServerComponent {
   /**
    * Search for issues.
    * <p/>
-   * Ruby: <code>Api.issues.find(hash_of_parameters)</code>
+   * Ruby: <code>Api.issues.find(hash_of_parameters)</code>. <b></B>Keys of parameters must be Ruby strings but not symbols</b>.
    * <p/>
    * <ul>
-   * TODO document parameters
+   *   <li>'issues': list of issue keys</li>
+   *   <li>'severities': list of severity to match. See constants in {@link org.sonar.api.rule.Severity}</li>
+   *   <li>'statuses': list of status to match. See constants in {@link Issue}</li>
+   *   <li>'resolutions': list of resolutions to match. See constants in {@link Issue}</li>
+   *   <li>'resolved': true to match only resolved issues, false to match only unresolved issues. By default no filtering is done.</li>
+   *   <li>'components': list of component keys to match, for example 'org.apache.struts:struts:org.apache.struts.Action'</li>
+   *   <li>'componentRoots': list of keys of root components. All the issues related to descendants of these roots are returned.</li>
+   *   <li>'rules': list of keys of rules to match. Format is &lt;repository&gt;:&lt;rule&gt;, for example 'squid:AvoidCycles'</li>
+   *   <li>'actionPlans': list of keys of the action plans to match. Note that plan names are not accepted.</li>
+   *   <li>'planned': true to get only issues associated to an action plan, false to get only non associated issues. By default no filtering is done.</li>
+   *   <li>'reporters': list of reporter logins. Note that reporters are defined only on "manual" issues.</li>
+   *   <li>'assignees': list of assignee logins.</li>
+   *   <li>'assigned': true to get only assigned issues, false to get only unassigned issues. By default no filtering is done.</li>
+   *   <li>'createdAfter': match all the issues created after the given date (inclusive). Both date and datetime ISO formats are supported: 2013-05-18 or 2010-05-18T15:50:45+0100</li>
+   *   <li>'createdBefore': match all the issues created before the given date (exclusive). Both date and datetime ISO formats are supported: 2013-05-18 or 2010-05-18T15:50:45+0100</li>
+   *   <li>'pageSize': TODO</li>
+   *   <li>'pageIndex': TODO</li>
+   *   <li>'sort': TODO</li>
+   *   <li>'asc': TODO </li>
    * </ul>
    */
   IssueQueryResult find(Map<String, Object> parameters);
