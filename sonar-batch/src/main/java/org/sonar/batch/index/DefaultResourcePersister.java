4ad3ba86653463e48bce0333a06128322837a44a
Simon Brandhof <simon.brandhof@gmail.com>
2013-06-06 09:49:49 +0200

    Fix cache of libraries

--- sonar-batch/src/main/java/org/sonar/batch/index/DefaultResourcePersister.java
+++ sonar-batch/src/main/java/org/sonar/batch/index/DefaultResourcePersister.java
@@ -67,7 +67,7 @@ public final class DefaultResourcePersister implements ResourcePersister {
   }
 
   private void addToCache(Resource resource, Snapshot snapshot) {
-    if (snapshot != null) {
+    if (snapshot != null && !ResourceUtils.isLibrary(resource)) {
       snapshotsByResource.put(resource, snapshot);
       resourceCache.add(resource);
       snapshotCache.put(resource.getEffectiveKey(), snapshot);
