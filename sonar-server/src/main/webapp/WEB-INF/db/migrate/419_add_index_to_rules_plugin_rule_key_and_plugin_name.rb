eff6924c72adb4f6c6677b0bfd3bef849c5d54e4
Julien Lancelot <julien.lancelot@gmail.com>
2013-07-11 10:34:53 +0200

    SONAR-4361 Add index on columns plugin_rule_key and plugin_name of the Rules table

---
+++ sonar-server/src/main/webapp/WEB-INF/db/migrate/419_add_index_to_rules_plugin_rule_key_and_plugin_name.rb
@@ -0,0 +1,34 @@
+#
+# SonarQube, open source software quality management tool.
+# Copyright (C) 2008-2013 SonarSource
+# mailto:contact AT sonarsource DOT com
+#
+# SonarQube is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 3 of the License, or (at your option) any later version.
+#
+# SonarQube is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+
+#
+# @since SonarQube 3.7
+#
+class AddIndexToRulesPluginRuleKeyAndPluginName < ActiveRecord::Migration
+
+  def self.up
+    begin
+      add_index :rules, [:plugin_rule_key, :plugin_name], :unique => true, :name => 'rules_plugin_key_and_name'
+    rescue
+      # already exists
+    end
+  end
+
+end
