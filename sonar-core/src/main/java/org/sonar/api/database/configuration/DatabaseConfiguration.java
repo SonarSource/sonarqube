4bd523b5f9c434766829db0b76a0918ad327af65
Simon Brandhof <simon.brandhof@gmail.com>
2013-07-16 14:46:26 +0200

    Revert drop of non-deprecated classes

---
+++ sonar-core/src/main/java/org/sonar/api/database/configuration/DatabaseConfiguration.java
@@ -0,0 +1,75 @@
+/*
+ * SonarQube, open source software quality management tool.
+ * Copyright (C) 2008-2013 SonarSource
+ * mailto:contact AT sonarsource DOT com
+ *
+ * SonarQube is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * SonarQube is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.api.database.configuration;
+
+import org.apache.commons.configuration.BaseConfiguration;
+import org.sonar.api.database.DatabaseSession;
+import org.sonar.jpa.session.DatabaseSessionFactory;
+
+import java.util.List;
+
+/**
+ * IMPORTANT : This class can't be moved to org.sonar.jpa.dao for backward-compatibility reasons.
+ * This class is still used in some plugins.
+ *
+ * @since 1.10
+ * @deprecated in 3.7. Replaced by {@link org.sonar.api.config.Settings}
+ */
+@Deprecated
+public class DatabaseConfiguration extends BaseConfiguration {
+  private DatabaseSessionFactory sessionFactory;
+  private DatabaseSession session;
+
+  public DatabaseConfiguration(DatabaseSessionFactory sessionFactory) {
+    this.sessionFactory = sessionFactory;
+    load();
+  }
+
+  public DatabaseConfiguration(DatabaseSession session) {
+    this.session = session;
+    load();
+  }
+
+  public final void load() {
+    clear();
+
+    // Ugly workaround before the move to myBatis
+    // Session is not up-to-date when Ruby on Rails inserts new rows in its own transaction. Seems like
+    // Hibernate keeps a cache...
+    getSession().commit();
+
+    List<Property> properties = getSession()
+      .createQuery("from " + Property.class.getSimpleName() + " p where p.resourceId is null and p.userId is null")
+      .getResultList();
+
+    if (properties != null) {
+      for (Property property : properties) {
+        setProperty(property.getKey(), property.getValue());
+      }
+    }
+  }
+
+  private DatabaseSession getSession() {
+    if (session != null) {
+      return session;
+    }
+    return sessionFactory.getSession();
+  }
+}
