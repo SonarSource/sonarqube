af60c537663d8db7b2ee1d6900ca5b859cd50b1f
Stas Vilchik <vilchiks@gmail.com>
2013-10-28 17:00:44 +0100

    SONAR-4725 Improvements for the list component due to the feedback

--- sonar-server/src/main/webapp/javascripts/select-list.js
+++ sonar-server/src/main/webapp/javascripts/select-list.js
@@ -1,4 +1,4 @@
-/*global Backbone:false*/
+/*global alert:false, Backbone:false, Spinner:false*/
 
 (function ($) {
 
@@ -28,10 +28,10 @@
       Backbone.Collection.prototype.fetch.call(this, settings);
     },
 
-    fetchNextPage: function() {
+    fetchNextPage: function(options) {
       if (this.more) {
         var nextPage = this.settings.data.page + 1,
-            settings = this.settings;
+            settings = $.extend(this.settings, options);
 
         settings.data.page = nextPage;
 
@@ -50,8 +50,10 @@
   var SelectListItemView = Backbone.View.extend({
     tagName: 'li',
 
+    checkboxTemplate: '<a class="select-list-list-checkbox"></a>',
+
     events: {
-      'click': 'toggle'
+      'click .select-list-list-checkbox': 'toggle'
     },
 
     initialize: function(options) {
@@ -60,21 +62,35 @@
     },
 
     render: function() {
-      this.$el.html(this.settings.format(this.model.toJSON()));
+      this.$el.empty()
+          .append(this.checkboxTemplate)
+          .append(this.settings.format(this.model.toJSON()));
       this.$el.toggleClass('selected', this.model.get('selected'));
+      this.$('.select-list-list-checkbox').attr('title',
+          this.model.get('selected') ?
+              this.settings.tooltips.deselect :
+              this.settings.tooltips.select);
     },
 
     toggle: function() {
       var selected = this.model.get('selected'),
-          model = this.model;
+          that = this;
+
+      this.$('.select-list-list-checkbox').addClass('with-spinner');
+      new Spinner(this.settings.spinnerSmall)
+          .spin(this.$('.select-list-list-checkbox')[0]);
 
       var url = selected ? this.settings.deselectUrl : this.settings.selectUrl;
       $.ajax({
           url: url,
           type: 'POST',
           data: { user: this.model.id }
-      }).done(function() {
-            model.set('selected', !selected);
+      })
+          .done(function() {
+            that.model.set('selected', !selected);
+          })
+          .fail(function() {
+            alert(that.settings.errorMessage);
           });
     }
   });
@@ -86,13 +102,13 @@
    */
 
   var SelectListView = Backbone.View.extend({
-    template:
-        '<div class="select-list-container">' +
+    template: function(l) {
+      return '<div class="select-list-container">' +
           '<div class="select-list-control">' +
             '<div class="select-list-check-control">' +
-              '<a class="select-list-control-button" name="selected">Selected</a>' +
-              '<a class="select-list-control-button" name="deselected">Deselected</a>' +
-              '<a class="select-list-control-button" name="all">All</a>' +
+              '<a class="select-list-control-button" name="selected">' + l.selected + '</a>' +
+              '<a class="select-list-control-button" name="deselected">' + l.deselected + '</a>' +
+              '<a class="select-list-control-button" name="all">' + l.all + '</a>' +
             '</div>' +
             '<div class="select-list-search-control">' +
               '<input type="text" placeholder="Search">' +
@@ -102,14 +118,14 @@
           '<div class="select-list-list-container">' +
             '<ul class="select-list-list"></ul>' +
           '</div>' +
-        '</div>',
+        '</div>';
+    },
 
     events: {
       'click .select-list-control-button[name=selected]': 'showSelected',
       'click .select-list-control-button[name=deselected]': 'showDeselected',
       'click .select-list-control-button[name=all]': 'showAll',
 
-      'keyup .select-list-search-control input': 'search',
       'click .select-list-search-control-clear': 'clearSearch'
     },
 
@@ -120,9 +136,10 @@
     },
 
     render: function() {
-      var that = this;
+      var that = this,
+          keyup = function() { that.search(); };
 
-      this.$el.html(this.template)
+      this.$el.html(this.template(this.settings.labels))
           .width(this.settings.width);
 
       this.$listContainer = this.$('.select-list-list-container')
@@ -131,6 +148,10 @@
           .on('scroll', function() { that.scroll(); });
 
       this.$list = this.$('.select-list-list');
+
+      this.$('.select-list-search-control input')
+          .focus()
+          .on('keyup', $.debounce(250, keyup));
 
       this.listItemViews = [];
     },
@@ -152,6 +173,7 @@
     },
 
     filterBySelection: function(filter) {
+      var that = this;
       filter = this.currentFilter = filter || this.currentFilter;
 
       if (filter != null) {
@@ -162,10 +184,18 @@
         this.$('.select-list-control-button').removeClass('active')
             .filter('[name=' + filter + ']').addClass('active');
 
+        this.showFetchSpinner();
+
         this.collection.fetch({
           url: this.settings.searchUrl,
           reset: true,
-          data: { selected: filter }
+          data: { selected: filter },
+          success: function() {
+            that.hideFetchSpinner();
+          },
+          error: function() {
+            alert(that.settings.errorMessage);
+          }
         });
       }
     },
@@ -184,16 +214,25 @@
 
     search: function() {
       var query = this.$('.select-list-search-control input').val(),
-          hasQuery = query.length > 0;
+          hasQuery = query.length > 0,
+          that = this;
 
       this.$('.select-list-check-control').toggleClass('disabled', hasQuery);
       this.$('.select-list-search-control').toggleClass('disabled', !hasQuery);
 
-      if (hasQuery > 0) {
+      if (hasQuery) {
+        this.showFetchSpinner();
+
         this.collection.fetch({
           url: this.settings.searchUrl,
           reset: true,
-          data: { query: query }
+          data: { query: query },
+          success: function() {
+            that.hideFetchSpinner();
+          },
+          error: function() {
+            alert(that.settings.errorMessage);
+          }
         });
       } else {
         this.filterBySelection();
@@ -209,14 +248,29 @@
       this.filterBySelection();
     },
 
+    showFetchSpinner: function() {
+      var options = $.extend(this.settings.spinnerBig, {
+        className: 'select-list-spinner'
+      });
+      new Spinner(options).spin(this.$el[0]);
+    },
+
+    hideFetchSpinner: function() {
+      this.$('.select-list-spinner').remove();
+    },
+
     scroll: function() {
       var scrollBottom = this.$listContainer.scrollTop() >=
           this.$list[0].scrollHeight - this.$listContainer.outerHeight(),
           that = this;
 
-      if (scrollBottom) {
+      if (scrollBottom && this.collection.more) {
         $.throttle(250, function() {
-          that.collection.fetchNextPage();
+            that.showFetchSpinner();
+
+            that.collection.fetchNextPage({
+              success: function() { that.hideFetchSpinner(); }
+            });
         })();
       }
     }
@@ -270,7 +324,59 @@
   window.SelectList.defaults = {
     width: '50%',
     height: 400,
-    format: function (item) { return item.value; }
+
+    format: function (item) { return item.value; },
+
+    labels: {
+      selected: 'Selected',
+      deselected: 'Deselected',
+      all: 'All'
+    },
+
+    tooltips: {
+      select: 'Click this to select item',
+      deselect: 'Click this to deselect item'
+    },
+
+    errorMessage: 'Something gone wrong, try to reload the page and try again.',
+
+    spinnerSmall: {
+      lines: 9, // The number of lines to draw
+      length: 0, // The length of each line
+      width: 2, // The line thickness
+      radius: 4, // The radius of the inner circle
+      corners: 1, // Corner roundness (0..1)
+      rotate: 0, // The rotation offset
+      direction: 1, // 1: clockwise, -1: counterclockwise
+      color: '#4b9fd5', // #rgb or #rrggbb or array of colors
+      speed: 2, // Rounds per second
+      trail: 60, // Afterglow percentage
+      shadow: false, // Whether to render a shadow
+      hwaccel: false, // Whether to use hardware acceleration
+      className: 'spinner', // The CSS class to assign to the spinner
+      zIndex: 2e9, // The z-index (defaults to 2000000000)
+      top: 'auto', // Top position relative to parent in px
+      left: 'auto' // Left position relative to parent in px
+    },
+
+    spinnerBig: {
+      lines: 9, // The number of lines to draw
+      length: 0, // The length of each line
+      width: 6, // The line thickness
+      radius: 16, // The radius of the inner circle
+      corners: 1, // Corner roundness (0..1)
+      rotate: 0, // The rotation offset
+      direction: 1, // 1: clockwise, -1: counterclockwise
+      color: '#4b9fd5', // #rgb or #rrggbb or array of colors
+      speed: 2, // Rounds per second
+      trail: 60, // Afterglow percentage
+      shadow: false, // Whether to render a shadow
+      hwaccel: false, // Whether to use hardware acceleration
+      className: 'spinner', // The CSS class to assign to the spinner
+      zIndex: 2e9, // The z-index (defaults to 2000000000)
+      top: 'auto', // Top position relative to parent in px
+      left: 'auto' // Left position relative to parent in px
+    }
   };
 
 })(jQuery);
