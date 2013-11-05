7b1313db3cd03c7e5647f178ea5689c1aeb826b5
Julien Lancelot <julien.lancelot@gmail.com>
2013-11-05 23:23:17 +0100

    SONAR-4700 Load module snapshot from project snapshot instead of searching snapshot by date

--- sonar-batch/src/main/java/org/sonar/batch/components/TimeMachineConfiguration.java
+++ sonar-batch/src/main/java/org/sonar/batch/components/TimeMachineConfiguration.java
@@ -23,9 +23,13 @@ import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.sonar.api.BatchExtension;
+import org.sonar.api.database.DatabaseSession;
+import org.sonar.api.database.model.Snapshot;
 import org.sonar.api.resources.Project;
 import org.sonar.api.resources.Qualifiers;
 
+import javax.persistence.Query;
+import java.util.Date;
 import java.util.List;
 
 import static com.google.common.collect.Lists.newLinkedList;
@@ -34,32 +38,45 @@ public class TimeMachineConfiguration implements BatchExtension {
 
   private static final Logger LOG = LoggerFactory.getLogger(TimeMachineConfiguration.class);
 
+  private final DatabaseSession session;
   private Project project;
   private final PeriodsDefinition periodsDefinition;
 
   private List<Period> periods;
   private List<PastSnapshot> modulePastSnapshots;
 
-  public TimeMachineConfiguration(Project project, PeriodsDefinition periodsDefinition, PastSnapshotFinderByDate pastSnapshotFinderByDate) {
+  public TimeMachineConfiguration(DatabaseSession session, Project project, PeriodsDefinition periodsDefinition) {
+    this.session = session;
     this.project = project;
     this.periodsDefinition = periodsDefinition;
-    initModulePastSnapshots(pastSnapshotFinderByDate);
+    initModulePastSnapshots();
   }
 
-  private void initModulePastSnapshots(PastSnapshotFinderByDate pastSnapshotFinderByDate) {
+  private void initModulePastSnapshots() {
     periods = newLinkedList();
     modulePastSnapshots = newLinkedList();
     for (PastSnapshot projectPastSnapshot : periodsDefinition.projectPastSnapshots()) {
-      PastSnapshot pastSnapshot = pastSnapshotFinderByDate.findByDate(project.getId(), projectPastSnapshot.getTargetDate());
-      if (pastSnapshot != null) {
-        pastSnapshot.setIndex(projectPastSnapshot.getIndex());
-        pastSnapshot.setMode(projectPastSnapshot.getMode());
-        pastSnapshot.setModeParameter(projectPastSnapshot.getModeParameter());
-        modulePastSnapshots.add(pastSnapshot);
-        periods.add(new Period(projectPastSnapshot.getIndex(), pastSnapshot.getTargetDate(), pastSnapshot.getDate()));
-        log(pastSnapshot);
-      }
+      Date snapshotDate = getSnapshotDate(projectPastSnapshot.getProjectSnapshot());
+
+      PastSnapshot pastSnapshot = new PastSnapshot(projectPastSnapshot.getMode(), projectPastSnapshot.getTargetDate(), projectPastSnapshot.getProjectSnapshot());
+      pastSnapshot.setIndex(projectPastSnapshot.getIndex());
+      pastSnapshot.setMode(projectPastSnapshot.getMode());
+      pastSnapshot.setModeParameter(projectPastSnapshot.getModeParameter());
+      modulePastSnapshots.add(pastSnapshot);
+      periods.add(new Period(projectPastSnapshot.getIndex(), projectPastSnapshot.getTargetDate(), snapshotDate));
+      log(pastSnapshot);
     }
+  }
+
+  private Date getSnapshotDate(Snapshot projectSnapshot) {
+    Query query = session
+      .createNativeQuery("select s.created_at from snapshots s where s.project_id=:projectId and (s.root_snapshot_id=:rootSnapshotId or s.id=:rootSnapshotId) and s.islast=:islast");
+    query.setParameter("projectId", project.getId());
+    query.setParameter("rootSnapshotId", projectSnapshot.getId());
+    query.setParameter("islast", Boolean.TRUE);
+
+    Date date = session.getSingleResult(query, null);
+    return date;
   }
 
   private void log(PastSnapshot pastSnapshot) {
