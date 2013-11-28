5a0dd61d0298c9be4566be6136b36d6a268fb9e7
Simon Brandhof <simon.brandhof@gmail.com>
2013-11-28 23:06:48 +0100

    SONAR-4926 Primary key of the table ISSUES is too short

---
+++ sonar-server/src/main/webapp/WEB-INF/db/migrate/434_move_issues_id_to_bigint.rb
@@ -0,0 +1,33 @@
+#
+# Sonar, entreprise quality control tool.
+# Copyright (C) 2008-2013 SonarSource
+# mailto:contact AT sonarsource DOT com
+#
+# SonarQube is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 3 of the License, or (at your option) any later version.
+#
+# SonarQube is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+
+#
+# Sonar 3.7.4
+# SONAR-4926
+#
+class MoveIssuesIdToBigint < ActiveRecord::Migration
+
+  def self.up
+    alter_to_big_primary_key('issues')
+    alter_to_big_primary_key('issue_changes')
+  end
+
+end
+
