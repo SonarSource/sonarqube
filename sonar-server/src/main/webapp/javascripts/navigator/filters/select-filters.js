bf4c11d4d9191ec479796c912d5d37639cf9f418
Stas Vilchik <vilchiks@gmail.com>
2013-12-17 13:12:52 +0600

    Measures Components: change default value to "any"

--- sonar-server/src/main/webapp/javascripts/navigator/filters/select-filters.js
+++ sonar-server/src/main/webapp/javascripts/navigator/filters/select-filters.js
@@ -238,7 +238,9 @@ window.SS = typeof window.SS === 'object' ? window.SS : {};
       var value = this.selection.map(function(item) {
             return item.get('text');
           }),
-          defaultValue = this.model.get('multiple') ? window.SS.phrases.all : window.SS.phrases.any;
+          defaultValue = this.model.has('defaultValue') ?
+              this.model.get('defaultValue') :
+              this.model.get('multiple') ? window.SS.phrases.all : window.SS.phrases.any;
 
       return this.isDefaultValue() ? defaultValue : value.join(', ');
     },
