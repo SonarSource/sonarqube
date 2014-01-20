877cdd56155379608dd79b08008b57f946cd6cdf
Simon Brandhof <simon.brandhof@gmail.com>
2014-01-20 14:56:40 +0100

    New API org.sonar.api.utils.text.CsvWriter

---
+++ sonar-plugin-api/src/main/java/org/sonar/api/utils/text/CsvWriter.java
@@ -0,0 +1,74 @@
+/*
+ * SonarQube, open source software quality management tool.
+ * Copyright (C) 2008-2013 SonarSource
+ * mailto:contact AT sonarsource DOT com
+ *
+ * SonarQube is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * SonarQube is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.api.utils.text;
+
+import org.apache.commons.lang.StringEscapeUtils;
+
+import java.io.IOException;
+import java.io.Writer;
+
+/**
+ * @since 4.2
+ */
+public class CsvWriter {
+
+  private final Writer writer;
+  private static final String VALUE_SEPARATOR = ",", LINE_SEPARATOR = "\r\n";
+
+  private CsvWriter(Writer writer) {
+    this.writer = writer;
+  }
+
+  public static CsvWriter of(Writer writer) {
+    return new CsvWriter(writer);
+  }
+
+  public CsvWriter values(String... values) {
+    for (int index = 0; index < values.length; index++) {
+      if (index > 0) {
+        write(VALUE_SEPARATOR);
+      }
+      String value = values[index];
+      if (value != null) {
+        write(StringEscapeUtils.escapeCsv(value));
+      }
+      if (index == values.length - 1) {
+        write(LINE_SEPARATOR);
+      }
+    }
+    return this;
+  }
+
+  public void close() {
+    try {
+      writer.close();
+    } catch (IOException e) {
+      throw new WriterException("Fail to close CSV output", e);
+    }
+  }
+
+  private void write(String s) {
+    try {
+      writer.append(s);
+    } catch (IOException e) {
+      throw new WriterException("Fail to generate CSV with value: " + s, e);
+    }
+  }
+}
---
+++ sonar-plugin-api/src/test/java/org/sonar/api/utils/text/CsvWriterTest.java
@@ -0,0 +1,101 @@
+/*
+ * SonarQube, open source software quality management tool.
+ * Copyright (C) 2008-2013 SonarSource
+ * mailto:contact AT sonarsource DOT com
+ *
+ * SonarQube is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * SonarQube is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.api.utils.text;
+
+import org.junit.Test;
+
+import java.io.*;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static org.fest.assertions.Fail.fail;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+
+public class CsvWriterTest {
+
+  @Test
+  public void write_csv() throws Exception {
+    StringWriter output = new StringWriter();
+    CsvWriter writer = CsvWriter.of(output);
+
+    writer.values("France", "Paris");
+    writer.values("Sweden", "Stockholm");
+    writer.close();
+
+    BufferedReader reader = new BufferedReader(new StringReader(output.toString()));
+    String line1 = reader.readLine();
+    assertThat(line1).isEqualTo("France,Paris");
+
+    String line2 = reader.readLine();
+    assertThat(line2).isEqualTo("Sweden,Stockholm");
+
+    assertThat(reader.readLine()).isNull();
+  }
+
+  @Test
+  public void escape_value() throws Exception {
+    StringWriter output = new StringWriter();
+    CsvWriter writer = CsvWriter.of(output);
+
+    writer.values("no double-quotes", "contains \"double-quotes\"", "contains , commas");
+    writer.close();
+
+    BufferedReader reader = new BufferedReader(new StringReader(output.toString()));
+    assertThat(reader.readLine()).isEqualTo("no double-quotes,\"contains \"\"double-quotes\"\"\",\"contains , commas\"");
+
+    assertThat(reader.readLine()).isNull();
+  }
+
+  @Test
+  public void fail_to_write_to_stream() throws Exception {
+    Writer output = mock(Writer.class);
+    IOException cause = new IOException("bad");
+    doThrow(cause).when(output).append(anyString());
+
+    CsvWriter writer = CsvWriter.of(output);
+
+    try {
+      writer.values("foo");
+      fail();
+    } catch (WriterException e) {
+      assertThat(e).hasMessage("Fail to generate CSV with value: foo");
+      assertThat(e.getCause()).isSameAs(cause);
+    }
+  }
+
+  @Test
+  public void fail_to_close_stream() throws Exception {
+    Writer output = mock(Writer.class);
+    IOException cause = new IOException("bad");
+    doThrow(cause).when(output).close();
+
+    CsvWriter writer = CsvWriter.of(output);
+    writer.values("foo");
+
+    try {
+      writer.close();
+      fail();
+    } catch (WriterException e) {
+      assertThat(e).hasMessage("Fail to close CSV output");
+      assertThat(e.getCause()).isSameAs(cause);
+    }
+  }
+}
