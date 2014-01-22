0285e6af2ea839968a3121afbbdf8c1e0e08fac1
Julien HENRY <julien.henry@sonarsource.com>
2014-01-22 10:10:55 +0100

    Change log level when accessing resource using its deprecated key

--- sonar-batch/src/main/java/org/sonar/batch/index/DefaultIndex.java
+++ sonar-batch/src/main/java/org/sonar/batch/index/DefaultIndex.java
@@ -639,7 +639,7 @@ public class DefaultIndex extends SonarIndex {
       for (Map.Entry<Resource, Bucket> entry : buckets.entrySet()) {
         Resource indexedResource = entry.getKey();
         if (res.getClass() == indexedResource.getClass() && res.getDeprecatedKey().equals(indexedResource.getDeprecatedKey())) {
-          LOG.warn("Resource " + res + " was found using deprecated key. Please update your plugin.");
+          LOG.debug("Resource " + res + " was found using deprecated key. Please update your plugin.");
           // Fix resource key
           Bucket bucket = entry.getValue();
           res.setKey(bucket.getResource().getKey());
