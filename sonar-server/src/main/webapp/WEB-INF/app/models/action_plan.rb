f942d3cba88c582592ce2a46f088ed6c3d065324
Julien Lancelot <julien.lancelot@sonarsource.com>
2014-01-28 09:18:15 +0100

    Add formatted deadline date on action plan ws

--- sonar-server/src/main/webapp/WEB-INF/app/models/action_plan.rb
+++ sonar-server/src/main/webapp/WEB-INF/app/models/action_plan.rb
@@ -26,6 +26,7 @@ class ActionPlan
     hash[:desc] = action_plan.description() if action_plan.description() && !action_plan.description().blank?
     hash[:userLogin] = action_plan.userLogin() if action_plan.userLogin()
     hash[:deadLine] = Api::Utils.format_datetime(action_plan.deadLine()) if action_plan.deadLine()
+    hash[:fDeadLine] = Api::Utils.format_localized_date(action_plan.deadLine()) if action_plan.deadLine()
     hash[:totalIssues] = action_plan.totalIssues() if action_plan.respond_to?('totalIssues')
     hash[:unresolvedIssues] = action_plan.unresolvedIssues() if action_plan.respond_to?('unresolvedIssues')
     hash[:createdAt] = Api::Utils.format_datetime(action_plan.createdAt()) if action_plan.createdAt()
