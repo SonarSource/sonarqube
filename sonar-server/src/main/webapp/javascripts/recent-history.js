526a69602c78156ffec742da5562493eab547f10
Jean-Baptiste Lievremont <jean-baptiste.lievremont@sonarsource.com>
2014-01-30 11:53:26 +0100

    SONAR-4935 Pass dashboard and time-period around when switching between projects

--- sonar-server/src/main/webapp/javascripts/recent-history.js
+++ sonar-server/src/main/webapp/javascripts/recent-history.js
@@ -55,6 +55,7 @@ Sonar.RecentHistory.prototype.populateRecentHistoryPanel = function () {
         + baseUrl
         + '/dashboard/index/'
         + resource['key']
+        + window.location.search
         + '"> '
         + resource['name']
         + '</a></li>');
