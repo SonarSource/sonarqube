f5dec2abf231427a38111a4720bf134a6a18f104
Julien Lancelot <julien.lancelot@sonarsource.com>
2014-02-04 17:38:51 +0100

    Fix quality flaw

--- sonar-server/src/main/java/org/sonar/server/exceptions/BadRequestException.java
+++ sonar-server/src/main/java/org/sonar/server/exceptions/BadRequestException.java
@@ -60,12 +60,12 @@ public class BadRequestException extends ServerException {
     return new BadRequestException(message);
   }
 
-  public static BadRequestException of(@Nullable  String message, List<Message> errors) {
+  public static BadRequestException of(String message, List<Message> errors) {
     return new BadRequestException(message, errors);
   }
 
   public static BadRequestException of(List<Message> errors) {
-    return new BadRequestException(null, errors);
+    return new BadRequestException(null, null, null, errors);
   }
 
   public static BadRequestException ofL10n(String l10nKey, Object... l10nParams) {
