5f137477f86b87338d7515798e4b466e99bc81f8
Julien Lancelot <julien.lancelot@sonarsource.com>
2014-02-28 17:11:58 +0100

    SONAR-4996 Add migrations on alerts and measure filters on debt

---
+++ sonar-server/src/main/webapp/WEB-INF/db/migrate/518_update_alerts_on_debt_to_minutes.rb
@@ -0,0 +1,57 @@
+#
+# SonarQube, open source software quality management tool.
+# Copyright (C) 2008-2013 SonarSource
+# mailto:contact AT sonarsource DOT com
+#
+# SonarQube is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 3 of the License, or (at your option) any later version.
+#
+# SonarQube is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+
+#
+# Sonar 4.3
+# SONAR-4996
+#
+class UpdateAlertsOnDebtToMinutes < ActiveRecord::Migration
+
+  class Property < ActiveRecord::Base
+
+  end
+
+  class Alert < ActiveRecord::Base
+
+  end
+
+  def self.up
+    hours_in_day_prop = Property.find_by_prop_key('sonar.technicalDebt.hoursInDay')
+    hours_in_day = hours_in_day_prop ? hours_in_day_prop : 8
+
+    metrics = Metric.find(:all, :conditions => ['name in (?)', ['sqale_index', 'new_technical_debt',
+                                                                'sqale_effort_to_grade_a', 'sqale_effort_to_grade_b', 'sqale_effort_to_grade_c', 'sqale_effort_to_grade_d',
+                                                                'blocker_remediation_cost', 'critical_remediation_cost', 'major_remediation_cost', 'minor_remediation_cost',
+                                                                'info_remediation_cost']])
+    alerts = Alert.all(:conditions => ['metric_id in (?)', metrics.map { |m| m.id } ])
+
+    alerts.each do |alert|
+      alert.value_error = convert_days_to_minutes(alert.value_error.to_f, hours_in_day) unless alert.value_error.blank?
+      alert.value_warning = convert_days_to_minutes(alert.value_warning.to_f, hours_in_day) unless alert.value_warning.blank?
+      alert.save!
+    end
+  end
+
+  def self.convert_days_to_minutes(hours, hours_in_day)
+    result = hours * hours_in_day * 60
+    # Round value
+    result.ceil
+  end
+end
---
+++ sonar-server/src/main/webapp/WEB-INF/db/migrate/519_update_measure_filters_on_debt_to_minutes.rb
@@ -0,0 +1,64 @@
+#
+# SonarQube, open source software quality management tool.
+# Copyright (C) 2008-2013 SonarSource
+# mailto:contact AT sonarsource DOT com
+#
+# SonarQube is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 3 of the License, or (at your option) any later version.
+#
+# SonarQube is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+
+#
+# Sonar 4.3
+# SONAR-4996
+#
+class UpdateMeasureFiltersOnDebtToMinutes < ActiveRecord::Migration
+
+  class Property < ActiveRecord::Base
+
+  end
+
+  class MeasureFilters < ActiveRecord::Base
+
+  end
+
+  def self.up
+    hours_in_day_prop = Property.find_by_prop_key('sonar.technicalDebt.hoursInDay')
+    hours_in_day = hours_in_day_prop ? hours_in_day_prop : 8
+
+    filters = MeasureFilter.all(:conditions => "data LIKE '%_metric=sqale_index%' OR data LIKE '%_metric=new_technical_debt%'" +
+        " OR data LIKE '%_metric=sqale_effort_to_grade_a%' OR data LIKE '%_metric=sqale_effort_to_grade_b%' OR data LIKE '%_metric=sqale_effort_to_grade_c%' " +
+        " OR data LIKE '%_metric=sqale_effort_to_grade_d%'"+
+        " OR data LIKE '%_metric=blocker_remediation_cost%' OR data LIKE '%_metric=critical_remediation_cost%' OR data LIKE '%_metric=major_remediation_cost%' "+
+        " OR data LIKE '%_metric=minor_remediation_cost%' OR data LIKE '%_metric=info_remediation_cost%'"
+    )
+    filters.each do |filter|
+      data = filter.data.to_s
+      data.scan(/c(\d+)_metric=(sqale_index|new_technical_debt|sqale_effort_to_grade_a|sqale_effort_to_grade_b|sqale_effort_to_grade_c|sqale_effort_to_grade_d|blocker_remediation_cost
+        |critical_remediation_cost|major_remediation_cost|minor_remediation_cost|info_remediation_cost)/) do |find|
+        index = find[0]
+        value = /c#{index}_val=((.+?)\|)?/.match(data)[2]
+        new_value = convert_days_to_minutes(value.to_f, hours_in_day)
+        filter.data = filter.data.sub("c#{index}_val=#{value}", "c#{index}_val=#{new_value}")
+      end
+      filter.save!
+    end
+  end
+
+  def self.convert_days_to_minutes(hours, hours_in_day)
+    result = hours * hours_in_day * 60
+    # Round value
+    result.ceil
+  end
+end
+
