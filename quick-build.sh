d9bdaff47db393ceaebad7d85e64ddd107ff65db
Simon Brandhof <simon.brandhof@gmail.com>
2014-03-06 22:27:02 +0100

    Fix logs of quick-build.sh

--- quick-build.sh
+++ quick-build.sh
@@ -7,9 +7,7 @@ echo ''
 echo ' WARNINGS' 
 echo '' 
 echo ' Unit tests are NOT executed.'
-echo ' Build for FIREFOX ONLY.'
 echo ''
 echo '-------------------------------------------------'
 
-# it is recommended to use maven 3 for faster builds
 mvn clean install -Dtest=false -DfailIfNoTests=false $*
