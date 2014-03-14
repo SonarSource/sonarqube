2547f373701301546314d24fcbe5cdeac6f56bca
Simon Brandhof <simon.brandhof@gmail.com>
2014-03-14 13:19:55 +0100

    SONAR-5143 remove sonar.inclusions and sonar.test.inclusions from UI

--- plugins/sonar-core-plugin/src/main/java/org/sonar/plugins/core/ExclusionProperties.java
+++ plugins/sonar-core-plugin/src/main/java/org/sonar/plugins/core/ExclusionProperties.java
@@ -34,22 +34,24 @@ class ExclusionProperties {
 
   static List<PropertyDefinition> definitions() {
     return ImmutableList.of(
+
+      // Do not display inclusions in UI
+      // https://jira.codehaus.org/browse/SONAR-5143
       PropertyDefinition.builder(CoreProperties.PROJECT_INCLUSIONS_PROPERTY)
         .name("Source File Inclusions")
         .multiValues(true)
         .category(CoreProperties.CATEGORY_EXCLUSIONS)
         .subCategory(CoreProperties.SUBCATEGORY_FILES_EXCLUSIONS)
-        .onQualifiers(Qualifiers.PROJECT)
-        .index(3)
+        .hidden()
         .build(),
       PropertyDefinition.builder(CoreProperties.PROJECT_TEST_INCLUSIONS_PROPERTY)
         .name("Test File Inclusions")
         .multiValues(true)
         .category(CoreProperties.CATEGORY_EXCLUSIONS)
         .subCategory(CoreProperties.SUBCATEGORY_FILES_EXCLUSIONS)
-        .onQualifiers(Qualifiers.PROJECT)
-        .index(5)
+        .hidden()
         .build(),
+
       PropertyDefinition.builder(CoreProperties.GLOBAL_EXCLUSIONS_PROPERTY)
         .name("Global Source File Exclusions")
         .multiValues(true)
--- sonar-core/src/main/java/org/sonar/core/persistence/DatabaseVersion.java
+++ sonar-core/src/main/java/org/sonar/core/persistence/DatabaseVersion.java
@@ -33,7 +33,7 @@ import java.util.List;
  */
 public class DatabaseVersion implements BatchComponent, ServerComponent {
 
-  public static final int LAST_VERSION = 498;
+  public static final int LAST_VERSION = 499;
 
   public static enum Status {
     UP_TO_DATE, REQUIRES_UPGRADE, REQUIRES_DOWNGRADE, FRESH_INSTALL
