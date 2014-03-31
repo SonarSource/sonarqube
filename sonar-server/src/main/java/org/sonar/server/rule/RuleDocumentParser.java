bed9043330a70cf4ee4ebcb61804ab7f9e6b9b17
Julien Lancelot <julien.lancelot@sonarsource.com>
2014-03-31 12:11:27 +0200

    Fix wrong display of rule sub characteristic key and name

--- sonar-server/src/main/java/org/sonar/server/rule/RuleDocumentParser.java
+++ sonar-server/src/main/java/org/sonar/server/rule/RuleDocumentParser.java
@@ -59,8 +59,8 @@ public class RuleDocumentParser {
         ruleBuilder
           .setDebtCharacteristicKey((String) ruleSource.get(RuleDocument.FIELD_CHARACTERISTIC_KEY))
           .setDebtCharacteristicName((String) ruleSource.get(RuleDocument.FIELD_CHARACTERISTIC_NAME))
-          .setDebtSubCharacteristicKey((String) ruleSource.get(RuleDocument.FIELD_CHARACTERISTIC_KEY))
-          .setDebtSubCharacteristicName((String) ruleSource.get(RuleDocument.FIELD_CHARACTERISTIC_NAME))
+          .setDebtSubCharacteristicKey((String) ruleSource.get(RuleDocument.FIELD_SUB_CHARACTERISTIC_KEY))
+          .setDebtSubCharacteristicName((String) ruleSource.get(RuleDocument.FIELD_SUB_CHARACTERISTIC_NAME))
           .setDebtRemediationFunction(
             new DefaultDebtRemediationFunction(DebtRemediationFunction.Type.valueOf((String) ruleSource.get(RuleDocument.FIELD_REMEDIATION_FUNCTION)),
               (String) ruleSource.get(RuleDocument.FIELD_REMEDIATION_COEFFICIENT),
