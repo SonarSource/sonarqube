569a517a2728403dfe821d1ffeb1203c4695d3d8
Julien Lancelot <julien.lancelot@sonarsource.com>
2014-04-03 10:39:52 +0200

    Fix unit test

--- sonar-server/src/test/java/org/sonar/server/rule/ws/RulesWsTest.java
+++ sonar-server/src/test/java/org/sonar/server/rule/ws/RulesWsTest.java
@@ -65,7 +65,7 @@ public class RulesWsTest {
     assertThat(list.since()).isEqualTo("4.3");
     assertThat(list.isPost()).isFalse();
     assertThat(list.isInternal()).isFalse();
-    assertThat(list.params()).hasSize(4);
+    assertThat(list.params()).hasSize(11);
 
     WebService.Action show = controller.action("show");
     assertThat(show).isNotNull();
