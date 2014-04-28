b2202080af8b51ab28581b48be2bfa760e23630c
Julien Lancelot <julien.lancelot@sonarsource.com>
2014-04-28 12:44:57 +0200

    Fix licence headers

--- sonar-server/src/main/java/org/sonar/server/rule2/RuleDao.java
+++ sonar-server/src/main/java/org/sonar/server/rule2/RuleDao.java
@@ -1,11 +1,30 @@
+/*
+ * SonarQube, open source software quality management tool.
+ * Copyright (C) 2008-2014 SonarSource
+ * mailto:contact AT sonarsource DOT com
+ *
+ * SonarQube is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * SonarQube is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 package org.sonar.server.rule2;
 
-import java.util.Collection;
-
+import org.sonar.api.rule.RuleKey;
 import org.sonar.core.persistence.MyBatis;
 import org.sonar.server.cluster.WorkQueue;
-import org.sonar.api.rule.RuleKey;
 import org.sonar.server.db.BaseDao;
+
+import java.util.Collection;
 
 public class RuleDao extends BaseDao<RuleDto, RuleKey> {
 
--- sonar-server/src/main/java/org/sonar/server/rule2/RuleIndex.java
+++ sonar-server/src/main/java/org/sonar/server/rule2/RuleIndex.java
@@ -1,15 +1,32 @@
+/*
+ * SonarQube, open source software quality management tool.
+ * Copyright (C) 2008-2014 SonarSource
+ * mailto:contact AT sonarsource DOT com
+ *
+ * SonarQube is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * SonarQube is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 package org.sonar.server.rule2;
 
 import org.elasticsearch.common.settings.ImmutableSettings;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.sonar.api.rule.RuleKey;
-import org.sonar.core.persistence.MyBatis;
 import org.sonar.core.profiling.Profiling;
 import org.sonar.server.cluster.WorkQueue;
 import org.sonar.server.search.BaseIndex;
