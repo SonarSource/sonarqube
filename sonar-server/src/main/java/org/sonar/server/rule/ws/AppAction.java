bdffba32464299ed924693ae5fac5597b0976610
Jean-Baptiste Lievremont <jean-baptiste.lievremont@sonarsource.com>
2014-05-28 17:29:14 +0200

    SONAR-5007 SONAR-5334 Remove messages from app WS for coding rules

--- sonar-server/src/main/java/org/sonar/server/rule/ws/AppAction.java
+++ sonar-server/src/main/java/org/sonar/server/rule/ws/AppAction.java
@@ -47,98 +47,6 @@ import java.util.Map;
  */
 public class AppAction implements RequestHandler {
 
-  private static final String[] MESSAGES = {
-    "all",
-    "any",
-    "apply",
-    "are_you_sure",
-    "bold",
-    "bulk_change",
-    "bulleted_point",
-    "cancel",
-    "change_verb",
-    "code",
-    "delete",
-    "Done",
-    "edit",
-    "markdown.helplink",
-    "moreCriteria",
-    "save",
-    "search_verb",
-    "severity",
-    "update_verb",
-
-    "severity.BLOCKER",
-    "severity.CRITICAL",
-    "severity.MAJOR",
-    "severity.MINOR",
-    "severity.INFO",
-
-    "coding_rules.activate",
-    "coding_rules.activate_in",
-    "coding_rules.activate_in_quality_profile",
-    "coding_rules.activate_in_all_quality_profiles",
-    "coding_rules.add_note",
-    "coding_rules.add_tags",
-    "coding_rules.available_since",
-    "coding_rules.bulk_change",
-    "coding_rules.change_severity",
-    "coding_rules.change_severity_in",
-    "coding_rules.change_details",
-    "coding_rules.extend_description",
-    "coding_rules.deactivate_in",
-    "coding_rules.deactivate",
-    "coding_rules.deactivate_in_quality_profile",
-    "coding_rules.deactivate_in_all_quality_profiles",
-    "coding_rules.found",
-    "coding_rules.inherits",
-    "coding_rules.key",
-    "coding_rules.new_search",
-    "coding_rules.no_results",
-    "coding_rules.no_tags",
-    "coding_rules.order",
-    "coding_rules.ordered_by",
-    "coding_rules.original",
-    "coding_rules.page",
-    "coding_rules.parameters",
-    "coding_rules.parameters.default_value",
-    "coding_rules.permalink",
-    "coding_rules.quality_profiles",
-    "coding_rules.quality_profile",
-    "coding_rules.repository",
-    "coding_rules.revert_to_parent_definition",
-    "coding_rules._rules",
-    "coding_rules.select_tag",
-
-    "coding_rules.filters.activation",
-    "coding_rules.filters.activation.active",
-    "coding_rules.filters.activation.inactive",
-    "coding_rules.filters.activation.help",
-    "coding_rules.filters.availableSince",
-    "coding_rules.filters.characteristic",
-    "coding_rules.filters.description",
-    "coding_rules.filters.quality_profile",
-    "coding_rules.filters.inheritance",
-    "coding_rules.filters.inheritance.inactive",
-    "coding_rules.filters.inheritance.not_inherited",
-    "coding_rules.filters.inheritance.inherited",
-    "coding_rules.filters.inheritance.overriden",
-    "coding_rules.filters.key",
-    "coding_rules.filters.language",
-    "coding_rules.filters.name",
-    "coding_rules.filters.repository",
-    "coding_rules.filters.severity",
-    "coding_rules.filters.status",
-    "coding_rules.filters.tag",
-
-    "coding_rules.facets.tags",
-    "coding_rules.facets.languages",
-    "coding_rules.facets.repositories",
-
-    "coding_rules.sort.creation_date",
-    "coding_rules.sort.name"
-  };
-
   private final Languages languages;
   private final RuleRepositories ruleRepositories;
   private final I18n i18n;
@@ -163,7 +71,6 @@ public class AppAction implements RequestHandler {
     addRuleRepositories(json);
     addStatuses(json);
     addCharacteristics(json);
-    addMessages(json);
     json.endObject().close();
   }
 
@@ -229,14 +136,6 @@ public class AppAction implements RequestHandler {
     json.name("characteristics").beginObject();
     for (DefaultDebtCharacteristic carac : caracById.values()) {
       json.prop(carac.key(), carac.isSub() ? caracById.get(carac.parentId()).name() + ": " + carac.name() : carac.name());
-    }
-    json.endObject();
-  }
-
-  private void addMessages(JsonWriter json) {
-    json.name("messages").beginObject();
-    for (String message : MESSAGES) {
-      json.prop(message, i18n.message(Locale.getDefault(), message, message));
     }
     json.endObject();
   }
