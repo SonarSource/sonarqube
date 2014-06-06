53c86d69a945096a84b1c2360f518713e13ec1c2
Stephane Gamard <stephane.gamard@searchbox.com>
2014-06-06 16:51:16 +0200

    SONAR-5082 - Fixed Migration script

--- sonar-server/src/main/webapp/WEB-INF/db/migrate/541_create_active_rule_index.rb
+++ sonar-server/src/main/webapp/WEB-INF/db/migrate/541_create_active_rule_index.rb
@@ -17,8 +17,14 @@
 # along with this program; if not, write to the Free Software Foundation,
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #
-class CreateLogTable < ActiveRecord::Migration
+class CreateActiveRuleIndex < ActiveRecord::Migration
 
+
+  class ActiveRuleParameters < ActiveRecord::Base
+  end
+
+  class ActiveRule < ActiveRecord::Base
+  end
 
   def self.up
 
