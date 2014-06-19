281d426e1e6ed20c24d3e2c8e8e89d07e9260e5b
Stephane Gamard <stephane.gamard@searchbox.com>
2014-06-19 17:21:27 +0200

    SONAR-5329 - Fixed license header

--- sonar-server/src/main/java/org/sonar/server/qualityprofile/QProfileActivity.java
+++ sonar-server/src/main/java/org/sonar/server/qualityprofile/QProfileActivity.java
@@ -19,32 +19,17 @@
  */
 package org.sonar.server.qualityprofile;
 
-import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Lists;
 import org.sonar.core.activity.Activity;
-import org.sonar.server.activity.index.ActivityQuery;
+import org.sonar.server.activity.index.ActivityDoc;
 
-import java.util.Collection;
+import java.util.Map;
 
 /**
  * @since 4.4
  */
-public class QProfileActivityQuery extends ActivityQuery {
+public class QProfileActivity extends ActivityDoc implements Activity {
 
-
-  Collection<String> qprofileKeys;
-
-  public QProfileActivityQuery() {
-    super();
-    this.setTypes(ImmutableSet.of(Activity.Type.QPROFILE));
-    qprofileKeys = Lists.newArrayList();
-  }
-
-  public Collection<String> getQprofileKeys() {
-    return qprofileKeys;
-  }
-
-  public void setQprofileKeys(Collection<String> qprofileKeys) {
-    this.qprofileKeys = qprofileKeys;
+  protected QProfileActivity(Map<String, Object> fields) {
+    super(fields);
   }
 }
