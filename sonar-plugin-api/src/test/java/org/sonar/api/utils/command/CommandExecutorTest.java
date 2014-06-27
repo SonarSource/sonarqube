eb4b39d96597335af1051ffc5ab4e96a65e453cf
Simon Brandhof <simon.brandhof@sonarsource.com>
2014-06-27 10:23:47 +0200

    Improve CommandExecutorTest

--- sonar-plugin-api/src/test/java/org/sonar/api/utils/command/CommandExecutorTest.java
+++ sonar-plugin-api/src/test/java/org/sonar/api/utils/command/CommandExecutorTest.java
@@ -139,7 +139,7 @@ public class CommandExecutorTest {
       // Future.get(), which is used by CommandExecutor, has not a precise timeout.
       // See http://stackoverflow.com/questions/23199820/future-get-timeout-precision-and-possible-alternatives
       // The deviation seems to be in both directions, so it implies to test something like >270ms instead of >300ms
-      assertThat(duration).as(e.getMessage()).isGreaterThan(270L);
+      assertThat(duration).as(e.getMessage()).isGreaterThan(250L);
     }
   }
 
