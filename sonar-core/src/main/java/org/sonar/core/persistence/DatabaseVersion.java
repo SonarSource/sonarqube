e7b58a71656a61b36449afdd76ba4eaacc44f950
Julien Lancelot <julien.lancelot@sonarsource.com>
2014-07-01 16:26:06 +0200

    SONAR-5426 Rule name and description must always be not null

--- sonar-core/src/main/java/org/sonar/core/persistence/DatabaseVersion.java
+++ sonar-core/src/main/java/org/sonar/core/persistence/DatabaseVersion.java
@@ -33,7 +33,7 @@ import java.util.List;
  */
 public class DatabaseVersion implements BatchComponent, ServerComponent {
 
-  public static final int LAST_VERSION = 555;
+  public static final int LAST_VERSION = 556;
 
   public static enum Status {
     UP_TO_DATE, REQUIRES_UPGRADE, REQUIRES_DOWNGRADE, FRESH_INSTALL
