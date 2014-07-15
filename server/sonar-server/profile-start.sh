3fbb1c5c454eba785c0e4a732a392c080c7f41ce
Simon Brandhof <simon.brandhof@sonarsource.com>
2014-07-15 14:47:46 +0200

    Remove sonar-server scripts that were moved to sonar-web

--- server/sonar-server/profile-start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-export MAVEN_OPTS='-Xmx512m -XX:MaxPermSize=160m -agentpath:/Applications/jprofiler8/bin/macos/libjprofilerti.jnilib=port=8849 '
-mvn org.apache.tomcat.maven:tomcat7-maven-plugin::run -Pstart-dev-server,h2 $*
