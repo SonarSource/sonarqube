1eb5dbdab56e6948ea96ed11e62a399ae9c7d2c3
Jean-Baptiste Lievremont <jean-baptiste.lievremont@sonarsource.com>
2014-07-21 16:08:39 +0200

    SONAR-5001 Split migration on rules description format (1 to add column, 1 to fill column)

--- server/sonar-web/src/main/webapp/WEB-INF/db/migrate/582_update_existing_rules_description_format.rb
+++ server/sonar-web/src/main/webapp/WEB-INF/db/migrate/582_update_existing_rules_description_format.rb
@@ -22,10 +22,14 @@
 # SonarQube 4.5
 # SONAR-5001
 #
-class AddRulesDescriptionFormat < ActiveRecord::Migration
+class UpdateExistingRulesDescriptionFormat < ActiveRecord::Migration
 
-  def self.up
-    add_column :rules, :description_format, :string, :null => true, :limit => 20
+  class Rule < ActiveRecord::Base
   end
 
+  def self.up
+    Rule.reset_column_information
+    Rule.update_all({:description_format => 'HTML', :updated_at => Time.now}, "plugin_name != 'manual' AND template_id IS NULL")
+    Rule.update_all({:description_format => 'MARKDOWN', :updated_at => Time.now}, "plugin_name = 'manual' OR template_id IS NOT NULL")
+  end
 end
