5b57bb4e680efc7b792dfbd6ffe24038c8871cd5
Simon Brandhof <simon.brandhof@sonarsource.com>
2014-07-24 22:26:03 +0200

    SONAR-4898 fix sonar-application-test

--- sonar-application-test/src/test/java/ForkTest.java
+++ sonar-application-test/src/test/java/ForkTest.java
@@ -1,7 +1,21 @@
 /*
- * Copyright (C) 2009-2014 SonarSource SA
- * All rights reserved
+ * SonarQube, open source software quality management tool.
+ * Copyright (C) 2008-2014 SonarSource
  * mailto:contact AT sonarsource DOT com
+ *
+ * SonarQube is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * SonarQube is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 import com.sonar.orchestrator.Orchestrator;
 import com.sonar.orchestrator.OrchestratorBuilder;
@@ -35,6 +49,7 @@ public class ForkTest {
   @Test
   public void start_and_stop() {
     OrchestratorBuilder builder = Orchestrator.builderEnv();
+    builder.setSonarVersion("4.5-SNAPSHOT");
     builder.addPlugin(MavenLocation.create("com.sonarsource.xoo", "sonar-xoo-plugin", "1.0-SNAPSHOT"));
     orchestrator = builder.build();
     orchestrator.start();
