92ade592733f45e3615c1c3012113401880335f7
Stas Vilchik <vilchiks@gmail.com>
2014-07-29 11:52:39 +0200

    SONAR-5479 Allow to filter on Lines and Lines of Code in the Component Viewer
    Do not show ncloc filter if there is no ncloc_data

--- server/sonar-web/src/main/js/tests/e2e/tests/component-viewer-lines-filters-spec.js
+++ server/sonar-web/src/main/js/tests/e2e/tests/component-viewer-lines-filters-spec.js
@@ -38,3 +38,32 @@ casper.test.begin(testName('Lines Filters'), function (test) {
         test.done();
       });
 });
+
+
+casper.test.begin(testName('Do Not Show Ncloc Filter If No Data'), function (test) {
+  casper
+      .start(lib.buildUrl('component-viewer#component=component'), function () {
+        lib.setDefaultViewport();
+        lib.mockRequest('/api/l10n/index', '{}');
+        lib.mockRequestFromFile('/api/components/app', 'app.json');
+        lib.mockRequestFromFile('/api/sources/show', 'source.json');
+        lib.mockRequestFromFile('/api/resources', 'resources-without-ncloc-data.json');
+      })
+
+      .then(function () {
+        casper.waitForSelector('.component-viewer-source .row');
+      })
+
+      .then(function () {
+        casper.click('.js-header-tab-basic');
+        casper.waitForSelector('[data-metric="ncloc"]');
+      })
+
+      .then(function () {
+        test.assertDoesntExist('.js-filter-ncloc');
+      })
+
+      .run(function () {
+        test.done();
+      });
+});
