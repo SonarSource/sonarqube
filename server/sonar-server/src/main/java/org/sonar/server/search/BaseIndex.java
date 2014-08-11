9c0d72f5bd2456f665d87c07025edad47e8f5d41
Stephane Gamard <stephane.gamard@searchbox.com>
2014-08-11 10:23:55 +0200

    fixed license header

--- server/sonar-server/src/main/java/org/sonar/server/search/BaseIndex.java
+++ server/sonar-server/src/main/java/org/sonar/server/search/BaseIndex.java
@@ -15,7 +15,7 @@
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software Foundation,
- * Inclient., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 package org.sonar.server.search;
 
