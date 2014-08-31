7e45359b0d0bceb9fbdad0c222bb7a5d5ba602df
Stephane Gamard <stephane.gamard@sonarsource.com>
2014-08-31 15:47:04 +0200

    SONAR-5529 - IssueDoc is our domain object and must implement Issue (from API)

--- server/sonar-server/src/main/java/org/sonar/server/issue/index/IssueDoc.java
+++ server/sonar-server/src/main/java/org/sonar/server/issue/index/IssueDoc.java
@@ -19,5 +19,118 @@
  */
 package org.sonar.server.issue.index;
 
-public class IssueDoc {
+import org.sonar.api.issue.Issue;
+import org.sonar.api.issue.IssueComment;
+import org.sonar.api.rule.RuleKey;
+import org.sonar.server.search.BaseDoc;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+public class IssueDoc extends BaseDoc implements Issue {
+
+  public IssueDoc(Map<String, Object> fields) {
+    super(fields);
+  }
+
+  @Override
+  public String key() {
+    return null;
+  }
+
+  @Override
+  public String componentKey() {
+    return null;
+  }
+
+  @Override
+  public RuleKey ruleKey() {
+    return null;
+  }
+
+  @Override
+  public String severity() {
+    return null;
+  }
+
+  @Override
+  public String message() {
+    return null;
+  }
+
+  @Override
+  public Integer line() {
+    return null;
+  }
+
+  @Override
+  public Double effortToFix() {
+    return null;
+  }
+
+  @Override
+  public String status() {
+    return null;
+  }
+
+  @Override
+  public String resolution() {
+    return null;
+  }
+
+  @Override
+  public String reporter() {
+    return null;
+  }
+
+  @Override
+  public String assignee() {
+    return null;
+  }
+
+  @Override
+  public Date creationDate() {
+    return null;
+  }
+
+  @Override
+  public Date updateDate() {
+    return null;
+  }
+
+  @Override
+  public Date closeDate() {
+    return null;
+  }
+
+  @Override
+  public String attribute(String key) {
+    return null;
+  }
+
+  @Override
+  public Map<String, String> attributes() {
+    return null;
+  }
+
+  @Override
+  public String authorLogin() {
+    return null;
+  }
+
+  @Override
+  public String actionPlanKey() {
+    return null;
+  }
+
+  @Override
+  public List<IssueComment> comments() {
+    return null;
+  }
+
+  @Override
+  public boolean isNew() {
+    return false;
+  }
 }
