d00bbc6e3934a05db5a26e1068ed4617892011a6
Julien Lancelot <julien.lancelot@sonarsource.com>
2014-10-03 11:12:21 +0200

    Fix quality flaw

--- server/sonar-server/src/main/java/org/sonar/server/search/Result.java
+++ server/sonar-server/src/main/java/org/sonar/server/search/Result.java
@@ -33,11 +33,8 @@ import org.slf4j.LoggerFactory;
 
 import javax.annotation.CheckForNull;
 import javax.annotation.Nullable;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+
+import java.util.*;
 
 public class Result<K> {
 
@@ -81,7 +78,7 @@ public class Result<K> {
       }
     } else if (HasAggregations.class.isAssignableFrom(aggregation.getClass())) {
       HasAggregations hasAggregations = (HasAggregations) aggregation;
-      for (Aggregation internalAggregation : ((HasAggregations) aggregation).getAggregations())
+      for (Aggregation internalAggregation : hasAggregations.getAggregations())
         this.processAggregation(internalAggregation);
     } else {
       LOGGER.warn("Cannot process {} type of aggregation", aggregation.getClass());
