d0ff15f62677b41d72e2c0ad63d799ce6778ad01
Stephane Gamard <stephane.gamard@sonarsource.com>
2014-10-08 12:17:35 +0200

    Fixed SearchClient error output to show request

--- server/sonar-server/src/main/java/org/sonar/server/search/SearchClient.java
+++ server/sonar-server/src/main/java/org/sonar/server/search/SearchClient.java
@@ -100,7 +100,7 @@ public class SearchClient extends TransportClient implements Startable {
       }
       return response;
     } catch (Exception e) {
-      LOGGER.error("could not execute request: " + response, e);
+      LOGGER.error("could not execute request: " + request, e);
       throw new IllegalStateException("ES error: ", e);
     }
   }
