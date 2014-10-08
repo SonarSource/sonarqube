6e8a241fbd106d6647cf0658937829a7912528ec
Stephane Gamard <stephane.gamard@sonarsource.com>
2014-10-08 09:32:45 +0200

    fixed sticky facets

--- server/sonar-server/src/main/java/org/sonar/server/rule/index/RuleIndex.java
+++ server/sonar-server/src/main/java/org/sonar/server/rule/index/RuleIndex.java
@@ -335,9 +335,9 @@ public class RuleIndex extends BaseIndex<Rule, RuleDto, RuleKey> {
   protected Map<String, AggregationBuilder> getFacets(QueryBuilder query, HashMap<String, FilterBuilder> filters) {
     Map<String, AggregationBuilder> aggregations = new HashMap<String, AggregationBuilder>();
 
-    BoolFilterBuilder langFacetFilter = FilterBuilders.boolFilter();// .must(FilterBuilders.queryFilter(query));
+    BoolFilterBuilder langFacetFilter = FilterBuilders.boolFilter().must(FilterBuilders.queryFilter(query));
     for (Map.Entry<String, FilterBuilder> filter : filters.entrySet()) {
-      if (StringUtils.equals(filter.getKey(), RuleNormalizer.RuleField.LANGUAGE.field())) {
+      if (!StringUtils.equals(filter.getKey(), RuleNormalizer.RuleField.LANGUAGE.field())) {
         langFacetFilter.must(filter.getValue());
       }
     }
@@ -356,9 +356,9 @@ public class RuleIndex extends BaseIndex<Rule, RuleDto, RuleKey> {
                 .size(10)
                 .minDocCount(1))));
 
-    BoolFilterBuilder tagsFacetFilter = FilterBuilders.boolFilter();// .must(FilterBuilders.queryFilter(query));
+    BoolFilterBuilder tagsFacetFilter = FilterBuilders.boolFilter().must(FilterBuilders.queryFilter(query));
     for (Map.Entry<String, FilterBuilder> filter : filters.entrySet()) {
-      if (StringUtils.equals(filter.getKey(), RuleNormalizer.RuleField._TAGS.field())) {
+      if (!StringUtils.equals(filter.getKey(), RuleNormalizer.RuleField._TAGS.field())) {
         tagsFacetFilter.must(filter.getValue());
       }
     }
@@ -377,9 +377,9 @@ public class RuleIndex extends BaseIndex<Rule, RuleDto, RuleKey> {
                 .size(10)
                 .minDocCount(1))));
 
-    BoolFilterBuilder repositoriesFacetFilter = FilterBuilders.boolFilter();// .must(FilterBuilders.queryFilter(query));
+    BoolFilterBuilder repositoriesFacetFilter = FilterBuilders.boolFilter().must(FilterBuilders.queryFilter(query));
     for (Map.Entry<String, FilterBuilder> filter : filters.entrySet()) {
-      if (StringUtils.equals(filter.getKey(), RuleNormalizer.RuleField.REPOSITORY.field())) {
+      if (!StringUtils.equals(filter.getKey(), RuleNormalizer.RuleField.REPOSITORY.field())) {
         repositoriesFacetFilter.must(filter.getValue());
       }
     }
