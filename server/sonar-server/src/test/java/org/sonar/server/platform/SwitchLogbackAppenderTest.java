545887882d1bba374d91d733badc03033d4ee331
Simon Brandhof <simon.brandhof@sonarsource.com>
2014-10-24 10:19:10 +0200

    Fix SwitchLogbackAppenderTest

--- server/sonar-server/src/test/java/org/sonar/server/platform/SwitchLogbackAppenderTest.java
+++ server/sonar-server/src/test/java/org/sonar/server/platform/SwitchLogbackAppenderTest.java
@@ -87,7 +87,7 @@ public class SwitchLogbackAppenderTest {
       assertThat(console.list).hasSize(1);
       assertThat(console.list.get(0).getMessage()).isEqualTo("a warning");
 
-      logger.warn("an error");
+      logger.error("an error");
       assertThat(analyisReports.list).hasSize(3);
       assertThat(analyisReports.list.get(2).getMessage()).isEqualTo("an error");
       assertThat(console.list).hasSize(2);
