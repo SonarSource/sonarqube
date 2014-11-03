dd8e6c8cb5924e702b3817edc774c6ec19d0d17e
Julien HENRY <julien.henry@sonarsource.com>
2014-11-03 11:54:47 +0100

    Fix stop.sh on Linux

--- stop.sh
+++ stop.sh
@@ -2,4 +2,10 @@
 
 # Shortcut to stop server. It must be already built.
 
-sh sonar-application/target/sonarqube-*/bin/macosx-universal-64/sonar.sh stop
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  OS='macosx-universal-64'
+else
+  OS='linux-x86-64'
+fi
+
+sh sonar-application/target/sonarqube-*/bin/$OS/sonar.sh stop
