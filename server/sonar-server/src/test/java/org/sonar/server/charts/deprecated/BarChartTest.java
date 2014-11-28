5061aeb3a45a63bdcfacb5940148d7d2a917eed2
Simon Brandhof <simon.brandhof@sonarsource.com>
2014-11-28 19:44:55 +0100

    Remove ignored tests

--- server/sonar-server/src/test/java/org/sonar/server/charts/deprecated/BarChartTest.java
+++ server/sonar-server/src/test/java/org/sonar/server/charts/deprecated/BarChartTest.java
@@ -110,24 +110,6 @@ public class BarChartTest extends BaseChartWebTest {
   }
 
   @Test
-  @Ignore
-  public void testBarChartTitle2() throws IOException {
-    Map<String, String> params = getDefaultParams();
-    params.put(BaseChartWeb.CHART_PARAM_TITLE, "JFreeChart by Servlet");
-    params.put(BaseChartWeb.CHART_PARAM_DIMENSIONS, "750x250");
-    params.put(BaseChartWeb.CHART_PARAM_TYPE, BaseChartWeb.BAR_CHART_VERTICAL);
-    params.put(BaseChartWeb.CHART_PARAM_CATEGORIES, "0+,5+,10+,20+,30+,60+,90+");
-    params.put(BaseChartWeb.CHART_PARAM_SERIES, "1,2,3");
-    params.put(BaseChartWeb.CHART_PARAM_CATEGORIES_AXISMARGIN_VISIBLE, "y");
-    params.put(BaseChartWeb.CHART_PARAM_RANGEAXIS_VISIBLE, "y");
-    params.put(BaseChartWeb.CHART_PARAM_VALUES, "100,50,75,92,30,58");
-    BarChart chart = new BarChart(params);
-    BufferedImage img = chart.getChartImage();
-    saveChart(img, "bar-chart-vertical-multi-series.png");
-    assertChartSizeGreaterThan(img, 100);
-  }
-
-  @Test
   public void testStackedBarCharteightyTwenty() throws IOException {
     Map<String, String> params = new HashMap<String, String>();
     params.put(BaseChartWeb.CHART_PARAM_TYPE, BaseChartWeb.STACKED_BAR_CHART);
