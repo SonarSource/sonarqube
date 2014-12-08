357f8f85cc94acc1216f5c0dd32f0218ad7758e5
Simon Brandhof <simon.brandhof@sonarsource.com>
2014-12-08 17:18:17 +0100

    Fix server-test-benchmarks assertions because of Lucene optimizations

--- server/sonar-server-benchmarks/src/test/java/org/sonar/server/benchmark/IssueIndexBenchmarkTest.java
+++ server/sonar-server-benchmarks/src/test/java/org/sonar/server/benchmark/IssueIndexBenchmarkTest.java
@@ -56,7 +56,7 @@ public class IssueIndexBenchmarkTest {
 
   private static final Logger LOGGER = LoggerFactory.getLogger("benchmarkIssues");
 
-  final static int PROJECTS = 1;
+  final static int PROJECTS = 100;
   final static int FILES_PER_PROJECT = 100;
   final static int ISSUES_PER_FILE = 100;
 
@@ -116,7 +116,7 @@ public class IssueIndexBenchmarkTest {
     tester.get(EsClient.class).prepareOptimize("issues").get();
     long dirSize = FileUtils.sizeOfDirectory(tester.getEsServerHolder().getHomeDir());
     LOGGER.info(String.format("ES dir: " + FileUtils.byteCountToDisplaySize(dirSize)));
-    benchmark.expectBetween("ES dir size (b)", dirSize, 385L * FileUtils.ONE_MB, 420L * FileUtils.ONE_MB);
+    benchmark.expectBetween("ES dir size (b)", dirSize, 200L * FileUtils.ONE_MB, 420L * FileUtils.ONE_MB);
   }
 
   private void benchmarkQueries() {
--- server/sonar-server-benchmarks/src/test/java/org/sonar/server/benchmark/SourceIndexBenchmarkTest.java
+++ server/sonar-server-benchmarks/src/test/java/org/sonar/server/benchmark/SourceIndexBenchmarkTest.java
@@ -86,7 +86,7 @@ public class SourceIndexBenchmarkTest {
     tester.get(EsClient.class).prepareOptimize(SourceLineIndexDefinition.INDEX).get();
     long dirSize = FileUtils.sizeOfDirectory(tester.getEsServerHolder().getHomeDir());
     LOGGER.info(String.format("ES dir: " + FileUtils.byteCountToDisplaySize(dirSize)));
-    benchmark.expectBetween("ES dir size (b)", dirSize, 80L * FileUtils.ONE_MB, 100L * FileUtils.ONE_MB);
+    benchmark.expectBetween("ES dir size (b)", dirSize, 68L * FileUtils.ONE_MB, 74L * FileUtils.ONE_MB);
   }
 
   private void benchmarkQueries() {
