2ecfe2818d3570698ea675771ad07a0fbf61fc95
Stas Vilchik <vilchiks@gmail.com>
2014-12-17 09:41:14 +0100

    Fix quality flaws

--- server/sonar-web/src/main/js/source-viewer/measures-overlay.js
+++ server/sonar-web/src/main/js/source-viewer/measures-overlay.js
@@ -3,67 +3,7 @@ define([
   'templates/source-viewer'
 ], function (Overlay, Templates) {
 
-  var $ = jQuery,
-      SOURCE_METRIC_LIST = [
-        'accessors',
-        'classes',
-        'functions',
-        'statements',
-        'ncloc',
-        'lines',
-        'generated_ncloc',
-        'generated_lines',
-        'complexity',
-        'function_complexity',
-        'comment_lines',
-        'comment_lines_density',
-        'public_api',
-        'public_undocumented_api',
-        'public_documented_api_density'
-      ],
-      COVERAGE_METRIC_LIST = [
-        'coverage',
-        'line_coverage',
-        'lines_to_cover',
-        'uncovered_lines',
-        'branch_coverage',
-        'conditions_to_cover',
-        'uncovered_conditions',
-        'it_coverage',
-        'it_line_coverage',
-        'it_lines_to_cover',
-        'it_uncovered_lines',
-        'it_branch_coverage',
-        'it_conditions_to_cover',
-        'it_uncovered_conditions',
-        'overall_coverage',
-        'overall_line_coverage',
-        'overall_lines_to_cover',
-        'overall_uncovered_lines',
-        'overall_branch_coverage',
-        'overall_conditions_to_cover',
-        'overall_uncovered_conditions'
-      ],
-      ISSUES_METRIC_LIST = [
-        'violations',
-        'sqale_index',
-        'sqale_debt_ratio',
-        'sqale_rating'
-      ],
-      DUPLICATIONS_METRIC_LIST = [
-        'duplicated_lines_density',
-        'duplicated_blocks',
-        'duplicated_lines'
-      ],
-
-      TESTS_METRIC_LIST = [
-        'tests',
-        'test_success_density',
-        'test_failures',
-        'test_errors',
-        'skipped_tests',
-        'test_execution_time'
-      ];
+  var $ = jQuery;
 
 
   return Overlay.extend({
@@ -113,6 +53,37 @@ define([
       return metrics;
     },
 
+
+    calcAdditionalMeasures: function (measures) {
+      if (measures.lines_to_cover && measures.uncovered_lines) {
+        measures.covered_lines = measures.lines_to_cover - measures.uncovered_lines;
+      }
+      if (measures.conditions_to_cover && measures.uncovered_conditions) {
+        measures.covered_conditions = measures.conditions_to_cover - measures.uncovered_conditions;
+      }
+      if (measures.it_lines_to_cover && measures.it_uncovered_lines) {
+        measures.it_covered_lines = measures.it_lines_to_cover - measures.it_uncovered_lines;
+      }
+      if (measures.it_conditions_to_cover && measures.it_uncovered_conditions) {
+        measures.it_covered_conditions = measures.it_conditions_to_cover - measures.it_uncovered_conditions;
+      }
+      return measures;
+    },
+
+
+    prepareMetrics: function (metrics) {
+      metrics = _.filter(metrics, function (metric) {
+        return metric.value != null;
+      });
+      return _.map(_.pairs(_.groupBy(metrics, 'domain')), function (domain) {
+        return {
+          name: domain[0],
+          metrics: domain[1]
+        };
+      });
+    },
+
+
     requestMeasures: function () {
       var that = this,
           url = baseUrl + '/api/resources',
@@ -130,30 +101,10 @@ define([
           measures[m.key] = m.frmt_val || m.data;
           measures[m.key + '_raw'] = m.val;
         });
-        if (measures.lines_to_cover && measures.uncovered_lines) {
-          measures.covered_lines = measures.lines_to_cover - measures.uncovered_lines;
-        }
-        if (measures.conditions_to_cover && measures.uncovered_conditions) {
-          measures.covered_conditions = measures.conditions_to_cover - measures.uncovered_conditions;
-        }
-        if (measures.it_lines_to_cover && measures.it_uncovered_lines) {
-          measures.it_covered_lines = measures.it_lines_to_cover - measures.it_uncovered_lines;
-        }
-        if (measures.it_conditions_to_cover && measures.it_uncovered_conditions) {
-          measures.it_covered_conditions = measures.it_conditions_to_cover - measures.it_uncovered_conditions;
-        }
-        metrics = _.filter(metrics, function (metric) {
-              return metric.value != null;
-            });
-        metrics = _.map(_.pairs(_.groupBy(metrics, 'domain')), function (domain) {
-          return {
-            name: domain[0],
-            metrics: domain[1]
-          };
-        });
+        measures = that.calcAdditionalMeasures(measures);
         that.model.set({
           measures: measures,
-          measuresToDisplay: metrics
+          measuresToDisplay: that.prepareMetrics(metrics)
         });
       });
     },
