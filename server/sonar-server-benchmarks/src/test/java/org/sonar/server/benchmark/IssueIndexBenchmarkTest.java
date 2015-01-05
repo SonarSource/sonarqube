5f60049b99e56722e1514f5ccd1e1674ccecf94a
Simon Brandhof <simon.brandhof@sonarsource.com>
2015-01-05 15:34:00 +0100

    Adjust timeout of IssueIndexBenchmarkTest

--- server/sonar-server-benchmarks/src/test/java/org/sonar/server/benchmark/IssueIndexBenchmarkTest.java
+++ server/sonar-server-benchmarks/src/test/java/org/sonar/server/benchmark/IssueIndexBenchmarkTest.java
@@ -112,7 +112,7 @@ public class IssueIndexBenchmarkTest {
     long period = System.currentTimeMillis() - start;
     long throughputPerSecond = 1000 * issues.count.get() / period;
     LOGGER.info(String.format("%d issues indexed in %d ms (%d docs/second)", issues.count.get(), period, throughputPerSecond));
-    benchmark.expectBetween("Throughput to index issues", throughputPerSecond, 2500L, 3000L);
+    benchmark.expectBetween("Throughput to index issues", throughputPerSecond, 2300L, 2800L);
 
     // be sure that physical files do not evolve during estimation of size
     tester.get(EsClient.class).prepareOptimize("issues").get();
