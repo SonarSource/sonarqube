1b4150491f057c53285660c01e8ba3d91f283003
Simon Brandhof <simon.brandhof@sonarsource.com>
2015-01-11 22:57:59 +0100

    Fix missing db commit in ProjectCleaner

--- server/sonar-server/src/main/java/org/sonar/core/computation/dbcleaner/ProjectCleaner.java
+++ server/sonar-server/src/main/java/org/sonar/core/computation/dbcleaner/ProjectCleaner.java
@@ -71,6 +71,7 @@ public class ProjectCleaner implements ServerComponent {
 
     deleteIndexedIssuesBefore(idUuidPair.getUuid(), configuration.maxLiveDateOfClosedIssues());
 
+    session.commit();
     logProfiling(start, settings);
     return this;
   }
