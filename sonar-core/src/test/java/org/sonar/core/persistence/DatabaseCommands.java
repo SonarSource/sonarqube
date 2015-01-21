420319a5ca47cb91cb593d36e2f72785d049ebad
Simon Brandhof <simon.brandhof@sonarsource.com>
2015-01-21 18:38:28 +0100

    Revert "Attempt to speed-up db tests on non-H2"
    This reverts commit d68baaa507dab20580ddee148a87d2970a1a716e.

--- sonar-core/src/test/java/org/sonar/core/persistence/DatabaseCommands.java
+++ sonar-core/src/test/java/org/sonar/core/persistence/DatabaseCommands.java
@@ -64,7 +64,7 @@ public abstract class DatabaseCommands {
   abstract List<String> resetPrimaryKey(String table, int minSequenceValue);
 
   String getTruncateCommand(String table) {
-    return "DELETE FROM " + table;
+    return "TRUNCATE TABLE " + table;
   }
 
   public static DatabaseCommands forDialect(Dialect dialect) {
