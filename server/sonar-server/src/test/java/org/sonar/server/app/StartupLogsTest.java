b50745d4f87179c3b77f383efbe3e5b934915c1a
Simon Brandhof <simon.brandhof@sonarsource.com>
2015-01-31 23:04:52 +0100

    SONAR-6140 Ability to restrict list of HTTPS ciphers with the new propert sonar.web.https.ciphers

---
+++ server/sonar-server/src/test/java/org/sonar/server/app/StartupLogsTest.java
@@ -0,0 +1,104 @@
+/*
+ * SonarQube, open source software quality management tool.
+ * Copyright (C) 2008-2014 SonarSource
+ * mailto:contact AT sonarsource DOT com
+ *
+ * SonarQube is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * SonarQube is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.server.app;
+
+import org.apache.catalina.connector.Connector;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.coyote.http11.Http11Protocol;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.slf4j.Logger;
+
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.*;
+
+public class StartupLogsTest {
+
+  Tomcat tomcat = mock(Tomcat.class, Mockito.RETURNS_DEEP_STUBS);
+  Logger logger = mock(Logger.class);
+  StartupLogs sut = new StartupLogs(logger);
+
+  @Test
+  public void logAjp() throws Exception {
+    Connector connector = newConnector("AJP/1.3", "http");
+    when(tomcat.getService().findConnectors()).thenReturn(new Connector[] {connector});
+
+    sut.log(tomcat);
+
+    verify(logger).info("AJP/1.3 connector enabled on port 1234");
+    verifyNoMoreInteractions(logger);
+  }
+
+  @Test
+  public void logHttp() throws Exception {
+    Connector connector = newConnector("HTTP/1.1", "http");
+    when(tomcat.getService().findConnectors()).thenReturn(new Connector[] {connector});
+
+    sut.log(tomcat);
+
+    verify(logger).info("HTTP connector enabled on port 1234");
+    verifyNoMoreInteractions(logger);
+  }
+
+  @Test
+  public void logHttps_default_ciphers() throws Exception {
+    Connector connector = newConnector("HTTP/1.1", "https");
+    when(tomcat.getService().findConnectors()).thenReturn(new Connector[] {connector});
+
+    sut.log(tomcat);
+
+    verify(logger).info("HTTPS connector enabled on port 1234 | ciphers=JVM defaults");
+    verifyNoMoreInteractions(logger);
+  }
+
+  @Test
+  public void logHttps_overridden_ciphers() throws Exception {
+    Connector connector = newConnector("HTTP/1.1", "https");
+    connector.setProtocolHandlerClassName("org.apache.coyote.http11.Http11Protocol");
+    ((Http11Protocol) connector.getProtocolHandler()).setCiphers("SSL_RSA,TLS_RSA_WITH_RC4");
+    when(tomcat.getService().findConnectors()).thenReturn(new Connector[] {connector});
+
+    sut.log(tomcat);
+
+    verify(logger).info("HTTPS connector enabled on port 1234 | ciphers=SSL_RSA,TLS_RSA_WITH_RC4");
+    verifyNoMoreInteractions(logger);
+  }
+
+  @Test
+  public void unsupported_connector() throws Exception {
+    Connector connector = mock(Connector.class, Mockito.RETURNS_DEEP_STUBS);
+    when(connector.getProtocol()).thenReturn("SPDY/1.1");
+    when(connector.getScheme()).thenReturn("spdy");
+    when(tomcat.getService().findConnectors()).thenReturn(new Connector[] {connector});
+    try {
+      sut.log(tomcat);
+      fail();
+    } catch (IllegalArgumentException e) {
+      // expected
+    }
+  }
+
+  private Connector newConnector(String protocol, String schema) {
+    Connector httpConnector = new Connector(protocol);
+    httpConnector.setScheme(schema);
+    httpConnector.setPort(1234);
+    return httpConnector;
+  }
+}
