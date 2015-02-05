b2fae4d03a9296c469bd1f215511ac43fd79cd0a
Stas Vilchik <vilchiks@gmail.com>
2015-02-05 16:40:36 +0100

    highlight current selected file on the drilldown page

--- server/sonar-web/src/main/js/drilldown/app.js
+++ server/sonar-web/src/main/js/drilldown/app.js
@@ -16,6 +16,8 @@ requirejs([
   App.addInitializer(function () {
     $('.js-drilldown-link').on('click', function (e) {
       e.preventDefault();
+      $(e.currentTarget).closest('table').find('.selected').removeClass('selected');
+      $(e.currentTarget).closest('tr').addClass('selected');
       var uuid = $(e.currentTarget).data('uuid'),
           viewer = new SourceViewer();
       App.viewerRegion.show(viewer);
