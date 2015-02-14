e438b571a986b2850880aab530f980da5f951c1e
Simon Brandhof <simon.brandhof@sonarsource.com>
2015-02-14 11:14:10 +0100

    Speed-up build

--- quick-build.sh
+++ quick-build.sh
@@ -1,41 +1,6 @@
 #!/bin/sh
 
-export MAVEN_OPTS='-Xmx256m'
-
-echo ''
-echo ''
-echo '                 uuuuuuu'
-echo '             uu$$$$$$$$$$$uu'
-echo '          uu$$$$$$$$$$$$$$$$$uu'
-echo '         u$$$$$$$$$$$$$$$$$$$$$u'
-echo '        u$$$$$$$$$$$$$$$$$$$$$$$u'
-echo '       u$$$$$$$$$$$$$$$$$$$$$$$$$u'
-echo '       u$$$$$$$$$$$$$$$$$$$$$$$$$u'
-echo '       u$$$$$$"   "$$$"   "$$$$$$u'
-echo '       "$$$$"      u$u       $$$$"'
-echo '        $$$u       u$u       u$$$'
-echo '        $$$u      u$$$u      u$$$'
-echo '         "$$$$uu$$$   $$$uu$$$$"'
-echo '          "$$$$$$$"   "$$$$$$$"'
-echo '            u$$$$$$$u$$$$$$$u'
-echo '             u$"$"$"$"$"$"$u'
-echo '  uuu        $$u$ $ $ $ $u$$       uuu'
-echo ' u$$$$        $$$$$u$u$u$$$       u$$$$'
-echo '  $$$$$uu      "$$$$$$$$$"     uu$$$$$$'
-echo 'u$$$$$$$$$$$uu    """""    uuuu$$$$$$$$$$'
-echo '$$$$"""$$$$$$$$$$uuu   uu$$$$$$$$$"""$$$"'
-echo ' """      ""$$$$$$$$$$$uu ""$"""'
-echo '           uuuu ""$$$$$$$$$$uuu'
-echo '  u$$$uuu$$$$$$$$$uu ""$$$$$$$$$$$uuu$$$'
-echo '  $$$$$$$$$$""""           ""$$$$$$$$$$$"'
-echo '   "$$$$$"                      ""$$$$""'
-echo '     $$$"                         $$$$"'
-echo ''
-echo ''
-echo '           TESTS ARE DISABLED'
-echo '               ARE U CRAZY ?'
-echo ''
-echo ''
-echo ''
-
-mvn clean install -e -DskipTests -Pdev $*
+# Parallel executions of maven modules and tests.
+# Half of CPU core are used in to keep other half for OS and other programs.
+# As long as web JS tests are not stable, it's recommended to execute: quick-build.sh -DskipWebTests=true
+mvn clean install -T0.5C -DforkCount=0.5C $*
---
+++ unsafe-build.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+export MAVEN_OPTS='-Xmx256m'
+
+echo ''
+echo ''
+echo '                 uuuuuuu'
+echo '             uu$$$$$$$$$$$uu'
+echo '          uu$$$$$$$$$$$$$$$$$uu'
+echo '         u$$$$$$$$$$$$$$$$$$$$$u'
+echo '        u$$$$$$$$$$$$$$$$$$$$$$$u'
+echo '       u$$$$$$$$$$$$$$$$$$$$$$$$$u'
+echo '       u$$$$$$$$$$$$$$$$$$$$$$$$$u'
+echo '       u$$$$$$"   "$$$"   "$$$$$$u'
+echo '       "$$$$"      u$u       $$$$"'
+echo '        $$$u       u$u       u$$$'
+echo '        $$$u      u$$$u      u$$$'
+echo '         "$$$$uu$$$   $$$uu$$$$"'
+echo '          "$$$$$$$"   "$$$$$$$"'
+echo '            u$$$$$$$u$$$$$$$u'
+echo '             u$"$"$"$"$"$"$u'
+echo '  uuu        $$u$ $ $ $ $u$$       uuu'
+echo ' u$$$$        $$$$$u$u$u$$$       u$$$$'
+echo '  $$$$$uu      "$$$$$$$$$"     uu$$$$$$'
+echo 'u$$$$$$$$$$$uu    """""    uuuu$$$$$$$$$$'
+echo '$$$$"""$$$$$$$$$$uuu   uu$$$$$$$$$"""$$$"'
+echo ' """      ""$$$$$$$$$$$uu ""$"""'
+echo '           uuuu ""$$$$$$$$$$uuu'
+echo '  u$$$uuu$$$$$$$$$uu ""$$$$$$$$$$$uuu$$$'
+echo '  $$$$$$$$$$""""           ""$$$$$$$$$$$"'
+echo '   "$$$$$"                      ""$$$$""'
+echo '     $$$"                         $$$$"'
+echo ''
+echo ''
+echo '           TESTS ARE DISABLED'
+echo '               ARE U CRAZY ?'
+echo ''
+echo ''
+echo ''
+
+# Parallel executions of maven modules and tests.
+# Half of CPU core are used in to keep other half for OS and other programs.
+
+mvn clean install -T0.5C -e -DskipTests -Pdev $*
