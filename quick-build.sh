4d764ff88f98a73608dadb216a30f600e5615d02
Simon Brandhof <simon.brandhof@sonarsource.com>
2015-02-16 09:13:16 +0100

    build.sh should always be fast -> moving quick-build to build and unsafe-build to quick-build

--- quick-build.sh
+++ quick-build.sh
@@ -1,6 +1,44 @@
 #!/bin/sh
 
+export MAVEN_OPTS='-Xmx256m'
+
+echo ''
+echo ''
+echo '                 uuuuuuu'
+echo '             uu$$$$$$$$$$$uu'
+echo '          uu$$$$$$$$$$$$$$$$$uu'
+echo '         u$$$$$$$$$$$$$$$$$$$$$u'
+echo '        u$$$$$$$$$$$$$$$$$$$$$$$u'
+echo '       u$$$$$$$$$$$$$$$$$$$$$$$$$u'
+echo '       u$$$$$$$$$$$$$$$$$$$$$$$$$u'
+echo '       u$$$$$$"   "$$$"   "$$$$$$u'
+echo '       "$$$$"      u$u       $$$$"'
+echo '        $$$u       u$u       u$$$'
+echo '        $$$u      u$$$u      u$$$'
+echo '         "$$$$uu$$$   $$$uu$$$$"'
+echo '          "$$$$$$$"   "$$$$$$$"'
+echo '            u$$$$$$$u$$$$$$$u'
+echo '             u$"$"$"$"$"$"$u'
+echo '  uuu        $$u$ $ $ $ $u$$       uuu'
+echo ' u$$$$        $$$$$u$u$u$$$       u$$$$'
+echo '  $$$$$uu      "$$$$$$$$$"     uu$$$$$$'
+echo 'u$$$$$$$$$$$uu    """""    uuuu$$$$$$$$$$'
+echo '$$$$"""$$$$$$$$$$uuu   uu$$$$$$$$$"""$$$"'
+echo ' """      ""$$$$$$$$$$$uu ""$"""'
+echo '           uuuu ""$$$$$$$$$$uuu'
+echo '  u$$$uuu$$$$$$$$$uu ""$$$$$$$$$$$uuu$$$'
+echo '  $$$$$$$$$$""""           ""$$$$$$$$$$$"'
+echo '   "$$$$$"                      ""$$$$""'
+echo '     $$$"                         $$$$"'
+echo ''
+echo ''
+echo '           TESTS ARE DISABLED'
+echo '               ARE U CRAZY ?'
+echo ''
+echo ''
+echo ''
+
 # Parallel executions of maven modules and tests.
 # Half of CPU core are used in to keep other half for OS and other programs.
-# As long as web JS tests are not stable, it's recommended to execute: quick-build.sh -DskipWebTests=true
-mvn clean install -T0.5C -DforkCount=0.5C $*
+
+mvn clean install -T0.5C -B -e -DskipTests=true -Pdev $*
