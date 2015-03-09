d18561062728a32a5856461567e33d182ff15202
Jean-Baptiste Lievremont <jean-baptiste.lievremont@sonarsource.com>
2015-03-09 14:28:36 +0100

    SONAR-6247 Add createdInLast parameter to search issues by lifetime

--- server/sonar-server/src/test/java/org/sonar/server/issue/ws/SearchActionMediumTest.java
+++ server/sonar-server/src/test/java/org/sonar/server/issue/ws/SearchActionMediumTest.java
@@ -88,7 +88,7 @@ public class SearchActionMediumTest {
     assertThat(show.isPost()).isFalse();
     assertThat(show.isInternal()).isFalse();
     assertThat(show.responseExampleAsString()).isNotEmpty();
-    assertThat(show.params()).hasSize(39);
+    assertThat(show.params()).hasSize(40);
   }
 
   @Test
