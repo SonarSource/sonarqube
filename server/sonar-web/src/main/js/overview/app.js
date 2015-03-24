050b98e007089615436e1e1888399ba1237c17f4
Stas Vilchik <vilchiks@gmail.com>
2015-03-24 13:43:05 +0100

    wip

--- server/sonar-web/src/main/js/overview/app.js
+++ server/sonar-web/src/main/js/overview/app.js
@@ -1,12 +1,14 @@
 requirejs([
   'overview/layout',
   'overview/models/state',
+  'overview/views/gate-view',
   'overview/views/size-view',
   'overview/views/issues-view',
   'overview/views/coverage-view',
   'overview/views/duplications-view'
 ], function (Layout,
              State,
+             GateView,
              SizeView,
              IssuesView,
              CoverageView,
@@ -20,6 +22,7 @@ requirejs([
       el: '.overview',
       model: this.state
     }).render();
+    this.layout.gateRegion.show(new GateView());
     this.layout.sizeRegion.show(new SizeView());
     this.layout.issuesRegion.show(new IssuesView());
     this.layout.coverageRegion.show(new CoverageView());
