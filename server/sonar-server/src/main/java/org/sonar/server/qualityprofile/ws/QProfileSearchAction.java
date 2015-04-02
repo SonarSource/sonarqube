c758df02ed6c84bdeaff46aa9fc4648fd296c945
Jean-Baptiste Lievremont <jean-baptiste.lievremont@sonarsource.com>
2015-04-02 10:55:37 +0200

    SONAR-6294 Update response example, only write active rule count when needed

--- server/sonar-server/src/main/java/org/sonar/server/qualityprofile/ws/QProfileSearchAction.java
+++ server/sonar-server/src/main/java/org/sonar/server/qualityprofile/ws/QProfileSearchAction.java
@@ -118,7 +118,7 @@ public class QProfileSearchAction implements BaseQProfileWsAction {
       json.beginObject()
         .prop(FIELD_KEY, nullUnlessNeeded(FIELD_KEY, key, fields))
         .prop(FIELD_NAME, nullUnlessNeeded(FIELD_NAME, profile.name(), fields))
-        .prop(FIELD_ACTIVE_RULE_COUNT, activeRuleCountByKey.get(key));
+        .prop(FIELD_ACTIVE_RULE_COUNT, nullUnlessNeeded(FIELD_ACTIVE_RULE_COUNT, activeRuleCountByKey.get(key), fields));
       writeLanguageFields(json, profile, fields);
       writeParentFields(json, profile, fields, profilesByKey);
       // Special case for booleans
