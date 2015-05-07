f8f2f5a95b8a485dd283222afbf13fdc822240bb
Jean-Baptiste Lievremont <jean-baptiste.lievremont@sonarsource.com>
2015-05-07 17:33:10 +0200

    SONAR-6427 SONAR-6428 Fix l10n of dashboard titles

--- server/sonar-web/src/main/js/common/handlebars-extensions.js
+++ server/sonar-web/src/main/js/common/handlebars-extensions.js
@@ -552,4 +552,14 @@
     return window.formatMeasureVariation(measure, type);
   });
 
+  Handlebars.registerHelper('dashboardL10n', function (dashboardName) {
+    var l10nKey = 'dashboard.' + dashboardName + '.name';
+    var l10nLabel = window.t(l10nKey);
+    if (l10nLabel !== l10nKey) {
+      return l10nLabel;
+    } else {
+      return dashboardName;
+    }
+  });
+
 })();
