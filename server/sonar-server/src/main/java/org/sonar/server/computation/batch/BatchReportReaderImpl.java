03e6eeb3fa98c2bfcb8582c5cea22d0aef52471c
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2015-05-27 18:03:31 +0200

    SONAR-6589 add caching of report MetaData in CEBatchReportReader

--- server/sonar-server/src/main/java/org/sonar/server/computation/batch/BatchReportReaderImpl.java
+++ server/sonar-server/src/main/java/org/sonar/server/computation/batch/BatchReportReaderImpl.java
@@ -37,6 +37,8 @@ import org.sonar.server.util.CloseableIterator;
 
 public class BatchReportReaderImpl implements BatchReportReader {
   private final org.sonar.batch.protocol.output.BatchReportReader delegate;
+  // caching of metadata which are read often
+  private BatchReport.Metadata metadata;
 
   public BatchReportReaderImpl(BatchReportDirectoryHolder batchReportDirectoryHolder) {
     this.delegate = new org.sonar.batch.protocol.output.BatchReportReader(batchReportDirectoryHolder.getDirectory());
@@ -44,7 +46,10 @@ public class BatchReportReaderImpl implements BatchReportReader {
 
   @Override
   public BatchReport.Metadata readMetadata() {
-    return delegate.readMetadata();
+    if (this.metadata == null) {
+      this.metadata = delegate.readMetadata();
+    }
+    return this.metadata;
   }
 
   @Override
