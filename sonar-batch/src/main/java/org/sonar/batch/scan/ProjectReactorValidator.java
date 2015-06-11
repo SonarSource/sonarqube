0a76c5949c28fca263260d9b9787fa612ff1037d
Julien Lancelot <julien.lancelot@sonarsource.com>
2015-06-11 10:10:57 +0200

    SONAR-6636 Remove module checks from batch requiring database

--- sonar-batch/src/main/java/org/sonar/batch/scan/ProjectReactorValidator.java
+++ sonar-batch/src/main/java/org/sonar/batch/scan/ProjectReactorValidator.java
@@ -20,21 +20,16 @@
 package org.sonar.batch.scan;
 
 import com.google.common.base.Joiner;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.Nullable;
 import org.apache.commons.lang.StringUtils;
 import org.sonar.api.CoreProperties;
 import org.sonar.api.batch.bootstrap.ProjectDefinition;
 import org.sonar.api.batch.bootstrap.ProjectReactor;
 import org.sonar.api.config.Settings;
-import org.sonar.api.resources.Qualifiers;
 import org.sonar.api.utils.SonarException;
 import org.sonar.core.component.ComponentKeys;
-import org.sonar.core.resource.ResourceDao;
-import org.sonar.core.resource.ResourceDto;
-
-import javax.annotation.Nullable;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * This class aims at validating project reactor
@@ -44,20 +39,12 @@ public class ProjectReactorValidator {
 
   private static final String SONAR_PHASE = "sonar.phase";
   private final Settings settings;
-  private final ResourceDao resourceDao;
-
-  public ProjectReactorValidator(Settings settings, @Nullable ResourceDao resourceDao) {
-    this.settings = settings;
-    this.resourceDao = resourceDao;
-  }
 
   public ProjectReactorValidator(Settings settings) {
-    this(settings, null);
+    this.settings = settings;
   }
 
   public void validate(ProjectReactor reactor) {
-    preventAutomaticProjectCreationIfNeeded(reactor);
-
     String branch = settings.getString(CoreProperties.PROJECT_BRANCH_PROPERTY);
     String rootProjectKey = ComponentKeys.createKey(reactor.getRoot().getKey(), branch);
 
@@ -75,38 +62,10 @@ public class ProjectReactorValidator {
     }
   }
 
-  private void preventAutomaticProjectCreationIfNeeded(ProjectReactor reactor) {
-    if (resourceDao != null && settings.getBoolean(CoreProperties.CORE_PREVENT_AUTOMATIC_PROJECT_CREATION)) {
-      String projectKey = reactor.getRoot().getKeyWithBranch();
-      if (resourceDao.findByKey(projectKey) == null) {
-        throw new SonarException(String.format("Unable to scan non-existing project \"%s\"", projectKey));
-      }
-    }
-  }
-
   private void validateModule(ProjectDefinition moduleDef, List<String> validationMessages, @Nullable String branch, String rootProjectKey) {
     if (!ComponentKeys.isValidModuleKey(moduleDef.getKey())) {
       validationMessages.add(String.format("\"%s\" is not a valid project or module key. "
         + "Allowed characters are alphanumeric, '-', '_', '.' and ':', with at least one non-digit.", moduleDef.getKey()));
-    } else if (resourceDao != null && isSubProject(moduleDef)) {
-      // SONAR-4692 Validate root project is the same than previous analysis to avoid module with same key in different projects
-      String moduleKey = ComponentKeys.createKey(moduleDef.getKey(), branch);
-      ResourceDto rootInDB = resourceDao.getRootProjectByComponentKey(moduleKey);
-      if (rootInDB == null || Qualifiers.LIBRARY.equals(rootInDB.getQualifier())) {
-        // This is a new module or previously a library so OK
-        return;
-      }
-      if (rootInDB.getKey().equals(moduleKey)) {
-        // SONAR-4245 current subproject is actually a root project in SQ DB
-        throw new SonarException(
-          String.format("The project '%s' is already defined in SonarQube but not as a module of project '%s'. "
-            + "If you really want to stop directly analysing project '%s', please first delete it from SonarQube and then relaunch the analysis of project '%s'.",
-            moduleKey, rootProjectKey, moduleKey, rootProjectKey));
-      }
-      if (!rootProjectKey.equals(rootInDB.getKey())) {
-        // SONAR-4692 current subproject is already a subproject in another project
-        throw new SonarException(String.format("Module \"%s\" is already part of project \"%s\"", moduleDef.getKey(), rootInDB.getKey()));
-      }
     }
   }
 
@@ -114,10 +73,6 @@ public class ProjectReactorValidator {
     if (settings.getString(SONAR_PHASE) != null) {
       validationMessages.add(String.format("Property \"%s\" is deprecated. Please remove it from your configuration.", SONAR_PHASE));
     }
-  }
-
-  private boolean isSubProject(ProjectDefinition moduleDef) {
-    return moduleDef.getParent() != null;
   }
 
   private void validateBranch(List<String> validationMessages, @Nullable String branch) {
