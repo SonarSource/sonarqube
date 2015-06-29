9849b523a2bcc018b84f5b11e0fb3e2bd5d3d13a
Julien HENRY <henryju@yahoo.fr>
2015-06-29 11:24:01 +0200

    Use incremental mode to do pull request analysis

--- travis.sh
+++ travis.sh
@@ -36,10 +36,9 @@ PRANALYSIS)
   then
     echo "Start pullrequest analysis"
     mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar -B -e -V -Dmaven.test.failure.ignore=true -Dclirr=true \
-     -Dsonar.analysis.mode=preview \
+     -Dsonar.analysis.mode=incremental \
      -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
      -Dsonar.github.repository=$SONAR_GITHUB_REPOSITORY \
-     -Dsonar.forceUpdate=true \
      -Dsonar.github.login=$SONAR_GITHUB_LOGIN \
      -Dsonar.github.oauth=$SONAR_GITHUB_OAUTH \
      -Dsonar.host.url=$SONAR_HOST_URL \
