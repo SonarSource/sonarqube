c2d281a185f6c7611ba666116fe453239079ae39
David Gageot <david@gageot.net>
2015-07-13 10:06:30 +0200

    Use latest version of travis-utils

--- travis.sh
+++ travis.sh
@@ -3,7 +3,8 @@
 set -euo pipefail
 
 function installTravisTools {
-  curl -sSL https://raw.githubusercontent.com/sonarsource/travis-utils/v6/install.sh | bash
+  curl -sSL https://raw.githubusercontent.com/sonarsource/travis-utils/v10/install.sh | bash
+  source /tmp/travis-utils/env.sh
 }
 
 case "$JOB" in
