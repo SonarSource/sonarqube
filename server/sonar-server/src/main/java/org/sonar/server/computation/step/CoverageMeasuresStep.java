79ea8743f2fc5fb7e24ee99dc76da025fd5efd7d
Julien Lancelot <julien.lancelot@sonarsource.com>
2015-07-21 18:26:10 +0200

    SONAR-6605 Move coverage measures with variations to NewCoverageMeasuresStep

--- server/sonar-server/src/main/java/org/sonar/server/computation/step/CoverageMeasuresStep.java
+++ server/sonar-server/src/main/java/org/sonar/server/computation/step/CoverageMeasuresStep.java
@@ -26,51 +26,36 @@ import org.sonar.server.computation.formula.Formula;
 import org.sonar.server.computation.formula.FormulaExecutorComponentVisitor;
 import org.sonar.server.computation.formula.coverage.LinesAndConditionsWithUncoveredFormula;
 import org.sonar.server.computation.formula.coverage.LinesAndConditionsWithUncoveredMetricKeys;
-import org.sonar.server.computation.formula.coverage.LinesAndConditionsWithUncoveredVariationFormula;
 import org.sonar.server.computation.formula.coverage.SingleWithUncoveredFormula;
 import org.sonar.server.computation.formula.coverage.SingleWithUncoveredMetricKeys;
-import org.sonar.server.computation.formula.coverage.SingleWithUncoveredVariationFormula;
 import org.sonar.server.computation.measure.MeasureRepository;
 import org.sonar.server.computation.metric.MetricRepository;
-import org.sonar.server.computation.period.PeriodsHolder;
 
 /**
  * Computes coverage measures on files and then aggregates them on higher components.
  */
 public class CoverageMeasuresStep implements ComputationStep {
   private static final ImmutableList<Formula> COVERAGE_FORMULAS = ImmutableList.<Formula>of(
-      // code
-      new CodeCoverageFormula(),
-      new NewCodeCoverageFormula(),
-      new ItCoverageFormula(),
-      new NewItCoverageFormula(),
-      new OverallCodeCoverageFormula(),
-      new NewOverallCodeCoverageFormula(),
-      // branch
-      new BranchCoverageFormula(),
-      new NewBranchCoverageFormula(),
-      new ItBranchCoverageFormula(),
-      new NewItBranchCoverageFormula(),
-      new OverallBranchCoverageFormula(),
-      new NewOverallBranchCoverageFormula(),
-      // line
-      new LineCoverageFormula(),
-      new NewLineCoverageFormula(),
-      new ItLineCoverageFormula(),
-      new NewItLinesCoverageFormula(),
-      new OverallLineCoverageFormula(),
-      new NewOverallLineCoverageFormula()
-  );
+    // code
+    new CodeCoverageFormula(),
+    new ItCoverageFormula(),
+    new OverallCodeCoverageFormula(),
+    // branch
+    new BranchCoverageFormula(),
+    new ItBranchCoverageFormula(),
+    new OverallBranchCoverageFormula(),
+    // line
+    new LineCoverageFormula(),
+    new ItLineCoverageFormula(),
+    new OverallLineCoverageFormula()
+    );
 
   private final TreeRootHolder treeRootHolder;
-  private final PeriodsHolder periodsHolder;
   private final MetricRepository metricRepository;
   private final MeasureRepository measureRepository;
 
-  public CoverageMeasuresStep(TreeRootHolder treeRootHolder,
-    PeriodsHolder periodsHolder, MetricRepository metricRepository, MeasureRepository measureRepository) {
+  public CoverageMeasuresStep(TreeRootHolder treeRootHolder, MetricRepository metricRepository, MeasureRepository measureRepository) {
     this.treeRootHolder = treeRootHolder;
-    this.periodsHolder = periodsHolder;
     this.metricRepository = metricRepository;
     this.measureRepository = measureRepository;
   }
@@ -78,7 +63,6 @@ public class CoverageMeasuresStep implements ComputationStep {
   @Override
   public void execute() {
     FormulaExecutorComponentVisitor.newBuilder(metricRepository, measureRepository)
-      .withVariationSupport(periodsHolder)
       .buildFor(COVERAGE_FORMULAS)
       .visit(treeRootHolder.getRoot());
   }
@@ -94,17 +78,6 @@ public class CoverageMeasuresStep implements ComputationStep {
     }
   }
 
-  private static class NewCodeCoverageFormula extends LinesAndConditionsWithUncoveredVariationFormula {
-    public NewCodeCoverageFormula() {
-      super(
-        new LinesAndConditionsWithUncoveredMetricKeys(
-          CoreMetrics.NEW_LINES_TO_COVER_KEY, CoreMetrics.NEW_CONDITIONS_TO_COVER_KEY,
-          CoreMetrics.NEW_UNCOVERED_LINES_KEY, CoreMetrics.NEW_UNCOVERED_CONDITIONS_KEY
-        ),
-        CoreMetrics.NEW_COVERAGE_KEY);
-    }
-  }
-
   private static class ItCoverageFormula extends LinesAndConditionsWithUncoveredFormula {
     private ItCoverageFormula() {
       super(
@@ -114,18 +87,6 @@ public class CoverageMeasuresStep implements ComputationStep {
         ),
         CoreMetrics.IT_COVERAGE_KEY);
     }
-  }
-
-  private static class NewItCoverageFormula extends LinesAndConditionsWithUncoveredVariationFormula {
-    private NewItCoverageFormula() {
-      super(
-        new LinesAndConditionsWithUncoveredMetricKeys(
-          CoreMetrics.NEW_IT_LINES_TO_COVER_KEY, CoreMetrics.NEW_IT_CONDITIONS_TO_COVER_KEY,
-          CoreMetrics.NEW_IT_UNCOVERED_LINES_KEY, CoreMetrics.NEW_IT_UNCOVERED_CONDITIONS_KEY
-        ),
-        CoreMetrics.NEW_IT_COVERAGE_KEY);
-    }
-
   }
 
   private static class OverallCodeCoverageFormula extends LinesAndConditionsWithUncoveredFormula {
@@ -139,17 +100,6 @@ public class CoverageMeasuresStep implements ComputationStep {
     }
   }
 
-  private static class NewOverallCodeCoverageFormula extends LinesAndConditionsWithUncoveredVariationFormula {
-    public NewOverallCodeCoverageFormula() {
-      super(
-        new LinesAndConditionsWithUncoveredMetricKeys(
-          CoreMetrics.NEW_OVERALL_LINES_TO_COVER_KEY, CoreMetrics.NEW_OVERALL_CONDITIONS_TO_COVER_KEY,
-          CoreMetrics.NEW_OVERALL_UNCOVERED_LINES_KEY, CoreMetrics.NEW_OVERALL_UNCOVERED_CONDITIONS_KEY
-        ),
-        CoreMetrics.NEW_OVERALL_COVERAGE_KEY);
-    }
-  }
-
   private static class BranchCoverageFormula extends SingleWithUncoveredFormula {
     public BranchCoverageFormula() {
       super(
@@ -157,16 +107,6 @@ public class CoverageMeasuresStep implements ComputationStep {
           CoreMetrics.CONDITIONS_TO_COVER_KEY, CoreMetrics.UNCOVERED_CONDITIONS_KEY
         ),
         CoreMetrics.BRANCH_COVERAGE_KEY);
-    }
-  }
-
-  private static class NewBranchCoverageFormula extends SingleWithUncoveredVariationFormula {
-    public NewBranchCoverageFormula() {
-      super(
-        new SingleWithUncoveredMetricKeys(
-          CoreMetrics.NEW_CONDITIONS_TO_COVER_KEY, CoreMetrics.NEW_UNCOVERED_CONDITIONS_KEY
-        ),
-        CoreMetrics.NEW_BRANCH_COVERAGE_KEY);
     }
   }
 
@@ -180,30 +120,11 @@ public class CoverageMeasuresStep implements ComputationStep {
     }
   }
 
-  private static class NewItBranchCoverageFormula extends SingleWithUncoveredVariationFormula {
-    public NewItBranchCoverageFormula() {
-      super(
-        new SingleWithUncoveredMetricKeys(
-          CoreMetrics.NEW_IT_CONDITIONS_TO_COVER_KEY, CoreMetrics.NEW_IT_UNCOVERED_CONDITIONS_KEY
-        ),
-        CoreMetrics.NEW_IT_BRANCH_COVERAGE_KEY);
-    }
-  }
-
   private static class OverallBranchCoverageFormula extends SingleWithUncoveredFormula {
     public OverallBranchCoverageFormula() {
       super(
         new SingleWithUncoveredMetricKeys(CoreMetrics.OVERALL_CONDITIONS_TO_COVER_KEY, CoreMetrics.OVERALL_UNCOVERED_CONDITIONS_KEY),
         CoreMetrics.OVERALL_BRANCH_COVERAGE_KEY);
-    }
-  }
-
-  private static class NewOverallBranchCoverageFormula extends SingleWithUncoveredVariationFormula {
-    public NewOverallBranchCoverageFormula() {
-      super(
-        new SingleWithUncoveredMetricKeys(
-          CoreMetrics.NEW_OVERALL_CONDITIONS_TO_COVER_KEY, CoreMetrics.NEW_OVERALL_UNCOVERED_CONDITIONS_KEY),
-        CoreMetrics.NEW_OVERALL_BRANCH_COVERAGE_KEY);
     }
   }
 
@@ -215,27 +136,11 @@ public class CoverageMeasuresStep implements ComputationStep {
     }
   }
 
-  private static class NewLineCoverageFormula extends SingleWithUncoveredVariationFormula {
-    public NewLineCoverageFormula() {
-      super(
-        new SingleWithUncoveredMetricKeys(CoreMetrics.NEW_LINES_TO_COVER_KEY, CoreMetrics.NEW_UNCOVERED_LINES_KEY),
-        CoreMetrics.NEW_LINE_COVERAGE_KEY);
-    }
-  }
-
   private static class ItLineCoverageFormula extends SingleWithUncoveredFormula {
     public ItLineCoverageFormula() {
       super(
         new SingleWithUncoveredMetricKeys(CoreMetrics.IT_LINES_TO_COVER_KEY, CoreMetrics.IT_UNCOVERED_LINES_KEY),
         CoreMetrics.IT_LINE_COVERAGE_KEY);
-    }
-  }
-
-  private static class NewItLinesCoverageFormula extends SingleWithUncoveredVariationFormula {
-    public NewItLinesCoverageFormula() {
-      super(
-        new SingleWithUncoveredMetricKeys(CoreMetrics.NEW_IT_LINES_TO_COVER_KEY, CoreMetrics.NEW_IT_UNCOVERED_LINES_KEY),
-        CoreMetrics.NEW_IT_LINE_COVERAGE_KEY);
     }
   }
 
@@ -246,16 +151,6 @@ public class CoverageMeasuresStep implements ComputationStep {
           CoreMetrics.OVERALL_LINES_TO_COVER_KEY, CoreMetrics.OVERALL_UNCOVERED_LINES_KEY
         ),
         CoreMetrics.OVERALL_LINE_COVERAGE_KEY);
-    }
-  }
-
-  private static class NewOverallLineCoverageFormula extends SingleWithUncoveredVariationFormula {
-    public NewOverallLineCoverageFormula() {
-      super(
-        new SingleWithUncoveredMetricKeys(
-          CoreMetrics.NEW_OVERALL_LINES_TO_COVER_KEY, CoreMetrics.NEW_OVERALL_UNCOVERED_LINES_KEY
-        ),
-        CoreMetrics.NEW_OVERALL_LINE_COVERAGE_KEY);
     }
   }
 
--- server/sonar-server/src/test/java/org/sonar/server/computation/step/CoverageMeasuresStepTest.java
+++ server/sonar-server/src/test/java/org/sonar/server/computation/step/CoverageMeasuresStepTest.java
@@ -26,13 +26,9 @@ import org.sonar.api.measures.CoreMetrics;
 import org.sonar.server.computation.batch.TreeRootHolderRule;
 import org.sonar.server.computation.component.FileAttributes;
 import org.sonar.server.computation.formula.coverage.LinesAndConditionsWithUncoveredMetricKeys;
-import org.sonar.server.computation.measure.Measure;
 import org.sonar.server.computation.measure.MeasureRepoEntry;
 import org.sonar.server.computation.measure.MeasureRepositoryRule;
-import org.sonar.server.computation.measure.MeasureVariations;
 import org.sonar.server.computation.metric.MetricRepositoryRule;
-import org.sonar.server.computation.period.Period;
-import org.sonar.server.computation.period.PeriodsHolderRule;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.sonar.server.computation.component.Component.Type.DIRECTORY;
@@ -55,12 +51,7 @@ public class CoverageMeasuresStepTest {
 
   @Rule
   public TreeRootHolderRule treeRootHolder = new TreeRootHolderRule();
-  @Rule
-  public PeriodsHolderRule periodsHolder = new PeriodsHolderRule()
-    .setPeriods(
-      new Period(2, "mode2", null, 52L, 96L),
-      new Period(5, "mode5", null, 52L, 96L)
-    );
+
   @Rule
   public MetricRepositoryRule metricRepository = new MetricRepositoryRule()
     .add(CoreMetrics.LINES_TO_COVER)
@@ -68,47 +59,28 @@ public class CoverageMeasuresStepTest {
     .add(CoreMetrics.UNCOVERED_LINES)
     .add(CoreMetrics.UNCOVERED_CONDITIONS)
     .add(CoreMetrics.COVERAGE)
-    .add(CoreMetrics.NEW_LINES_TO_COVER)
-    .add(CoreMetrics.NEW_CONDITIONS_TO_COVER)
-    .add(CoreMetrics.NEW_UNCOVERED_LINES)
-    .add(CoreMetrics.NEW_UNCOVERED_CONDITIONS)
-    .add(CoreMetrics.NEW_COVERAGE)
+    .add(CoreMetrics.BRANCH_COVERAGE)
+    .add(CoreMetrics.LINE_COVERAGE)
+
     .add(CoreMetrics.IT_LINES_TO_COVER)
     .add(CoreMetrics.IT_CONDITIONS_TO_COVER)
     .add(CoreMetrics.IT_UNCOVERED_LINES)
     .add(CoreMetrics.IT_UNCOVERED_CONDITIONS)
     .add(CoreMetrics.IT_COVERAGE)
-    .add(CoreMetrics.NEW_IT_LINES_TO_COVER)
-    .add(CoreMetrics.NEW_IT_CONDITIONS_TO_COVER)
-    .add(CoreMetrics.NEW_IT_UNCOVERED_LINES)
-    .add(CoreMetrics.NEW_IT_UNCOVERED_CONDITIONS)
-    .add(CoreMetrics.NEW_IT_COVERAGE)
+    .add(CoreMetrics.IT_BRANCH_COVERAGE)
+    .add(CoreMetrics.IT_LINE_COVERAGE)
+
     .add(CoreMetrics.OVERALL_LINES_TO_COVER)
     .add(CoreMetrics.OVERALL_CONDITIONS_TO_COVER)
     .add(CoreMetrics.OVERALL_UNCOVERED_LINES)
     .add(CoreMetrics.OVERALL_UNCOVERED_CONDITIONS)
     .add(CoreMetrics.OVERALL_COVERAGE)
-    .add(CoreMetrics.NEW_OVERALL_LINES_TO_COVER)
-    .add(CoreMetrics.NEW_OVERALL_UNCOVERED_LINES)
-    .add(CoreMetrics.NEW_OVERALL_UNCOVERED_CONDITIONS)
-    .add(CoreMetrics.NEW_OVERALL_CONDITIONS_TO_COVER)
-    .add(CoreMetrics.NEW_OVERALL_COVERAGE)
-    .add(CoreMetrics.BRANCH_COVERAGE)
-    .add(CoreMetrics.NEW_BRANCH_COVERAGE)
-    .add(CoreMetrics.IT_BRANCH_COVERAGE)
-    .add(CoreMetrics.NEW_IT_BRANCH_COVERAGE)
     .add(CoreMetrics.OVERALL_BRANCH_COVERAGE)
-    .add(CoreMetrics.NEW_OVERALL_BRANCH_COVERAGE)
-    .add(CoreMetrics.LINE_COVERAGE)
-    .add(CoreMetrics.NEW_LINE_COVERAGE)
-    .add(CoreMetrics.IT_LINE_COVERAGE)
-    .add(CoreMetrics.NEW_IT_LINE_COVERAGE)
-    .add(CoreMetrics.OVERALL_LINE_COVERAGE)
-    .add(CoreMetrics.NEW_OVERALL_LINE_COVERAGE);
+    .add(CoreMetrics.OVERALL_LINE_COVERAGE);
   @Rule
   public MeasureRepositoryRule measureRepository = MeasureRepositoryRule.create(treeRootHolder, metricRepository);
 
-  CoverageMeasuresStep underTest = new CoverageMeasuresStep(treeRootHolder, periodsHolder, metricRepository, measureRepository);
+  CoverageMeasuresStep underTest = new CoverageMeasuresStep(treeRootHolder, metricRepository, measureRepository);
 
   @Before
   public void setUp() throws Exception {
@@ -135,7 +107,7 @@ public class CoverageMeasuresStepTest {
     LinesAndConditionsWithUncoveredMetricKeys metricKeys = new LinesAndConditionsWithUncoveredMetricKeys(
       CoreMetrics.LINES_TO_COVER_KEY, CoreMetrics.CONDITIONS_TO_COVER_KEY,
       CoreMetrics.UNCOVERED_LINES_KEY, CoreMetrics.UNCOVERED_CONDITIONS_KEY
-    );
+      );
     String codeCoverageKey = CoreMetrics.COVERAGE_KEY;
     String lineCoverageKey = CoreMetrics.LINE_COVERAGE_KEY;
     String branchCoverageKey = CoreMetrics.BRANCH_COVERAGE_KEY;
@@ -148,7 +120,7 @@ public class CoverageMeasuresStepTest {
     LinesAndConditionsWithUncoveredMetricKeys metricKeys = new LinesAndConditionsWithUncoveredMetricKeys(
       CoreMetrics.IT_LINES_TO_COVER_KEY, CoreMetrics.IT_CONDITIONS_TO_COVER_KEY,
       CoreMetrics.IT_UNCOVERED_LINES_KEY, CoreMetrics.IT_UNCOVERED_CONDITIONS_KEY
-    );
+      );
     String codeCoverageKey = CoreMetrics.IT_COVERAGE_KEY;
     String lineCoverageKey = CoreMetrics.IT_LINE_COVERAGE_KEY;
     String branchCoverageKey = CoreMetrics.IT_BRANCH_COVERAGE_KEY;
@@ -161,7 +133,7 @@ public class CoverageMeasuresStepTest {
     LinesAndConditionsWithUncoveredMetricKeys metricKeys = new LinesAndConditionsWithUncoveredMetricKeys(
       CoreMetrics.OVERALL_LINES_TO_COVER_KEY, CoreMetrics.OVERALL_CONDITIONS_TO_COVER_KEY,
       CoreMetrics.OVERALL_UNCOVERED_LINES_KEY, CoreMetrics.OVERALL_UNCOVERED_CONDITIONS_KEY
-    );
+      );
     String codeCoverageKey = CoreMetrics.OVERALL_COVERAGE_KEY;
     String lineCoverageKey = CoreMetrics.OVERALL_LINE_COVERAGE_KEY;
     String branchCoverageKey = CoreMetrics.OVERALL_BRANCH_COVERAGE_KEY;
@@ -212,89 +184,4 @@ public class CoverageMeasuresStepTest {
     assertThat(toEntries(measureRepository.getAddedRawMeasures(ROOT_REF))).containsOnly(nonFileRepoEntries);
   }
 
-  @Test
-  public void verify_aggregates_variations_for_new_code_line_and_branch_Coverage() {
-    LinesAndConditionsWithUncoveredMetricKeys metricKeys = new LinesAndConditionsWithUncoveredMetricKeys(
-      CoreMetrics.NEW_LINES_TO_COVER_KEY, CoreMetrics.NEW_CONDITIONS_TO_COVER_KEY,
-      CoreMetrics.NEW_UNCOVERED_LINES_KEY, CoreMetrics.NEW_UNCOVERED_CONDITIONS_KEY
-    );
-    String codeCoverageKey = CoreMetrics.NEW_COVERAGE_KEY;
-    String lineCoverageKey = CoreMetrics.NEW_LINE_COVERAGE_KEY;
-    String branchCoverageKey = CoreMetrics.NEW_BRANCH_COVERAGE_KEY;
-
-    verify_aggregates_variations(metricKeys, codeCoverageKey, lineCoverageKey, branchCoverageKey);
-  }
-
-  @Test
-  public void verify_aggregates_variations_for_new_IT_code_line_and_branch_Coverage() {
-    LinesAndConditionsWithUncoveredMetricKeys metricKeys = new LinesAndConditionsWithUncoveredMetricKeys(
-      CoreMetrics.NEW_IT_LINES_TO_COVER_KEY, CoreMetrics.NEW_IT_CONDITIONS_TO_COVER_KEY,
-      CoreMetrics.NEW_IT_UNCOVERED_LINES_KEY, CoreMetrics.NEW_IT_UNCOVERED_CONDITIONS_KEY
-    );
-    String codeCoverageKey = CoreMetrics.NEW_IT_COVERAGE_KEY;
-    String lineCoverageKey = CoreMetrics.NEW_IT_LINE_COVERAGE_KEY;
-    String branchCoverageKey = CoreMetrics.NEW_IT_BRANCH_COVERAGE_KEY;
-
-    verify_aggregates_variations(metricKeys, codeCoverageKey, lineCoverageKey, branchCoverageKey);
-  }
-
-  @Test
-  public void verify_aggregates_variations_for_new_Overall_code_line_and_branch_Coverage() {
-    LinesAndConditionsWithUncoveredMetricKeys metricKeys = new LinesAndConditionsWithUncoveredMetricKeys(
-      CoreMetrics.NEW_OVERALL_LINES_TO_COVER_KEY, CoreMetrics.NEW_OVERALL_CONDITIONS_TO_COVER_KEY,
-      CoreMetrics.NEW_OVERALL_UNCOVERED_LINES_KEY, CoreMetrics.NEW_OVERALL_UNCOVERED_CONDITIONS_KEY
-    );
-    String codeCoverageKey = CoreMetrics.NEW_OVERALL_COVERAGE_KEY;
-    String lineCoverageKey = CoreMetrics.NEW_OVERALL_LINE_COVERAGE_KEY;
-    String branchCoverageKey = CoreMetrics.NEW_OVERALL_BRANCH_COVERAGE_KEY;
-
-    verify_aggregates_variations(metricKeys, codeCoverageKey, lineCoverageKey, branchCoverageKey);
-  }
-
-  private void verify_aggregates_variations(LinesAndConditionsWithUncoveredMetricKeys metricKeys, String codeCoverageKey, String lineCoverageKey, String branchCoverageKey) {
-    measureRepository
-      .addRawMeasure(FILE_1_REF, metricKeys.getLines(), measureWithVariation(3000L, 2000L))
-      .addRawMeasure(FILE_1_REF, metricKeys.getConditions(), measureWithVariation(300L, 400L))
-      .addRawMeasure(FILE_1_REF, metricKeys.getUncoveredLines(), measureWithVariation(30L, 200L))
-      .addRawMeasure(FILE_1_REF, metricKeys.getUncoveredConditions(), measureWithVariation(9L, 16L))
-
-      .addRawMeasure(FILE_2_REF, metricKeys.getLines(), measureWithVariation(2000L, 3000L))
-      .addRawMeasure(FILE_2_REF, metricKeys.getConditions(), measureWithVariation(400L, 300L))
-      .addRawMeasure(FILE_2_REF, metricKeys.getUncoveredLines(), measureWithVariation(200L, 30L))
-      .addRawMeasure(FILE_2_REF, metricKeys.getUncoveredConditions(), measureWithVariation(16L, 9L))
-
-      .addRawMeasure(UNIT_TEST_FILE_REF, metricKeys.getLines(), measureWithVariation(1000L, 2000L))
-      .addRawMeasure(UNIT_TEST_FILE_REF, metricKeys.getConditions(), measureWithVariation(100L, 400L))
-      .addRawMeasure(UNIT_TEST_FILE_REF, metricKeys.getUncoveredLines(), measureWithVariation(10L, 200L))
-      .addRawMeasure(UNIT_TEST_FILE_REF, metricKeys.getUncoveredConditions(), measureWithVariation(3L, 16L));
-
-    underTest.execute();
-
-    assertThat(toEntries(measureRepository.getAddedRawMeasures(FILE_1_REF))).containsOnly(
-      entryOf(codeCoverageKey, measureWithVariation(98.8d, 91d)),
-      entryOf(lineCoverageKey, measureWithVariation(99d, 90d)),
-      entryOf(branchCoverageKey, measureWithVariation(97d, 96d))
-    );
-    assertThat(toEntries(measureRepository.getAddedRawMeasures(FILE_2_REF))).containsOnly(
-      entryOf(codeCoverageKey, measureWithVariation(91d, 98.8d)),
-      entryOf(lineCoverageKey, measureWithVariation(90d, 99d)),
-      entryOf(branchCoverageKey, measureWithVariation(96d, 97d))
-    );
-    assertThat(toEntries(measureRepository.getAddedRawMeasures(UNIT_TEST_FILE_REF))).isEmpty();
-
-    MeasureRepoEntry[] nonFileRepoEntries = {
-      entryOf(codeCoverageKey, measureWithVariation(95.5d, 95.5d)),
-      entryOf(lineCoverageKey, measureWithVariation(95.4d, 95.4d)),
-      entryOf(branchCoverageKey, measureWithVariation(96.4d, 96.4d))
-    };
-
-    assertThat(toEntries(measureRepository.getAddedRawMeasures(DIRECTORY_REF))).containsOnly(nonFileRepoEntries);
-    assertThat(toEntries(measureRepository.getAddedRawMeasures(SUB_MODULE_REF))).containsOnly(nonFileRepoEntries);
-    assertThat(toEntries(measureRepository.getAddedRawMeasures(MODULE_REF))).containsOnly(nonFileRepoEntries);
-    assertThat(toEntries(measureRepository.getAddedRawMeasures(ROOT_REF))).containsOnly(nonFileRepoEntries);
-  }
-
-  private static Measure measureWithVariation(double variation2, double variation5) {
-    return newMeasureBuilder().setVariations(new MeasureVariations(null, variation2, null, null, variation5)).createNoValue();
-  }
 }
