9ab07e28ce0128a4004d3ce9187ca98f159838e4
Julien Lancelot <julien.lancelot@sonarsource.com>
2015-07-22 14:30:59 +0200

    SONAR-6605 Move complexity formulas to its own step

--- server/sonar-server/src/main/java/org/sonar/server/computation/step/ComputationSteps.java
+++ server/sonar-server/src/main/java/org/sonar/server/computation/step/ComputationSteps.java
@@ -63,6 +63,7 @@ public class ComputationSteps {
       CoreMetricFormulaExecutorStep.class,
       LanguageDistributionMeasuresStep.class,
       UnitTestMeasuresStep.class,
+      ComplexityMeasuresStep.class,
 
       // SQALE measures depend on issues
       SqaleMeasuresStep.class,
