5adfc76bd08520e6184199c76562cea038d4c49b
Stas Vilchik <vilchiks@gmail.com>
2015-07-31 14:06:14 +0200

    SONAR-6660 rewrite setup page

--- server/sonar-web/src/main/js/apps/maintenance/main-view.js
+++ server/sonar-web/src/main/js/apps/maintenance/main-view.js
@@ -2,12 +2,21 @@ define([
   './templates'
 ], function () {
 
+  var $ = jQuery;
+
   return Marionette.ItemView.extend({
     template: Templates['maintenance-main'],
 
+    events: {
+      'click #start-migration': 'startMigration'
+    },
+
     initialize: function () {
       var that = this;
-      this.requestOptions = { type: 'GET', url: baseUrl + '/api/system/status' };
+      this.requestOptions = {
+        type: 'GET',
+        url: baseUrl + '/api/system/' + (this.options.setup ? 'db_migration_status' : 'status')
+      };
       setInterval(function () {
         that.refresh();
       }, 5000);
@@ -19,6 +28,21 @@ define([
         that.model.set(r);
         that.render();
       });
+    },
+
+    startMigration: function () {
+      var that = this;
+      Backbone.ajax({
+        url: baseUrl + '/api/system/migrate_db',
+        type: 'POST'
+      }).done(function (r) {
+        that.model.set(r);
+        that.render();
+      })
+    },
+
+    onRender: function () {
+      $('.page-simple').toggleClass('panel-warning', this.model.get('state') === 'MIGRATION_REQUIRED');
     },
 
     serializeData: function () {
--- server/sonar-web/src/main/webapp/WEB-INF/app/controllers/setup_controller.rb
+++ server/sonar-web/src/main/webapp/WEB-INF/app/controllers/setup_controller.rb
@@ -21,34 +21,9 @@ class SetupController < ApplicationController
   skip_before_filter :check_database_version, :check_authentication
 
   SECTION=Navigation::SECTION_CONFIGURATION
-  
-  verify :method => :post, :only => [ :setup_database ], :redirect_to => { :action => :index }
-    
+
   def index
-    if !ActiveRecord::Base.connected?
-      render :template => 'setup/dbdown', :layout => 'nonav'
-    elsif DatabaseMigrationManager.instance.requires_migration?
-      render :template => 'setup/form', :layout => 'nonav'
-    elsif DatabaseMigrationManager.instance.migration_running?
-      render :template => 'setup/migration_running', :layout => 'nonav'
-    elsif DatabaseMigrationManager.instance.migration_failed?
-      render :template => 'setup/failed', :layout => 'nonav'
-    else
-      # migration succeeded, or no need for migration
-      render :template => 'setup/db_uptodate', :layout => 'nonav' 
-    end
-  end
-
-  def setup_database
-    # Ask the DB migration manager to start the migration
-    # => No need to check for authorizations (actually everybody can run the upgrade)
-    # nor concurrent calls (this is handled directly by DatabaseMigrationManager)
-    DatabaseMigrationManager.instance.start_migration
-    redirect_to :action => :index
-  end
-
-  def maintenance
-    render :template => 'setup/maintenance', :layout => 'nonav'
+    render :template => 'setup/index', :layout => 'nonav'
   end
   
 end
