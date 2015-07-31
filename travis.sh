2aa70d1ad9253ac31901e776644b6213489e7f15
Simon Brandhof <simon.brandhof@sonarsource.com>
2015-07-31 11:43:12 +0200

    Upgrade to travis-utils v16

--- travis.sh
+++ travis.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 function installTravisTools {
   mkdir ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v15 | tar zx --strip-components 1 -C ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v16 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
 
