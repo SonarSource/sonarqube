3248c684a4499976fbed64b0b00c72ab696b8989
Simon Brandhof <simon.brandhof@sonarsource.com>
2015-08-02 20:28:57 +0200

    Support absent protobuf arrays in ProtobufJsonFormat
    Protobuf does not support concept of absent arrays (repeated field).
    It does not distinguish empty arrays from absent arrays.
    ProtobufJsonFormat introduces a naming convention to mark an
    empty array field as absent. In this case the field is not output
    to JSON.

--- sonar-core/src/test/java/org/sonar/core/util/ProtobufJsonFormatTest.java
+++ sonar-core/src/test/java/org/sonar/core/util/ProtobufJsonFormatTest.java
@@ -25,9 +25,12 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.sonar.api.utils.text.JsonWriter;
+import org.sonar.core.test.Test.JsonArrayTest;
+import org.sonar.core.test.Test.JsonTest;
 
 import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.sonar.core.util.ProtobufJsonFormat.toJson;
 
 public class ProtobufJsonFormatTest {
 
@@ -35,49 +38,100 @@ public class ProtobufJsonFormatTest {
   public ExpectedException expectedException = ExpectedException.none();
 
   @Test
-  public void convert_protobuf_to_json() throws Exception {
-    org.sonar.core.test.Test.Fake protobuf = org.sonar.core.test.Test.Fake.newBuilder()
-      .setAString("foo")
-      .setAnInt(10)
-      .setALong(100L)
-      .setABoolean(true)
-      .setADouble(3.14)
-      .setAnEnum(org.sonar.core.test.Test.FakeEnum.GREEN)
+  public void convert_protobuf_to_json() {
+    JsonTest protobuf = JsonTest.newBuilder()
+      .setStringField("foo")
+      .setIntField(10)
+      .setLongField(100L)
+      .setDoubleField(3.14)
+      .setBooleanField(true)
+      .setEnumField(org.sonar.core.test.Test.FakeEnum.GREEN)
       .addAllAnArray(asList("one", "two", "three"))
-      .setANestedMessage(org.sonar.core.test.Test.NestedFake.newBuilder().setLabel("bar").build())
+      .setNested(org.sonar.core.test.Test.NestedJsonTest.newBuilder().setLabel("bar").build())
       .build();
 
-    StringWriter json = new StringWriter();
-    JsonWriter jsonWriter = JsonWriter.of(json);
-    ProtobufJsonFormat.write(protobuf, jsonWriter);
-
-    assertThat(json.toString())
-      .isEqualTo("{\"aString\":\"foo\",\"anInt\":10,\"aLong\":100,\"aDouble\":3.14,\"aBoolean\":true,\"anEnum\":\"GREEN\",\"anArray\":[\"one\",\"two\",\"three\"],\"aNestedMessage\":{\"label\":\"bar\"}}");
+    assertThat(toJson(protobuf))
+      .isEqualTo(
+        "{\"stringField\":\"foo\",\"intField\":10,\"longField\":100,\"doubleField\":3.14,\"booleanField\":true,\"enumField\":\"GREEN\",\"nested\":{\"label\":\"bar\"},\"anArray\":[\"one\",\"two\",\"three\"]}");
   }
 
   @Test
-  public void protobuf_bytes_field_can_not_be_converted_to_json() throws Exception {
+  public void protobuf_bytes_field_can_not_be_converted_to_json() {
     expectedException.expect(RuntimeException.class);
-    expectedException.expectMessage("JSON format does not support the binary field 'someBytes'");
+    expectedException.expectMessage("JSON format does not support type 'BYTE_STRING' of field 'bytesField'");
 
-    org.sonar.core.test.Test.Fake protobuf = org.sonar.core.test.Test.Fake.newBuilder()
-      .setSomeBytes(ByteString.copyFrom(new byte[]{2, 4}))
+    JsonTest protobuf = JsonTest.newBuilder()
+      .setBytesField(ByteString.copyFrom(new byte[]{2, 4}))
       .build();
 
     ProtobufJsonFormat.write(protobuf, JsonWriter.of(new StringWriter()));
   }
 
   @Test
-  public void protobuf_empty_strings_are_not_output() throws Exception {
-    org.sonar.core.test.Test.Fake protobuf = org.sonar.core.test.Test.Fake.newBuilder().build();
+  public void protobuf_absent_fields_are_not_output() {
+    JsonTest msg = JsonTest.newBuilder().build();
 
-    // field is not set but value is "", not null
-    assertThat(protobuf.hasAString()).isFalse();
-    assertThat(protobuf.getAString()).isEqualTo("");
+    // fields are absent
+    assertThat(msg.hasStringField()).isFalse();
+    assertThat(msg.hasIntField()).isFalse();
 
-    StringWriter json = new StringWriter();
-    JsonWriter jsonWriter = JsonWriter.of(json);
-    ProtobufJsonFormat.write(protobuf, jsonWriter);
-    assertThat(json.toString()).isEqualTo("{}");
+    // the repeated field "anArray" is always present. This is the standard behavior of protobuf. It
+    // does not make the difference between null and empty arrays.
+    assertThat(toJson(msg)).isEqualTo("{\"anArray\":[]}");
+  }
+
+  @Test
+  public void protobuf_present_and_empty_string_field_is_output() {
+    JsonTest msg = JsonTest.newBuilder().setStringField("").build();
+
+    // field is present
+    assertThat(msg.hasStringField()).isTrue();
+    assertThat(msg.getStringField()).isEqualTo("");
+
+    assertThat(toJson(msg)).contains("\"stringField\":\"\"");
+  }
+
+
+  @Test
+  public void protobuf_empty_array_marked_as_present_is_output() {
+    JsonArrayTest msg = JsonArrayTest.newBuilder()
+      .setANullableArrayPresentIfEmpty(true)
+      .build();
+
+    // repeated field "aNullableArray" is marked as present through the boolean field "aNullableArrayPresentIfEmpty"
+    assertThat(msg.hasANullableArrayPresentIfEmpty()).isTrue();
+    assertThat(msg.getANullableArrayPresentIfEmpty()).isTrue();
+
+    // JSON contains the repeated field, but not the boolean marker field
+    assertThat(toJson(msg)).isEqualTo("{\"aNullableArray\":[]}");
+  }
+
+  @Test
+  public void protobuf_empty_array_marked_as_absent_is_not_output() {
+    JsonArrayTest msg = JsonArrayTest.newBuilder()
+      .setANullableArrayPresentIfEmpty(false)
+      .build();
+
+    // repeated field "aNullableArray" is marked as absent through the boolean field "aNullableArrayPresentIfEmpty"
+    assertThat(msg.hasANullableArrayPresentIfEmpty()).isTrue();
+    assertThat(msg.getANullableArrayPresentIfEmpty()).isFalse();
+
+    // JSON does not contain the array nor the boolean marker
+    assertThat(toJson(msg)).isEqualTo("{}");
+  }
+
+  @Test
+  public void protobuf_non_empty_array_is_output_even_if_not_marked_as_present() {
+    JsonArrayTest msg = JsonArrayTest.newBuilder()
+      .addANullableArray("foo")
+      .build();
+
+    // repeated field "aNullableArray" is present, but the boolean marker "aNullableArrayPresentIfEmpty"
+    // is not set.
+    assertThat(msg.hasANullableArrayPresentIfEmpty()).isFalse();
+    assertThat(msg.getANullableArrayPresentIfEmpty()).isFalse();
+
+    // JSON contains the array but not the boolean marker
+    assertThat(toJson(msg)).isEqualTo("{\"aNullableArray\":[\"foo\"]}");
   }
 }
