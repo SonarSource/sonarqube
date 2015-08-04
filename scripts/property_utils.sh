9b6151d1283f6a34f268c662e5e965f6dee25487
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2015-08-04 14:11:51 +0200

    [SCRIPTS] add support to set non existing property in property_utils

--- scripts/property_utils.sh
+++ scripts/property_utils.sh
@@ -19,24 +19,36 @@ function cnt_lines() {
   cat $1 | wc -l
 }
 
-function set_property() {
+function write_prop() {
   PROPERTY=$1
   VALUE=$2
   FILE=$3
 
   # uncomment below to help debug calls to set_property
-  # echo "setting property $PROPERTY to value $VALUE in $FILE"
+  #echo "setting property $PROPERTY to value $VALUE in $FILE"
+  
+  echo "" >> $FILE
+  echo "${PROPERTY}=${VALUE}" >> $FILE 
+}
 
-  # delete line of specified property
-  LINE_COUNT=$(cnt_lines $FILE)
+function set_property() {
+  PROPERTY=$1
+  VALUE=$2
+  FILE=$3
+
   REGEXP="${1//\./\\\.}\s*="
-  sed -i $SED_DISABLE_BACKUP "/${REGEXP}/d" $FILE
+  if [ $(grep $REGEXP $FILE | wc -l) -eq 0 ]; then
+    write_prop $1 $2 $3
+  else
+    # delete line of specified property
+    LINE_COUNT=$(cnt_lines $FILE)
+    sed -i $SED_DISABLE_BACKUP "/${REGEXP}/d" $FILE
 
-  # add property if at least one line deleted
-  NEW_LINE_COUNT=$(cnt_lines $FILE)
-  if [ $LINE_COUNT -gt $NEW_LINE_COUNT ]; then
-    echo "" >> $FILE
-    echo "${PROPERTY}=${VALUE}" >> $FILE 
+    # add property if at least one line deleted
+    NEW_LINE_COUNT=$(cnt_lines $FILE)
+    if [ $LINE_COUNT -gt $NEW_LINE_COUNT ]; then
+      write_prop $1 $2 $3
+    fi
   fi
 }
 
