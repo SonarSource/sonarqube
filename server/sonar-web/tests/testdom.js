7cacd8b64068c1b14492cff955ad7c3edbd7abb7
Stas Vilchik <vilchiks@gmail.com>
2015-08-26 14:27:45 +0200

    Revert "install mocha tests for reactjs components"
    This reverts commit 0ed0fff9863770f14642bb949f2f97cc9f05bb5b.

--- server/sonar-web/tests/testdom.js
+++
@@ -1,7 +0,0 @@
-if (typeof document === 'undefined') {
-  var jsdom = require('jsdom').jsdom;
-  var markup = '<html><body></body></html>';
-  global.document = jsdom(markup);
-  global.window = document.parentWindow;
-  global.navigator = { userAgent: 'node.js' };
-}
