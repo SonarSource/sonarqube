c1c0835da24ef3abefda174c6c4b12ba804f5614
Stas Vilchik <vilchiks@gmail.com>
2015-09-11 09:58:36 +0200

    SONAR-6793 Make it possible to collapse WS response example

--- server/sonar-web/src/main/js/apps/api-documentation/action-view.js
+++ server/sonar-web/src/main/js/apps/api-documentation/action-view.js
@@ -1,22 +1,3 @@
-/*
- * SonarQube, open source software quality management tool.
- * Copyright (C) 2008-2014 SonarSource
- * mailto:contact AT sonarsource DOT com
- *
- * SonarQube is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
- *
- * SonarQube is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- */
 define([
   './templates'
 ], function () {
@@ -32,7 +13,8 @@ define([
     },
 
     events: {
-      'click .js-show-response-example': 'onShowResponseExampleClick'
+      'click .js-show-response-example': 'onShowResponseExampleClick',
+      'click .js-hide-response-example': 'onHideResponseExampleClick'
     },
 
     onRender: function () {
@@ -43,6 +25,11 @@ define([
     onShowResponseExampleClick: function (e) {
       e.preventDefault();
       this.fetchResponse();
+    },
+
+    onHideResponseExampleClick: function (e) {
+      e.preventDefault();
+      this.model.unset('responseExample');
     },
 
     fetchResponse: function () {
