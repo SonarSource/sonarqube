cec1c507a40486b46111a20bf7852a0c68a49dbb
Teryk Bellahsene <teryk.bellahsene@sonarsource.com>
2015-09-16 17:32:06 +0200

    Improve description one last time with PO validation

--- server/sonar-server/src/main/java/org/sonar/server/qualityprofile/ws/SearchAction.java
+++ server/sonar-server/src/main/java/org/sonar/server/qualityprofile/ws/SearchAction.java
@@ -73,7 +73,8 @@ public class SearchAction implements QProfileWsAction {
 
     action
       .createParam(PARAM_DEFAULTS)
-      .setDescription(format("Return the quality profiles marked as default. If provided, then the parameters '%s', '%s' and '%s' must not be set.",
+      .setDescription(format("Return the quality profile marked as default for each language. " +
+        "If provided, then the parameters '%s', '%s' and '%s' must not be set.",
         PARAM_LANGUAGE, PARAM_PROJECT_KEY, PARAM_PROFILE_NAME))
       .setDefaultValue(false)
       .setBooleanPossibleValues();
