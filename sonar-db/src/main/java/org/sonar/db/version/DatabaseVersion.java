6326bd027fb4789274933eb0c0c2f484ccad4b54
Simon Brandhof <simon.brandhof@sonarsource.com>
2015-09-19 00:08:53 +0200

    Fix merge with JS and DB migration changes

--- sonar-db/src/main/java/org/sonar/db/version/DatabaseVersion.java
+++ sonar-db/src/main/java/org/sonar/db/version/DatabaseVersion.java
@@ -29,7 +29,7 @@ import org.sonar.db.MyBatis;
 
 public class DatabaseVersion {
 
-  public static final int LAST_VERSION = 936;
+  public static final int LAST_VERSION = 937;
 
   /**
    * The minimum supported version which can be upgraded. Lower
