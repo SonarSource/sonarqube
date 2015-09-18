86b158854981eefac8955ece83e7b3d9761583ba
Julien Lancelot <julien.lancelot@sonarsource.com>
2015-09-18 15:55:38 +0200

    SONAR-6347 Increase max value of duplications_index id column

--- sonar-db/src/main/java/org/sonar/db/version/DatabaseVersion.java
+++ sonar-db/src/main/java/org/sonar/db/version/DatabaseVersion.java
@@ -29,7 +29,7 @@ import org.sonar.db.MyBatis;
 
 public class DatabaseVersion {
 
-  public static final int LAST_VERSION = 938;
+  public static final int LAST_VERSION = 939;
 
   /**
    * The minimum supported version which can be upgraded. Lower
