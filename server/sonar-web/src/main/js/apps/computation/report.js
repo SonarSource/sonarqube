1930d3927a57af7007c46605ae2a99357218b4ed
Simon Brandhof <simon.brandhof@sonarsource.com>
2015-09-19 00:08:53 +0200

    Fix merge of branch feature/ce-monitoring
    - JS changes (ES2015)
    - DB migration conflicts

--- server/sonar-web/src/main/js/apps/computation/report.js
+++ server/sonar-web/src/main/js/apps/computation/report.js
@@ -1,8 +1,7 @@
 import Backbone from 'backbone';
 
-<<<<<<< d3fd3a3175fac49d0c2874dc33e06497d4505de1
 export default Backbone.Model.extend({
-  idAttribute: 'key',
+  idAttribute: 'id',
 
   getDuration: function () {
     var duration = null;
@@ -15,35 +14,13 @@ export default Backbone.Model.extend({
         minutes: Math.floor(diff / (1000 * 60)) % 60,
         hours: Math.floor(diff / (1000 * 60 * 60)) % 24
       };
-=======
-  return Backbone.Model.extend({
-    getDuration: function () {
-      var duration = null;
-      if (this.has('startedAt')) {
-        var startedAtMoment = moment(this.get('startedAt')),
-            finishedAtMoment = moment(this.get('finishedAt') || new Date()),
-            diff = finishedAtMoment.diff(startedAtMoment);
-        duration = {
-          seconds: Math.floor(diff / 1000) % 60,
-          minutes: Math.floor(diff / (1000 * 60)) % 60,
-          hours: Math.floor(diff / (1000 * 60 * 60)) % 24
-        };
-      }
-      return duration;
-    },
-
-    isDanger: function () {
-      var dangerStatuses = ['CANCELLED', 'FAILED'];
-      return dangerStatuses.indexOf(this.get('status')) !== -1;
->>>>>>> SONAR-6834 use new API
     }
     return duration;
   },
 
   isDanger: function () {
-    var dangerStatuses = ['CANCELLED', 'FAILED'];
+    var dangerStatuses = ['CANCELED', 'FAILED'];
     return dangerStatuses.indexOf(this.get('status')) !== -1;
   }
 });
-
 
--- server/sonar-web/src/main/js/apps/computation/router.js
+++ server/sonar-web/src/main/js/apps/computation/router.js
@@ -17,24 +17,12 @@ export default Backbone.Router.extend({
     this.current();
   },
 
-<<<<<<< d3fd3a3175fac49d0c2874dc33e06497d4505de1
   current: function () {
     this.options.reports.fetch({ q: 'queue' });
   },
-=======
-    current: function () {
-      this.options.reports.fetch({ q: 'queue' });
-    },
-
-    past: function () {
-      this.options.reports.fetch({ q: 'activity' });
-    }
-  });
->>>>>>> SONAR-6834 use new API
 
   past: function () {
-    this.options.reports.fetch({ q: 'history' });
+    this.options.reports.fetch({ q: 'activity' });
   }
 });
-
 
