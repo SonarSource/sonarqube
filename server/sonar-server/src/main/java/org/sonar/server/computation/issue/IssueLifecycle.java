199acf48e9b9b0732e38d350fb22a344383be6ca
Julien Lancelot <julien.lancelot@sonarsource.com>
2015-10-09 12:23:09 +0200

    SONAR-6717 Issue attributes are kept

--- server/sonar-server/src/main/java/org/sonar/server/computation/issue/IssueLifecycle.java
+++ server/sonar-server/src/main/java/org/sonar/server/computation/issue/IssueLifecycle.java
@@ -19,13 +19,14 @@
  */
 package org.sonar.server.computation.issue;
 
+import com.google.common.annotations.VisibleForTesting;
 import javax.annotation.Nullable;
 import org.sonar.api.issue.Issue;
-import org.sonar.core.util.Uuids;
 import org.sonar.core.issue.DefaultIssue;
 import org.sonar.core.issue.IssueChangeContext;
 import org.sonar.core.issue.IssueUpdater;
 import org.sonar.core.issue.workflow.IssueWorkflow;
+import org.sonar.core.util.Uuids;
 import org.sonar.server.computation.analysis.AnalysisMetadataHolder;
 
 /**
@@ -44,10 +45,15 @@ public class IssueLifecycle {
   private final DebtCalculator debtCalculator;
 
   public IssueLifecycle(AnalysisMetadataHolder analysisMetadataHolder, IssueWorkflow workflow, IssueUpdater updater, DebtCalculator debtCalculator) {
+    this(IssueChangeContext.createScan(analysisMetadataHolder.getAnalysisDate()), workflow, updater, debtCalculator);
+  }
+
+  @VisibleForTesting
+  IssueLifecycle(IssueChangeContext changeContext, IssueWorkflow workflow, IssueUpdater updater, DebtCalculator debtCalculator) {
     this.workflow = workflow;
     this.updater = updater;
     this.debtCalculator = debtCalculator;
-    this.changeContext = IssueChangeContext.createScan(analysisMetadataHolder.getAnalysisDate());
+    this.changeContext = changeContext;
   }
 
   public void initNewOpenIssue(DefaultIssue issue) {
@@ -70,6 +76,7 @@ public class IssueLifecycle {
     raw.setAssignee(base.assignee());
     raw.setAuthorLogin(base.authorLogin());
     raw.setTags(base.tags());
+    raw.setAttributes(base.attributes());
     raw.setDebt(debtCalculator.calculate(raw));
     raw.setOnDisabledRule(base.isOnDisabledRule());
     if (base.manualSeverity()) {
@@ -78,8 +85,6 @@ public class IssueLifecycle {
     } else {
       updater.setPastSeverity(raw, base.severity(), changeContext);
     }
-
-    // TODO attributes
 
     // fields coming from raw
     updater.setPastLine(raw, base.getLine());
