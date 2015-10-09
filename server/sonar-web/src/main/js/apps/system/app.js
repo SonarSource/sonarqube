db0f7557f165b95b3e3ae61affe4279f0541a673
Stas Vilchik <vilchiks@gmail.com>
2015-10-09 10:48:05 +0200

    SONAR-6830 apply feedback

--- server/sonar-web/src/main/js/apps/system/app.js
+++ server/sonar-web/src/main/js/apps/system/app.js
@@ -3,39 +3,53 @@ import {setLogLevel} from '../../api/system';
 
 const LOG_LEVELS = ['INFO', 'DEBUG', 'TRACE'];
 
-let cell = document.querySelector('#sonarqube-logs-level td:last-child');
-if (cell) {
-  let currentValue = cell.textContent.trim();
+window.requestMessages().done(() => {
+  let cell = $('#sonarqube-logs-level').find('td:last-child');
+  if (cell.length) {
+    let currentValue = cell.text().trim();
+    cell.empty();
 
-  while (cell.hasChildNodes()) {
-    cell.removeChild(cell.lastChild);
+    let select = $('<select>');
+    cell.append(select);
+
+    LOG_LEVELS.forEach(logLevel => {
+      let option = $('<option>');
+      option.prop('value', logLevel);
+      option.text(logLevel);
+      option.prop('selected', logLevel === currentValue);
+      select.append(option);
+    });
+
+    let format = (state) => {
+      let className = state.id !== 'INFO' ? 'text-danger' : '';
+      return `<span class="${className}">${state.id}</span>`;
+    };
+
+    let warning = $('<div>')
+        .addClass('spacer-top text-danger')
+        .text(window.t('system.log_level.warning'));
+
+    function placeWarning () {
+      if (select.val() === 'INFO') {
+        warning.detach();
+      } else {
+        warning.insertAfter(select);
+      }
+    }
+
+    placeWarning();
+
+    $(select)
+        .select2({
+          width: '120px',
+          minimumResultsForSearch: 999,
+          formatResult: format,
+          formatSelection: format
+        })
+        .on('change', () => {
+          let newValue = select.val();
+          setLogLevel(newValue);
+          placeWarning();
+        });
   }
-
-  let select = document.createElement('select');
-  cell.appendChild(select);
-
-  LOG_LEVELS.forEach(logLevel => {
-    let option = document.createElement('option');
-    option.value = logLevel;
-    option.text = logLevel;
-    option.selected = logLevel === currentValue;
-    select.appendChild(option);
-  });
-
-  let format = (state) => {
-    let className = state.id !== 'INFO' ? 'text-danger' : '';
-    return `<span class="${className}">${state.id}</span>`;
-  };
-
-  $(select)
-      .select2({
-        width: '120px',
-        minimumResultsForSearch: 999,
-        formatResult: format,
-        formatSelection: format
-      })
-      .on('change', () => {
-        let newValue = select.value;
-        setLogLevel(newValue);
-      });
-}
+});
