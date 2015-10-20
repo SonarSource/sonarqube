145b71e8eca5b376c8238c79a2fada61bbf15744
Julien Lancelot <julien.lancelot@sonarsource.com>
2015-10-20 12:12:28 +0200

    Fix some quality flaws

--- server/sonar-server/src/test/java/org/sonar/server/computation/source/RangeOffsetConverterTest.java
+++ server/sonar-server/src/test/java/org/sonar/server/computation/source/RangeOffsetConverterTest.java
@@ -20,17 +20,22 @@
 
 package org.sonar.server.computation.source;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.sonar.batch.protocol.output.BatchReport;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Fail.failBecauseExceptionWasNotThrown;
+import static org.sonar.server.computation.source.RangeOffsetConverter.offsetToString;
 
-public class RangeOffsetHelperTest {
+public class RangeOffsetConverterTest {
+
+  @Rule
+  public ExpectedException thrown = ExpectedException.none();
 
   @Test
   public void append_range() {
-    assertThat(RangeOffsetHelper.offsetToString(BatchReport.TextRange.newBuilder()
+    assertThat(offsetToString(BatchReport.TextRange.newBuilder()
       .setStartLine(1).setEndLine(1)
       .setStartOffset(2).setEndOffset(3)
       .build(), 1, 5)).isEqualTo("2,3");
@@ -38,7 +43,7 @@ public class RangeOffsetHelperTest {
 
   @Test
   public void append_range_not_finishing_in_current_line() {
-    assertThat(RangeOffsetHelper.offsetToString(BatchReport.TextRange.newBuilder()
+    assertThat(offsetToString(BatchReport.TextRange.newBuilder()
       .setStartLine(1).setEndLine(3)
       .setStartOffset(2).setEndOffset(3)
       .build(), 1, 5)).isEqualTo("2,5");
@@ -46,7 +51,7 @@ public class RangeOffsetHelperTest {
 
   @Test
   public void append_range_that_began_in_previous_line_and_finish_in_current_line() {
-    assertThat(RangeOffsetHelper.offsetToString(BatchReport.TextRange.newBuilder()
+    assertThat(offsetToString(BatchReport.TextRange.newBuilder()
       .setStartLine(1).setEndLine(3)
       .setStartOffset(2).setEndOffset(3)
       .build(), 3, 5)).isEqualTo("0,3");
@@ -54,7 +59,7 @@ public class RangeOffsetHelperTest {
 
   @Test
   public void append_range_that_began_in_previous_line_and_not_finishing_in_current_line() {
-    assertThat(RangeOffsetHelper.offsetToString(BatchReport.TextRange.newBuilder()
+    assertThat(offsetToString(BatchReport.TextRange.newBuilder()
       .setStartLine(1).setEndLine(3)
       .setStartOffset(2).setEndOffset(3)
       .build(), 2, 5)).isEqualTo("0,5");
@@ -62,7 +67,7 @@ public class RangeOffsetHelperTest {
 
   @Test
   public void do_nothing_if_offset_is_empty() {
-    assertThat(RangeOffsetHelper.offsetToString(BatchReport.TextRange.newBuilder()
+    assertThat(offsetToString(BatchReport.TextRange.newBuilder()
       .setStartLine(1).setEndLine(1)
       .setStartOffset(0).setEndOffset(0)
       .build(), 1, 5)).isEmpty();
@@ -70,43 +75,37 @@ public class RangeOffsetHelperTest {
 
   @Test
   public void fail_when_end_offset_is_before_start_offset() {
-    try {
-      RangeOffsetHelper.offsetToString(BatchReport.TextRange.newBuilder()
-        .setStartLine(1).setEndLine(1)
-        .setStartOffset(4).setEndOffset(2)
-        .build(),
-        1, 5);
-      failBecauseExceptionWasNotThrown(IllegalArgumentException.class);
-    } catch (IllegalArgumentException e) {
-      assertThat(e).hasMessage("End offset 2 cannot be defined before start offset 4 on line 1");
-    }
+    thrown.expect(IllegalArgumentException.class);
+    thrown.expectMessage("End offset 2 cannot be defined before start offset 4 on line 1");
+
+    offsetToString(BatchReport.TextRange.newBuilder()
+      .setStartLine(1).setEndLine(1)
+      .setStartOffset(4).setEndOffset(2)
+      .build(),
+      1, 5);
   }
 
   @Test
   public void fail_when_end_offset_is_higher_than_line_length() {
-    try {
-      RangeOffsetHelper.offsetToString(BatchReport.TextRange.newBuilder()
-        .setStartLine(1).setEndLine(1)
-        .setStartOffset(4).setEndOffset(10)
-        .build(),
-        1, 5);
-      failBecauseExceptionWasNotThrown(IllegalArgumentException.class);
-    } catch (IllegalArgumentException e) {
-      assertThat(e).hasMessage("End offset 10 is defined outside the length (5) of the line 1");
-    }
+    thrown.expect(IllegalArgumentException.class);
+    thrown.expectMessage("End offset 10 is defined outside the length (5) of the line 1");
+
+    offsetToString(BatchReport.TextRange.newBuilder()
+      .setStartLine(1).setEndLine(1)
+      .setStartOffset(4).setEndOffset(10)
+      .build(),
+      1, 5);
   }
 
   @Test
   public void fail_when_start_offset_is_higher_than_line_length() {
-    try {
-      RangeOffsetHelper.offsetToString(BatchReport.TextRange.newBuilder()
-        .setStartLine(1).setEndLine(1)
-        .setStartOffset(10).setEndOffset(11)
-        .build(),
-        1, 5);
-      failBecauseExceptionWasNotThrown(IllegalArgumentException.class);
-    } catch (IllegalArgumentException e) {
-      assertThat(e).hasMessage("Start offset 10 is defined outside the length (5) of the line 1");
-    }
+    thrown.expect(IllegalArgumentException.class);
+    thrown.expectMessage("Start offset 10 is defined outside the length (5) of the line 1");
+
+    offsetToString(BatchReport.TextRange.newBuilder()
+      .setStartLine(1).setEndLine(1)
+      .setStartOffset(10).setEndOffset(11)
+      .build(),
+      1, 5);
   }
 }
