6ec3fcf19a77c31b9fe137eabc56334948d39ac8
Simon Brandhof <simon.brandhof@sonarsource.com>
2015-11-25 16:11:49 +0100

    SONAR-7073 revert fix of SNAPSHOTS.SCOPE
    Because of O.R.A.C.L.E.
    It allowed us to create a column with name "scope" but now it fails to alter it because reserved name (only in Oracle btw).

--- sonar-db/src/main/java/org/sonar/db/version/DatabaseVersion.java
+++ sonar-db/src/main/java/org/sonar/db/version/DatabaseVersion.java
@@ -29,7 +29,7 @@ import org.sonar.db.MyBatis;
 
 public class DatabaseVersion {
 
-  public static final int LAST_VERSION = 1002;
+  public static final int LAST_VERSION = 1001;
 
   /**
    * The minimum supported version which can be upgraded. Lower
