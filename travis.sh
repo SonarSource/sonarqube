7c10d53bf9e2c2b8260c37c7c50d9bb9326324f7
Simon Brandhof <simon.brandhof@sonarsource.com>
2015-11-30 09:22:05 +0100

    Analyze PR only on master merge

--- travis.sh
+++ travis.sh
@@ -16,7 +16,7 @@ function strongEcho {
 case "$TARGET" in
 
 CI)
-  if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ -n "$GITHUB_TOKEN" ]; then
+  if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "${TRAVIS_BRANCH}" == "master" ] && [ -n "$GITHUB_TOKEN" ]; then
     # For security reasons environment variables are not available on the pull requests
     # coming from outside repositories
     # http://docs.travis-ci.com/user/pull-requests/#Security-Restrictions-when-testing-Pull-Requests
