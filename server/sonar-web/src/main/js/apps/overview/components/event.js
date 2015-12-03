236a3e3e12da1d0418da922e89dbd2bfdd2d5ff5
Stas Vilchik <vilchiks@gmail.com>
2015-12-03 12:31:19 +0100

    SONAR-7063 apply feedback

--- server/sonar-web/src/main/js/apps/overview/components/event.js
+++ server/sonar-web/src/main/js/apps/overview/components/event.js
@@ -20,10 +20,11 @@ export const Event = React.createClass({
   render () {
     const { event } = this.props;
     return <li className="spacer-top">
-      <p data-toggle="tooltip" title={event.text}>
+      <p>
         <strong className="js-event-type">{window.t('event.category', event.type)}</strong>
         :&nbsp;
         <span className="js-event-name">{event.name}</span>
+        { event.text && <i className="spacer-left icon-help" data-toggle="tooltip" title={event.text}/> }
       </p>
       <p className="note little-spacer-top js-event-date">{moment(event.date).format('LL')}</p>
     </li>;
