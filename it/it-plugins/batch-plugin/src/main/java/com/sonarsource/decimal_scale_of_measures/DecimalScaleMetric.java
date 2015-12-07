b15b27496d360e31d3438332009f19d3c9515d5b
Simon Brandhof <simon.brandhof@sonarsource.com>
2015-12-07 16:02:26 +0100

    Fix compilation of IT

--- it/it-plugins/batch-plugin/src/main/java/com/sonarsource/decimal_scale_of_measures/DecimalScaleMetric.java
+++ it/it-plugins/batch-plugin/src/main/java/com/sonarsource/decimal_scale_of_measures/DecimalScaleMetric.java
@@ -1,6 +1,5 @@
 package com.sonarsource.decimal_scale_of_measures;
 
-import java.io.Serializable;
 import java.util.Collections;
 import java.util.List;
 import org.sonar.api.measures.Metric;
@@ -10,7 +9,7 @@ public class DecimalScaleMetric implements Metrics {
 
   public static final String KEY = "decimal_scale";
 
-  private static final Metric<> METRIC = new Metric.Builder(KEY, "Decimal Scale", Metric.ValueType.FLOAT)
+  private static final Metric METRIC = new Metric.Builder(KEY, "Decimal Scale", Metric.ValueType.FLOAT)
     .setDescription("Numeric metric with overridden decimal scale")
     .setDecimalScale(4)
     .create();
@@ -20,7 +19,7 @@ public class DecimalScaleMetric implements Metrics {
     return Collections.singletonList(definition());
   }
 
-  public static Metric<Serializable> definition() {
-    return  METRIC;
+  public static Metric definition() {
+    return METRIC;
   }
 }
