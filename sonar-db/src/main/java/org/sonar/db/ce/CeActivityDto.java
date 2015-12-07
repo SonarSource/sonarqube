neufeldj14/sonartest

SONAR-6900 add snapshot_id column in ce_activity db table

sonar-db/src/main/java/org/sonar/db/ce/CeActivityDto.java
@@ -35,6 +35,7 @@ public class CeActivityDto {
 
   private String uuid;
   private String componentUuid;
+  private Integer snapshotId;
   private Status status;
   private String taskType;
   private boolean isLast;
@@ -163,6 +164,16 @@ public class CeActivityDto {
   public void setExecutionTimeMs(@Nullable Long l) {
     checkArgument(l == null || l >= 0, "Execution time must be positive: %s", l);
     this.executionTimeMs = l;
+  }
+
+  @CheckForNull
+  public Integer getSnapshotId() {
+    return snapshotId;
+  }
+
+  public CeActivityDto setSnapshotId(@Nullable Integer snapshotId) {
+    this.snapshotId = snapshotId;
+    return this;
   }
 
   @Override

sonar-db/src/test/java/org/sonar/db/ce/CeActivityDaoTest.java
@@ -61,6 +61,7 @@ public class CeActivityDaoTest {
     assertThat(saved.get().getStartedAt()).isEqualTo(1_500_000_000_000L);
     assertThat(saved.get().getExecutedAt()).isEqualTo(1_500_000_000_500L);
     assertThat(saved.get().getExecutionTimeMs()).isEqualTo(500L);
+    assertThat(saved.get().getSnapshotId()).isEqualTo(123_456);
   }
 
   @Test
@@ -242,6 +243,7 @@ public class CeActivityDaoTest {
     dto.setStartedAt(1_500_000_000_000L);
     dto.setExecutedAt(1_500_000_000_500L);
     dto.setExecutionTimeMs(500L);
+    dto.setSnapshotId(123_456);
     underTest.insert(db.getSession(), dto);
   }
 

SonarSource/sonarqube

sonar-db/src/main/java/org/sonar/db/ce/CeActivityDto.java
@@ -35,6 +35,7 @@ public class CeActivityDto {
 
   private String uuid;
   private String componentUuid;
+  private Long snapshotId;
   private Status status;
   private String taskType;
   private boolean isLast;
@@ -163,6 +164,16 @@ public class CeActivityDto {
   public void setExecutionTimeMs(@Nullable Long l) {
     checkArgument(l == null || l >= 0, "Execution time must be positive: %s", l);
     this.executionTimeMs = l;
+  }
+
+  @CheckForNull
+  public Long getSnapshotId() {
+    return snapshotId;
+  }
+
+  public CeActivityDto setSnapshotId(@Nullable Long snapshotId) {
+    this.snapshotId = snapshotId;
+    return this;
   }
 
   @Override

sonar-db/src/test/java/org/sonar/db/ce/CeActivityDaoTest.java
@@ -61,6 +61,7 @@ public class CeActivityDaoTest {
     assertThat(saved.get().getStartedAt()).isEqualTo(1_500_000_000_000L);
     assertThat(saved.get().getExecutedAt()).isEqualTo(1_500_000_000_500L);
     assertThat(saved.get().getExecutionTimeMs()).isEqualTo(500L);
+    assertThat(saved.get().getSnapshotId()).isEqualTo(123_456);
     assertThat(saved.get().toString()).isNotEmpty();
   }
 
@@ -250,6 +251,7 @@ public class CeActivityDaoTest {
     dto.setStartedAt(1_500_000_000_000L);
     dto.setExecutedAt(1_500_000_000_500L);
     dto.setExecutionTimeMs(500L);
+    dto.setSnapshotId(123_456L);
     underTest.insert(db.getSession(), dto);
   }
 