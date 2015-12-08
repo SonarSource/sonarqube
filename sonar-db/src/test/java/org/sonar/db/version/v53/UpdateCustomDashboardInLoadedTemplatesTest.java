13047c4b9e3dcc980c9e34a9b80d443e879e3405
Julien Lancelot <julien.lancelot@sonarsource.com>
2015-12-08 09:47:37 +0100

    SONAR-7030 Add migration to update LOADED_TEMPLATES
    In order to not have 2 default dashboards 'Custom', the entry in LOADED_TEMPLATES is renamed from the old name 'Dashboard' to the new one 'Custom'

---
+++ sonar-db/src/test/java/org/sonar/db/version/v53/UpdateCustomDashboardInLoadedTemplatesTest.java
@@ -0,0 +1,64 @@
+/*
+ * SonarQube, open source software quality management tool.
+ * Copyright (C) 2008-2014 SonarSource
+ * mailto:contact AT sonarsource DOT com
+ *
+ * SonarQube is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * SonarQube is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.db.version.v53;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.sonar.api.utils.System2;
+import org.sonar.db.DbTester;
+import org.sonar.db.version.MigrationStep;
+
+public class UpdateCustomDashboardInLoadedTemplatesTest {
+
+  static final String TABLE = "loaded_templates";
+
+  @Rule
+  public DbTester db = DbTester.createForSchema(System2.INSTANCE, UpdateCustomDashboardInLoadedTemplatesTest.class, "schema.sql");
+
+  MigrationStep migration;
+
+  @Before
+  public void setUp() {
+    db.executeUpdateSql("truncate table " + TABLE);
+
+    migration = new UpdateCustomDashboardInLoadedTemplates(db.database());
+  }
+
+  @Test
+  public void migrate_empty_db() throws Exception {
+    migration.execute();
+  }
+
+  @Test
+  public void migrate() throws Exception {
+    db.prepareDbUnit(this.getClass(), "migrate.xml");
+    migration.execute();
+    db.assertDbUnit(this.getClass(), "migrate-result.xml", TABLE);
+  }
+
+  @Test
+  public void not_migrate_already_migrated_data() throws Exception {
+    db.prepareDbUnit(this.getClass(), "not_migrate_already_migrated_data.xml");
+    migration.execute();
+    db.assertDbUnit(this.getClass(), "not_migrate_already_migrated_data.xml", TABLE);
+  }
+
+}
