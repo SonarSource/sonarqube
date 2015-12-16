4bba0c42a269fefe019cd9a9c54f595ff2956d61
Julien HENRY <julien.henry@sonarsource.com>
2015-12-16 11:41:39 +0100

    Add unit test for Java CPD Engine

---
+++ sonar-batch/src/test/resources/org/sonar/batch/cpd/ManyStatements.java
@@ -0,0 +1,11 @@
+package org.foo;
+
+public class ManyStatements {
+  
+  void foo() {
+    int A1 = 0; int B = 0; int C = 0; int D = 0; int E = 0; int F = 0; int G = 0; int H = 0; int I = 0; int J = 0; int K = 0;
+    int A2 = 0; int B = 0; int C = 0; int D = 0; int E = 0; int F = 0; int G = 0; int H = 0; int I = 0; int J = 0; int K = 0; 
+    int A1 = 0; int B = 0; int C = 0; int D = 0; int E = 0; int F = 0; int G = 0; int H = 0; int I = 0; int J = 0; int K = 0; 
+  }
+
+}
