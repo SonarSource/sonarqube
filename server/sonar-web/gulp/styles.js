0f480aa5db8fc2fd5611b6fe1c517ef0040459ba
Stas Vilchik <vilchiks@gmail.com>
2015-12-28 15:18:16 +0100

    improve css build

--- server/sonar-web/gulp/styles.js
+++ server/sonar-web/gulp/styles.js
@@ -1,38 +1,31 @@
 var path = require('path');
 
 var gulp = require('gulp');
-var concat = require('gulp-concat');
 var gulpif = require('gulp-if');
 var less = require('gulp-less');
 var nano = require('gulp-cssnano');
 var autoprefixer = require('gulp-autoprefixer');
 
 
+var nanoOptions = {
+  discardComments: { removeAll: true }
+};
+
+var autoprefixerOptions = {
+  browsers: [
+    'last 3 Chrome versions',
+    'last 3 Firefox versions',
+    'Safari >= 8',
+    'Edge >= 12',
+    'IE 11'
+  ]
+};
+
+
 module.exports.styles = function (output, production) {
-  return gulp.src([
-        'src/main/less/jquery-ui.less',
-        'src/main/less/select2.less',
-        'src/main/less/select2-sonar.less',
-
-        'src/main/less/init.less',
-        'src/main/less/components.less',
-        'src/main/less/pages.less',
-
-        'src/main/less/style.less',
-
-        'src/main/less/*.less'
-      ])
+  return gulp.src(['src/main/less/sonar.less'])
       .pipe(less())
-      .pipe(autoprefixer({
-        browsers: [
-          'last 3 Chrome versions',
-          'last 3 Firefox versions',
-          'Safari >= 8',
-          'Edge >= 12',
-          'IE 11'
-        ]
-      }))
-      .pipe(gulpif(production, nano()))
-      .pipe(concat('sonar.css'))
+      .pipe(autoprefixer(autoprefixerOptions))
+      .pipe(gulpif(production, nano(nanoOptions)))
       .pipe(gulp.dest(path.join(output, 'css')));
 };
