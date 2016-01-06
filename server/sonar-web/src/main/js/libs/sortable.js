0ee46b37498ef58ebd1069eafe930ebac06be8a5
Stas Vilchik <vilchiks@gmail.com>
2016-01-06 17:10:57 +0100

    deprecate some code

--- server/sonar-web/src/main/js/libs/sortable.js
+++ server/sonar-web/src/main/js/libs/sortable.js
@@ -17,6 +17,13 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
+
+/*
+ * DEPRECATED
+ *
+ * Found only single usage on the Components page.
+ */
+
 (function($) {
 
   function stripe(rows) {
