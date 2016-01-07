93598d6f84ba12c14150f160604605c68b568272
Julien Lancelot <julien.lancelot@sonarsource.com>
2016-01-07 10:23:09 +0100

    SONAR-7176 Add index on CE_QUEUE.COMPONENT_UUID

--- sonar-db/src/main/java/org/sonar/db/version/DatabaseVersion.java
+++ sonar-db/src/main/java/org/sonar/db/version/DatabaseVersion.java
@@ -29,7 +29,7 @@ import org.sonar.db.MyBatis;
 
 public class DatabaseVersion {
 
-  public static final int LAST_VERSION = 1006;
+  public static final int LAST_VERSION = 1007;
 
   /**
    * The minimum supported version which can be upgraded. Lower
