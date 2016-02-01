5c03e3bf05097c98a88223339cf300b613b61c0d
Stas Vilchik <vilchiks@gmail.com>
2016-02-01 17:42:16 +0100

    SONAR-7011 fix UsersPageTest

--- it/it-tests/src/test/java/it/administration/UsersPageTest.java
+++ it/it-tests/src/test/java/it/administration/UsersPageTest.java
@@ -22,65 +22,28 @@ package it.administration;
 import com.sonar.orchestrator.Orchestrator;
 import com.sonar.orchestrator.selenium.Selenese;
 import it.Category1Suite;
-import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
-import org.sonarqube.ws.client.PostRequest;
-import org.sonarqube.ws.client.WsClient;
 import util.selenium.SeleneseTest;
-
-import static util.ItUtils.newAdminWsClient;
 
 public class UsersPageTest {
 
   @ClassRule
   public static Orchestrator orchestrator = Category1Suite.ORCHESTRATOR;
-  private static WsClient adminWsClient;
-
-  @BeforeClass
-  public static void setUp() {
-    adminWsClient = newAdminWsClient(orchestrator);
-  }
 
   @Test
   public void generate_and_revoke_user_token() throws Exception {
     Selenese selenese = Selenese.builder().setHtmlTestsInClasspath("generate_and_revoke_user_token",
-      "/administration/UsersUITest/generate_and_revoke_user_token.html"
+      "/administration/UsersPageTest/generate_and_revoke_user_token.html"
     ).build();
     new SeleneseTest(selenese).runOn(orchestrator);
   }
 
   @Test
   public void admin_should_change_its_own_password() throws Exception {
-    createUser("users-page-user", "User");
-    makeAdmin("users-page-user");
-
     Selenese selenese = Selenese.builder().setHtmlTestsInClasspath("admin_should_change_its_own_password",
-      "/administration/UsersUITest/admin_should_change_its_own_password.html"
+      "/administration/UsersPageTest/admin_should_change_its_own_password.html"
     ).build();
     new SeleneseTest(selenese).runOn(orchestrator);
-
-    deactivateUser("users-page-user");
-  }
-
-  private static void createUser(String login, String name) {
-    adminWsClient.wsConnector().call(
-      new PostRequest("api/users/create")
-        .setParam("login", login)
-        .setParam("name", name)
-        .setParam("password", "password"));
-  }
-
-  private static void makeAdmin(String login) {
-    adminWsClient.wsConnector().call(
-      new PostRequest("api/permissions/add_user")
-        .setParam("login", login)
-        .setParam("permission", "admin"));
-  }
-
-  private static void deactivateUser(String login) {
-    adminWsClient.wsConnector().call(
-      new PostRequest("api/users/deactivate")
-        .setParam("login", login));
   }
 }
