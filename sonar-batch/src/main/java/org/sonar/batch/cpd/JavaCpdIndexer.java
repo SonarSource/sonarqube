f73dbafaa20c7e4d14d6b4ac8aa72f01ed07e25b
Duarte Meneses <duarte.meneses@sonarsource.com>
2016-02-08 10:40:42 +0100

    Improve quality

--- sonar-batch/src/main/java/org/sonar/batch/cpd/JavaCpdIndexer.java
+++ sonar-batch/src/main/java/org/sonar/batch/cpd/JavaCpdIndexer.java
@@ -26,7 +26,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.List;
-import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.sonar.api.CoreProperties;
