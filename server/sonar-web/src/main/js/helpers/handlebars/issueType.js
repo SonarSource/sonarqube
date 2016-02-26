23827bba027ce552866c8df33bd7ae6603499fbe
Stas Vilchik <vilchiks@gmail.com>
2016-02-26 10:22:04 +0100

    SONAR-7333 display issue type in the issue box

---
+++ server/sonar-web/src/main/js/helpers/handlebars/issueType.js
@@ -0,0 +1,31 @@
+/*
+ * SonarQube
+ * Copyright (C) 2009-2016 SonarSource SA
+ * mailto:contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+import Handlebars from 'handlebars/runtime';
+
+import { translate } from '../../helpers/l10n';
+
+module.exports = function (issueType) {
+  const isCodeSmell = issueType === 'CODE_SMELL';
+  const className = 'badge ' + (isCodeSmell ? 'badge-warning' : 'badge-danger');
+
+  return new Handlebars.default.SafeString(
+      `<span class="${className}">${translate('issue.type', issueType)}</span>`
+  );
+};
