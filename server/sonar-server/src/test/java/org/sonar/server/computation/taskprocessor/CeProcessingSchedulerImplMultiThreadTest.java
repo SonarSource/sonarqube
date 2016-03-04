1b239a703582f3f95fcb220dbb9f13b25a00811e
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2016-03-04 14:55:46 +0100

    SONAR-7183 fix reliability CeProcessingSchedulerImplMultiThreadTest

--- server/sonar-server/src/test/java/org/sonar/server/computation/taskprocessor/CeProcessingSchedulerImplMultiThreadTest.java
+++ server/sonar-server/src/test/java/org/sonar/server/computation/taskprocessor/CeProcessingSchedulerImplMultiThreadTest.java
@@ -40,7 +40,7 @@ public class CeProcessingSchedulerImplMultiThreadTest {
 
   @Test
   public void when_workerCount_is_more_than_1_CeWorkerCallable_runs_on_as_many_different_threads() throws InterruptedException {
-    int workerCount = 4;
+    int workerCount = 2;
 
     ceConfiguration
         .setWorkerCount(workerCount)
@@ -55,8 +55,8 @@ public class CeProcessingSchedulerImplMultiThreadTest {
 
       underTest.startScheduling();
 
-      // scheduling starts only after 10ms, leave 100ms for the tasks to run
-      Thread.sleep(100);
+      // scheduling starts only after 10ms, leave 500ms for the tasks to run and make use of all available threads
+      Thread.sleep(500);
 
       assertThat(ceWorkerRunnable.getThreadNames()).hasSize(workerCount);
     }
@@ -72,7 +72,7 @@ public class CeProcessingSchedulerImplMultiThreadTest {
 
   private static class ThreadNameRecordingCeWorkerCallable implements CeWorkerCallable {
     private final Set<String> threadNames = new HashSet<>();
-    private final int maxCallCount = 10;
+    private final int maxCallCount = 100;
     private int callCount = 0;
 
     @Override
