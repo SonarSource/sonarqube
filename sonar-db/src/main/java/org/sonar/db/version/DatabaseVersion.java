8aab02476cfa6cf173c2468408c49ed88b0ac3fc
Simon Brandhof <simon.brandhof@sonarsource.com>
2016-03-08 15:02:39 +0100

    SONAR-7378 Drop unused index ISSUES_STATUS

--- sonar-db/src/main/java/org/sonar/db/version/DatabaseVersion.java
+++ sonar-db/src/main/java/org/sonar/db/version/DatabaseVersion.java
@@ -29,7 +29,7 @@ import org.sonar.db.MyBatis;
 
 public class DatabaseVersion {
 
-  public static final int LAST_VERSION = 1113;
+  public static final int LAST_VERSION = 1114;
 
   /**
    * The minimum supported version which can be upgraded. Lower
