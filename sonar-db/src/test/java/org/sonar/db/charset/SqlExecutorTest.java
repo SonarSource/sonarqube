b002476c05fbfde56185b8da27847b09a940e6d6
Julien Lancelot <julien.lancelot@sonarsource.com>
2016-04-26 10:50:18 +0200

    SONAR-7549 Fix SqlExecutorTest on MySQL and MsSQL
    connection.commit() should not be used on these 2 DB
    executeInsert should use parametrized query

--- sonar-db/src/test/java/org/sonar/db/charset/SqlExecutorTest.java
+++ sonar-db/src/test/java/org/sonar/db/charset/SqlExecutorTest.java
@@ -23,15 +23,12 @@ import com.google.common.collect.ImmutableMap;
 import java.sql.Connection;
 import java.util.List;
 import java.util.Map;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.sonar.api.utils.System2;
 import org.sonar.db.DbTester;
-import org.sonar.db.dialect.H2;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assume.assumeTrue;
 
 public class SqlExecutorTest {
 
@@ -44,20 +41,10 @@ public class SqlExecutorTest {
   @Rule
   public DbTester dbTester = DbTester.create(System2.INSTANCE);
 
-  @Before
-  public void disableIfNotH2() {
-    // TODO dbTester.selectFirst() returns keys with different case
-    // depending on target db (lower-case for MySQL but upper-case for H2).
-    // It has to be fixed in order to reactive this test for all dbs.
-    assumeTrue(dbTester.database().getDialect().getId().equals(H2.ID));
-  }
-
   @Test
   public void executeSelect_executes_PreparedStatement() throws Exception {
     dbTester.executeInsert(USERS_DB_TABLE, ImmutableMap.of(LOGIN_DB_COLUMN, "login1", NAME_DB_COLUMN, "name one"));
     dbTester.executeInsert(USERS_DB_TABLE, ImmutableMap.of(LOGIN_DB_COLUMN, "login2", NAME_DB_COLUMN, "name two"));
-
-    dbTester.commit();
 
     try (Connection connection = dbTester.openConnection()) {
       List<String[]> users = underTest.executeSelect(connection, "select " + LOGIN_DB_COLUMN + ", " + NAME_DB_COLUMN + " from users order by id", new SqlExecutor.StringsConverter(
@@ -73,13 +60,12 @@ public class SqlExecutorTest {
   @Test
   public void executeUpdate_executes_PreparedStatement() throws Exception {
     dbTester.executeInsert(USERS_DB_TABLE, ImmutableMap.of(LOGIN_DB_COLUMN, "the_login", NAME_DB_COLUMN, "the name"));
-    dbTester.commit();
 
     try (Connection connection = dbTester.openConnection()) {
       underTest.executeUpdate(connection, "update users set " + NAME_DB_COLUMN + "='new name' where " + LOGIN_DB_COLUMN + "='the_login'");
-      connection.commit();
     }
-    Map<String, Object> row = dbTester.selectFirst("select " + NAME_DB_COLUMN + " from users where " + LOGIN_DB_COLUMN + "='the_login'");
+    Map<String, Object> row = dbTester.selectFirst("select " + NAME_DB_COLUMN + " as NAME from users where " + LOGIN_DB_COLUMN + "='the_login'");
+    assertThat(row).isNotEmpty();
     assertThat(row.get("NAME")).isEqualTo("new name");
   }
 
