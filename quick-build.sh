a16db01d8fb1a5fa876374815baf0e68e0bb836d
Janos Gyerik <janos@kronos>
2016-05-11 07:30:24 +0200

    use here-document instead of many echos

--- quick-build.sh
+++ quick-build.sh
@@ -2,41 +2,43 @@
 
 export MAVEN_OPTS='-Xmx256m'
 
-echo ''
-echo ''
-echo '                 uuuuuuu'
-echo '             uu$$$$$$$$$$$uu'
-echo '          uu$$$$$$$$$$$$$$$$$uu'
-echo '         u$$$$$$$$$$$$$$$$$$$$$u'
-echo '        u$$$$$$$$$$$$$$$$$$$$$$$u'
-echo '       u$$$$$$$$$$$$$$$$$$$$$$$$$u'
-echo '       u$$$$$$$$$$$$$$$$$$$$$$$$$u'
-echo '       u$$$$$$"   "$$$"   "$$$$$$u'
-echo '       "$$$$"      u$u       $$$$"'
-echo '        $$$u       u$u       u$$$'
-echo '        $$$u      u$$$u      u$$$'
-echo '         "$$$$uu$$$   $$$uu$$$$"'
-echo '          "$$$$$$$"   "$$$$$$$"'
-echo '            u$$$$$$$u$$$$$$$u'
-echo '             u$"$"$"$"$"$"$u'
-echo '  uuu        $$u$ $ $ $ $u$$       uuu'
-echo ' u$$$$        $$$$$u$u$u$$$       u$$$$'
-echo '  $$$$$uu      "$$$$$$$$$"     uu$$$$$$'
-echo 'u$$$$$$$$$$$uu    """""    uuuu$$$$$$$$$$'
-echo '$$$$"""$$$$$$$$$$uuu   uu$$$$$$$$$"""$$$"'
-echo ' """      ""$$$$$$$$$$$uu ""$"""'
-echo '           uuuu ""$$$$$$$$$$uuu'
-echo '  u$$$uuu$$$$$$$$$uu ""$$$$$$$$$$$uuu$$$'
-echo '  $$$$$$$$$$""""           ""$$$$$$$$$$$"'
-echo '   "$$$$$"                      ""$$$$""'
-echo '     $$$"                         $$$$"'
-echo ''
-echo ''
-echo '           TESTS ARE DISABLED'
-echo '               ARE U CRAZY ?'
-echo ''
-echo ''
-echo ''
+cat << 'EOF'
+
+
+                 uuuuuuu
+             uu$$$$$$$$$$$uu
+          uu$$$$$$$$$$$$$$$$$uu
+         u$$$$$$$$$$$$$$$$$$$$$u
+        u$$$$$$$$$$$$$$$$$$$$$$$u
+       u$$$$$$$$$$$$$$$$$$$$$$$$$u
+       u$$$$$$$$$$$$$$$$$$$$$$$$$u
+       u$$$$$$"   "$$$"   "$$$$$$u
+       "$$$$"      u$u       $$$$"
+        $$$u       u$u       u$$$
+        $$$u      u$$$u      u$$$
+         "$$$$uu$$$   $$$uu$$$$"
+          "$$$$$$$"   "$$$$$$$"
+            u$$$$$$$u$$$$$$$u
+             u$"$"$"$"$"$"$u
+  uuu        $$u$ $ $ $ $u$$       uuu
+ u$$$$        $$$$$u$u$u$$$       u$$$$
+  $$$$$uu      "$$$$$$$$$"     uu$$$$$$
+u$$$$$$$$$$$uu    """""    uuuu$$$$$$$$$$
+$$$$"""$$$$$$$$$$uuu   uu$$$$$$$$$"""$$$"
+ """      ""$$$$$$$$$$$uu ""$"""
+           uuuu ""$$$$$$$$$$uuu
+  u$$$uuu$$$$$$$$$uu ""$$$$$$$$$$$uuu$$$
+  $$$$$$$$$$""""           ""$$$$$$$$$$$"
+   "$$$$$"                      ""$$$$""
+     $$$"                         $$$$"
+
+
+           TESTS ARE DISABLED
+               ARE U CRAZY ?
+
+
+
+EOF
 
 ./stop.sh
 mvn clean install -B -e -V -DskipTests=true -Dsource.skip=true -Denforcer.skip=true -Pdev $*
