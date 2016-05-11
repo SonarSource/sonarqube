64fa9230e6346dd4bacb1855705630e877fd9f17
Julien Lancelot <julien.lancelot@sonarsource.com>
2016-05-11 11:26:20 +0200

    SONAR-7150 Fix IT

--- it/it-tests/src/test/java/it/analysis/BatchTest.java
+++ it/it-tests/src/test/java/it/analysis/BatchTest.java
@@ -161,7 +161,7 @@ public class BatchTest {
     Resource project = sonar.find(new ResourceQuery("com.sonarsource.it.projects.batch:duplicate-source").setMetrics("files", "directories"));
     // 2 main files and 1 test file all with same deprecated key
     assertThat(project.getMeasureIntValue("files")).isEqualTo(2);
-    assertThat(project.getMeasureIntValue("directories")).isEqualTo(3);
+    assertThat(project.getMeasureIntValue("directories")).isEqualTo(2);
   }
 
   /**
