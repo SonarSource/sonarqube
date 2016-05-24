507d10430f0af60a922814ca9df267bb75efa898
Simon Brandhof <simon.brandhof@sonarsource.com>
2016-05-24 10:14:51 +0200

    Fix timeout of server startup perf test
    Faster thanks to the move to Java 8

--- tests/perf/src/test/java/org/sonarsource/sonarqube/perf/server/ServerTest.java
+++ tests/perf/src/test/java/org/sonarsource/sonarqube/perf/server/ServerTest.java
@@ -58,7 +58,7 @@ public class ServerTest extends PerfTestCase {
       // compare dates of first and last log
       long firstLogDate = ServerLogs.extractFirstDate(readLogLines(orchestrator)).getTime();
       long startedAtDate = extractStartedAtDate(orchestrator);
-      assertDurationAround(startedAtDate - firstLogDate, 45000);
+      assertDurationAround(startedAtDate - firstLogDate, 38_000);
 
       ServerLogs.clear(orchestrator);
       orchestrator.stop();
@@ -66,7 +66,7 @@ public class ServerTest extends PerfTestCase {
       List<String> lines = readLogLines(orchestrator);
       long firstStopLogDate = ServerLogs.extractFirstDate(lines).getTime();
       long stopDate = extractStopDate(lines);
-      assertDurationLessThan(stopDate - firstStopLogDate, 10000);
+      assertDurationLessThan(stopDate - firstStopLogDate, 10_000);
 
     } finally {
       orchestrator.stop();
