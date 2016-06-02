4ff3df89d499e71065e3645b5afa28f74b06fc44
Simon Brandhof <simon.brandhof@sonarsource.com>
2016-06-02 21:58:25 +0200

    Remove unused module microbenchmark-template

--- microbenchmark-template/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# The command-line arguments can be used to list the benchmarks to be executed.
-# By default all benchmarks are executed.
-# Example: run.sh org.sonar.microbenchmark.SerializationBenchmark
-
-mvn clean install
-java -jar target/microbenchmark.jar $*
