a9782184dff358de24000d1db4c8e1efb13fc525
Julien Lancelot <julien.lancelot@sonarsource.com>
2016-06-14 14:50:24 +0200

    DO NOT PUSH - Active QA

--- travis.sh
+++ travis.sh
@@ -29,7 +29,7 @@ CI)
         -Pdeploy-sonarsource \
         -B -e -V
 
-  elif [[ "${TRAVIS_BRANCH}" == "branch-"* ]] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  elif [[ "${TRAVIS_BRANCH}" == "feature/jl/jwt"* ]] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     strongEcho 'Build and deploy'
 
     # get current version from pom
