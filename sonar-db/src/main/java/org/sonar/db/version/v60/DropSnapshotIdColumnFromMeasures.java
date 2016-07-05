3edbee5c5ae12c3739cea81a7c51a52d984fd93a
Simon Brandhof <simon.brandhof@sonarsource.com>
2016-07-05 23:21:30 +0200

    SONAR-7780 Drop column PROJECT_MEASURES.SNAPSHOT_ID

--- sonar-db/src/main/java/org/sonar/db/version/v60/DropSnapshotIdColumnFromMeasures.java
+++ sonar-db/src/main/java/org/sonar/db/version/v60/DropSnapshotIdColumnFromMeasures.java
@@ -17,18 +17,24 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-package org.sonar.db.version;
+package org.sonar.db.version.v60;
 
-import org.junit.Test;
-import org.sonar.core.platform.ComponentContainer;
+import java.sql.SQLException;
+import org.sonar.db.Database;
+import org.sonar.db.version.DdlChange;
+import org.sonar.db.version.DropColumnsBuilder;
 
-import static org.assertj.core.api.Assertions.assertThat;
+public class DropSnapshotIdColumnFromMeasures extends DdlChange {
 
-public class MigrationStepModuleTest {
-  @Test
-  public void verify_count_of_added_MigrationStep_types() {
-    ComponentContainer container = new ComponentContainer();
-    new MigrationStepModule().configure(container);
-    assertThat(container.size()).isEqualTo(127);
+  private static final String TABLE = "project_measures";
+
+  public DropSnapshotIdColumnFromMeasures(Database db) {
+    super(db);
   }
+
+  @Override
+  public void execute(Context context) throws SQLException {
+    context.execute(new DropColumnsBuilder(getDatabase().getDialect(), TABLE, "snapshot_id").build());
+  }
+
 }
