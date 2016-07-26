4b8e398f903d3daa69b35c1e10c0c67a5852c2f0
Teryk Bellahsene <teryk.bellahsene@sonarsource.com>
2016-07-26 16:03:49 +0200

    SONAR-7917 Fix WidgetDao

--- sonar-db/src/test/java/org/sonar/db/dashboard/WidgetDaoTest.java
+++ sonar-db/src/test/java/org/sonar/db/dashboard/WidgetDaoTest.java
@@ -23,6 +23,7 @@ import java.util.Collection;
 import org.junit.Rule;
 import org.junit.Test;
 import org.sonar.api.utils.System2;
+import org.sonar.db.DbSession;
 import org.sonar.db.DbTester;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -30,20 +31,39 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class WidgetDaoTest {
 
   @Rule
-  public DbTester dbTester = DbTester.create(System2.INSTANCE);
+  public DbTester db = DbTester.create(System2.INSTANCE);
+  DbSession dbSession = db.getSession();
 
-  WidgetDao dao = dbTester.getDbClient().widgetDao();
+  WidgetDao underTest = db.getDbClient().widgetDao();
 
   @Test
   public void should_select_all() {
-    dbTester.prepareDbUnit(this.getClass(), "before.xml");
+    db.prepareDbUnit(this.getClass(), "before.xml");
 
-    Collection<WidgetDto> widgets = dao.findAll(dbTester.getSession());
+    Collection<WidgetDto> widgets = underTest.findAll(db.getSession());
     assertThat(widgets).hasSize(5);
     for (WidgetDto widget : widgets) {
       assertThat(widget.getId()).isNotNull();
       assertThat(widget.getName()).isNotNull();
       assertThat(widget.getDescription()).isNotNull();
     }
+  }
+
+  @Test
+  public void select_by_dashboard_key() {
+    db.prepareDbUnit(this.getClass(), "before.xml");
+
+    Collection<WidgetDto> result = underTest.findByDashboard(dbSession, 1L);
+
+    assertThat(result).hasSize(5);
+  }
+
+  @Test
+  public void select_by_key() {
+    db.prepareDbUnit(this.getClass(), "before.xml");
+
+    WidgetDto result = underTest.selectByKey(dbSession, 1L);
+
+    assertThat(result).isNotNull();
   }
 }
