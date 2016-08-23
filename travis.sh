9a67b9c31a7a1febceb01b0ab985e0433c554961
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2016-08-23 15:11:08 +0200

    DO NOT MERGE enable QA on all builds

--- travis.sh
+++ travis.sh
@@ -17,9 +17,6 @@ function strongEcho {
 case "$TARGET" in
 
 CI)
-  if [ "${TRAVIS_BRANCH}" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-    strongEcho 'Build and deploy'
-
     # Do not deploy a SNAPSHOT version but the release version related to this build
     set_maven_build_version $TRAVIS_BUILD_NUMBER
 
@@ -28,54 +25,6 @@ CI)
         -Pdeploy-sonarsource \
         -B -e -V
 
-  elif [[ "${TRAVIS_BRANCH}" == "branch-"* ]] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-    strongEcho 'Build and deploy'
-
-    # get current version from pom
-    CURRENT_VERSION=`maven_expression "project.version"`
-
-    if [[ $CURRENT_VERSION =~ "-SNAPSHOT" ]]; then
-      echo "======= Found SNAPSHOT version ======="
-      # Do not deploy a SNAPSHOT version but the release version related to this build
-      set_maven_build_version $TRAVIS_BUILD_NUMBER
-    else
-      echo "======= Found RELEASE version ======="
-    fi
-
-    # analysis is currently executed by SonarSource internal infrastructure
-    mvn deploy \
-        -Pdeploy-sonarsource \
-        -B -e -V
-
-
-
-  elif [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ -n "${GITHUB_TOKEN:-}" ]; then
-    strongEcho 'Build and analyze pull request, no deploy'
-
-    # No need for Maven phase "install" as the generated JAR file does not need to be installed
-    # in Maven local repository. Phase "verify" is enough.
-
-    export MAVEN_OPTS="-Xmx1G -Xms128m"
-    mvn org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar \
-        -Dclirr=true \
-        -Dsonar.analysis.mode=issues \
-        -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
-        -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
-        -Dsonar.github.oauth=$GITHUB_TOKEN \
-        -Dsonar.host.url=$SONAR_HOST_URL \
-        -Dsonar.login=$SONAR_TOKEN \
-        -B -e -V
-
-  else
-    strongEcho 'Build, no analysis, no deploy'
-
-    # No need for Maven phase "install" as the generated JAR file does not need to be installed
-    # in Maven local repository. Phase "verify" is enough.
-
-    mvn verify \
-        -Dmaven.test.redirectTestOutputToFile=false \
-        -B -e -V
-  fi
   ;;
 
 WEB)
