558f0931a7a98cbbef3933ca941af98128b59672
Simon Brandhof <simon.brandhof@sonarsource.com>
2016-09-08 17:09:14 +0200

    Fix Quality flaws

--- sonar-duplications/src/main/java/org/sonar/duplications/detector/original/OriginalCloneDetectionAlgorithm.java
+++ sonar-duplications/src/main/java/org/sonar/duplications/detector/original/OriginalCloneDetectionAlgorithm.java
@@ -156,7 +156,7 @@ public final class OriginalCloneDetectionAlgorithm {
           // fixed order) is equal to f(j) and only report in this case.
 
           Block first = currentBlocksGroup.first(originResourceId);
-          if (first.getIndexInFile() == j - 2) {
+          if (first != null && first.getIndexInFile() == j - 2) {
             // Godin: We report clones, which start in i-1 and end in j-2, so length is j-2-(i-1)+1=j-i
             reportClones(sameHashBlocksGroups[i], currentBlocksGroup, j - i);
           }
