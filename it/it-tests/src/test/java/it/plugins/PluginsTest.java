cdcce30730746b264b3644226382765bfb1d5766
Julien HENRY <julien.henry@sonarsource.com>
2016-09-29 16:30:05 +0200

    Exclude FxCop plugin from ITs since it is interefing with C# and VB.NET plugins

--- it/it-tests/src/test/java/it/plugins/PluginsTest.java
+++ it/it-tests/src/test/java/it/plugins/PluginsTest.java
@@ -74,6 +74,9 @@ public class PluginsTest {
     // temporary incompatibility with cobol 3.1 java.lang.NoClassDefFoundError: com/sonarsource/cobol/api/pp/EnterPreprocessorEvent
     "maifcobolplugin",
 
+    // Waiting for new release of C# and VB.NET plugins, since fxcop was previously part of those plugins => fail with duplicate props
+    "fxcop",
+
     // SONAR-7770 Realm plugins cannot be installed as no external configuration is used
     "crowd","ldap","pam"
   );
