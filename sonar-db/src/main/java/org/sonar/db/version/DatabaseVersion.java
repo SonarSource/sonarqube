cb688183c3589700902102167eaa57c399335318
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2016-10-19 11:02:08 +0200

    SONAR-8134 add organization uuid to unique index of table GROUP_ROLES

--- sonar-db/src/main/java/org/sonar/db/version/DatabaseVersion.java
+++ sonar-db/src/main/java/org/sonar/db/version/DatabaseVersion.java
@@ -30,7 +30,7 @@ import org.sonar.db.MyBatis;
 
 public class DatabaseVersion {
 
-  public static final int LAST_VERSION = 1_417;
+  public static final int LAST_VERSION = 1_418;
 
   /**
    * The minimum supported version which can be upgraded. Lower
