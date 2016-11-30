29c6d9a8b4cb05fd69de580f14e82fff6373b9f6
Julien Lancelot <julien.lancelot@sonarsource.com>
2016-11-30 15:35:41 +0100

    SONAR-8449 Replace WsTester by WsActionTester in GlobalNavigationActionTest

--- server/sonar-server/src/test/java/org/sonar/server/ui/ws/GlobalNavigationActionTest.java
+++ server/sonar-server/src/test/java/org/sonar/server/ui/ws/GlobalNavigationActionTest.java
@@ -26,111 +26,115 @@ import org.sonar.api.config.Settings;
 import org.sonar.api.resources.ResourceType;
 import org.sonar.api.resources.ResourceTypeTree;
 import org.sonar.api.resources.ResourceTypes;
-import org.sonar.api.utils.System2;
 import org.sonar.api.web.NavigationSection;
 import org.sonar.api.web.Page;
 import org.sonar.api.web.UserRole;
 import org.sonar.api.web.View;
 import org.sonar.core.permission.GlobalPermissions;
-import org.sonar.db.DbSession;
-import org.sonar.db.DbTester;
 import org.sonar.server.tester.UserSessionRule;
 import org.sonar.server.ui.Views;
-import org.sonar.server.ws.WsTester;
+import org.sonar.server.ws.WsActionTester;
+import org.sonar.test.JsonAssert;
 
 public class GlobalNavigationActionTest {
 
   @Rule
-  public DbTester dbTester = DbTester.create(System2.INSTANCE);
-  @Rule
   public UserSessionRule userSessionRule = UserSessionRule.standalone();
 
-  private DbSession session = dbTester.getSession();
+  private Settings settings = new MapSettings();
 
-  private WsTester wsTester;
+  private WsActionTester ws;
 
   @Test
   public void empty_call() throws Exception {
-    wsTester = new WsTester(new NavigationWs(new GlobalNavigationAction(new Views(userSessionRule), new MapSettings(), new ResourceTypes())));
+    init();
 
-    wsTester.newGetRequest("api/navigation", "global").execute().assertJson(getClass(), "empty.json");
+    executeAndVerify("empty.json");
   }
 
   @Test
   public void with_root_qualifiers() throws Exception {
-    ResourceTypes resourceTypes = new ResourceTypes(
-      new ResourceTypeTree[] {
-        ResourceTypeTree.builder()
-          .addType(ResourceType.builder("POL").build())
-          .addType(ResourceType.builder("LOP").build())
-          .addRelations("POL", "LOP")
-          .build(),
-        ResourceTypeTree.builder()
-          .addType(ResourceType.builder("PAL").build())
-          .addType(ResourceType.builder("LAP").build())
-          .addRelations("PAL", "LAP")
-          .build()
-      });
-    wsTester = new WsTester(new NavigationWs(new GlobalNavigationAction(new Views(userSessionRule), new MapSettings(), resourceTypes)));
+    init(new View[] {}, new ResourceTypeTree[] {
+      ResourceTypeTree.builder()
+        .addType(ResourceType.builder("POL").build())
+        .addType(ResourceType.builder("LOP").build())
+        .addRelations("POL", "LOP")
+        .build(),
+      ResourceTypeTree.builder()
+        .addType(ResourceType.builder("PAL").build())
+        .addType(ResourceType.builder("LAP").build())
+        .addRelations("PAL", "LAP")
+        .build()
+    });
 
-    wsTester.newGetRequest("api/navigation", "global").execute().assertJson(getClass(), "with_qualifiers.json");
+    executeAndVerify("with_qualifiers.json");
   }
 
   @Test
   public void only_logo() throws Exception {
-    wsTester = new WsTester(new NavigationWs(new GlobalNavigationAction(new Views(userSessionRule),
-      new MapSettings()
-        .setProperty("sonar.lf.logoUrl", "http://some-server.tld/logo.png")
-        .setProperty("sonar.lf.logoWidthPx", "123"),
-      new ResourceTypes())));
+    init();
+    settings.setProperty("sonar.lf.logoUrl", "http://some-server.tld/logo.png");
+    settings.setProperty("sonar.lf.logoWidthPx", "123");
 
-    wsTester.newGetRequest("api/navigation", "global").execute().assertJson(getClass(), "only_logo.json");
+    executeAndVerify("only_logo.json");
   }
 
   @Test
   public void nominal_call_for_anonymous() throws Exception {
-    nominalSetup();
+    init(createViews(), new ResourceTypeTree[] {});
+    addNominalSettings();
 
-    wsTester.newGetRequest("api/navigation", "global").execute().assertJson(getClass(), "anonymous.json");
+    executeAndVerify("anonymous.json");
   }
 
   @Test
   public void nominal_call_for_user() throws Exception {
-    nominalSetup();
+    init(createViews(), new ResourceTypeTree[] {});
+    addNominalSettings();
 
     userSessionRule.login("obiwan");
 
-    wsTester.newGetRequest("api/navigation", "global").execute().assertJson(getClass(), "user.json");
+    executeAndVerify("user.json");
   }
 
   @Test
   public void nominal_call_for_admin() throws Exception {
-    nominalSetup();
+    init(createViews(), new ResourceTypeTree[] {});
+    addNominalSettings();
 
     userSessionRule.login("obiwan").setGlobalPermissions(GlobalPermissions.SYSTEM_ADMIN);
 
-    wsTester.newGetRequest("api/navigation", "global").execute().assertJson(getClass(), "admin.json");
+    executeAndVerify("admin.json");
   }
 
   @Test
   public void nominal_call_for_user_without_configured_dashboards() throws Exception {
-    nominalSetup();
+    init(createViews(), new ResourceTypeTree[] {});
+    addNominalSettings();
 
     userSessionRule.login("anakin");
 
-    wsTester.newGetRequest("api/navigation", "global").execute().assertJson(getClass(), "anonymous.json");
+    executeAndVerify("anonymous.json");
   }
 
-  private void nominalSetup() {
-    session.commit();
-
-    Settings settings = new MapSettings()
-      .setProperty("sonar.lf.logoUrl", "http://some-server.tld/logo.png")
-      .setProperty("sonar.lf.logoWidthPx", "123");
-    wsTester = new WsTester(new NavigationWs(new GlobalNavigationAction(createViews(), settings, new ResourceTypes())));
+  private void init() {
+    init(new View[] {}, new ResourceTypeTree[] {});
   }
 
-  private Views createViews() {
+  private void init(View[] views, ResourceTypeTree[] resourceTypeTrees) {
+    ws = new WsActionTester(new GlobalNavigationAction(new Views(userSessionRule, views), settings, new ResourceTypes(resourceTypeTrees)));
+  }
+
+  private void executeAndVerify(String json) {
+    JsonAssert.assertJson(ws.newRequest().execute().getInput()).isSimilarTo(getClass().getResource(GlobalNavigationActionTest.class.getSimpleName() + "/" + json));
+  }
+
+  private void addNominalSettings() {
+    settings.setProperty("sonar.lf.logoUrl", "http://some-server.tld/logo.png");
+    settings.setProperty("sonar.lf.logoWidthPx", "123");
+  }
+
+  private View[] createViews() {
     Page page = new Page() {
       @Override
       public String getTitle() {
@@ -168,6 +172,6 @@ public class GlobalNavigationActionTest {
         return "admin_page";
       }
     }
-    return new Views(userSessionRule, new View[] {page, controller, new AdminPage()});
+    return new View[] {page, controller, new AdminPage()};
   }
 }
