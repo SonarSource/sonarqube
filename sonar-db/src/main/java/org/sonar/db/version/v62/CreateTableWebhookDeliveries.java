cb07ea331a5e4e7bc650d59dd2e7583e82658ad8
Simon Brandhof <simon.brandhof@sonarsource.com>
2016-12-06 20:56:55 +0100

    SONAR-5471 create webhook_deliveries indices in java

--- sonar-db/src/main/java/org/sonar/db/version/v62/CreateTableWebhookDeliveries.java
+++ sonar-db/src/main/java/org/sonar/db/version/v62/CreateTableWebhookDeliveries.java
@@ -21,8 +21,10 @@ package org.sonar.db.version.v62;
 
 import java.sql.SQLException;
 import org.sonar.db.Database;
+import org.sonar.db.version.CreateIndexBuilder;
 import org.sonar.db.version.CreateTableBuilder;
 import org.sonar.db.version.DdlChange;
+import org.sonar.db.version.VarcharColumnDef;
 
 import static org.sonar.db.version.BigIntegerColumnDef.newBigIntegerColumnDefBuilder;
 import static org.sonar.db.version.BooleanColumnDef.newBooleanColumnDefBuilder;
@@ -32,17 +34,23 @@ import static org.sonar.db.version.VarcharColumnDef.UUID_SIZE;
 import static org.sonar.db.version.VarcharColumnDef.newVarcharColumnDefBuilder;
 
 public class CreateTableWebhookDeliveries extends DdlChange {
+
+  private static final String TABLE_NAME = "webhook_deliveries";
+
   public CreateTableWebhookDeliveries(Database db) {
     super(db);
   }
 
   @Override
   public void execute(Context context) throws SQLException {
+    VarcharColumnDef componentUuidColumn = newVarcharColumnDefBuilder().setColumnName("component_uuid").setLimit(UUID_SIZE).setIsNullable(false).build();
+    VarcharColumnDef ceTaskUuidColumn = newVarcharColumnDefBuilder().setColumnName("ce_task_uuid").setLimit(UUID_SIZE).setIsNullable(false).build();
+
     context.execute(
-      new CreateTableBuilder(getDialect(), "webhook_deliveries")
+      new CreateTableBuilder(getDialect(), TABLE_NAME)
         .addPkColumn(newVarcharColumnDefBuilder().setColumnName("uuid").setLimit(UUID_SIZE).setIsNullable(false).build())
-        .addColumn(newVarcharColumnDefBuilder().setColumnName("component_uuid").setLimit(UUID_SIZE).setIsNullable(false).build())
-        .addColumn(newVarcharColumnDefBuilder().setColumnName("ce_task_uuid").setLimit(UUID_SIZE).setIsNullable(false).build())
+        .addColumn(componentUuidColumn)
+        .addColumn(ceTaskUuidColumn)
         .addColumn(newVarcharColumnDefBuilder().setColumnName("name").setLimit(100).setIsNullable(false).build())
         .addColumn(newVarcharColumnDefBuilder().setColumnName("url").setLimit(2000).setIsNullable(false).build())
         .addColumn(newBooleanColumnDefBuilder().setColumnName("success").setIsNullable(false).build())
@@ -51,6 +59,19 @@ public class CreateTableWebhookDeliveries extends DdlChange {
         .addColumn(newClobColumnDefBuilder().setColumnName("payload").setIsNullable(false).build())
         .addColumn(newClobColumnDefBuilder().setColumnName("error_stacktrace").setIsNullable(true).build())
         .addColumn(newBigIntegerColumnDefBuilder().setColumnName("created_at").setIsNullable(false).build())
+        .build());
+
+    context.execute(
+      new CreateIndexBuilder(getDialect())
+        .setTable(TABLE_NAME)
+        .setName("component_uuid")
+        .addColumn(componentUuidColumn)
+        .build());
+    context.execute(
+      new CreateIndexBuilder(getDialect())
+        .setTable(TABLE_NAME)
+        .setName("ce_task_uuid")
+        .addColumn(ceTaskUuidColumn)
         .build());
   }
 }
