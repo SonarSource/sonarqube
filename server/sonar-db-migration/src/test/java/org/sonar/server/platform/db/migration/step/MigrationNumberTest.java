11aebbb3baab99a0764ed33651e6dbdb9aadb61e
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2016-12-09 11:47:24 +0100

    fixup! DO NOT MERGE bump orchestrator version because 640 does not exist anymore

---
+++ server/sonar-db-migration/src/test/java/org/sonar/server/platform/db/migration/step/MigrationNumberTest.java
@@ -0,0 +1,9 @@
+package org.sonar.server.platform.db.migration.step;
+
+import static org.junit.Assert.*;
+
+public class MigrationNumberTest {
+  private MigrationNumber underTest = new MigrationNumber();
+
+
+}
