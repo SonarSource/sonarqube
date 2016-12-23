cc65eb695394d533c0124c53742a5bb36f948a70
Stas Vilchik <vilchiks@gmail.com>
2016-12-23 10:43:18 +0100

    expose moment.js for web extensions

--- server/sonar-web/src/main/js/app/utils/exposeLibraries.js
+++ server/sonar-web/src/main/js/app/utils/exposeLibraries.js
@@ -17,10 +17,12 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
+import moment from 'moment';
 import * as measures from '../../helpers/measures';
 import * as request from '../../helpers/request';
 
 const exposeLibraries = () => {
+  window.moment = moment;
   window.SonarMeasures = measures;
   window.SonarRequest = request;
 };
