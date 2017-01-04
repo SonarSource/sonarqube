f2fb3f2d6710906709cefb9b5ffd5880432abbea
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2017-01-04 17:29:55 +0100

    [script] avoid side effects in call_patches function

--- scripts/patches_utils.sh
+++ scripts/patches_utils.sh
@@ -10,9 +10,10 @@ PATCHES_HOME=scripts/patches
 # $1: name(s) of patches to call, separated by a colon
 # all other arguments are passed as is to the patches
 function call_patches() {
-  PATCHES=$1
-  ARGS=${@:2}
-
+  local PATCHES=$1
+  local ARGS=${@:2}
+  local savedIFS=$IFS
+  
   IFS=','
   for PATCH in $PATCHES; do
     #echo "calling $PATCHES_HOME/$PATCH.sh $ARGS"
@@ -20,6 +21,7 @@ function call_patches() {
     echo "******** $PATCH *******"
     $PATCHES_HOME/$PATCH.sh $ARGS
   done
+  IFS=$savedIFS
 
   echo ""
 }
