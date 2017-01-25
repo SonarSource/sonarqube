8053ca38f2889c72f1c0d66eaf0f4cb854ed52ab
Daniel Schwarz <daniel.schwarz@sonarsource.com>
2017-01-25 10:32:03 +0100

    SONAR-8694 use only one component search request for all qualifiers

--- server/sonar-server/src/main/java/org/sonar/server/component/index/ComponentIndex.java
+++ server/sonar-server/src/main/java/org/sonar/server/component/index/ComponentIndex.java
@@ -21,6 +21,8 @@ package org.sonar.server.component.index;
 
 import com.google.common.annotations.VisibleForTesting;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -30,6 +32,13 @@ import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.bucket.filters.FiltersAggregationBuilder;
+import org.elasticsearch.search.aggregations.bucket.filters.InternalFilters;
+import org.elasticsearch.search.aggregations.bucket.filters.InternalFilters.Bucket;
+import org.elasticsearch.search.aggregations.metrics.tophits.InternalTopHits;
+import org.elasticsearch.search.aggregations.metrics.tophits.TopHitsBuilder;
 import org.sonar.core.util.stream.Collectors;
 import org.sonar.server.es.BaseIndex;
 import org.sonar.server.es.EsClient;
@@ -47,6 +56,8 @@ import static org.sonar.server.component.index.ComponentIndexDefinition.TYPE_COM
 
 public class ComponentIndex extends BaseIndex {
 
+  private static final String FILTERS_AGGREGATION_NAME = "filters";
+  private static final String DOCS_AGGREGATION_NAME = "docs";
   private final UserSession userSession;
 
   public ComponentIndex(EsClient client, UserSession userSession) {
@@ -54,33 +65,50 @@ public class ComponentIndex extends BaseIndex {
     this.userSession = userSession;
   }
 
-  public List<String> search(ComponentIndexQuery query) {
+  public List<ComponentsPerQualifier> search(ComponentIndexQuery query) {
     return search(query, ComponentIndexSearchFeature.values());
   }
 
   @VisibleForTesting
-  List<String> search(ComponentIndexQuery query, ComponentIndexSearchFeature... features) {
+  List<ComponentsPerQualifier> search(ComponentIndexQuery query, ComponentIndexSearchFeature... features) {
+    Collection<String> qualifiers = query.getQualifiers();
+    if (qualifiers.isEmpty()) {
+      return Collections.emptyList();
+    }
+
     SearchRequestBuilder request = getClient()
       .prepareSearch(INDEX_COMPONENTS)
       .setTypes(TYPE_COMPONENT)
-      .setFetchSource(false);
+      .setQuery(createQuery(query, features))
+      .addAggregation(createAggregation(query))
 
-    query.getLimit().ifPresent(request::setSize);
+      // the search hits are part of the aggregations
+      .setSize(0);
 
-    request.setQuery(createQuery(query, features));
+    SearchResponse response = request.get();
 
-    SearchResponse searchResponse = request.get();
+    return aggregationsToQualifiers(response);
+  }
 
-    return Arrays.stream(searchResponse.getHits().hits())
-      .map(SearchHit::getId)
-      .collect(Collectors.toList());
+  private static FiltersAggregationBuilder createAggregation(ComponentIndexQuery query) {
+    FiltersAggregationBuilder filtersAggregation = AggregationBuilders.filters(FILTERS_AGGREGATION_NAME)
+      .subAggregation(createSubAggregation(query));
+
+    query.getQualifiers().stream()
+      .forEach(q -> filtersAggregation.filter(q, termQuery(FIELD_QUALIFIER, q)));
+
+    return filtersAggregation;
+  }
+
+  private static TopHitsBuilder createSubAggregation(ComponentIndexQuery query) {
+    TopHitsBuilder sub = AggregationBuilders.topHits(DOCS_AGGREGATION_NAME);
+    query.getLimit().ifPresent(sub::setSize);
+    return sub.setFetchSource(false);
   }
 
   private QueryBuilder createQuery(ComponentIndexQuery query, ComponentIndexSearchFeature... features) {
     BoolQueryBuilder esQuery = boolQuery();
     esQuery.filter(createAuthorizationFilter());
-
-    query.getQualifier().ifPresent(q -> esQuery.filter(termQuery(FIELD_QUALIFIER, q)));
 
     BoolQueryBuilder featureQuery = boolQuery();
 
@@ -105,5 +133,25 @@ public class ComponentIndex extends BaseIndex {
 
     return QueryBuilders.hasParentQuery(TYPE_AUTHORIZATION,
       QueryBuilders.boolQuery().must(matchAllQuery()).filter(groupsAndUser));
+  }
+
+  private static List<ComponentsPerQualifier> aggregationsToQualifiers(SearchResponse response) {
+    InternalFilters filtersAgg = response.getAggregations().get(FILTERS_AGGREGATION_NAME);
+    List<Bucket> buckets = filtersAgg.getBuckets();
+    return buckets.stream()
+      .map(ComponentIndex::bucketToQualifier)
+      .collect(Collectors.toList(buckets.size()));
+  }
+
+  private static ComponentsPerQualifier bucketToQualifier(Bucket bucket) {
+    InternalTopHits docs = bucket.getAggregations().get(DOCS_AGGREGATION_NAME);
+
+    SearchHits hitList = docs.getHits();
+    SearchHit[] hits = hitList.getHits();
+
+    List<String> componentUuids = Arrays.stream(hits).map(SearchHit::getId)
+      .collect(Collectors.toList(hits.length));
+
+    return new ComponentsPerQualifier(bucket.getKey(), componentUuids, hitList.totalHits());
   }
 }
