8606d7630b131439668c3c32251cee5bec30d8dc
Daniel Schwarz <daniel.schwarz@sonarsource.com>
2017-02-02 17:40:05 +0100

    SONAR-8736 backdate issues that are caused by quality profile changes

--- server/sonar-server/src/main/java/org/sonar/server/computation/task/projectanalysis/container/ProjectAnalysisTaskContainerPopulator.java
+++ server/sonar-server/src/main/java/org/sonar/server/computation/task/projectanalysis/container/ProjectAnalysisTaskContainerPopulator.java
@@ -56,6 +56,7 @@ import org.sonar.server.computation.task.projectanalysis.issue.IntegrateIssuesVi
 import org.sonar.server.computation.task.projectanalysis.issue.IssueAssigner;
 import org.sonar.server.computation.task.projectanalysis.issue.IssueCache;
 import org.sonar.server.computation.task.projectanalysis.issue.IssueCounter;
+import org.sonar.server.computation.task.projectanalysis.issue.IssueCreationDateCalculator;
 import org.sonar.server.computation.task.projectanalysis.issue.IssueLifecycle;
 import org.sonar.server.computation.task.projectanalysis.issue.IssueVisitors;
 import org.sonar.server.computation.task.projectanalysis.issue.LoadComponentUuidsHavingOpenIssuesVisitor;
@@ -136,7 +137,7 @@ public final class ProjectAnalysisTaskContainerPopulator implements ContainerPop
    * List of all objects to be injected in the picocontainer dedicated to computation stack.
    * Does not contain the steps declared in {@link ReportComputationSteps#orderedStepClasses()}.
    */
-  private static List componentClasses() {
+  private static List<Object> componentClasses() {
     return Arrays.asList(
       PostProjectAnalysisTasksExecutor.class,
       ComputationStepExecutor.class,
@@ -205,6 +206,7 @@ public final class ProjectAnalysisTaskContainerPopulator implements ContainerPop
       NewEffortCalculator.class,
       NewEffortAggregator.class,
       IssueAssigner.class,
+      IssueCreationDateCalculator.class,
       IssueCounter.class,
       MovedIssueVisitor.class,
 
