4b8a9d1f2f07a7f5f0a0bcbc1294fe8c8c5e7634
Stas Vilchik <vilchiks@gmail.com>
2017-02-09 16:09:15 +0100

    SONAR-8427 "Creation Date" facet might include more issues than expected

--- server/sonar-web/src/main/js/apps/issues/facets/creation-date-facet.js
+++ server/sonar-web/src/main/js/apps/issues/facets/creation-date-facet.js
@@ -64,7 +64,7 @@ export default BaseFacet.extend({
       let date = moment();
       values = [];
       times(10, () => {
-        values.push({ count: 0, val: date.toDate().toString() });
+        values.push({ count: 0, val: date.toDate() });
         date = date.subtract(1, 'days');
       });
       values.reverse();
@@ -72,7 +72,9 @@ export default BaseFacet.extend({
     values = values.map(v => {
       const format = that.options.app.state.getFacetMode() === 'count' ? 'SHORT_INT' : 'SHORT_WORK_DUR';
       const text = formatMeasure(v.count, format);
-      return { ...v, text };
+      const val = query.createdAfter != null && moment(v.val).isBefore(query.createdAfter) ?
+          query.createdAfter : v.val;
+      return { ...v, val, text };
     });
     return this.$('.js-barchart').barchart(values);
   },
