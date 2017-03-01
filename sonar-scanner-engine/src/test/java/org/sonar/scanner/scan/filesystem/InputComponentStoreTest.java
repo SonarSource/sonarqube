b3c5c13513518dd030b6736cea1a48518ed6634a
Janos Gyerik <janos.gyerik@sonarsource.com>
2017-03-01 10:49:39 +0100

    Fix InputComponentStoreTest

--- sonar-scanner-engine/src/test/java/org/sonar/scanner/scan/filesystem/InputComponentStoreTest.java
+++ sonar-scanner-engine/src/test/java/org/sonar/scanner/scan/filesystem/InputComponentStoreTest.java
@@ -21,6 +21,7 @@ package org.sonar.scanner.scan.filesystem;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.io.File;
 import java.nio.charset.StandardCharsets;
 import java.util.LinkedList;
 import java.util.List;
@@ -28,11 +29,13 @@ import java.util.List;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
+import org.sonar.api.batch.bootstrap.ProjectDefinition;
 import org.sonar.api.batch.fs.InputFile;
 import org.sonar.api.batch.fs.InputFile.Status;
 import org.sonar.api.batch.fs.InputFile.Type;
 import org.sonar.api.batch.fs.InputPath;
 import org.sonar.api.batch.fs.internal.DefaultInputFile;
+import org.sonar.api.batch.fs.internal.DefaultInputModule;
 import org.sonar.api.batch.fs.internal.TestInputFileBuilder;
 
 public class InputComponentStoreTest {
@@ -42,12 +45,25 @@ public class InputComponentStoreTest {
   @Test
   public void should_add_input_file() throws Exception {
     InputComponentStore cache = new InputComponentStore();
-    DefaultInputFile fooFile = new TestInputFileBuilder("struts", "src/main/java/Foo.java")
-      .setModuleBaseDir(temp.newFolder().toPath())
+
+    String mod1key = "struts";
+    File mod1baseDir = temp.newFolder();
+    ProjectDefinition mod1def = ProjectDefinition.create().setKey(mod1key);
+    mod1def.setBaseDir(mod1baseDir);
+    cache.put(new DefaultInputModule(mod1def, TestInputFileBuilder.nextBatchId()));
+
+    String mod2key = "struts-core";
+    File mod2baseDir = temp.newFolder();
+    ProjectDefinition mod2def = ProjectDefinition.create().setKey(mod2key);
+    mod2def.setBaseDir(mod2baseDir);
+    cache.put(new DefaultInputModule(mod2def, TestInputFileBuilder.nextBatchId()));
+
+    DefaultInputFile fooFile = new TestInputFileBuilder(mod1key, "src/main/java/Foo.java")
+      .setModuleBaseDir(mod1baseDir.toPath())
       .setPublish(true)
       .build();
     cache.put(fooFile);
-    cache.put(new TestInputFileBuilder("struts-core", "src/main/java/Bar.java")
+    cache.put(new TestInputFileBuilder(mod2key, "src/main/java/Bar.java")
       .setLanguage("bla")
       .setPublish(false)
       .setType(Type.MAIN)
@@ -57,12 +73,12 @@ public class InputComponentStoreTest {
       .setModuleBaseDir(temp.newFolder().toPath())
       .build());
 
-    DefaultInputFile loadedFile = (DefaultInputFile) cache.getFile("struts-core", "src/main/java/Bar.java");
+    DefaultInputFile loadedFile = (DefaultInputFile) cache.getFile(mod2key, "src/main/java/Bar.java");
     assertThat(loadedFile.relativePath()).isEqualTo("src/main/java/Bar.java");
     assertThat(loadedFile.charset()).isEqualTo(StandardCharsets.UTF_8);
 
-    assertThat(cache.filesByModule("struts")).hasSize(1);
-    assertThat(cache.filesByModule("struts-core")).hasSize(1);
+    assertThat(cache.filesByModule(mod1key)).hasSize(1);
+    assertThat(cache.filesByModule(mod2key)).hasSize(1);
     assertThat(cache.allFiles()).hasSize(2);
     for (InputPath inputPath : cache.allFiles()) {
       assertThat(inputPath.relativePath()).startsWith("src/main/java/");
@@ -75,9 +91,9 @@ public class InputComponentStoreTest {
     cache.remove(fooFile);
     assertThat(cache.allFiles()).hasSize(1);
 
-    cache.removeModule("struts");
-    assertThat(cache.filesByModule("struts")).hasSize(0);
-    assertThat(cache.filesByModule("struts-core")).hasSize(1);
+    cache.removeModule(mod1key);
+    assertThat(cache.filesByModule(mod1key)).hasSize(0);
+    assertThat(cache.filesByModule(mod2key)).hasSize(1);
     assertThat(cache.allFiles()).hasSize(1);
   }
 
