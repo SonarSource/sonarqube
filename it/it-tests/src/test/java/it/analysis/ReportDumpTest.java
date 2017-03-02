0a547ba79c4affb9b6ff0b669ee4a5e87ef16479
Julien HENRY <julien.henry@sonarsource.com>
2017-03-02 10:46:08 +0100

    SONAR-8826 Fix QA

--- it/it-tests/src/test/java/it/analysis/ReportDumpTest.java
+++ it/it-tests/src/test/java/it/analysis/ReportDumpTest.java
@@ -54,9 +54,10 @@ public class ReportDumpTest {
     // verify properties
     Properties props = new Properties();
     props.load(new StringReader(FileUtils.readFileToString(metadata, StandardCharsets.UTF_8)));
-    assertThat(props).hasSize(5);
+    assertThat(props).hasSize(6);
     assertThat(props.getProperty("projectKey")).isEqualTo("dump_metadata_of_uploaded_report");
     assertThat(props.getProperty("ceTaskId")).isNotEmpty();
+    // FIXME assertThat(props.getProperty("serverVersion")).isEqualTo(orchestrator.getServer().version().toString());
     verifyUrl(props.getProperty("serverUrl"));
     verifyUrl(props.getProperty("dashboardUrl"));
     verifyUrl(props.getProperty("ceTaskUrl"));
