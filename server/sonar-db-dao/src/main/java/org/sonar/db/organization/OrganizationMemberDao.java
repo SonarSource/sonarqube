0cfd2c2d53484285b5e6f92bbe4b9debf3d04e35
Teryk Bellahsene <teryk.bellahsene@sonarsource.com>
2017-03-09 12:21:14 +0100

    SONAR-8892 Add an organization member in database

--- server/sonar-db-dao/src/main/java/org/sonar/db/organization/OrganizationMemberDao.java
+++ server/sonar-db-dao/src/main/java/org/sonar/db/organization/OrganizationMemberDao.java
@@ -17,18 +17,18 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-package org.sonar.db;
 
-import org.junit.Test;
-import org.sonar.core.platform.ComponentContainer;
+package org.sonar.db.organization;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import org.sonar.db.Dao;
+import org.sonar.db.DbSession;
 
-public class DaoModuleTest {
-  @Test
-  public void verify_count_of_added_components() {
-    ComponentContainer container = new ComponentContainer();
-    new DaoModule().configure(container);
-    assertThat(container.size()).isEqualTo(2 + 46);
+public class OrganizationMemberDao implements Dao {
+  public void insert(DbSession dbSession, OrganizationMemberDto organizationMemberDto) {
+    mapper(dbSession).insert(organizationMemberDto);
+  }
+
+  private OrganizationMemberMapper mapper(DbSession dbSession) {
+    return dbSession.getMapper(OrganizationMemberMapper.class);
   }
 }
