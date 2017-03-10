94f348c28389a02a9ff0c9674fa48210987aa302
Simon Brandhof <simon.brandhof@sonarsource.com>
2017-03-10 15:01:21 +0100

    SONAR-8816 drop web startup barrier from Compute Engine

--- server/sonar-ce/src/test/java/org/sonar/ce/container/ComputeEngineContainerImplTest.java
+++ server/sonar-ce/src/test/java/org/sonar/ce/container/ComputeEngineContainerImplTest.java
@@ -107,7 +107,7 @@ public class ComputeEngineContainerImplTest {
     );
     assertThat(picoContainer.getParent().getParent().getParent().getComponentAdapters()).hasSize(
       COMPONENTS_IN_LEVEL_1_AT_CONSTRUCTION
-        + 25 // level 1
+        + 23 // level 1
         + 47 // content of DaoModule
         + 3 // content of EsSearchModule
         + 57 // content of CorePropertyDefinitions
