a9b3e0d9594c02f9fb4f7dae71dbf84f1dba6bc1
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2017-03-10 17:47:12 +0100

    SONAR-8931 add index on LOADED_TEMPLATES.TEMPLATE_TYPE

--- server/sonar-db-migration/src/main/java/org/sonar/server/platform/db/migration/version/v64/DbVersion64.java
+++ server/sonar-db-migration/src/main/java/org/sonar/server/platform/db/migration/version/v64/DbVersion64.java
@@ -37,6 +37,7 @@ public class DbVersion64 implements DbVersion {
       .add(1607, "Drop unique index on RULES_PROFILES.ORGANIZATION_UUID and KEE", DropUniqueIndexOnQualityProfileOrganizationUuidAndKey.class)
       .add(1608, "Make RULES_PROFILES.KEE unique", MakeQualityProfileKeyUnique.class)
       .add(1609, "Clean LOADED_TEMPLATES rows without type", CleanLoadedTemplateOrphans.class)
-      .add(1610, "Extend size of column LOADED_TEMPLATES.TEMPLATE_TYPE", ExtendLoadedTemplateTypeColumn.class);
+      .add(1610, "Extend size of column LOADED_TEMPLATES.TEMPLATE_TYPE", ExtendLoadedTemplateTypeColumn.class)
+      .add(1611, "Add index LOADED_TEMPLATES_TYPE", AddIndexLoadedTemplatesType.class);
   }
 }
