b9efe69de5c09aed0283a3bbed59a318c3722907
Simon Brandhof <simon.brandhof@sonarsource.com>
2017-03-13 16:47:19 +0100

    SONAR-8857 support organizations in api/qualityprofiles/backup

--- server/sonar-server/src/test/java/org/sonar/server/qualityprofile/ws/BackupActionTest.java
+++ server/sonar-server/src/test/java/org/sonar/server/qualityprofile/ws/BackupActionTest.java
@@ -19,64 +19,148 @@
  */
 package org.sonar.server.qualityprofile.ws;
 
-import java.io.PrintWriter;
-import java.io.Writer;
+import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.runners.MockitoJUnitRunner;
-import org.mockito.stubbing.Answer;
-import org.sonar.db.DbClient;
-import org.sonar.db.DbSession;
+import org.junit.rules.ExpectedException;
+import org.sonar.api.resources.Languages;
+import org.sonar.api.server.ws.WebService;
+import org.sonar.db.DbTester;
+import org.sonar.db.organization.OrganizationDto;
+import org.sonar.db.qualityprofile.QualityProfileDto;
 import org.sonar.db.qualityprofile.QualityProfileTesting;
+import org.sonar.server.exceptions.NotFoundException;
 import org.sonar.server.language.LanguageTesting;
+import org.sonar.server.organization.DefaultOrganizationProvider;
+import org.sonar.server.organization.TestDefaultOrganizationProvider;
 import org.sonar.server.qualityprofile.QProfileBackuper;
-import org.sonar.server.qualityprofile.QProfileFactory;
-import org.sonar.server.qualityprofile.QProfileRef;
-import org.sonar.server.ws.WsTester;
-import org.sonar.server.ws.WsTester.Result;
+import org.sonar.server.tester.UserSessionRule;
+import org.sonar.server.ws.TestResponse;
+import org.sonar.server.ws.WsActionTester;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
-@RunWith(MockitoJUnitRunner.class)
 public class BackupActionTest {
 
-  private static final String SOME_PROFILE_KEY = "polop-palap-xoo-12345";
+  private static final String A_LANGUAGE = "xoo";
 
-  private QProfileBackuper backuper = mock(QProfileBackuper.class);
-  private QProfileFactory profileFactory = mock(QProfileFactory.class);
-  private WsTester tester = new WsTester(new QProfilesWs(
-    mock(RuleActivationActions.class),
-    mock(BulkRuleActivationActions.class),
-    new BackupAction(mock(DbClient.class), backuper, profileFactory, LanguageTesting.newLanguages("xoo"))));
+  @Rule
+  public ExpectedException expectedException = ExpectedException.none();
+  @Rule
+  public DbTester db = DbTester.create();
+  @Rule
+  public UserSessionRule userSession = UserSessionRule.standalone();
+
+  private QProfileBackuper backuper = new QProfileBackuper(null, db.getDbClient());
+  private DefaultOrganizationProvider defaultOrganizationProvider = TestDefaultOrganizationProvider.from(db);
+  private QProfileWsSupport wsSupport = new QProfileWsSupport(db.getDbClient(), userSession, defaultOrganizationProvider);
+  private Languages languages = LanguageTesting.newLanguages(A_LANGUAGE);
+  private WsActionTester tester = new WsActionTester(new BackupAction(db.getDbClient(), backuper, wsSupport, languages));
 
   @Test
-  public void backup_profile() throws Exception {
-    when(profileFactory.find(any(DbSession.class), eq(QProfileRef.fromKey(SOME_PROFILE_KEY)))).thenReturn(QualityProfileTesting.newQualityProfileDto().setKey(SOME_PROFILE_KEY));
+  public void test_definition() {
+    WebService.Action definition = tester.getDef();
 
-    final String response = "<polop><palap/></polop>";
-    doAnswer(new Answer<Void>() {
-      @Override
-      public Void answer(InvocationOnMock invocation) throws Throwable {
-        Writer w = (Writer) invocation.getArguments()[1];
-        new PrintWriter(w).print(response);
-        w.close();
-        return null;
-      }
-    }).when(backuper).backup(eq(SOME_PROFILE_KEY), any(Writer.class));
+    assertThat(definition.key()).isEqualTo("backup");
+    assertThat(definition.responseExampleAsString()).isNotEmpty();
+    assertThat(definition.isInternal()).isFalse();
+    assertThat(definition.isPost()).isFalse();
 
-    Result result = tester.newGetRequest("api/qualityprofiles", "backup").setParam(QProfileRef.PARAM_PROFILE_KEY, SOME_PROFILE_KEY).execute();
-    assertThat(result.outputAsString()).isEqualTo(response);
-    result.assertHeader("Content-Disposition", "attachment; filename=" + SOME_PROFILE_KEY + ".xml");
+    // parameters
+    assertThat(definition.params()).hasSize(4);
+    assertThat(definition.param("language")).isNotNull();
+    assertThat(definition.param("profileKey")).isNotNull();
+    assertThat(definition.param("profileName")).isNotNull();
+    WebService.Param orgParam = definition.param("organization");
+    assertThat(orgParam).isNotNull();
+    assertThat(orgParam.since()).isEqualTo("6.4");
   }
 
-  @Test(expected = IllegalArgumentException.class)
-  public void fail_on_missing_key() throws Exception {
-    tester.newGetRequest("api/qualityprofiles", "backup").execute();
+  @Test
+  public void returns_backup_of_profile_with_specified_key() throws Exception {
+    QualityProfileDto profile = db.qualityProfiles().insertQualityProfile(QualityProfileTesting.newQualityProfileDto());
+
+    TestResponse response = tester.newRequest().setParam("profileKey", profile.getKey()).execute();
+    assertThat(response.getMediaType()).isEqualTo("application/xml");
+    assertThat(response.getInput()).isXmlEqualTo(xmlForProfileWithoutRules(profile));
+    assertThat(response.getHeader("Content-Disposition")).isEqualTo("attachment; filename=" + profile.getKey() + ".xml");
+  }
+
+  @Test
+  public void returns_backup_of_profile_with_specified_name_on_default_organization() throws Exception {
+    QualityProfileDto profile = newProfile(db.getDefaultOrganization());
+    db.qualityProfiles().insertQualityProfile(profile);
+
+    TestResponse response = tester.newRequest()
+      .setParam("language", profile.getLanguage())
+      .setParam("profileName", profile.getName())
+      .execute();
+    assertThat(response.getInput()).isXmlEqualTo(xmlForProfileWithoutRules(profile));
+  }
+
+  @Test
+  public void returns_backup_of_profile_with_specified_name_and_organization() throws Exception {
+    OrganizationDto org = db.organizations().insert();
+    QualityProfileDto profile = newProfile(org);
+    db.qualityProfiles().insertQualityProfile(profile);
+
+    TestResponse response = tester.newRequest()
+      .setParam("organization", org.getKey())
+      .setParam("language", profile.getLanguage())
+      .setParam("profileName", profile.getName())
+      .execute();
+    assertThat(response.getInput()).isXmlEqualTo(xmlForProfileWithoutRules(profile));
+  }
+
+  @Test
+  public void throws_NotFoundException_if_profile_with_specified_key_does_not_exist() throws Exception {
+    expectedException.expect(NotFoundException.class);
+    expectedException.expectMessage("Quality Profile with key 'missing' does not exist");
+
+    tester.newRequest().setParam("profileKey", "missing").execute();
+  }
+
+  @Test
+  public void throws_NotFoundException_if_specified_organization_does_not_exist() throws Exception {
+    expectedException.expect(NotFoundException.class);
+    expectedException.expectMessage("No organization with key 'the-missing-org'");
+
+    tester.newRequest()
+      .setParam("organization", "the-missing-org")
+      .setParam("language", A_LANGUAGE)
+      .setParam("profileName", "the-name")
+      .execute();
+  }
+
+  @Test
+  public void throws_NotFoundException_if_profile_name_exists_but_in_another_organization() throws Exception {
+    OrganizationDto org1 = db.organizations().insert();
+    QualityProfileDto profileInOrg1 = newProfile(org1);
+    db.qualityProfiles().insertQualityProfile(profileInOrg1);
+    OrganizationDto org2 = db.organizations().insert();
+    QualityProfileDto profileInOrg2 = newProfile(org2).setLanguage(profileInOrg1.getLanguage());
+
+    expectedException.expect(NotFoundException.class);
+    expectedException.expectMessage("Quality Profile for language 'xoo' and name '" + profileInOrg1.getName() + "' does not exist in organization '" + org2.getKey() +"'");
+
+    tester.newRequest()
+      .setParam("organization", org2.getKey())
+      .setParam("language", profileInOrg1.getLanguage())
+      .setParam("profileName", profileInOrg1.getName())
+      .execute();
+  }
+
+  private static String xmlForProfileWithoutRules(QualityProfileDto profile) {
+    return "<?xml version='1.0' encoding='UTF-8'?>" +
+      "<profile>" +
+      "  <name>" + profile.getName() + "</name>" +
+      "  <language>" + profile.getLanguage() + "</language>" +
+      "  <rules/>" +
+      "</profile>";
+  }
+
+  private static QualityProfileDto newProfile(OrganizationDto org) {
+    return QualityProfileTesting.newQualityProfileDto()
+      .setLanguage("xoo")
+      .setOrganizationUuid(org.getUuid());
   }
 }
