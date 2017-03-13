3ca49631ce270c477e01e79681d529c681ac0ffc
Daniel Schwarz <daniel.schwarz@sonarsource.com>
2017-03-13 17:08:02 +0100

    SONAR-8857 better error messages for quality profile reference params

--- server/sonar-server/src/test/java/org/sonar/server/qualityprofile/ws/QProfileReferenceTest.java
+++ server/sonar-server/src/test/java/org/sonar/server/qualityprofile/ws/QProfileReferenceTest.java
@@ -138,7 +138,7 @@ public class QProfileReferenceTest {
     req.setParam("profileName", "the name");
 
     expectedException.expect(IllegalArgumentException.class);
-    expectedException.expectMessage("Both profile language and name must be set");
+    expectedException.expectMessage("If no quality profile key is specified, language and name must be set");
 
     QProfileReference.from(req);
   }
