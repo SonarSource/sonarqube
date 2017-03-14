675ccdbc10a681bda0038a78e35f32d2eedbaf0e
Julien Lancelot <julien.lancelot@sonarsource.com>
2017-03-14 11:20:51 +0100

    SONAR-8899 Move assign code from IssueService to AssignAction

--- server/sonar-server/src/main/java/org/sonar/server/issue/ws/AssignAction.java
+++ server/sonar-server/src/main/java/org/sonar/server/issue/ws/AssignAction.java
@@ -19,16 +19,29 @@
  */
 package org.sonar.server.issue.ws;
 
+import com.google.common.base.Strings;
 import com.google.common.io.Resources;
+import java.util.Date;
+import javax.annotation.CheckForNull;
+import javax.annotation.Nullable;
 import org.apache.commons.lang.BooleanUtils;
 import org.sonar.api.server.ws.Request;
 import org.sonar.api.server.ws.Response;
 import org.sonar.api.server.ws.WebService;
+import org.sonar.api.utils.System2;
+import org.sonar.core.issue.DefaultIssue;
+import org.sonar.core.issue.IssueChangeContext;
 import org.sonar.core.util.Uuids;
-import org.sonar.server.issue.IssueService;
+import org.sonar.db.DbClient;
+import org.sonar.db.DbSession;
+import org.sonar.db.user.UserDto;
+import org.sonar.server.issue.IssueFieldsSetter;
+import org.sonar.server.issue.IssueFinder;
+import org.sonar.server.issue.IssueUpdater;
 import org.sonar.server.user.UserSession;
 
 import static com.google.common.base.Strings.emptyToNull;
+import static org.sonar.server.ws.WsUtils.checkFound;
 import static org.sonarqube.ws.client.issue.IssuesWsParameters.ACTION_ASSIGN;
 import static org.sonarqube.ws.client.issue.IssuesWsParameters.PARAM_ASSIGNEE;
 import static org.sonarqube.ws.client.issue.IssuesWsParameters.PARAM_ISSUE;
@@ -36,14 +49,24 @@ import static org.sonarqube.ws.client.issue.IssuesWsParameters.PARAM_ISSUE;
 public class AssignAction implements IssuesWsAction {
 
   private static final String DEPRECATED_PARAM_ME = "me";
+  private static final String ASSIGN_TO_ME_VALUE = "_me";
 
+  private final System2 system2;
   private final UserSession userSession;
-  private final IssueService issueService;
+  private final DbClient dbClient;
+  private final IssueFinder issueFinder;
+  private final IssueFieldsSetter issueFieldsSetter;
+  private final IssueUpdater issueUpdater;
   private final OperationResponseWriter responseWriter;
 
-  public AssignAction(UserSession userSession, IssueService issueService, OperationResponseWriter responseWriter) {
+  public AssignAction(System2 system2, UserSession userSession, DbClient dbClient, IssueFinder issueFinder, IssueFieldsSetter issueFieldsSetter, IssueUpdater issueUpdater,
+    OperationResponseWriter responseWriter) {
+    this.system2 = system2;
     this.userSession = userSession;
-    this.issueService = issueService;
+    this.dbClient = dbClient;
+    this.issueFinder = issueFinder;
+    this.issueFieldsSetter = issueFieldsSetter;
+    this.issueUpdater = issueUpdater;
     this.responseWriter = responseWriter;
   }
 
@@ -61,8 +84,7 @@ public class AssignAction implements IssuesWsAction {
       .setRequired(true)
       .setExampleValue(Uuids.UUID_EXAMPLE_01);
     action.createParam(PARAM_ASSIGNEE)
-      // TODO document absent value for unassign, and "_me" for assigning to me
-      .setDescription("Login of the assignee")
+      .setDescription("Login of the assignee. When not set, it will unassign the issue. Use '%s' to assign to current user", ASSIGN_TO_ME_VALUE)
       .setExampleValue("admin");
     action.createParam(DEPRECATED_PARAM_ME)
       .setDescription("(deprecated) Assign the issue to the logged-in user. Replaced by the parameter assignee=_me")
@@ -72,16 +94,39 @@ public class AssignAction implements IssuesWsAction {
 
   @Override
   public void handle(Request request, Response response) throws Exception {
-    String assignee = emptyToNull(request.param(PARAM_ASSIGNEE));
-    if ("_me".equals(assignee) || BooleanUtils.isTrue(request.paramAsBoolean(DEPRECATED_PARAM_ME))) {
-      // Permission is currently checked by IssueService. We still
-      // check that user is authenticated in order to get his login.
-      userSession.checkLoggedIn();
-      assignee = userSession.getLogin();
-    }
+    userSession.checkLoggedIn();
+    String assignee = getAssignee(request);
     String key = request.mandatoryParam(PARAM_ISSUE);
-    issueService.assign(key, assignee);
-
+    assign(key, assignee);
     responseWriter.write(key, request, response);
+  }
+
+  private void assign(String issueKey, @Nullable String assignee) {
+    try (DbSession dbSession = dbClient.openSession(false)) {
+      DefaultIssue issue = issueFinder.getByKey(dbSession, issueKey).toDefaultIssue();
+      UserDto user = getUser(dbSession, assignee);
+      IssueChangeContext context = IssueChangeContext.createUser(new Date(system2.now()), userSession.getLogin());
+      if (issueFieldsSetter.assign(issue, user, context)) {
+        issueUpdater.saveIssue(dbSession, issue, context, null);
+      }
+    }
+  }
+
+  @CheckForNull
+  private String getAssignee(Request request) {
+    String assignee = emptyToNull(request.param(PARAM_ASSIGNEE));
+    if (ASSIGN_TO_ME_VALUE.equals(assignee) || BooleanUtils.isTrue(request.paramAsBoolean(DEPRECATED_PARAM_ME))) {
+      return userSession.getLogin();
+    }
+    return assignee;
+  }
+
+  @CheckForNull
+  private UserDto getUser(DbSession dbSession, @Nullable String assignee) {
+    if (Strings.isNullOrEmpty(assignee)) {
+      return null;
+    }
+    UserDto user = dbClient.userDao().selectByLogin(dbSession, assignee);
+    return checkFound(user != null && user.isActive() ? user : null, "Unknown user: %s", assignee);
   }
 }
