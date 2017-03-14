06d11cc9c7989250c7601415824ce101e6d5bf65
Eric Hartmann <hartmann.eric@gmail.com>
2017-03-14 16:53:55 +0100

    SONAR-8935 Simplify tests and make faster tests

--- server/sonar-process-monitor/src/test/java/org/sonar/application/TestAppState.java
+++ server/sonar-process-monitor/src/test/java/org/sonar/application/TestAppState.java
@@ -25,7 +25,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
 import javax.annotation.Nonnull;
-import org.assertj.core.util.VisibleForTesting;
 import org.sonar.process.ProcessId;
 
 public class TestAppState implements AppState {
