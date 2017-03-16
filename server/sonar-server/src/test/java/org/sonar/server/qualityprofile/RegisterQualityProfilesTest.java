d6cf059ad53b0138e480e29297aa9ae8b1d522fb
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2017-03-16 10:55:37 +0100

    SONAR-8888 load DefinedQProfiles even when no startup leader

--- server/sonar-server/src/test/java/org/sonar/server/qualityprofile/RegisterQualityProfilesTest.java
+++ server/sonar-server/src/test/java/org/sonar/server/qualityprofile/RegisterQualityProfilesTest.java
@@ -72,8 +72,17 @@ public class RegisterQualityProfilesTest {
     mockedActiveRuleIndexer);
 
   @Test
+  public void start_fails_if_DefinedQProfileRepository_has_not_been_initialized() {
+    expectedException.expect(IllegalStateException.class);
+    expectedException.expectMessage("initialize must be called first");
+    
+    underTest.start();
+  }
+
+  @Test
   public void no_action_in_DB_nothing_to_index_when_there_is_no_DefinedQProfile() {
     RegisterQualityProfiles underTest = new RegisterQualityProfiles(definedQProfileRepositoryRule, mockedDbClient, null, null, mockedActiveRuleIndexer);
+    definedQProfileRepositoryRule.initialize();
 
     underTest.start();
 
@@ -91,6 +100,7 @@ public class RegisterQualityProfilesTest {
     String[] formattedDates = {formatDateTime(new Date(dates[0])), formatDateTime(new Date(dates[1]))};
     DefinedQProfile definedQProfile = definedQProfileRepositoryRule.add(FOO_LANGUAGE, "foo1");
     mockForQPInserts(uuids, dates);
+    definedQProfileRepositoryRule.initialize();
 
     underTest.start();
 
@@ -123,10 +133,10 @@ public class RegisterQualityProfilesTest {
     definedQProfileRepositoryRule.add(BAR_LANGUAGE, "bar1", true);
     definedQProfileRepositoryRule.add(BAR_LANGUAGE, "bar2", false);
     mockForQPInserts(new String[]{"uuid1", "uuid2", "uuid3", "uuid4"}, new Long[]{ 1L, 2L, 3L, 4L});
+    definedQProfileRepositoryRule.initialize();
 
     underTest.start();
 
-    assertThat(definedQProfileRepositoryRule.isInitialized()).isTrue();
     assertThat(getPersistedQP(dbTester.getDefaultOrganization(), FOO_LANGUAGE, "foo1").isDefault()).isFalse();
     assertThat(getPersistedQP(dbTester.getDefaultOrganization(), FOO_LANGUAGE, "foo2").isDefault()).isTrue();
     assertThat(getPersistedQP(dbTester.getDefaultOrganization(), BAR_LANGUAGE, "bar1").isDefault()).isTrue();
@@ -142,10 +152,10 @@ public class RegisterQualityProfilesTest {
     dbClient.loadedTemplateDao().insert(new LoadedTemplateDto(org1.getUuid(), definedQProfile.getLoadedTemplateType()), dbTester.getSession());
     dbTester.commit();
     mockForSingleQPInsert();
+    definedQProfileRepositoryRule.initialize();
 
     underTest.start();
 
-    assertThat(definedQProfileRepositoryRule.isInitialized()).isTrue();
     assertThat(dbClient.loadedTemplateDao().countByTypeAndKey(definedQProfile.getLoadedTemplateType(), org2.getUuid(), dbTester.getSession()))
       .isEqualTo(1);
     assertThat(dbTester.countRowsOfTable(dbTester.getSession(), TABLE_LOADED_TEMPLATES)).isEqualTo(3);
@@ -163,10 +173,10 @@ public class RegisterQualityProfilesTest {
     DefinedQProfile definedQProfile1 = definedQProfileRepositoryRule.add(FOO_LANGUAGE, name, true);
     DefinedQProfile definedQProfile2 = definedQProfileRepositoryRule.add(BAR_LANGUAGE, name, true);
     mockForQPInserts(new String[]{uuid1, uuid2}, new Long[]{date1, date2});
+    definedQProfileRepositoryRule.initialize();
 
     underTest.start();
 
-    assertThat(definedQProfileRepositoryRule.isInitialized()).isTrue();
     OrganizationDto organization = dbTester.getDefaultOrganization();
     QualityProfileDto dto = getPersistedQP(organization, FOO_LANGUAGE, name);
     String uuidQP1 = dto.getKee();
