408295ca5c76d8c497daff2d57cfff3515759442
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2017-03-17 17:16:26 +0100

    SONAR-6547 remove now useless ClearRulesOverloadedDebt

--- server/sonar-server/src/main/java/org/sonar/server/platform/platformlevel/PlatformLevelStartup.java
+++ server/sonar-server/src/main/java/org/sonar/server/platform/platformlevel/PlatformLevelStartup.java
@@ -30,7 +30,6 @@ import org.sonar.server.qualityprofile.CachingRuleActivatorContextFactory;
 import org.sonar.server.qualityprofile.DefinedQProfileLoader;
 import org.sonar.server.qualityprofile.RegisterQualityProfiles;
 import org.sonar.server.rule.RegisterRules;
-import org.sonar.server.startup.ClearRulesOverloadedDebt;
 import org.sonar.server.startup.DeleteOldAnalysisReportsFromFs;
 import org.sonar.server.startup.DisplayLogOnDeprecatedProjects;
 import org.sonar.server.startup.GeneratePluginIndex;
@@ -64,7 +63,6 @@ public class PlatformLevelStartup extends PlatformLevel {
       RegisterPermissionTemplates.class,
       RenameDeprecatedPropertyKeys.class,
       DisplayLogOnDeprecatedProjects.class,
-      ClearRulesOverloadedDebt.class,
       DeleteOldAnalysisReportsFromFs.class);
 
     // RegisterServletFilters makes the WebService engine of Level4 served by the MasterServletFilter, therefor it
