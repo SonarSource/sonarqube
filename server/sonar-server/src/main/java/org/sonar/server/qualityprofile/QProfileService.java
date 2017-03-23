fa926d324a3166cd3d08d5c5aefcbead7d532195
Daniel Schwarz <daniel.schwarz@sonarsource.com>
2017-03-23 11:00:24 +0100

    provide session to QProfileService

--- server/sonar-server/src/main/java/org/sonar/server/qualityprofile/QProfileService.java
+++ server/sonar-server/src/main/java/org/sonar/server/qualityprofile/QProfileService.java
@@ -73,7 +73,9 @@ public class QProfileService {
    */
   public List<ActiveRuleChange> deactivate(ActiveRuleKey key) {
     verifyAdminPermission();
-    return ruleActivator.deactivate(key);
+    try (DbSession dbSession = db.openSession(false)) {
+      return ruleActivator.deactivateAndUpdateIndex(dbSession, key);
+    }
   }
 
   public BulkChangeResult bulkActivate(RuleQuery ruleQuery, String profile, @Nullable String severity) {
