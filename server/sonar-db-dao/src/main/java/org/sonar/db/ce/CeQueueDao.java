5357cc49e1ff0055b9a6250f987c6ff20f7d8055
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2017-03-27 12:26:36 +0200

    SONAR-8985 SONAR-9040 rename CeQueueDao#updateIfStatus to updateIf
    for method to take in parameter types rather than Strings
    to support upcoming new properties to update and new conditions

--- server/sonar-db-dao/src/main/java/org/sonar/db/ce/CeQueueDao.java
+++ server/sonar-db-dao/src/main/java/org/sonar/db/ce/CeQueueDao.java
@@ -120,7 +120,9 @@ public class CeQueueDao implements Dao {
   }
 
   private Optional<CeQueueDto> tryToPeek(DbSession session, String taskUuid) {
-    int touchedRows = mapper(session).updateIfStatus(taskUuid, IN_PROGRESS, system2.now(), system2.now(), PENDING);
+    int touchedRows = mapper(session).updateIf(taskUuid,
+      new UpdateIf.NewProperties(IN_PROGRESS, system2.now(), system2.now()),
+      new UpdateIf.OldProperties(PENDING));
     if (touchedRows != 1) {
       return Optional.absent();
     }
