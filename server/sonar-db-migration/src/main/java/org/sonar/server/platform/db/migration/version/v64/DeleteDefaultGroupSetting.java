ac8bbb6ab92fec7f313ee0c0634804a6da99dc41
Julien Lancelot <julien.lancelot@sonarsource.com>
2017-03-30 10:26:44 +0200

    SONAR-9014 Delete 'sonar.defaultGroup' setting from database

--- server/sonar-db-migration/src/main/java/org/sonar/server/platform/db/migration/version/v64/DeleteDefaultGroupSetting.java
+++ server/sonar-db-migration/src/main/java/org/sonar/server/platform/db/migration/version/v64/DeleteDefaultGroupSetting.java
@@ -17,25 +17,23 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-
 package org.sonar.server.platform.db.migration.version.v64;
 
-import org.junit.Test;
+import java.sql.SQLException;
+import org.sonar.db.Database;
+import org.sonar.server.platform.db.migration.step.DataChange;
 
-import static org.sonar.server.platform.db.migration.version.DbVersionTestUtils.verifyMigrationCount;
-import static org.sonar.server.platform.db.migration.version.DbVersionTestUtils.verifyMinimumMigrationNumber;
+public class DeleteDefaultGroupSetting extends DataChange {
 
-public class DbVersion64Test {
-  private DbVersion64 underTest = new DbVersion64();
-
-  @Test
-  public void migrationNumber_starts_at_1600() {
-    verifyMinimumMigrationNumber(underTest, 1600);
+  public DeleteDefaultGroupSetting(Database db) {
+    super(db);
   }
 
-  @Test
-  public void verify_migration_count() {
-    verifyMigrationCount(underTest, 21);
+  @Override
+  protected void execute(Context context) throws SQLException {
+    context.prepareUpsert("DELETE FROM properties WHERE prop_key=?")
+      .setString(1, "sonar.defaultGroup")
+      .execute().commit();
   }
 
 }
