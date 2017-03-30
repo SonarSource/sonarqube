785012843b158f356848f560829e591ca7ea8f79
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2017-03-30 18:50:44 +0200

    SONAR-9057 make CeQueueCleaner startable and run as startup task

--- server/sonar-ce/src/main/java/org/sonar/ce/container/ComputeEngineContainerImpl.java
+++ server/sonar-ce/src/main/java/org/sonar/ce/container/ComputeEngineContainerImpl.java
@@ -68,6 +68,7 @@ import org.sonar.process.logging.LogbackHelper;
 import org.sonar.server.component.ComponentCleanerService;
 import org.sonar.server.component.ComponentFinder;
 import org.sonar.server.component.index.ComponentIndexer;
+import org.sonar.server.computation.queue.CeQueueCleaner;
 import org.sonar.server.computation.queue.PurgeCeActivities;
 import org.sonar.server.computation.task.projectanalysis.ProjectAnalysisTaskModule;
 import org.sonar.server.computation.taskprocessor.CeTaskProcessorModule;
@@ -398,6 +399,7 @@ public class ComputeEngineContainerImpl implements ComputeEngineContainer {
       LogServerId.class,
       ServerLifecycleNotifier.class,
       PurgeCeActivities.class,
+      CeQueueCleaner.class
     };
   }
 
