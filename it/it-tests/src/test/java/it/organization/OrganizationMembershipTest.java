31c5fb107d4c439b71363406a805b19f7ee0a943
Julien Lancelot <julien.lancelot@sonarsource.com>
2017-04-10 16:56:13 +0200

    SONAR-9018 Fix ITs as users are members of 'members' group

--- it/it-tests/src/test/java/it/organization/OrganizationMembershipTest.java
+++ it/it-tests/src/test/java/it/organization/OrganizationMembershipTest.java
@@ -193,7 +193,7 @@ public class OrganizationMembershipTest {
       .shouldHaveTotal(1)
       .addMember("foo")
       .shouldHaveTotal(2);
-    page.getMembersByIdx(0).shouldBeNamed("admin", "Administrator").shouldHaveGroups(1);
+    page.getMembersByIdx(0).shouldBeNamed("admin", "Administrator").shouldHaveGroups(2);
     page.getMembersByIdx(1).shouldBeNamed("foo", "foo").shouldHaveGroups(0);
   }
 
@@ -220,16 +220,18 @@ public class OrganizationMembershipTest {
     adminClient.organizations().addMember(orgKey, "foo");
 
     MembersPage page = nav.logIn().asAdmin().openOrganizationMembers(orgKey);
+    // foo user
     page.getMembersByIdx(1)
       .manageGroupsOpen()
       .manageGroupsSelect("owners")
       .manageGroupsSave()
       .shouldHaveGroups(1);
+    // admin user
     page.getMembersByIdx(0)
       .manageGroupsOpen()
       .manageGroupsSelect("owners")
       .manageGroupsSave()
-      .shouldHaveGroups(0);
+      .shouldHaveGroups(1);
   }
 
   @Test
