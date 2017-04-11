d2aef03a3830823ae277ad935e01194bd99cb0df
Teryk Bellahsene <teryk.bellahsene@sonarsource.com>
2017-04-11 11:42:31 +0200

    SONAR-9054 return org pages in WS api/navigation/organization

--- server/sonar-server/src/main/java/org/sonar/server/ui/ws/OrganizationAction.java
+++ server/sonar-server/src/main/java/org/sonar/server/ui/ws/OrganizationAction.java
@@ -19,17 +19,22 @@
  */
 package org.sonar.server.ui.ws;
 
+import java.util.List;
+import java.util.function.Consumer;
 import org.sonar.api.server.ws.Request;
 import org.sonar.api.server.ws.Response;
 import org.sonar.api.server.ws.WebService;
 import org.sonar.api.utils.text.JsonWriter;
+import org.sonar.api.web.page.Page;
 import org.sonar.db.DbClient;
 import org.sonar.db.DbSession;
 import org.sonar.db.organization.OrganizationDto;
-import org.sonar.server.organization.DefaultOrganizationProvider;
 import org.sonar.db.permission.OrganizationPermission;
+import org.sonar.server.organization.DefaultOrganizationProvider;
+import org.sonar.server.ui.PageRepository;
 import org.sonar.server.user.UserSession;
 
+import static org.sonar.server.ws.KeyExamples.KEY_ORG_EXAMPLE_001;
 import static org.sonar.server.ws.WsUtils.checkFoundWithOptional;
 
 public class OrganizationAction implements NavigationWsAction {
@@ -40,11 +45,13 @@ public class OrganizationAction implements NavigationWsAction {
   private final DbClient dbClient;
   private final DefaultOrganizationProvider defaultOrganizationProvider;
   private final UserSession userSession;
+  private final PageRepository pageRepository;
 
-  public OrganizationAction(DbClient dbClient, DefaultOrganizationProvider defaultOrganizationProvider, UserSession userSession) {
+  public OrganizationAction(DbClient dbClient, DefaultOrganizationProvider defaultOrganizationProvider, UserSession userSession, PageRepository pageRepository) {
     this.dbClient = dbClient;
     this.defaultOrganizationProvider = defaultOrganizationProvider;
     this.userSession = userSession;
+    this.pageRepository = pageRepository;
   }
 
   @Override
@@ -59,7 +66,7 @@ public class OrganizationAction implements NavigationWsAction {
     projectNavigation.createParam(PARAM_ORGANIZATION)
       .setRequired(true)
       .setDescription("the organization key")
-      .setExampleValue("my-org");
+      .setExampleValue(KEY_ORG_EXAMPLE_001);
   }
 
   @Override
@@ -84,8 +91,28 @@ public class OrganizationAction implements NavigationWsAction {
       .prop("canAdmin", userSession.hasPermission(OrganizationPermission.ADMINISTER, organization))
       .prop("canProvisionProjects", userSession.hasPermission(OrganizationPermission.PROVISION_PROJECTS, organization))
       .prop("canDelete", organization.isGuarded() ? userSession.isSystemAdministrator() : userSession.hasPermission(OrganizationPermission.ADMINISTER, organization))
-      .prop("isDefault", organization.getKey().equals(defaultOrganizationProvider.get().getKey()))
-      .endObject();
+      .prop("isDefault", organization.getKey().equals(defaultOrganizationProvider.get().getKey()));
+    List<Page> pages = pageRepository.getOrganizationPages(false);
+    json.name("pages");
+    writePages(json, pages);
+    if (userSession.hasPermission(OrganizationPermission.ADMINISTER, organization)) {
+      List<Page> adminPages = pageRepository.getOrganizationPages(true);
+      json.name("adminPages");
+      writePages(json, adminPages);
+    }
+    json.endObject();
+  }
 
+  private static void writePages(JsonWriter json, List<Page> pages) {
+    json.beginArray();
+    pages.forEach(writePage(json));
+    json.endArray();
+  }
+
+  private static Consumer<Page> writePage(JsonWriter json) {
+    return page -> json.beginObject()
+      .prop("key", page.getKey())
+      .prop("name", page.getName())
+      .endObject();
   }
 }
