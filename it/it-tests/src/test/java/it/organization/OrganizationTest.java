fad3d297be37f11564361832245e8663cd5aa14a
Julien Lancelot <julien.lancelot@sonarsource.com>
2017-04-11 11:59:25 +0200

    SONAR-9023 Fix ITs as 'user' now belongs to 'Members' group when being member of an organization

--- it/it-tests/src/test/java/it/organization/OrganizationTest.java
+++ it/it-tests/src/test/java/it/organization/OrganizationTest.java
@@ -369,7 +369,7 @@ public class OrganizationTest {
     List<WsUsers.GroupsWsResponse.Group> result = adminClient.users().groups(
       GroupsRequest.builder().setLogin(userLogin).setOrganization(KEY).build()).getGroupsList();
 
-    assertThat(result).extracting(WsUsers.GroupsWsResponse.Group::getName).containsOnly(groupName);
+    assertThat(result).extracting(WsUsers.GroupsWsResponse.Group::getName).containsOnly(groupName, "Members");
   }
 
   private WsComponents.SearchWsResponse searchSampleProject(String organizationKey, ComponentsService componentsService) {
