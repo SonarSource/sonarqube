af51c229c26e644b2377e11173a33abddd7ea887
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2017-04-12 14:34:46 +0200

    SONAR-9090 make projects privates based on permissions

--- server/sonar-db-migration/src/main/java/org/sonar/server/platform/db/migration/version/v64/DbVersion64.java
+++ server/sonar-db-migration/src/main/java/org/sonar/server/platform/db/migration/version/v64/DbVersion64.java
@@ -68,6 +68,7 @@ public class DbVersion64 implements DbVersion {
       .add(1637, "Make column PROJECTS.PRIVATE not nullable", MakeColumnProjectsPrivateNotNullable.class)
       .add(1638, "Add column ORGANIZATIONS.NEW_PROJECT_PRIVATE", AddColumnNewProjectPrivate.class)
       .add(1639, "Set ORGANIZATIONS.NEW_PROJECT_PRIVATE to false", SetNewProjectPrivateToFalse.class)
-      .add(1640, "Make column ORGANIZATIONS.NEW_PROJECT_PRIVATE not nullable", MakeColumnNewProjectPrivateNotNullable.class);
+      .add(1640, "Make column ORGANIZATIONS.NEW_PROJECT_PRIVATE not nullable", MakeColumnNewProjectPrivateNotNullable.class)
+      .add(1641, "Make components private based on permissions", MakeComponentsPrivateBasedOnPermissions.class);
   }
 }
