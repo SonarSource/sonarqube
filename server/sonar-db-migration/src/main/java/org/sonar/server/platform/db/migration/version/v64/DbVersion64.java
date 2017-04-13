9ee3c4c5c2f20075f49a86173b4ec5b6c63c8e17
Julien Lancelot <julien.lancelot@sonarsource.com>
2017-04-13 09:17:12 +0200

    SONAR-9085 Clean orphan rows in GROUPS_USERS

--- server/sonar-db-migration/src/main/java/org/sonar/server/platform/db/migration/version/v64/DbVersion64.java
+++ server/sonar-db-migration/src/main/java/org/sonar/server/platform/db/migration/version/v64/DbVersion64.java
@@ -53,6 +53,7 @@ public class DbVersion64 implements DbVersion {
       .add(1622, "Create 'Members' group in each organization", CreateMembersGroupsInEachOrganization.class)
       .add(1623, "Set organization members into 'Members' group", SetOrganizationMembersIntoMembersGroup.class)
       .add(1624, "Add ORGANIZATIONS.DEFAULT_GROUP_ID", AddDefaultGroupIdToOrganizations.class)
-      .add(1625, "Populate column ORGANIZATIONS.DEFAULT_GROUP_ID", PopulateColumnDefaultGroupIdOfOrganizations.class);
+      .add(1625, "Populate column ORGANIZATIONS.DEFAULT_GROUP_ID", PopulateColumnDefaultGroupIdOfOrganizations.class)
+      .add(1626, "Clean orphan rows in table GROUPS_USERS", CleanOrphanRowsInGroupsUsers.class);
   }
 }
