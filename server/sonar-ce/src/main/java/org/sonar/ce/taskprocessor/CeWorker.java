d1a80866ea5cc50445cc1eb9a760d040e3919ea9
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2017-04-18 12:35:48 +0200

    SONAR-8985 add unique identifier to CeWorker

--- server/sonar-ce/src/main/java/org/sonar/ce/taskprocessor/CeWorker.java
+++ server/sonar-ce/src/main/java/org/sonar/ce/taskprocessor/CeWorker.java
@@ -19,11 +19,15 @@
  */
 package org.sonar.ce.taskprocessor;
 
-import com.google.common.util.concurrent.ListeningScheduledExecutorService;
-import org.sonar.server.util.StoppableExecutorService;
+import java.util.concurrent.Callable;
+import org.sonar.ce.queue.CeQueue;
+import org.sonar.ce.queue.CeTask;
 
 /**
- * The {@link java.util.concurrent.ExecutorService} responsible for running {@link CeWorkerImpl}.
+ * Marker interface of the runnable in charge of polling the {@link CeQueue} and executing {@link CeTask}.
+ * {@link Callable#call()} returns a Boolean which is {@code true} when some a {@link CeTask} was processed,
+ * {@code false} otherwise.
  */
-public interface CeProcessingSchedulerExecutorService extends StoppableExecutorService, ListeningScheduledExecutorService {
+public interface CeWorker extends Callable<Boolean> {
+  String getUUID();
 }
