02cc6c36d46f4b50781b750cdae286fd28dd0979
Tom <thomas.verin@sonarsource.com>
2017-04-20 09:47:11 +0200

    Cancel the Travis build of a branch when a pull request exists
    It allows to not use slaves for nothing by executing twice
    (more or less) the same job. Building the pull request
    is enough in most cases.

--- travis.sh
+++ travis.sh
@@ -113,10 +113,15 @@ function fixBuildVersion {
 #
 function configureTravis {
   mkdir ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v33 | tar zx --strip-components 1 -C ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v35 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
 configureTravis
+
+# When pull request exists on the branch, then the job related to the branch does not need
+# to be executed and should be canceled. It does not book slaves for nothing.
+# @TravisCI please provide the feature natively, like at AppVeyor or CircleCI ;-)
+cancel_branch_build_with_pr
 
 case "$TARGET" in
 
