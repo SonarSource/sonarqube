8680a1efb9c19ae8d1971dcec1de6aead680ebcc
Tom <thomas.verin@sonarsource.com>
2017-04-21 10:32:24 +0200

    BUILD: increase ramdisk to 10Go

--- scripts/setupRamdisk.sh
+++ scripts/setupRamdisk.sh
@@ -15,14 +15,15 @@ du -sh $HOME
 #du -sh $HOME/maven
 #du -sh $HOME/phantomjs
 du -cksh $HOME/* | sort -hr | head -n 15
-du -cksh $HOME/.* | sort -hr | head -n 15
+du -cksh $HOME/otp/* | sort -hr | head -n 15
+ls -al $HOME/otp
 
 printf "${RED}move original home${NC}\n"
 sudo mv /home/travis /home/travis.ori
 printf "${RED}create ramdisk mount point${NC}\n"
 sudo mkdir -p /home/travis
 printf "${RED}create ramdisk${NC}\n"
-sudo mount -t tmpfs -o size=8192m tmps /home/travis
+sudo mount -t tmpfs -o size=10240m tmps /home/travis
 printf "${RED}copy home to ramdisk${NC}\n"
 time sudo cp -R /home/travis.ori/. /home/travis
 printf "${RED}give permissions to travis on its home in ramdisk${NC}\n"
