c7848bcba81638b019de6ad82afe0581b5b1161b
Tom <thomas.verin@sonarsource.com>
2017-04-21 10:46:55 +0200

    BUILD: copy only needed content to ramdisk

--- scripts/setupRamdisk.sh
+++ scripts/setupRamdisk.sh
@@ -14,11 +14,11 @@ du -sh $HOME
 #du -sh $HOME/jvm
 #du -sh $HOME/maven
 #du -sh $HOME/phantomjs
-du -cksh $HOME/* | sort -hr | head -n 15
-du -cksh $HOME/otp/* | sort -hr | head -n 15
-ls -alh $HOME/otp/
-ls -alh $HOME/otp/18.2.1
-ls -alh $HOME/otp/R16B03
+#du -cksh $HOME/* | sort -hr | head -n 15
+#du -cksh $HOME/otp/* | sort -hr | head -n 15
+#ls -alh $HOME/otp/
+#ls -alh $HOME/otp/18.2.1
+#ls -alh $HOME/otp/R16B03
 
 printf "${RED}move original home${NC}\n"
 sudo mv /home/travis /home/travis.ori
@@ -27,9 +27,13 @@ sudo mkdir -p /home/travis
 printf "${RED}create ramdisk${NC}\n"
 sudo mount -t tmpfs -o size=10240m tmps /home/travis
 printf "${RED}copy home to ramdisk${NC}\n"
-time sudo cp -R /home/travis.ori/. /home/travis
-printf "${RED}give permissions to travis on its home in ramdisk${NC}\n"
-sudo chown -R travis:travis /home/travis
+time sudo cp -R /home/travis.ori/.m2 /home/travis
+time sudo cp -R /home/travis.ori/.sonar /home/travis
+time sudo cp -R /home/travis.ori/jvm /home/travis
+time sudo cp -R /home/travis.ori/maven /home/travis
+time sudo cp -R /home/travis.ori/phantomjs /home/travis
+#printf "${RED}give permissions to travis on its home in ramdisk${NC}\n"
+#sudo chown -R travis:travis /home/travis
 
 printf "${RED}home content${NC}\n"
 ls -al $HOME
