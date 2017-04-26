61da4f3531f26c70abc767cc3f584effcbfe0576
Grégoire Aubert <gregoire.aubert@sonarsource.com>
2017-04-26 15:41:47 +0200

    SONAR-8992 Fix members add form, show only non members

--- server/sonar-web/src/main/js/apps/users/components/UsersSelectSearchValue.js
+++ server/sonar-web/src/main/js/apps/users/components/UsersSelectSearchValue.js
@@ -40,8 +40,8 @@ export default class UsersSelectSearchValue extends React.PureComponent {
           user.login &&
           <div className="Select-value-label">
             <Avatar hash={user.avatar} email={user.email} size={AVATAR_SIZE} />
-            <strong className="spacer-left">{user.login}</strong>
-            <span className="note little-spacer-left">{this.props.children}</span>
+            <strong className="spacer-left">{this.props.children}</strong>
+            <span className="note little-spacer-left">{user.login}</span>
           </div>}
       </div>
     );
