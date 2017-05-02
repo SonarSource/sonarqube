b261d34ef2daa42d42ea9d2522b6430c6766d4b4
tomverin <thomas.verin@sonarsource.com>
2017-05-02 14:26:00 +0200

    BUILD: use all available cpus

--- travis.sh
+++ travis.sh
@@ -137,7 +137,7 @@ BUILD)
 
   # Minimal Maven settings
   export MAVEN_OPTS="-Xmx1G -Xms128m"
-  MAVEN_ARGS="-Dmaven.test.redirectTestOutputToFile=false -Dsurefire.useFile=false -B -e -V -DbuildVersion=$BUILD_VERSION"
+  MAVEN_ARGS="-T1C -Dmaven.test.redirectTestOutputToFile=false -Dsurefire.useFile=false -B -e -V -DbuildVersion=$BUILD_VERSION"
 
   if [ "$TRAVIS_BRANCH" == "test/dr/travis-premium-vms-mt" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     echo 'Build and analyze test/dr/travis-premium-vms-mt'
@@ -151,8 +151,7 @@ BUILD)
 
     mvn org.jacoco:jacoco-maven-plugin:prepare-agent deploy \
           $MAVEN_ARGS \
-          -Pdeploy-sonarsource,release \
-          -T0.8C -DforkCount=0.8C
+          -Pdeploy-sonarsource,release           
 
   elif [[ "$TRAVIS_BRANCH" == "branch-"* ]] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     echo 'Build release branch'
