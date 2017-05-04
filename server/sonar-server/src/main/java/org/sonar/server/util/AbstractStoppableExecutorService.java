6ce1d6dd6ff69a7e641d8a83f10200d94ae221a9
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2017-05-04 12:00:06 +0200

    SONAR-7511 AbstractStoppableExecutorService produce warn logs
    rather than error logs

--- server/sonar-server/src/main/java/org/sonar/server/util/AbstractStoppableExecutorService.java
+++ server/sonar-server/src/main/java/org/sonar/server/util/AbstractStoppableExecutorService.java
@@ -54,11 +54,11 @@ public abstract class AbstractStoppableExecutorService<T extends ExecutorService
         delegate.shutdownNow();
         // Wait a while for tasks to respond to being canceled
         if (!delegate.awaitTermination(5, TimeUnit.SECONDS)) {
-          Loggers.get(getClass()).error(format("Pool %s did not terminate", getClass().getSimpleName()));
+          Loggers.get(getClass()).warn(format("Pool %s did not terminate", getClass().getSimpleName()));
         }
       }
     } catch (InterruptedException ie) {
-      Loggers.get(getClass()).error(format("Termination of pool %s failed", getClass().getSimpleName()), ie);
+      Loggers.get(getClass()).warn(format("Termination of pool %s failed", getClass().getSimpleName()), ie);
       // (Re-)Cancel if current thread also interrupted
       delegate.shutdownNow();
     }
