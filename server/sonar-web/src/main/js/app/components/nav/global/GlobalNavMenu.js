18a8b0a754b4fd6b44d677e4cd63155d826810d1
Stas Vilchik <vilchiks@gmail.com>
2017-05-05 14:56:09 +0200

    SONAR-8903 drop issues filter by last week

--- server/sonar-web/src/main/js/app/components/nav/global/GlobalNavMenu.js
+++ server/sonar-web/src/main/js/app/components/nav/global/GlobalNavMenu.js
@@ -62,7 +62,7 @@ export default class GlobalNavMenu extends React.PureComponent {
 
   renderIssuesLink() {
     const query = this.props.currentUser.isLoggedIn
-      ? { createdInLast: '1w', myIssues: 'true', resolved: 'false' }
+      ? { resolved: 'false', myIssues: 'true' }
       : { resolved: 'false' };
     const active = this.props.location.pathname === 'issues';
     return (
