3ead665e2f3ce5de27445424d5aa7b05561156ee
Julien Lancelot <julien.lancelot@sonarsource.com>
2017-05-23 09:03:24 +0200

    Fix JDK link download in travis build

--- travis.sh
+++ travis.sh
@@ -23,17 +23,16 @@ function installPhantomJs {
 # at each build.
 #
 function installJdk8 {
-  echo "Setup JDK 1.8u121"
+  echo "Setup JDK 1.8u131"
   mkdir -p ~/jvm
   pushd ~/jvm > /dev/null
-  if [ ! -d "jdk1.8.0_121" ]; then
-    echo "Download JDK8"
-    wget --no-check-certificate -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u121-b13/e9e7ea248e2c4826b92b3f075a80e441/jdk-8u121-linux-x64.tar.gz
-    tar xzf jdk-8u121-linux-x64.tar.gz
-    rm jdk-8u121-linux-x64.tar.gz
+  if [ ! -d "jdk1.8.0_131" ]; then
+    wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz
+    tar xzf jdk-8u131-linux-x64.tar.gz
+    rm jdk-8u131-linux-x64.tar.gz
   fi
   popd > /dev/null
-  export JAVA_HOME=~/jvm/jdk1.8.0_121
+  export JAVA_HOME=~/jvm/jdk1.8.0_131
   export PATH=$JAVA_HOME/bin:$PATH
 }
 
@@ -113,7 +112,7 @@ function fixBuildVersion {
 #
 function configureTravis {
   mkdir ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v35 | tar zx --strip-components 1 -C ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v36 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
 configureTravis
