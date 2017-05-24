993d39998e79d38d9dbe693c555966d7dd9181e0
Simon Brandhof <simon.brandhof@sonarsource.com>
2017-05-24 14:35:48 +0200

    SONAR-9302 make db column rules_profiles.is_built_in not null

--- server/sonar-db-migration/src/main/java/org/sonar/server/platform/db/migration/version/v65/DbVersion65.java
+++ server/sonar-db-migration/src/main/java/org/sonar/server/platform/db/migration/version/v65/DbVersion65.java
@@ -35,6 +35,7 @@ public class DbVersion65 implements DbVersion {
       .add(1705, "Make EVENTS.COMPONENT_UUID not nullable", MakeEventsComponentUuidNotNullable.class)
       .add(1706, "Recreate index EVENTS_COMPONENT_UUID", RecreateIndexEventsComponentUuid.class)
       .add(1707, "Add rules_profiles.is_built_in", AddBuiltInFlagToRulesProfiles.class)
-      .add(1708, "Set rules_profiles.is_built_in to false", SetRulesProfilesIsBuiltInToFalse.class);
+      .add(1708, "Set rules_profiles.is_built_in to false", SetRulesProfilesIsBuiltInToFalse.class)
+      .add(1709, "Make rules_profiles.is_built_in not null", MakeRulesProfilesIsBuiltInNotNullable.class);
   }
 }
