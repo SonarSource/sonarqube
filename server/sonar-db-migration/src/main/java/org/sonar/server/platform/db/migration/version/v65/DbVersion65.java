a9cd8d9ab2ed78c580af5283f36d581648168a09
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2017-05-30 15:00:22 +0200

    SONAR-9435 add db migration to clean orphans in PROJECT_LINKS

--- server/sonar-db-migration/src/main/java/org/sonar/server/platform/db/migration/version/v65/DbVersion65.java
+++ server/sonar-db-migration/src/main/java/org/sonar/server/platform/db/migration/version/v65/DbVersion65.java
@@ -34,6 +34,7 @@ public class DbVersion65 implements DbVersion {
       .add(1704, "Drop index EVENTS_COMPONENT_UUID", DropIndexEventsComponentUuid.class)
       .add(1705, "Make EVENTS.COMPONENT_UUID not nullable", MakeEventsComponentUuidNotNullable.class)
       .add(1706, "Recreate index EVENTS_COMPONENT_UUID", RecreateIndexEventsComponentUuid.class)
-      .add(1707, "Ensure ISSUE.PROJECT_UUID is consistent", EnsureIssueProjectUuidConsistencyOnIssues.class);
+      .add(1707, "Ensure ISSUE.PROJECT_UUID is consistent", EnsureIssueProjectUuidConsistencyOnIssues.class)
+      .add(1708, "Clean orphans from PROJECT_LINKS", CleanOrphanRowsInProjectLinks.class);
   }
 }
