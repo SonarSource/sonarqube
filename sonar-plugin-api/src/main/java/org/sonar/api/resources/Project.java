8feb19b3ab5bb9878c34a24fd3fbe3095ec5f948
Daniel Schwarz <daniel.schwarz@sonarsource.com>
2017-05-30 17:24:54 +0200

    (wip) restored Project#getLanguage, as it has to be currently overridden

--- sonar-plugin-api/src/main/java/org/sonar/api/resources/Project.java
+++ sonar-plugin-api/src/main/java/org/sonar/api/resources/Project.java
@@ -129,6 +129,14 @@ public class Project extends Resource implements Component {
     return definition.getDescription();
   }
 
+  /** 
+   * @deprecated since 4.2 use {@link org.sonar.api.batch.fs.FileSystem#languages()}
+   */
+  @Override
+  public Language getLanguage() {
+    throw new UnsupportedOperationException();
+  }
+
   @Override
   public String getScope() {
     return Scopes.PROJECT;
