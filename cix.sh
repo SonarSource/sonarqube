dbe612f6c473c78c35d1bb4c0f0c8da16775b16b
Teryk Bellahsene <teryk.bellahsene@sonarsource.com>
2017-06-01 11:12:12 +0200

    DO NOT MERGE - Launch only PostgreSql QA on developer branch

--- cix.sh
+++ cix.sh
@@ -7,21 +7,28 @@ case "$RUN_ACTIVITY" in
 
   run-db-unit-tests-*)
     DB_ENGINE=$(sed "s/run-db-unit-tests-//g" <<< $RUN_ACTIVITY)
-
-    ./run-db-unit-tests.sh "http://infra.internal.sonarsource.com/jenkins/orch-${DB_ENGINE}.properties"
+    if [ "${GITHUB_BRANCH}" == "PULLREQUEST-2104" ] ||  [ "${DB_ENGINE}" == "postgresql93" ]
+    then
+      ./run-db-unit-tests.sh "http://infra.internal.sonarsource.com/jenkins/orch-${DB_ENGINE}.properties"
+    fi
     ;;
 
   run-db-integration-tests-*)
     DB_ENGINE=$(sed "s/run-db-integration-tests-//g" <<< $RUN_ACTIVITY | cut -d \- -f 1)
     CATEGORY=$(sed "s/run-db-integration-tests-//g" <<< $RUN_ACTIVITY | cut -d \- -f 2)
 
-    ./run-integration-tests.sh "${CATEGORY}" "http://infra.internal.sonarsource.com/jenkins/orch-${DB_ENGINE}.properties"
+    if [ "${GITHUB_BRANCH}" == "PULLREQUEST-2104" ] ||  [ "${DB_ENGINE}" == "postgresql93" ]
+    then
+      ./run-integration-tests.sh "${CATEGORY}" "http://infra.internal.sonarsource.com/jenkins/orch-${DB_ENGINE}.properties"
+    fi
     ;;
 
   run-integration-tests-*)
     CATEGORY=$(sed "s/run-integration-tests-//g" <<< $RUN_ACTIVITY)
-
-    ./run-integration-tests.sh "${CATEGORY}" "http://infra.internal.sonarsource.com/jenkins/orch-embedded.properties"
+    if [ "${GITHUB_BRANCH}" == "PULLREQUEST-2104" ]
+    then
+      ./run-integration-tests.sh "${CATEGORY}" "http://infra.internal.sonarsource.com/jenkins/orch-embedded.properties"
+    fi
     ;;
 
   run-it-released-plugins)
@@ -34,7 +41,10 @@ case "$RUN_ACTIVITY" in
 
   run-upgrade-tests-*)
     DB_ENGINE=$(sed "s/run-upgrade-tests-//g" <<< $RUN_ACTIVITY)
-    ./run-upgrade-tests.sh "http://infra.internal.sonarsource.com/jenkins/orch-${DB_ENGINE}.properties"
+    if [ "${GITHUB_BRANCH}" == "PULLREQUEST-2104" ] ||  [ "${DB_ENGINE}" == "postgresql93" ]
+    then
+      ./run-upgrade-tests.sh "http://infra.internal.sonarsource.com/jenkins/orch-${DB_ENGINE}.properties"
+    fi
     ;;
 
   *)
