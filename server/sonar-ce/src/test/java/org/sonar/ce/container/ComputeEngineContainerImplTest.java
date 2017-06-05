5d0da5f6430ab4fb8db76f930a96a04e17351823
Daniel Schwarz <daniel.schwarz@sonarsource.com>
2017-06-05 11:50:51 +0200

    SONAR-9356 define property "sonar.onboardingTutorial.skip"

--- server/sonar-ce/src/test/java/org/sonar/ce/container/ComputeEngineContainerImplTest.java
+++ server/sonar-ce/src/test/java/org/sonar/ce/container/ComputeEngineContainerImplTest.java
@@ -137,7 +137,7 @@ public class ComputeEngineContainerImplTest {
         + 23 // level 1
         + 45 // content of DaoModule
         + 3 // content of EsSearchModule
-        + 56 // content of CorePropertyDefinitions
+        + 57 // content of CorePropertyDefinitions
     );
     assertThat(
       picoContainer.getComponentAdapters().stream()
