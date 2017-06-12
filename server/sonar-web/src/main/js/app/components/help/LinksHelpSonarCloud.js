fbc932a882b6dec72900f5242d0cead7ff03e4b2
Stas Vilchik <stas.vilchik@sonarsource.com>
2017-06-12 03:50:27 -0700

    UI: SONAR-9355 Create onboarding tutorial (#2137)

---
+++ server/sonar-web/src/main/js/app/components/help/LinksHelpSonarCloud.js
@@ -0,0 +1,45 @@
+/*
+ * SonarQube
+ * Copyright (C) 2009-2017 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+// @flow
+import React from 'react';
+import { Link } from 'react-router';
+import { translate } from '../../../helpers/l10n';
+
+type Props = { onClose: () => void };
+
+export default function LinksHelpSonarCloud({ onClose }: Props) {
+  return (
+    <div>
+      <h2 className="spacer-top spacer-bottom">{translate('help.section.links')}</h2>
+
+      <a href="https://about.sonarcloud.io/news/">{translate('footer.news')}</a>
+      {' - '}
+      <a href="https://about.sonarcloud.io/terms.pdf">{translate('footer.terms')}</a>
+      {' - '}
+      <a href="https://twitter.com/sonarqube">{translate('footer.twitter')}</a>
+      {' - '}
+      <a href="https://about.sonarcloud.io/get-started/">{translate('footer.get_started')}</a>
+      {' - '}
+      <a href="https://about.sonarcloud.io/contact/">{translate('footer.help')}</a>
+      {' - '}
+      <Link to="/about" onClick={onClose}>{translate('footer.about')}</Link>
+    </div>
+  );
+}
