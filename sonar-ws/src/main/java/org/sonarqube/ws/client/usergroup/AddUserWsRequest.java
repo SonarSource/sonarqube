1c5520825de0542df21045845d5d6277a5916637
Julien Lancelot <julien.lancelot@sonarsource.com>
2017-06-13 13:52:14 +0200

    Add missing UserGroupsService in WsClient

--- sonar-ws/src/main/java/org/sonarqube/ws/client/usergroup/AddUserWsRequest.java
+++ sonar-ws/src/main/java/org/sonarqube/ws/client/usergroup/AddUserWsRequest.java
@@ -63,7 +63,7 @@ public class AddUserWsRequest {
   }
 
   public static class Builder {
-    private long id;
+    private Long id;
     private String name;
     private String login;
     private String organization;
