49b5062ff42f81b66f3bb22069afba4120306de4
Grégoire Aubert <gregoire.aubert@sonarsource.com>
2017-06-20 17:07:51 +0200

    SONAR-9440 Fix usage of measures/search_history api

---
+++ server/sonar-web/src/main/js/apps/projectActivity/components/__tests__/ProjectActivityGraphs-test.js
@@ -0,0 +1,81 @@
+/*
+ * SonarQube
+ * Copyright (C) 2009-2017 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+import React from 'react';
+import { shallow } from 'enzyme';
+import ProjectActivityGraphs from '../ProjectActivityGraphs';
+
+const ANALYSES = [
+  {
+    key: 'A1',
+    date: '2016-10-27T16:33:50+0200',
+    events: [
+      {
+        key: 'E1',
+        category: 'VERSION',
+        name: '6.5-SNAPSHOT'
+      }
+    ]
+  },
+  {
+    key: 'A2',
+    date: '2016-10-27T12:21:15+0200',
+    events: []
+  },
+  {
+    key: 'A3',
+    date: '2016-10-26T12:17:29+0200',
+    events: [
+      {
+        key: 'E2',
+        category: 'VERSION',
+        name: '6.4'
+      },
+      {
+        key: 'E3',
+        category: 'OTHER',
+        name: 'foo'
+      }
+    ]
+  }
+];
+
+const DEFAULT_PROPS = {
+  analyses: ANALYSES,
+  leakPeriodDate: '2017-05-16T13:50:02+0200',
+  loading: false,
+  measuresHistory: [
+    {
+      metric: 'code_smells',
+      history: [
+        { date: new Date('2016-10-26T12:17:29+0200'), value: '2286' },
+        { date: new Date('2016-10-27T12:21:15+0200'), value: '1749' },
+        { date: new Date('2016-10-27T16:33:50+0200'), value: '500' }
+      ]
+    }
+  ],
+  metricsType: 'INT',
+  project: 'org.sonarsource.sonarqube:sonarqube',
+  query: { category: '', graph: 'overview', project: 'org.sonarsource.sonarqube:sonarqube' },
+  updateQuery: () => {}
+};
+
+it('should render correctly the graph and legends', () => {
+  expect(shallow(<ProjectActivityGraphs {...DEFAULT_PROPS} />)).toMatchSnapshot();
+});
