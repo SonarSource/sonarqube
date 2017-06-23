8c440dbca465878ef3696361bb3ef81a25a22876
Teryk Bellahsene <teryk.bellahsene@sonarsource.com>
2017-06-23 11:35:22 +0200

    SONAR-9448 Sanitize api/qualityprofiles/restore

--- server/sonar-server/src/main/java/org/sonar/server/qualityprofile/ws/RestoreAction.java
+++ server/sonar-server/src/main/java/org/sonar/server/qualityprofile/ws/RestoreAction.java
@@ -65,8 +65,8 @@ public class RestoreAction implements QProfileWsAction {
     WebService.NewAction action = controller.createAction(ACTION_RESTORE)
       .setSince("5.2")
       .setDescription("Restore a quality profile using an XML file. The restored profile name is taken from the backup file, " +
-        "so if a profile with the same name and language already exists, it will be overwritten. " +
-        "Require Administer Quality Profiles permission.")
+        "so if a profile with the same name and language already exists, it will be overwritten.<br> " +
+        "Requires to be logged in and the 'Administer Quality Profiles' permission.")
       .setPost(true)
       .setHandler(this);
 
