7ada6be4627ca5cd284d9aaffac1af50f3931192
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2017-06-28 10:58:19 +0200

    SONAR-9236 prepare for type change of ES property "norms"

--- server/sonar-server/src/main/java/org/sonar/server/es/NewIndex.java
+++ server/sonar-server/src/main/java/org/sonar/server/es/NewIndex.java
@@ -273,7 +273,9 @@ public class NewIndex {
         hash.putAll(ImmutableMap.of(
             "type", FIELD_TYPE_KEYWORD,
             "index", disableSearch ? INDEX_NOT_SEARCHABLE : INDEX_SEARCHABLE_FOR_KEYWORD,
-            "norms", ImmutableMap.of("enabled", String.valueOf(!disableNorms))));
+            "norms",
+            ImmutableMap.of("enabled", String.valueOf(!disableNorms)) // ES 5: replace with String.valueOf(!disableNorms)
+        ));
       } else {
         hash.put("type", "multi_field");
 
@@ -295,7 +297,9 @@ public class NewIndex {
             "type", FIELD_TYPE_KEYWORD,
             "index", INDEX_SEARCHABLE_FOR_KEYWORD,
             "term_vector", termVectorWithPositionOffsets ? "with_positions_offsets" : "no",
-            "norms", ImmutableMap.of("enabled", "false")));
+            "norms",
+            ImmutableMap.of("enabled", "false") // ES 5: replace with "false"
+        ));
 
         hash.put("fields", multiFields);
       }
