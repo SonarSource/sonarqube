06ee234f17c62f3296deb212e9b1ab20d812db42
Grégoire Aubert <gregoire.aubert@sonarsource.com>
2017-06-29 11:46:24 +0200

    SONAR-9482 Display the number of missing Sonar Way rules on the QP page

---
+++ server/sonar-web/src/main/js/apps/quality-profiles/details/ProfileRulesDeprecatedWarning.js
@@ -0,0 +1,44 @@
+/*
+ * SonarQube
+ * Copyright (C) 2009-2017 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+// @flow
+import React from 'react';
+import { Link } from 'react-router';
+import Tooltip from '../../../components/controls/Tooltip';
+import { getDeprecatedActiveRulesUrl } from '../../../helpers/urls';
+import { translate } from '../../../helpers/l10n';
+
+type Props = { activeDeprecatedRules: number, organization: ?string, profile: string };
+
+export default function ProfileRulesDeprecatedWarning(props: Props) {
+  const url = getDeprecatedActiveRulesUrl({ qprofile: props.profile }, props.organization);
+  return (
+    <div className="quality-profile-rules-deprecated clearfix">
+      <span className="pull-left">
+        {translate('quality_profiles.deprecated_rules')}
+        <Tooltip overlay={translate('quality_profiles.deprecated_rules_description')}>
+          <i className="icon-help spacer-left" />
+        </Tooltip>
+      </span>
+      <Link className="pull-right" to={url}>
+        {props.activeDeprecatedRules}
+      </Link>
+    </div>
+  );
+}
---
+++ server/sonar-web/src/main/js/apps/quality-profiles/details/ProfileRulesSonarWayComparison.js
@@ -0,0 +1,47 @@
+/*
+ * SonarQube
+ * Copyright (C) 2009-2017 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+// @flow
+import React from 'react';
+import { Link } from 'react-router';
+import Tooltip from '../../../components/controls/Tooltip';
+import { translate } from '../../../helpers/l10n';
+
+type Props = {
+  organization: ?string,
+  profile: string,
+  sonarway: string,
+  sonarWayMissingRules: number
+};
+
+export default function ProfileRulesSonarWayComparison(props: Props) {
+  return (
+    <div className="quality-profile-rules-sonarway-missing clearfix">
+      <span className="pull-left">
+        {translate('quality_profiles.sonarway_missing_rules')}
+        <Tooltip overlay={translate('quality_profiles.sonarway_missing_rules_description')}>
+          <i className="icon-help spacer-left" />
+        </Tooltip>
+      </span>
+      <Link className="pull-right">
+        {props.sonarWayMissingRules}
+      </Link>
+    </div>
+  );
+}
