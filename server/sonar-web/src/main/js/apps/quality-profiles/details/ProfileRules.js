a59d256e075821233cfa9bf11a5351a32825ddd1
Grégoire Aubert <gregoire.aubert@sonarsource.com>
2017-06-29 11:46:24 +0200

    SONAR-9482 Display the number of missing Sonar Way rules on the QP page

--- server/sonar-web/src/main/js/apps/quality-profiles/details/ProfileRules.js
+++ server/sonar-web/src/main/js/apps/quality-profiles/details/ProfileRules.js
@@ -21,12 +21,14 @@
 import React from 'react';
 import { Link } from 'react-router';
 import { keyBy } from 'lodash';
-import ProfileRulesRow from './ProfileRulesRow';
+import ProfileRulesRowOfType from './ProfileRulesRowOfType';
+import ProfileRulesRowTotal from './ProfileRulesRowTotal';
+import ProfileRulesDeprecatedWarning from './ProfileRulesDeprecatedWarning';
+import ProfileRulesSonarWayComparison from './ProfileRulesSonarWayComparison';
 import { searchRules, takeFacet } from '../../../api/rules';
-import { translate, translateWithParameters } from '../../../helpers/l10n';
-import { formatMeasure } from '../../../helpers/measures';
-import { getRulesUrl, getDeprecatedActiveRulesUrl } from '../../../helpers/urls';
-import IssueTypeIcon from '../../../components/ui/IssueTypeIcon';
+import { getQualityProfiles } from '../../../api/quality-profiles';
+import { getRulesUrl } from '../../../helpers/urls';
+import { translate } from '../../../helpers/l10n';
 import type { Profile } from '../propTypes';
 
 const TYPES = ['BUG', 'VULNERABILITY', 'CODE_SMELL'];
@@ -38,20 +40,24 @@ type Props = {
 };
 
 type State = {
-  total: ?number,
   activatedTotal: ?number,
+  activatedByType?: { [string]: ?{ val: string, count: ?number } },
   allByType?: { [string]: ?{ val: string, count: ?number } },
-  activatedByType?: { [string]: ?{ val: string, count: ?number } }
+  compareToSonarWay: ?{ profile: string, profileName: string, missingRuleCount: number },
+  loading: boolean,
+  total: ?number
 };
 
 export default class ProfileRules extends React.PureComponent {
   mounted: boolean;
   props: Props;
   state: State = {
-    total: null,
     activatedTotal: null,
+    activatedByType: keyBy(TYPES.map(t => ({ val: t, count: null })), 'val'),
     allByType: keyBy(TYPES.map(t => ({ val: t, count: null })), 'val'),
-    activatedByType: keyBy(TYPES.map(t => ({ val: t, count: null })), 'val')
+    compareToSonarWay: null,
+    loading: true,
+    total: null
   };
 
   componentDidMount() {
@@ -67,6 +73,16 @@ export default class ProfileRules extends React.PureComponent {
 
   componentWillUnmount() {
     this.mounted = false;
+  }
+
+  loadProfile() {
+    if (this.props.profile.isBuiltIn) {
+      return Promise.resolve(null);
+    }
+    return getQualityProfiles({
+      compareToSonarWay: true,
+      profile: this.props.profile.key
+    });
   }
 
   loadAllRules() {
@@ -87,197 +103,44 @@ export default class ProfileRules extends React.PureComponent {
   }
 
   loadRules() {
-    Promise.all([this.loadAllRules(), this.loadActivatedRules()]).then(responses => {
+    this.setState({ loading: true });
+    Promise.all([
+      this.loadAllRules(),
+      this.loadActivatedRules(),
+      this.loadProfile()
+    ]).then(responses => {
       if (this.mounted) {
-        const [allRules, activatedRules] = responses;
+        const [allRules, activatedRules, showProfile] = responses;
         this.setState({
-          total: allRules.total,
           activatedTotal: activatedRules.total,
           allByType: keyBy(takeFacet(allRules, 'types'), 'val'),
-          activatedByType: keyBy(takeFacet(activatedRules, 'types'), 'val')
+          activatedByType: keyBy(takeFacet(activatedRules, 'types'), 'val'),
+          compareToSonarWay: showProfile && showProfile.compareToSonarWay,
+          loading: false,
+          total: allRules.total
         });
       }
     });
   }
 
-  getTooltip(count: ?number, total: ?number) {
-    if (count == null || total == null) {
-      return '';
-    }
-
-    return translateWithParameters(
-      'quality_profiles.x_activated_out_of_y',
-      formatMeasure(count, 'INT'),
-      formatMeasure(total, 'INT')
-    );
-  }
-
-  getTooltipForType(type: string) {
-    if (
-      this.state.activatedByType &&
-      this.state.activatedByType[type] &&
-      this.state.allByType &&
-      this.state.allByType[type]
-    ) {
-      const { count } = this.state.activatedByType[type];
-      const total = this.state.allByType[type].count;
-      return this.getTooltip(count, total);
-    }
-  }
-
-  renderActiveTitle() {
-    return (
-      <strong>
-        {translate('total')}
-      </strong>
-    );
-  }
-
-  renderActiveCount() {
-    const rulesUrl = getRulesUrl(
-      {
-        qprofile: this.props.profile.key,
-        activation: 'true'
-      },
-      this.props.organization
-    );
-
-    if (this.state.activatedTotal == null) {
-      return null;
-    }
-
-    return (
-      <Link to={rulesUrl}>
-        <strong>
-          {formatMeasure(this.state.activatedTotal, 'SHORT_INT')}
-        </strong>
-      </Link>
-    );
-  }
-
-  renderActiveTotal() {
-    const rulesUrl = getRulesUrl(
-      {
-        qprofile: this.props.profile.key,
-        activation: 'false'
-      },
-      this.props.organization
-    );
-
-    if (this.state.total == null || this.state.activatedTotal == null) {
-      return null;
-    }
-
-    if (this.state.total === this.state.activatedTotal) {
-      return <span className="note text-muted">0</span>;
-    }
-
-    return (
-      <Link to={rulesUrl} className="small text-muted">
-        <strong>
-          {formatMeasure(this.state.total - this.state.activatedTotal, 'SHORT_INT')}
-        </strong>
-      </Link>
-    );
-  }
-
-  renderTitleForType(type: string) {
-    return (
-      <span>
-        <IssueTypeIcon query={type} className="little-spacer-right" />
-        {translate('issue.type', type, 'plural')}
-      </span>
-    );
-  }
-
-  renderCountForType(type: string) {
-    const rulesUrl = getRulesUrl(
-      {
-        qprofile: this.props.profile.key,
-        activation: 'true',
-        types: type
-      },
-      this.props.organization
-    );
-
-    const count = this.state.activatedByType && this.state.activatedByType[type]
+  getRulesCountForType(type: string): ?number {
+    return this.state.activatedByType && this.state.activatedByType[type]
       ? this.state.activatedByType[type].count
       : null;
-
-    if (count == null) {
-      return null;
-    }
-
-    return (
-      <Link to={rulesUrl}>
-        {formatMeasure(count, 'SHORT_INT')}
-      </Link>
-    );
   }
 
-  renderTotalForType(type: string) {
-    const rulesUrl = getRulesUrl(
-      {
-        qprofile: this.props.profile.key,
-        activation: 'false',
-        types: type
-      },
-      this.props.organization
-    );
-
-    const count = this.state.activatedByType && this.state.activatedByType[type]
-      ? this.state.activatedByType[type].count
-      : null;
-
-    const total = this.state.allByType && this.state.allByType[type]
+  getRulesTotalForType(type: string): ?number {
+    return this.state.allByType && this.state.allByType[type]
       ? this.state.allByType[type].count
       : null;
-
-    if (count == null || total == null) {
-      return null;
-    }
-
-    if (total === count) {
-      return <span className="note text-muted">0</span>;
-    }
-
-    return (
-      <Link to={rulesUrl} className="small text-muted">
-        {formatMeasure(total - count, 'SHORT_INT')}
-      </Link>
-    );
-  }
-
-  renderDeprecated() {
-    const { profile } = this.props;
-
-    if (profile.activeDeprecatedRuleCount === 0) {
-      return null;
-    }
-
-    const url = getDeprecatedActiveRulesUrl({ qprofile: profile.key }, this.props.organization);
-
-    return (
-      <div className="quality-profile-rules-deprecated clearfix">
-        <div className="pull-left">
-          {translate('quality_profiles.deprecated_rules')}
-        </div>
-        <div className="pull-right">
-          <Link to={url}>
-            {profile.activeDeprecatedRuleCount}
-          </Link>
-        </div>
-      </div>
-    );
   }
 
   render() {
+    const { organization, profile } = this.props;
+    const { compareToSonarWay } = this.state;
     const activateMoreUrl = getRulesUrl(
-      {
-        qprofile: this.props.profile.key,
-        activation: 'false'
-      },
-      this.props.organization
+      { qprofile: profile.key, activation: 'false' },
+      organization
     );
 
     return (
@@ -289,38 +152,53 @@ export default class ProfileRules extends React.PureComponent {
                 <th>
                   <h2>{translate('rules')}</h2>
                 </th>
-                <th>Active</th>
-                <th>Inactive</th>
+                <th>{translate('active')}</th>
+                <th>{translate('inactive')}</th>
               </tr>
             </thead>
             <tbody>
-              <ProfileRulesRow
+              <ProfileRulesRowTotal
                 key="all"
-                renderTitle={this.renderActiveTitle.bind(this)}
-                renderCount={this.renderActiveCount.bind(this)}
-                renderTotal={this.renderActiveTotal.bind(this)}
+                count={this.state.activatedTotal}
+                organization={organization}
+                qprofile={profile.key}
+                total={this.state.total}
               />
               {TYPES.map(type => (
-                <ProfileRulesRow
+                <ProfileRulesRowOfType
                   key={type}
-                  renderTitle={this.renderTitleForType.bind(this, type)}
-                  renderCount={this.renderCountForType.bind(this, type)}
-                  renderTotal={this.renderTotalForType.bind(this, type)}
+                  count={this.getRulesCountForType(type)}
+                  organization={organization}
+                  qprofile={profile.key}
+                  total={this.getRulesTotalForType(type)}
+                  type={type}
                 />
               ))}
             </tbody>
           </table>
 
           {this.props.canAdmin &&
-            !this.props.profile.isBuiltIn &&
+            !profile.isBuiltIn &&
             <div className="text-right big-spacer-top">
               <Link to={activateMoreUrl} className="button js-activate-rules">
                 {translate('quality_profiles.activate_more')}
               </Link>
             </div>}
         </div>
-
-        {this.renderDeprecated()}
+        {profile.activeDeprecatedRuleCount > 0 &&
+          <ProfileRulesDeprecatedWarning
+            activeDeprecatedRules={profile.activeDeprecatedRuleCount}
+            organization={organization}
+            profile={profile.key}
+          />}
+        {compareToSonarWay != null &&
+          compareToSonarWay.missingRuleCount > 0 &&
+          <ProfileRulesSonarWayComparison
+            organization={organization}
+            profile={profile.key}
+            sonarway={compareToSonarWay.profile}
+            sonarWayMissingRules={compareToSonarWay.missingRuleCount}
+          />}
       </div>
     );
   }
