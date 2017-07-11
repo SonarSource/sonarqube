2e3ad26ae18990a4841a29707298d2acd3f6cd5c
Julien HENRY <henryju@yahoo.fr>
2017-07-11 10:29:18 +0200

    SONAR-9198 Do not warn when reading property sets with getStringArray

--- sonar-scanner-engine/src/test/java/org/sonar/scanner/config/DefaultConfigurationTest.java
+++ sonar-scanner-engine/src/test/java/org/sonar/scanner/config/DefaultConfigurationTest.java
@@ -28,6 +28,7 @@ import org.sonar.api.config.Configuration;
 import org.sonar.api.config.Encryption;
 import org.sonar.api.config.PropertyDefinition;
 import org.sonar.api.config.PropertyDefinitions;
+import org.sonar.api.config.PropertyFieldDefinition;
 import org.sonar.api.utils.log.LogTester;
 import org.sonar.api.utils.log.LoggerLevel;
 
@@ -51,18 +52,40 @@ public class DefaultConfigurationTest {
 
     assertThat(config.get("multiA")).hasValue("a,b");
     assertThat(logTester.logs(LoggerLevel.WARN))
-      .contains("Access to the multi-valued property 'multiA' should be made using 'getStringArray' method. The SonarQube plugin using this property should be updated.");
+      .contains(
+        "Access to the multi-values/property set property 'multiA' should be made using 'getStringArray' method. The SonarQube plugin using this property should be updated.");
 
     logTester.clear();
 
     assertThat(config.getStringArray("single")).containsExactly("foo");
     assertThat(logTester.logs(LoggerLevel.WARN))
-      .contains("Property 'single' is not declared as multi-valued but was read using 'getStringArray' method. The SonarQube plugin declaring this property should be updated.");
+      .contains(
+        "Property 'single' is not declared as multi-values/property set but was read using 'getStringArray' method. The SonarQube plugin declaring this property should be updated.");
 
     logTester.clear();
 
     assertThat(config.get("notDeclared")).hasValue("c,d");
     assertThat(config.getStringArray("notDeclared")).containsExactly("c", "d");
+    assertThat(logTester.logs(LoggerLevel.WARN)).isEmpty();
+  }
+
+  @Test
+  public void accessingPropertySetPropertiesShouldBeConsistentWithDeclaration() {
+    Configuration config = new DefaultConfiguration(new PropertyDefinitions(Arrays.asList(
+      PropertyDefinition.builder("props").fields(PropertyFieldDefinition.build("foo1").name("Foo1").build(), PropertyFieldDefinition.build("foo2").name("Foo2").build()).build())),
+      new Encryption(null),
+      mock(AnalysisMode.class),
+      ImmutableMap.of("props", "1,2", "props.1.foo1", "a", "props.1.foo2", "b")) {
+    };
+
+    assertThat(config.get("props")).hasValue("1,2");
+    assertThat(logTester.logs(LoggerLevel.WARN))
+      .contains(
+        "Access to the multi-values/property set property 'props' should be made using 'getStringArray' method. The SonarQube plugin using this property should be updated.");
+
+    logTester.clear();
+
+    assertThat(config.getStringArray("props")).containsExactly("1", "2");
     assertThat(logTester.logs(LoggerLevel.WARN)).isEmpty();
   }
 
