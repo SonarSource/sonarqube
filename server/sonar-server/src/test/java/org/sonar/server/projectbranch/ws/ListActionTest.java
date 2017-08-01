4c7c001d362d1cd45ab8173d390f63e27312695a
Simon Brandhof <simon.brandhof@sonarsource.com>
2017-08-01 23:18:43 +0200

    SONAR-9616 add WS api/project_branches/list

---
+++ server/sonar-server/src/test/java/org/sonar/server/projectbranch/ws/ListActionTest.java
@@ -0,0 +1,154 @@
+/*
+ * SonarQube
+ * Copyright (C) 2009-2017 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.server.projectbranch.ws;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.sonar.api.server.ws.WebService;
+import org.sonar.api.utils.System2;
+import org.sonar.api.web.UserRole;
+import org.sonar.db.DbTester;
+import org.sonar.db.RowNotFoundException;
+import org.sonar.db.component.BranchDto;
+import org.sonar.db.component.BranchKeyType;
+import org.sonar.db.component.BranchType;
+import org.sonar.db.component.ComponentDto;
+import org.sonar.db.component.ComponentTesting;
+import org.sonar.server.tester.UserSessionRule;
+import org.sonar.server.ws.WsActionTester;
+import org.sonarqube.ws.MediaTypes;
+import org.sonarqube.ws.WsBranches.ListWsResponse;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+import static org.sonar.test.JsonAssert.assertJson;
+
+public class ListActionTest {
+
+  @Rule
+  public ExpectedException expectedException = ExpectedException.none();
+
+  @Rule
+  public DbTester db = DbTester.create(System2.INSTANCE);
+
+  @Rule
+  public UserSessionRule userSession = UserSessionRule.standalone();
+
+  public WsActionTester tester = new WsActionTester(new ListAction(db.getDbClient(), userSession));
+
+  @Test
+  public void test_definition() {
+    WebService.Action definition = tester.getDef();
+    assertThat(definition.key()).isEqualTo("list");
+    assertThat(definition.isPost()).isFalse();
+    assertThat(definition.params()).extracting(WebService.Param::key).containsExactlyInAnyOrder("project");
+    assertThat(definition.since()).isEqualTo("6.6");
+  }
+
+  @Test
+  public void fail_if_missing_project_parameter() {
+    expectedException.expect(IllegalArgumentException.class);
+    expectedException.expectMessage("The 'project' parameter is missing");
+
+    tester.newRequest().execute();
+  }
+
+  @Test
+  public void fail_if_not_a_reference_on_project() {
+    ComponentDto project = db.components().insertPrivateProject();
+    ComponentDto file = db.components().insertComponent(ComponentTesting.newFileDto(project));
+    userSession.logIn().addProjectPermission(UserRole.USER, project);
+
+    expectedException.expect(IllegalArgumentException.class);
+    expectedException.expectMessage("Invalid project key");
+
+    tester.newRequest()
+      .setParam("project", file.getDbKey())
+      .execute();
+  }
+
+  @Test
+  public void fail_if_project_does_not_exist() {
+    expectedException.expect(RowNotFoundException.class);
+    expectedException.expectMessage("Component key 'foo' not found");
+
+    tester.newRequest()
+      .setParam("project", "foo")
+      .execute();
+  }
+
+  @Test
+  public void test_project_with_zero_branches() {
+    ComponentDto project = db.components().insertPrivateProject();
+    userSession.logIn().addProjectPermission(UserRole.USER, project);
+
+    String json = tester.newRequest()
+      .setParam("project", project.getDbKey())
+      .setMediaType(MediaTypes.JSON)
+      .execute()
+      .getInput();
+    assertJson(json).isSimilarTo("{\"branches\": []}");
+  }
+
+  @Test
+  public void test_project_with_branches() {
+    ComponentDto project = db.components().insertPrivateProject();
+    insertBranch(project, "feature/bar");
+    insertBranch(project, "feature/foo");
+    userSession.logIn().addProjectPermission(UserRole.USER, project);
+
+    ListWsResponse response = tester.newRequest()
+      .setParam("project", project.getDbKey())
+      .executeProtobuf(ListWsResponse.class);
+
+    assertThat(response.getBranchesList())
+      .extracting(ListWsResponse.Branch::getName, ListWsResponse.Branch::getType)
+      .containsExactlyInAnyOrder(
+        tuple("feature/foo", ListWsResponse.BranchType.LONG), tuple("feature/bar", ListWsResponse.BranchType.LONG));
+  }
+
+  @Test
+  public void test_example() {
+    ComponentDto project = db.components().insertPrivateProject();
+    insertBranch(project, "feature/bar");
+    insertBranch(project, "feature/foo");
+    userSession.logIn().addProjectPermission(UserRole.USER, project);
+
+    String json = tester.newRequest()
+      .setParam("project", project.getDbKey())
+      .execute()
+      .getInput();
+
+    assertJson(json).isSimilarTo(tester.getDef().responseExampleAsString());
+  }
+
+  private void insertBranch(ComponentDto project, String branchName) {
+    ComponentDto branch = db.components().insertProjectBranch(project, branchName);
+    BranchDto branchDto = new BranchDto();
+    branchDto.setUuid(branch.uuid());
+    branchDto.setProjectUuid(project.uuid());
+    branchDto.setBranchType(BranchType.LONG);
+    branchDto.setKeeType(BranchKeyType.BRANCH);
+    branchDto.setKey(branchName);
+    db.getDbClient().branchDao().insert(db.getSession(), branchDto);
+    db.commit();
+  }
+}
