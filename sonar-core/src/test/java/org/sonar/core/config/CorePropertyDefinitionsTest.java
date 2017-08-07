f60c94e26af631f5c75ce6dca04b46db090af070
Janos Gyerik <janos.gyerik@sonarsource.com>
2017-08-07 13:38:09 +0200

    Expect +1 core property def

--- sonar-core/src/test/java/org/sonar/core/config/CorePropertyDefinitionsTest.java
+++ sonar-core/src/test/java/org/sonar/core/config/CorePropertyDefinitionsTest.java
@@ -33,7 +33,7 @@ public class CorePropertyDefinitionsTest {
   @Test
   public void all() {
     List<PropertyDefinition> defs = CorePropertyDefinitions.all();
-    assertThat(defs).hasSize(60);
+    assertThat(defs).hasSize(62);
   }
 
   @Test
