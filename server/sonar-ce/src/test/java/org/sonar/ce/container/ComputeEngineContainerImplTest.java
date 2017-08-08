1c3c6cbeedcb073f5d34b4fefcced6619de4d83c
Duarte Meneses <duarte.meneses@sonarsource.com>
2017-08-08 09:48:06 +0200

    SONAR-9674 Load branch information from scanner report

--- server/sonar-ce/src/test/java/org/sonar/ce/container/ComputeEngineContainerImplTest.java
+++ server/sonar-ce/src/test/java/org/sonar/ce/container/ComputeEngineContainerImplTest.java
@@ -137,7 +137,7 @@ public class ComputeEngineContainerImplTest {
         + 25 // level 1
         + 48 // content of DaoModule
         + 3 // content of EsSearchModule
-        + 61 // content of CorePropertyDefinitions
+        + 60 // content of CorePropertyDefinitions
     );
     assertThat(
       picoContainer.getComponentAdapters().stream()
