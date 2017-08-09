0eebdbd1def37688a571f4ab5b84e9a229b9f5a7
Grégoire Aubert <gregoire.aubert@sonarsource.com>
2017-08-09 09:56:42 +0200

    SONAR-9608 Fix some issues with Applications and Portfolios on measures page

--- server/sonar-web/src/main/js/apps/component-measures/components/App.js
+++ server/sonar-web/src/main/js/apps/component-measures/components/App.js
@@ -179,6 +179,7 @@ export default class App extends React.PureComponent {
             domain={query.metric}
             leakPeriod={leakPeriod}
             metrics={metrics}
+            router={this.props.router}
             selected={query.selected}
             updateQuery={this.updateQuery}
           />}
