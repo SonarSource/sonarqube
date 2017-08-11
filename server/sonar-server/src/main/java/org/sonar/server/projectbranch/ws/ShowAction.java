50328a09b14418b47232bb75665040fc755d6b58
Julien Lancelot <julien.lancelot@sonarsource.com>
2017-08-11 19:37:42 +0200

    SONAR-9616 Create api/projectbranches/show

--- server/sonar-server/src/main/java/org/sonar/server/projectbranch/ws/ShowAction.java
+++ server/sonar-server/src/main/java/org/sonar/server/projectbranch/ws/ShowAction.java
@@ -22,9 +22,11 @@ package org.sonar.server.projectbranch.ws;
 import com.google.common.collect.Multimap;
 import com.google.common.io.Resources;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
+import java.util.Optional;
+import javax.annotation.Nullable;
 import org.sonar.api.server.ws.Request;
 import org.sonar.api.server.ws.Response;
 import org.sonar.api.server.ws.WebService;
@@ -32,115 +34,119 @@ import org.sonar.api.web.UserRole;
 import org.sonar.db.DbClient;
 import org.sonar.db.DbSession;
 import org.sonar.db.component.BranchDto;
-import org.sonar.db.component.BranchKeyType;
 import org.sonar.db.component.ComponentDto;
 import org.sonar.db.measure.MeasureDto;
 import org.sonar.db.metric.MetricDto;
+import org.sonar.server.component.ComponentFinder;
 import org.sonar.server.user.UserSession;
 import org.sonar.server.ws.WsUtils;
 import org.sonarqube.ws.WsBranches;
 import org.sonarqube.ws.WsBranches.Branch.Status;
+import org.sonarqube.ws.WsBranches.ShowWsResponse;
 
-import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkState;
 import static java.util.Arrays.asList;
 import static org.sonar.api.measures.CoreMetrics.ALERT_STATUS_KEY;
 import static org.sonar.api.measures.CoreMetrics.BUGS_KEY;
 import static org.sonar.api.measures.CoreMetrics.CODE_SMELLS_KEY;
 import static org.sonar.api.measures.CoreMetrics.VULNERABILITIES_KEY;
-import static org.sonar.api.resources.Qualifiers.PROJECT;
 import static org.sonar.core.util.Protobuf.setNullable;
 import static org.sonar.core.util.stream.MoreCollectors.index;
-import static org.sonar.core.util.stream.MoreCollectors.toList;
 import static org.sonar.core.util.stream.MoreCollectors.uniqueIndex;
 import static org.sonar.db.component.BranchType.LONG;
 import static org.sonar.db.component.BranchType.SHORT;
-import static org.sonar.server.ws.KeyExamples.KEY_PROJECT_EXAMPLE_001;
-import static org.sonarqube.ws.client.projectbranches.ProjectBranchesParameters.ACTION_LIST;
-import static org.sonarqube.ws.client.projectbranches.ProjectBranchesParameters.PARAM_PROJECT;
+import static org.sonar.server.ws.KeyExamples.KEY_BRANCH_EXAMPLE_001;
+import static org.sonar.server.ws.KeyExamples.KEY_FILE_EXAMPLE_001;
+import static org.sonarqube.ws.client.projectbranches.ProjectBranchesParameters.ACTION_SHOW;
+import static org.sonarqube.ws.client.projectbranches.ProjectBranchesParameters.PARAM_BRANCH;
+import static org.sonarqube.ws.client.projectbranches.ProjectBranchesParameters.PARAM_COMPONENT;
 
-public class ListAction implements BranchWsAction {
+public class ShowAction implements BranchWsAction {
 
   private final DbClient dbClient;
   private final UserSession userSession;
+  private final ComponentFinder componentFinder;
 
-  public ListAction(DbClient dbClient, UserSession userSession) {
+  public ShowAction(DbClient dbClient, UserSession userSession, ComponentFinder componentFinder) {
     this.dbClient = dbClient;
     this.userSession = userSession;
+    this.componentFinder = componentFinder;
   }
 
   @Override
   public void define(WebService.NewController context) {
-    WebService.NewAction action = context.createAction(ACTION_LIST)
+    WebService.NewAction action = context.createAction(ACTION_SHOW)
       .setSince("6.6")
-      .setDescription("List the branches of a project")
-      .setResponseExample(Resources.getResource(getClass(), "list-example.json"))
+      .setDescription("Show branch information of a project")
+      .setResponseExample(Resources.getResource(getClass(), "show-example.json"))
       .setInternal(true)
       .setHandler(this);
 
     action
-      .createParam(PARAM_PROJECT)
-      .setDescription("Project key")
-      .setExampleValue(KEY_PROJECT_EXAMPLE_001)
+      .createParam(PARAM_COMPONENT)
+      .setDescription("Component key")
+      .setExampleValue(KEY_FILE_EXAMPLE_001)
+      .setRequired(true);
+
+    action
+      .createParam(PARAM_BRANCH)
+      .setDescription("Branch key")
+      .setExampleValue(KEY_BRANCH_EXAMPLE_001)
       .setRequired(true);
   }
 
   @Override
   public void handle(Request request, Response response) throws Exception {
-    String projectKey = request.mandatoryParam(PARAM_PROJECT);
+    String projectKey = request.mandatoryParam(PARAM_COMPONENT);
+    String branchName = request.mandatoryParam(PARAM_BRANCH);
 
     try (DbSession dbSession = dbClient.openSession(false)) {
-      ComponentDto project = WsUtils.checkFoundWithOptional(
-        dbClient.componentDao().selectByKey(dbSession, projectKey),
-        "Project key '%s' not found", projectKey);
-
-      userSession.checkComponentPermission(UserRole.USER, project);
-      checkArgument(project.isEnabled() && PROJECT.equals(project.qualifier()), "Invalid project key");
+      ComponentDto component = componentFinder.getByKeyAndBranch(dbSession, projectKey, branchName);
+      userSession.checkComponentPermission(UserRole.USER, component);
+      ComponentDto project = componentFinder.getByUuid(dbSession, component.projectUuid());
 
       List<MetricDto> metrics = dbClient.metricDao().selectByKeys(dbSession, asList(ALERT_STATUS_KEY, BUGS_KEY, VULNERABILITIES_KEY, CODE_SMELLS_KEY));
       Map<Integer, MetricDto> metricsById = metrics.stream().collect(uniqueIndex(MetricDto::getId));
       Map<String, Integer> metricIdsByKey = metrics.stream().collect(uniqueIndex(MetricDto::getKey, MetricDto::getId));
 
-      Collection<BranchDto> branches = dbClient.branchDao().selectByComponent(dbSession, project);
-      Map<String, BranchDto> mergeBranchesByUuid = dbClient.branchDao()
-        .selectByUuids(dbSession, branches.stream().map(BranchDto::getMergeBranchUuid).filter(Objects::nonNull).collect(toList()))
-        .stream().collect(uniqueIndex(BranchDto::getUuid));
+      BranchDto branch = getBranch(dbSession, component.projectUuid());
+      String mergeBranchUuid = branch.getMergeBranchUuid();
+      BranchDto mergeBranch = mergeBranchUuid == null ? null : getBranch(dbSession, mergeBranchUuid);
+
       Multimap<String, MeasureDto> measuresByComponentUuids = dbClient.measureDao()
-        .selectByComponentsAndMetrics(dbSession, branches.stream().map(BranchDto::getUuid).collect(toList()), metricsById.keySet())
+        .selectByComponentsAndMetrics(dbSession, Collections.singletonList(branch.getUuid()), metricsById.keySet())
         .stream().collect(index(MeasureDto::getComponentUuid));
 
-      WsBranches.ListWsResponse.Builder protobufResponse = WsBranches.ListWsResponse.newBuilder();
-      branches.stream()
-        .filter(b -> b.getKeeType().equals(BranchKeyType.BRANCH))
-        .forEach(b -> addToProtobuf(protobufResponse, b, project, mergeBranchesByUuid, metricIdsByKey, measuresByComponentUuids));
-      WsUtils.writeProtobuf(protobufResponse.build(), request, response);
+      WsUtils.writeProtobuf(buildResponse(branch, project, mergeBranch, metricIdsByKey, measuresByComponentUuids), request, response);
     }
   }
 
-  private static void addToProtobuf(WsBranches.ListWsResponse.Builder response, BranchDto branch, ComponentDto project, Map<String, BranchDto> mergeBranchesByUuid,
+  private BranchDto getBranch(DbSession dbSession, String uuid) {
+    Optional<BranchDto> branch = dbClient.branchDao().selectByUuid(dbSession, uuid);
+    checkState(branch != null, "Branch uuid '%s' not found", uuid);
+    return branch.get();
+  }
+
+  private static ShowWsResponse buildResponse(BranchDto branch, ComponentDto project, @Nullable BranchDto mergeBranch,
     Map<String, Integer> metricIdsByKey, Multimap<String, MeasureDto> measuresByComponentUuids) {
-    WsBranches.Branch.Builder builder = response.addBranchesBuilder();
+    WsBranches.Branch.Builder builder = WsBranches.Branch.newBuilder();
     setNullable(branch.getKey(), builder::setName);
     builder.setProject(project.getKey());
     builder.setIsMain(branch.isMain());
     builder.setType(WsBranches.Branch.BranchType.valueOf(branch.getBranchType().name()));
-    String mergeBranchUuid = branch.getMergeBranchUuid();
-    if (mergeBranchUuid != null) {
-      BranchDto mergeBranch = mergeBranchesByUuid.get(mergeBranchUuid);
-      checkState(mergeBranch != null, "Component uuid '%s' cannot be found", mergeBranch);
+    if (mergeBranch != null) {
       setNullable(mergeBranch.getKey(), builder::setMergeBranch);
     }
 
+    Status.Builder statusBuilder = Status.newBuilder();
     Collection<MeasureDto> componentMeasures = measuresByComponentUuids.get(branch.getUuid());
     if (branch.getBranchType().equals(LONG)) {
-      Status.Builder statusBuilder = Status.newBuilder();
       int qualityGateStatusMetricId = metricIdsByKey.get(ALERT_STATUS_KEY);
       componentMeasures.stream().filter(m -> m.getMetricId() == qualityGateStatusMetricId).findAny()
-        .ifPresent(measure -> builder.setStatus(statusBuilder.setQualityGateStatus(measure.getData())));
+        .ifPresent(measure -> statusBuilder.setQualityGateStatus(measure.getData()));
     }
 
     if (branch.getBranchType().equals(SHORT)) {
-      Status.Builder statusBuilder = Status.newBuilder();
       int bugsMetricId = metricIdsByKey.get(BUGS_KEY);
       componentMeasures.stream().filter(m -> m.getMetricId() == bugsMetricId).findAny()
         .ifPresent(measure -> statusBuilder.setBugs(measure.getValue().intValue()));
@@ -152,9 +158,10 @@ public class ListAction implements BranchWsAction {
       int codeSmellMetricId = metricIdsByKey.get(CODE_SMELLS_KEY);
       componentMeasures.stream().filter(m -> m.getMetricId() == codeSmellMetricId).findAny()
         .ifPresent(measure -> statusBuilder.setCodeSmells(measure.getValue().intValue()));
-      builder.setStatus(statusBuilder);
     }
-    builder.build();
+
+    builder.setStatus(statusBuilder);
+    return ShowWsResponse.newBuilder().setBranch(builder).build();
   }
 
 }
