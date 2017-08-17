fd1e079dc9e49c7871c1a33078561f961d162dcc
Stas Vilchik <stas.vilchik@sonarsource.com>
2017-08-17 14:40:19 +0200

    refactor ProjectPageExtension

--- server/sonar-web/src/main/js/app/components/extensions/ExtensionNotFound.tsx
+++ server/sonar-web/src/main/js/app/components/extensions/ExtensionNotFound.tsx
@@ -17,8 +17,7 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-// @flow
-import React from 'react';
+import * as React from 'react';
 import { Link } from 'react-router';
 
 export default class ExtensionNotFound extends React.PureComponent {
