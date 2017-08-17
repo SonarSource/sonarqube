fae038c5e6c24dc805eb635b6efcd5bdca7ba615
Simon Brandhof <simon.brandhof@sonarsource.com>
2017-08-17 14:24:07 +0200

    DO NOT MERGE

--- travis.sh
+++ travis.sh
@@ -128,59 +128,10 @@ BUILD)
   export MAVEN_OPTS="-Xmx1G -Xms128m"
   MAVEN_ARGS="-T 1C -Dmaven.test.redirectTestOutputToFile=false -Dsurefire.useFile=false -B -e -V -DbuildVersion=$BUILD_VERSION -Dtests.es.logger.level=WARN"
 
-  if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-    echo 'Build and analyze master'
-
-    # Fetch all commit history so that SonarQube has exact blame information
-    # for issue auto-assignment
-    # This command can fail with "fatal: --unshallow on a complete repository does not make sense"
-    # if there are not enough commits in the Git repository (even if Travis executed git clone --depth 50).
-    # For this reason errors are ignored with "|| true"
-    git fetch --unshallow || true
-
-    mvn org.jacoco:jacoco-maven-plugin:prepare-agent deploy \
+  mvn deploy \
           $MAVEN_ARGS \
-          -Pdeploy-sonarsource,release
-    if [[ "$TRAVIS_BUILD_NUMBER" == *0 ]]; then
-      mvn sonar:sonar \
-          -Dsonar.host.url=$SONAR_HOST_URL \
-          -Dsonar.login=$SONAR_TOKEN \
-          -Dsonar.projectVersion=$INITIAL_VERSION
-    else
-      mvn sonar:sonar \
-          -Dsonar.incremental=true \
-          -Dsonar.host.url=$SONAR_HOST_URL \
-          -Dsonar.login=$SONAR_TOKEN \
-          -Dsonar.projectVersion=$INITIAL_VERSION
-    fi
+          -Pdeploy-sonarsource -DskipTests
 
-  elif [[ "$TRAVIS_BRANCH" == "branch-"* ]] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-    echo 'Build release branch'
-
-    mvn deploy $MAVEN_ARGS -Pdeploy-sonarsource,release
-
-  elif [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ -n "${GITHUB_TOKEN:-}" ]; then
-    echo 'Build and analyze internal pull request'
-
-    mvn org.jacoco:jacoco-maven-plugin:prepare-agent deploy \
-        $MAVEN_ARGS \
-        -Dsource.skip=true \
-        -Pdeploy-sonarsource
-    mvn sonar:sonar \
-        -Dsonar.analysis.mode=preview \
-        -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
-        -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
-        -Dsonar.github.oauth=$GITHUB_TOKEN \
-        -Dsonar.host.url=$SONAR_HOST_URL \
-        -Dsonar.login=$SONAR_TOKEN
-
-  else
-    echo 'Build feature branch or external pull request'
-
-    mvn install $MAVEN_ARGS -Dsource.skip=true
-  fi
-
-  ./run-integration-tests.sh "Lite" ""
   ;;
 
 WEB_TESTS)
