fcafbdc178f70dd1d0c822be753a075eae66bc07
Stas Vilchik <stas.vilchik@sonarsource.com>
2017-08-22 10:10:56 +0200

    csv

--- server/sonar-web/src/main/js/helpers/csv.ts
+++ server/sonar-web/src/main/js/helpers/csv.ts
@@ -17,8 +17,7 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-// @flow
-export function csvEscape(value /*: string */) /*: string */ {
+export function csvEscape(value: string): string {
   const escaped = value.replace(/"/g, '\\"');
   return `"${escaped}"`;
 }
