668a9839b6103f1efe3282179c52db3348702062
Julien Lancelot <julien.lancelot@sonarsource.com>
2017-08-23 15:19:55 +0200

    SONAR-9616 Add IT on api/project_branches/list

--- tests/src/test/java/org/sonarqube/tests/Category2Suite.java
+++ tests/src/test/java/org/sonarqube/tests/Category2Suite.java
@@ -23,6 +23,7 @@ import com.sonar.orchestrator.Orchestrator;
 import org.junit.ClassRule;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+import org.sonarqube.tests.branch.BranchTest;
 import org.sonarqube.tests.issue.AutoAssignTest;
 import org.sonarqube.tests.issue.CommonRulesTest;
 import org.sonarqube.tests.issue.CustomRulesTest;
@@ -97,7 +98,9 @@ import static util.ItUtils.xooPlugin;
   TechnicalDebtMeasureVariationTest.class,
   TechnicalDebtTest.class,
   // ui
-  IssuesPageTest.class
+  IssuesPageTest.class,
+  // branch
+  BranchTest.class
 })
 public class Category2Suite {
 
