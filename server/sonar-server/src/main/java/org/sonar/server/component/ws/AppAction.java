5ee9d5749de682b11e3530d1ebf88a6391e2587b
Julien Lancelot <julien.lancelot@sonarsource.com>
2017-08-24 13:59:12 +0200

    SONAR-9616 Sanitize AppAction
    - Remove not used "period" parameter
    - Remove not used SQALE_RATING_KEY, SQALE_DEBT_RATIO_KEY and TECHNICAL_DEBT_KEY metrics

--- server/sonar-server/src/main/java/org/sonar/server/component/ws/AppAction.java
+++ server/sonar-server/src/main/java/org/sonar/server/component/ws/AppAction.java
@@ -19,6 +19,7 @@
  */
 package org.sonar.server.component.ws;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Maps;
 import java.util.List;
 import java.util.Map;
@@ -27,7 +28,6 @@ import java.util.stream.Collectors;
 import javax.annotation.CheckForNull;
 import javax.annotation.Nullable;
 import org.apache.commons.lang.BooleanUtils;
-import org.sonar.api.measures.CoreMetrics;
 import org.sonar.api.measures.Metric;
 import org.sonar.api.server.ws.Request;
 import org.sonar.api.server.ws.Response;
@@ -46,18 +46,35 @@ import org.sonar.server.component.ComponentFinder;
 import org.sonar.server.component.ComponentFinder.ParamNames;
 import org.sonar.server.user.UserSession;
 
-import static com.google.common.collect.Lists.newArrayList;
+import static com.google.common.base.Preconditions.checkArgument;
+import static org.sonar.api.measures.CoreMetrics.COVERAGE;
+import static org.sonar.api.measures.CoreMetrics.COVERAGE_KEY;
+import static org.sonar.api.measures.CoreMetrics.DUPLICATED_LINES_DENSITY;
+import static org.sonar.api.measures.CoreMetrics.DUPLICATED_LINES_DENSITY_KEY;
+import static org.sonar.api.measures.CoreMetrics.LINES;
+import static org.sonar.api.measures.CoreMetrics.LINES_KEY;
+import static org.sonar.api.measures.CoreMetrics.TESTS;
+import static org.sonar.api.measures.CoreMetrics.TESTS_KEY;
+import static org.sonar.api.measures.CoreMetrics.VIOLATIONS;
+import static org.sonar.api.measures.CoreMetrics.VIOLATIONS_KEY;
 import static org.sonar.core.util.Uuids.UUID_EXAMPLE_01;
+import static org.sonar.server.component.ComponentFinder.ParamNames.*;
+import static org.sonar.server.component.ComponentFinder.ParamNames.COMPONENT_ID_AND_COMPONENT;
+import static org.sonar.server.ws.KeyExamples.KEY_BRANCH_EXAMPLE_001;
 import static org.sonar.server.ws.KeyExamples.KEY_PROJECT_EXAMPLE_001;
+import static org.sonarqube.ws.client.measure.MeasuresWsParameters.PARAM_BRANCH;
 
 public class AppAction implements ComponentsWsAction {
 
   private static final String PARAM_COMPONENT_ID = "componentId";
   private static final String PARAM_COMPONENT = "component";
   private static final String PARAM_PERIOD = "period";
-  static final List<String> METRIC_KEYS = newArrayList(CoreMetrics.LINES_KEY, CoreMetrics.VIOLATIONS_KEY,
-    CoreMetrics.COVERAGE_KEY, CoreMetrics.DUPLICATED_LINES_DENSITY_KEY, CoreMetrics.TESTS_KEY,
-    CoreMetrics.TECHNICAL_DEBT_KEY, CoreMetrics.SQALE_RATING_KEY, CoreMetrics.SQALE_DEBT_RATIO_KEY);
+  private static final List<String> METRIC_KEYS = ImmutableList.of(
+    LINES_KEY,
+    VIOLATIONS_KEY,
+    COVERAGE_KEY,
+    DUPLICATED_LINES_DENSITY_KEY,
+    TESTS_KEY);
 
   private final DbClient dbClient;
 
@@ -91,12 +108,6 @@ public class AppAction implements ComponentsWsAction {
       .setDescription("Component key")
       .setExampleValue(KEY_PROJECT_EXAMPLE_001)
       .setSince("6.4");
-
-    action
-      .createParam(PARAM_PERIOD)
-      .setDescription("User leak Period in order to get differential measures")
-      .setDeprecatedSince("6.4")
-      .setPossibleValues(1);
   }
 
   @Override
@@ -105,14 +116,14 @@ public class AppAction implements ComponentsWsAction {
       ComponentDto component = componentFinder.getByUuidOrKey(session,
         request.param(PARAM_COMPONENT_ID),
         request.param(PARAM_COMPONENT),
-        ParamNames.COMPONENT_ID_AND_COMPONENT);
+        COMPONENT_ID_AND_COMPONENT);
       userSession.checkComponentPermission(UserRole.USER, component);
 
       JsonWriter json = response.newJsonWriter();
       json.beginObject();
       Map<String, MeasureDto> measuresByMetricKey = measuresByMetricKey(component, session);
       appendComponent(json, component, userSession, session);
-      appendPermissions(json, component, userSession);
+      appendPermissions(json, userSession);
       appendMeasures(json, measuresByMetricKey);
       json.endObject();
       json.close();
@@ -128,7 +139,7 @@ public class AppAction implements ComponentsWsAction {
       session);
     boolean isFavourite = propertyDtos.size() == 1;
 
-    json.prop("key", component.getDbKey());
+    json.prop("key", component.getKey());
     json.prop("uuid", component.uuid());
     json.prop("path", component.path());
     json.prop("name", component.name());
@@ -140,26 +151,29 @@ public class AppAction implements ComponentsWsAction {
 
     // Do not display parent project if parent project and project are the same
     boolean displayParentProject = parentProject != null && !parentProject.uuid().equals(project.uuid());
-    json.prop("subProject", displayParentProject ? parentProject.getDbKey() : null);
+    json.prop("subProject", displayParentProject ? parentProject.getKey() : null);
     json.prop("subProjectName", displayParentProject ? parentProject.longName() : null);
-    json.prop("project", project.getDbKey());
+    json.prop("project", project.getKey());
     json.prop("projectName", project.longName());
+    String branch = project.getBranch();
+    if (branch != null) {
+      json.prop("branch", branch);
+    }
 
     json.prop("fav", isFavourite);
   }
 
-  private static void appendPermissions(JsonWriter json, ComponentDto component, UserSession userSession) {
-    boolean hasBrowsePermission = userSession.hasComponentPermission(UserRole.USER, component);
-    json.prop("canMarkAsFavorite", userSession.isLoggedIn() && hasBrowsePermission);
+  private static void appendPermissions(JsonWriter json, UserSession userSession) {
+    json.prop("canMarkAsFavorite", userSession.isLoggedIn());
   }
 
   private static void appendMeasures(JsonWriter json, Map<String, MeasureDto> measuresByMetricKey) {
     json.name("measures").beginObject();
-    json.prop("lines", formatMeasure(measuresByMetricKey, CoreMetrics.LINES));
-    json.prop("coverage", formatCoverageMeasure(measuresByMetricKey));
-    json.prop("duplicationDensity", formatMeasure(measuresByMetricKey, CoreMetrics.DUPLICATED_LINES_DENSITY));
-    json.prop("issues", formatMeasure(measuresByMetricKey, CoreMetrics.VIOLATIONS));
-    json.prop("tests", formatMeasure(measuresByMetricKey, CoreMetrics.TESTS));
+    json.prop("lines", formatMeasure(measuresByMetricKey, LINES));
+    json.prop("coverage", formatMeasure(measuresByMetricKey, COVERAGE));
+    json.prop("duplicationDensity", formatMeasure(measuresByMetricKey, DUPLICATED_LINES_DENSITY));
+    json.prop("issues", formatMeasure(measuresByMetricKey, VIOLATIONS));
+    json.prop("tests", formatMeasure(measuresByMetricKey, TESTS));
     json.endObject();
   }
 
@@ -178,12 +192,6 @@ public class AppAction implements ComponentsWsAction {
       return null;
     }
     return dbClient.componentDao().selectOrFailByUuid(session, componentDto.getRootUuid());
-  }
-
-  @CheckForNull
-  private static String formatCoverageMeasure(Map<String, MeasureDto> measuresByMetricKey) {
-    MeasureDto coverage = measuresByMetricKey.get(CoreMetrics.COVERAGE_KEY);
-    return formatMeasure(coverage, CoreMetrics.COVERAGE);
   }
 
   @CheckForNull
