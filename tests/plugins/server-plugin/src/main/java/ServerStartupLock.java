d42dbb96cbcefb6f631fc9ddb52ec8755e26f19e
Simon Brandhof <simon.brandhof@sonarsource.com>
2017-08-28 13:24:15 +0200

    SONAR-9739 improve integration tests of api/system/status

---
+++ tests/plugins/server-plugin/src/main/java/ServerStartupLock.java
@@ -0,0 +1,61 @@
+/*
+ * SonarQube
+ * Copyright (C) 2009-2017 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+import java.io.File;
+import java.util.Optional;
+import org.sonar.api.Startable;
+import org.sonar.api.config.Configuration;
+import org.sonar.api.server.ServerSide;
+import org.sonar.api.utils.log.Logger;
+import org.sonar.api.utils.log.Loggers;
+
+@ServerSide
+public class ServerStartupLock implements Startable {
+
+  private final Configuration configuration;
+  private static final Logger LOGGER = Loggers.get(ServerStartupLock.class);
+
+  public ServerStartupLock(Configuration configuration) {
+    this.configuration = configuration;
+  }
+
+  @Override
+  public void start() {
+    Optional<String> path = configuration.get("sonar.test.serverStartupLock.path");
+    if (path.isPresent()) {
+      File lock = new File(path.get());
+      try {
+        while (lock.exists()) {
+          LOGGER.info("ServerStartupLock - Waiting for file to be deleted: " + lock.getAbsolutePath());
+          Thread.sleep(100L);
+        }
+        LOGGER.info("ServerStartupLock - File deleted. Resuming startup.");
+      } catch (InterruptedException e) {
+        LOGGER.info("ServerStartupLock - interrupted");
+        Thread.currentThread().interrupt();
+      }
+    }
+  }
+
+  @Override
+  public void stop() {
+    // nothing to do
+  }
+}
