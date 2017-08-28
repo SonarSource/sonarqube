93e75a9602ba57fb1b9f2d7622ca6c1ce0b7c4e4
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2017-08-28 15:08:58 +0200

    SONAR-9739 drop authentication on api/system/health

--- server/sonar-server/src/main/java/org/sonar/server/platform/ws/SafeModeHealthActionModule.java
+++ server/sonar-server/src/main/java/org/sonar/server/platform/ws/SafeModeHealthActionModule.java
@@ -35,6 +35,6 @@ public class SafeModeHealthActionModule extends Module {
       EsStatusCheck.class,
 
       HealthCheckerImpl.class,
-      SafeModeHealthAction.class);
+      HealthAction.class);
   }
 }
