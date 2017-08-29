8dd65a8144d9271af7c6184a503404a7dd12a01c
Julien Lancelot <julien.lancelot@sonarsource.com>
2017-08-29 12:07:16 +0200

    SONAR-9616 Use single ES query in api/project_branches/list

--- server/sonar-server/src/main/java/org/sonar/server/projectbranch/ws/ListAction.java
+++ server/sonar-server/src/main/java/org/sonar/server/projectbranch/ws/ListAction.java
@@ -24,8 +24,8 @@ import java.util.Collection;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.function.Function;
 import javax.annotation.Nullable;
-import org.elasticsearch.action.search.SearchResponse;
 import org.sonar.api.server.ws.Request;
 import org.sonar.api.server.ws.Response;
 import org.sonar.api.server.ws.WebService;
@@ -39,9 +39,7 @@ import org.sonar.db.component.ComponentDto;
 import org.sonar.db.measure.MeasureDto;
 import org.sonar.db.metric.MetricDto;
 import org.sonar.server.component.ComponentFinder;
-import org.sonar.server.es.Facets;
-import org.sonar.server.es.SearchOptions;
-import org.sonar.server.issue.IssueQuery;
+import org.sonar.server.issue.index.BranchStatistics;
 import org.sonar.server.issue.index.IssueIndex;
 import org.sonar.server.user.UserSession;
 import org.sonar.server.ws.WsUtils;
@@ -57,11 +55,7 @@ import static org.sonar.core.util.stream.MoreCollectors.toList;
 import static org.sonar.core.util.stream.MoreCollectors.uniqueIndex;
 import static org.sonar.db.component.BranchType.LONG;
 import static org.sonar.db.component.BranchType.SHORT;
-import static org.sonar.server.rule.index.RuleIndex.FACET_TYPES;
 import static org.sonar.server.ws.KeyExamples.KEY_PROJECT_EXAMPLE_001;
-import static org.sonarqube.ws.Common.RuleType.BUG;
-import static org.sonarqube.ws.Common.RuleType.CODE_SMELL;
-import static org.sonarqube.ws.Common.RuleType.VULNERABILITY;
 import static org.sonarqube.ws.client.projectbranches.ProjectBranchesParameters.ACTION_LIST;
 import static org.sonarqube.ws.client.projectbranches.ProjectBranchesParameters.PARAM_PROJECT;
 
@@ -112,19 +106,24 @@ public class ListAction implements BranchWsAction {
       Map<String, MeasureDto> qualityGateMeasuresByComponentUuids = dbClient.measureDao()
         .selectByComponentsAndMetrics(dbSession, branches.stream().map(BranchDto::getUuid).collect(toList()), singletonList(qualityGateMetric.getId()))
         .stream().collect(uniqueIndex(MeasureDto::getComponentUuid));
+      Map<String, BranchStatistics> branchStatisticsByBranchUuid = issueIndex.searchBranchStatistics(branches.stream()
+        .filter(b -> b.getBranchType().equals(SHORT))
+        .map(BranchDto::getUuid).collect(toList()))
+        .stream().collect(uniqueIndex(BranchStatistics::getBranchUuid, Function.identity()));
 
       WsBranches.ListWsResponse.Builder protobufResponse = WsBranches.ListWsResponse.newBuilder();
       branches.stream()
         .filter(b -> b.getKeeType().equals(BranchKeyType.BRANCH))
-        .forEach(b -> addBranch(protobufResponse, b, mergeBranchesByUuid, qualityGateMeasuresByComponentUuids.get(b.getUuid())));
+        .forEach(b -> addBranch(protobufResponse, b, mergeBranchesByUuid, qualityGateMeasuresByComponentUuids.get(b.getUuid()), branchStatisticsByBranchUuid.get(b.getUuid())));
       WsUtils.writeProtobuf(protobufResponse.build(), request, response);
     }
   }
 
-  private void addBranch(WsBranches.ListWsResponse.Builder response, BranchDto branch, Map<String, BranchDto> mergeBranchesByUuid, @Nullable MeasureDto qualityGateMeasure) {
+  private static void addBranch(WsBranches.ListWsResponse.Builder response, BranchDto branch, Map<String, BranchDto> mergeBranchesByUuid, @Nullable MeasureDto qualityGateMeasure,
+    BranchStatistics branchStatistics) {
     WsBranches.Branch.Builder builder = toBranchBuilder(branch, Optional.ofNullable(mergeBranchesByUuid.get(branch.getMergeBranchUuid())));
     setLongLivingBranchStatus(builder, branch, qualityGateMeasure);
-    setShortLivingBranchStatus(builder, branch);
+    setShortLivingBranchStatus(builder, branch, branchStatistics);
     response.addBranches(builder);
   }
 
@@ -153,19 +152,14 @@ public class ListAction implements BranchWsAction {
     }
   }
 
-  private void setShortLivingBranchStatus(WsBranches.Branch.Builder builder, BranchDto branch) {
+  private static void setShortLivingBranchStatus(WsBranches.Branch.Builder builder, BranchDto branch, @Nullable BranchStatistics branchStatistics) {
     if (!branch.getBranchType().equals(BranchType.SHORT)) {
       return;
     }
-    SearchResponse searchResponse = issueIndex.search(IssueQuery.builder().branchUuid(branch.getUuid()).build(), new SearchOptions()
-      .setLimit(0)
-      .addFacets(FACET_TYPES));
-    Facets facets = new Facets(searchResponse);
-    Map<String, Long> typesFacet = Objects.requireNonNull(facets.get(FACET_TYPES), "Facet types does not exists");
     WsBranches.Branch.Status.Builder statusBuilder = WsBranches.Branch.Status.newBuilder();
-    setNullable(typesFacet.get(BUG.name()), v -> statusBuilder.setBugs(v.intValue()));
-    setNullable(typesFacet.get(VULNERABILITY.name()), v -> statusBuilder.setVulnerabilities(v.intValue()));
-    setNullable(typesFacet.get(CODE_SMELL.name()), v -> statusBuilder.setCodeSmells(v.intValue()));
+    statusBuilder.setBugs(branchStatistics == null ? 0L : branchStatistics.getBugs());
+    statusBuilder.setVulnerabilities(branchStatistics == null ? 0L : branchStatistics.getVulnerabilities());
+    statusBuilder.setCodeSmells(branchStatistics == null ? 0L : branchStatistics.getCodeSmells());
     builder.setStatus(statusBuilder);
   }
 
