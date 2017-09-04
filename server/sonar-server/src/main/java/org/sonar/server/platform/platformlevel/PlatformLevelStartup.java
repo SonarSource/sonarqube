f815bc36c21a601685d85ac9bab9256387aaae9c
Simon Brandhof <simon.brandhof@sonarsource.com>
2017-09-04 17:51:02 +0200

    Do not trigger twice the notification thread

--- server/sonar-server/src/main/java/org/sonar/server/platform/platformlevel/PlatformLevelStartup.java
+++ server/sonar-server/src/main/java/org/sonar/server/platform/platformlevel/PlatformLevelStartup.java
@@ -21,7 +21,6 @@ package org.sonar.server.platform.platformlevel;
 
 import org.sonar.server.app.ProcessCommandWrapper;
 import org.sonar.server.es.IndexerStartupTask;
-import org.sonar.server.notification.NotificationModule;
 import org.sonar.server.organization.DefaultOrganizationEnforcer;
 import org.sonar.server.platform.ServerLifecycleNotifier;
 import org.sonar.server.platform.web.RegisterServletFilters;
@@ -30,8 +29,8 @@ import org.sonar.server.qualityprofile.BuiltInQProfileInsertImpl;
 import org.sonar.server.qualityprofile.BuiltInQProfileLoader;
 import org.sonar.server.qualityprofile.BuiltInQProfileUpdateImpl;
 import org.sonar.server.qualityprofile.BuiltInQualityProfilesNotificationDispatcher;
-import org.sonar.server.qualityprofile.BuiltInQualityProfilesUpdateListener;
 import org.sonar.server.qualityprofile.BuiltInQualityProfilesNotificationTemplate;
+import org.sonar.server.qualityprofile.BuiltInQualityProfilesUpdateListener;
 import org.sonar.server.qualityprofile.RegisterQualityProfiles;
 import org.sonar.server.rule.RegisterRules;
 import org.sonar.server.rule.WebServerRuleFinder;
@@ -62,8 +61,6 @@ public class PlatformLevelStartup extends PlatformLevel {
       RegisterRules.class);
     add(BuiltInQProfileLoader.class);
     addIfStartupLeader(
-      // TODO Should we put it in level 2 ?
-      NotificationModule.class,
       BuiltInQualityProfilesNotificationDispatcher.class,
       BuiltInQualityProfilesNotificationTemplate.class,
       BuiltInQualityProfilesUpdateListener.class,
