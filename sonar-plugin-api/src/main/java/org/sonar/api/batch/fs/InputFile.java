a19af040824349b4be8be4c6f179c2887bf97076
Julien HENRY <julien.henry@sonarsource.com>
2017-09-08 10:55:33 +0200

    Improve Javadoc

--- sonar-plugin-api/src/main/java/org/sonar/api/batch/fs/InputFile.java
+++ sonar-plugin-api/src/main/java/org/sonar/api/batch/fs/InputFile.java
@@ -64,7 +64,7 @@ public interface InputFile extends IndexedFile {
    * <code>/path/to/module/src/main/java/com/Foo.java</code>.
    * <br>
    * Relative path is not null and is normalized ('foo/../foo' is replaced by 'foo').
-   * @deprecated since 6.6 use {@link #inputStream()}, {@link #filename()} or {@link #uri()}
+   * @deprecated since 6.6 use {@link #inputStream()} for file content, {@link #filename()} for file name, {@link #uri()} for an unique identifier, and {@link #toString()} for logging
    */
   @Deprecated
   @Override
@@ -76,7 +76,7 @@ public interface InputFile extends IndexedFile {
    * This is not canonical path. Symbolic links are not resolved. For example if /project/src links
    * to /tmp/src and basedir is /project, then this method returns /project/src/index.php. Use
    * {@code file().getCanonicalPath()} to resolve symbolic link.
-   * @deprecated since 6.6 use {@link #inputStream()}, {@link #filename()} or {@link #uri()}
+   * @deprecated since 6.6 use {@link #inputStream()} for file content, {@link #filename()} for file name, {@link #uri()} for an unique identifier, and {@link #toString()} for logging
    */
   @Deprecated
   @Override
@@ -86,7 +86,7 @@ public interface InputFile extends IndexedFile {
    * The underlying absolute {@link java.io.File}. It should not be used to read the file in the filesystem.
    * @see #contents()
    * @see #inputStream()
-   * @deprecated since 6.6 use {@link #inputStream()}, {@link #filename()} or {@link #uri()}
+   * @deprecated since 6.6 use {@link #inputStream()} for file content, {@link #filename()} for file name, {@link #uri()} for an unique identifier, and {@link #toString()} for logging
    */
   @Deprecated
   @Override
@@ -98,7 +98,7 @@ public interface InputFile extends IndexedFile {
    * @see #contents()
    * @see #inputStream()
    * @since 5.1
-   * @deprecated since 6.6 use {@link #inputStream()}, {@link #filename()} or {@link #uri()}
+   * @deprecated since 6.6 use {@link #inputStream()} for file content, {@link #filename()} for file name, {@link #uri()} for an unique identifier, and {@link #toString()} for logging
    */
   @Deprecated
   @Override
@@ -197,4 +197,10 @@ public interface InputFile extends IndexedFile {
    * @since 6.0
    */
   Charset charset();
+
+  /**
+   * Return a string to identify this file (suitable for logs).
+   */
+  @Override
+  String toString();
 }
