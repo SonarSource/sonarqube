ab70de1e12e5ec126e0ee4268dd89a0fad4cc607
Janos Gyerik <janos.gyerik@sonarsource.com>
2017-09-11 11:23:27 +0200

    Add ProjectBranches and related

---
+++ sonar-scanner-engine/src/main/java/org/sonar/scanner/scan/branch/BranchInfo.java
@@ -0,0 +1,50 @@
+/*
+ * SonarQube
+ * Copyright (C) 2009-2017 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.scanner.scan.branch;
+
+import javax.annotation.concurrent.Immutable;
+
+/**
+ * Container class for information about a branch.
+ */
+@Immutable
+public class BranchInfo {
+  private final String name;
+  private final BranchType type;
+  private final boolean isMain;
+
+  public BranchInfo(String name, BranchType type, boolean isMain) {
+    this.name = name;
+    this.type = type;
+    this.isMain = isMain;
+  }
+
+  public String name() {
+    return name;
+  }
+
+  public BranchType type() {
+    return type;
+  }
+
+  public boolean isMain() {
+    return isMain;
+  }
+}
