9feaca8b325b9e4f03a542b3c8a553f441dca422
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2017-09-12 12:37:15 +0200

    SONAR-9144 support leak period when computing issue statistics
    during report processing

---
+++ server/sonar-server/src/main/java/org/sonar/server/issue/notification/DistributedMetricStatsInt.java
@@ -0,0 +1,57 @@
+/*
+ * SonarQube
+ * Copyright (C) 2009-2017 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.server.issue.notification;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class DistributedMetricStatsInt {
+  private MetricStatsInt globalStats = new MetricStatsInt();
+  private Map<String, MetricStatsInt> statsPerLabel = new HashMap<>();
+
+  DistributedMetricStatsInt increment(String label, boolean onLeak) {
+    this.globalStats.increment(onLeak);
+    statsPerLabel.computeIfAbsent(label, l -> new MetricStatsInt()).increment(onLeak);
+    return this;
+  }
+
+  Map<String, MetricStatsInt> getForLabels() {
+    return Collections.unmodifiableMap(statsPerLabel);
+  }
+
+  public Optional<MetricStatsInt> getForLabel(String label) {
+    return Optional.ofNullable(statsPerLabel.get(label));
+  }
+
+  public int getOnLeak() {
+    return globalStats.getOnLeak();
+  }
+
+  public int getOffLeak() {
+    return globalStats.getOffLeak();
+  }
+
+  public int getTotal() {
+    return globalStats.getTotal();
+  }
+
+}
--- server/sonar-server/src/main/java/org/sonar/server/issue/notification/NewIssuesStatistics.java
+++ server/sonar-server/src/main/java/org/sonar/server/issue/notification/NewIssuesStatistics.java
@@ -19,18 +19,14 @@
  */
 package org.sonar.server.issue.notification;
 
-import com.google.common.collect.HashMultiset;
-import com.google.common.collect.Multiset;
-import com.google.common.collect.Multisets;
 import java.util.EnumMap;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.function.Predicate;
 import org.sonar.api.issue.Issue;
 import org.sonar.api.rule.RuleKey;
 import org.sonar.api.utils.Duration;
 
-import static com.google.common.base.Preconditions.checkArgument;
 import static org.sonar.server.issue.notification.NewIssuesStatistics.Metric.ASSIGNEE;
 import static org.sonar.server.issue.notification.NewIssuesStatistics.Metric.COMPONENT;
 import static org.sonar.server.issue.notification.NewIssuesStatistics.Metric.RULE;
@@ -38,19 +34,21 @@ import static org.sonar.server.issue.notification.NewIssuesStatistics.Metric.SEV
 import static org.sonar.server.issue.notification.NewIssuesStatistics.Metric.TAG;
 
 public class NewIssuesStatistics {
-  private Map<String, Stats> assigneesStatistics = new LinkedHashMap<>();
-  private Stats globalStatistics = new Stats();
+  private final Predicate<Issue> onLeakPredicate;
+  private final Map<String, Stats> assigneesStatistics = new LinkedHashMap<>();
+  private final Stats globalStatistics;
+
+  public NewIssuesStatistics(Predicate<Issue> onLeakPredicate) {
+    this.onLeakPredicate = onLeakPredicate;
+    this.globalStatistics = new Stats(onLeakPredicate);
+  }
 
   public void add(Issue issue) {
     globalStatistics.add(issue);
     String login = issue.assignee();
     if (login != null) {
-      getOrCreate(login).add(issue);
+      assigneesStatistics.computeIfAbsent(login, a -> new Stats(onLeakPredicate)).add(issue);
     }
-  }
-
-  private Stats getOrCreate(String assignee) {
-    return assigneesStatistics.computeIfAbsent(assignee, a -> new Stats());
   }
 
   public Map<String, Stats> assigneesStatistics() {
@@ -66,7 +64,7 @@ public class NewIssuesStatistics {
   }
 
   enum Metric {
-    SEVERITY(true), TAG(true), COMPONENT(true), ASSIGNEE(true), DEBT(false), RULE(true);
+    SEVERITY(true), TAG(true), COMPONENT(true), ASSIGNEE(true), EFFORT(false), RULE(true);
     private final boolean isComputedByDistribution;
 
     Metric(boolean isComputedByDistribution) {
@@ -79,59 +77,52 @@ public class NewIssuesStatistics {
   }
 
   public static class Stats {
-    private final Map<Metric, Multiset<String>> distributions = new EnumMap<>(Metric.class);
-    private long debtInMinutes = 0L;
+    private final Predicate<Issue> onLeakPredicate;
+    private final Map<Metric, DistributedMetricStatsInt> distributions = new EnumMap<>(Metric.class);
+    private MetricStatsLong effortStats = new MetricStatsLong();
 
-    public Stats() {
+    public Stats(Predicate<Issue> onLeakPredicate) {
+      this.onLeakPredicate = onLeakPredicate;
       for (Metric metric : Metric.values()) {
         if (metric.isComputedByDistribution()) {
-          distributions.put(metric, HashMultiset.<String>create());
+          distributions.put(metric, new DistributedMetricStatsInt());
         }
       }
     }
 
     public void add(Issue issue) {
-      distributions.get(SEVERITY).add(issue.severity());
-      distributions.get(COMPONENT).add(issue.componentUuid());
+      boolean isOnLeak = onLeakPredicate.test(issue);
+      distributions.get(SEVERITY).increment(issue.severity(), isOnLeak);
+      distributions.get(COMPONENT).increment(issue.componentUuid(), isOnLeak);
       RuleKey ruleKey = issue.ruleKey();
       if (ruleKey != null) {
-        distributions.get(RULE).add(ruleKey.toString());
+        distributions.get(RULE).increment(ruleKey.toString(), isOnLeak);
       }
-      if (issue.assignee() != null) {
-        distributions.get(ASSIGNEE).add(issue.assignee());
+      String assignee = issue.assignee();
+      if (assignee != null) {
+        distributions.get(ASSIGNEE).increment(assignee, isOnLeak);
       }
       for (String tag : issue.tags()) {
-        distributions.get(TAG).add(tag);
+        distributions.get(TAG).increment(tag, isOnLeak);
       }
-      Duration debt = issue.debt();
-      if (debt != null) {
-        debtInMinutes += debt.toMinutes();
+      Duration effort = issue.effort();
+      if (effort != null) {
+        effortStats.add(effort.toMinutes(), isOnLeak);
       }
     }
 
-    public int countForMetric(Metric metric) {
-      return distributionFor(metric).size();
+    public DistributedMetricStatsInt getDistributedMetricStats(Metric metric) {
+      return distributions.get(metric);
     }
 
-    public int countForMetric(Metric metric, String label) {
-      return distributionFor(metric).count(label);
-    }
-
-    public Duration debt() {
-      return Duration.create(debtInMinutes);
+    public MetricStatsLong effort() {
+      return effortStats;
     }
 
     public boolean hasIssues() {
-      return !distributionFor(SEVERITY).isEmpty();
+      return getDistributedMetricStats(SEVERITY).getTotal() > 0;
     }
 
-    public List<Multiset.Entry<String>> statsForMetric(Metric metric) {
-      return Multisets.copyHighestCountFirst(distributionFor(metric)).entrySet().asList();
-    }
-
-    private Multiset<String> distributionFor(Metric metric) {
-      checkArgument(metric.isComputedByDistribution());
-      return distributions.get(metric);
-    }
   }
+
 }
