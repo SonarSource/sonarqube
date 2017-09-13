4ef0e845481dc751289437418d8c3933262cd2ac
Grégoire Aubert <gregoire.aubert@sonarsource.com>
2017-09-13 11:23:00 +0200

    SONAR-9802 SONAR-9803 Create system info page structure, with header and actions

--- server/sonar-web/src/main/js/apps/system/components/__tests__/PageHeader-test.tsx
+++ server/sonar-web/src/main/js/apps/system/components/__tests__/PageHeader-test.tsx
@@ -17,20 +17,18 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-import { RouterState, RouteComponent, IndexRouteProps } from 'react-router';
+import * as React from 'react';
+import { shallow } from 'enzyme';
+import PageHeader from '../PageHeader';
 
-const routes = [
-  {
-    getIndexRoute(_: RouterState, callback: (err: any, route: IndexRouteProps) => any) {
-      import('./components/App').then(i => callback(null, { component: i.default }));
-    }
-  },
-  {
-    path: 'old',
-    getComponent(_: RouterState, callback: (err: any, component: RouteComponent) => any) {
-      import('./main').then(i => callback(null, (i as any).default));
-    }
-  }
-];
+it('should render correctly', () => {
+  expect(
+    shallow(<PageHeader isCluster={true} loading={false} logLevel="INFO" showActions={true} />)
+  ).toMatchSnapshot();
+});
 
-export default routes;
+it('should show a loading spinner and no actions', () => {
+  expect(
+    shallow(<PageHeader isCluster={true} loading={true} logLevel="INFO" showActions={false} />)
+  ).toMatchSnapshot();
+});
