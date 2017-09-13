45dfbc2ff7edeb20a419f99f9552adecc15887ad
Grégoire Aubert <gregoire.aubert@sonarsource.com>
2017-09-13 11:23:00 +0200

    SONAR-9802 SONAR-9803 Create system info page structure, with header and actions

--- server/sonar-web/src/main/js/apps/system/components/StandAloneSections.tsx
+++ server/sonar-web/src/main/js/apps/system/components/StandAloneSections.tsx
@@ -17,20 +17,14 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-import { RouterState, RouteComponent, IndexRouteProps } from 'react-router';
+import * as React from 'react';
 
-const routes = [
-  {
-    getIndexRoute(_: RouterState, callback: (err: any, route: IndexRouteProps) => any) {
-      import('./components/App').then(i => callback(null, { component: i.default }));
-    }
-  },
-  {
-    path: 'old',
-    getComponent(_: RouterState, callback: (err: any, component: RouteComponent) => any) {
-      import('./main').then(i => callback(null, (i as any).default));
-    }
+interface Props {
+  sysInfoData: object;
+}
+
+export default class StandAloneSections extends React.PureComponent<Props> {
+  render() {
+    return <div>StandAloneSections</div>;
   }
-];
-
-export default routes;
+}
--- server/sonar-web/src/main/js/apps/system/components/__tests__/ChangeLogLevelForm-test.tsx
+++ server/sonar-web/src/main/js/apps/system/components/__tests__/ChangeLogLevelForm-test.tsx
@@ -17,20 +17,22 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-import { RouterState, RouteComponent, IndexRouteProps } from 'react-router';
+import * as React from 'react';
+import { shallow } from 'enzyme';
+import ChangeLogLevelForm from '../ChangeLogLevelForm';
 
-const routes = [
-  {
-    getIndexRoute(_: RouterState, callback: (err: any, route: IndexRouteProps) => any) {
-      import('./components/App').then(i => callback(null, { component: i.default }));
-    }
-  },
-  {
-    path: 'old',
-    getComponent(_: RouterState, callback: (err: any, component: RouteComponent) => any) {
-      import('./main').then(i => callback(null, (i as any).default));
-    }
-  }
-];
+it('should render correctly', () => {
+  expect(
+    shallow(
+      <ChangeLogLevelForm infoMsg="Foo" logLevel="INFO" onChange={() => {}} onClose={() => {}} />
+    )
+  ).toMatchSnapshot();
+});
 
-export default routes;
+it('should display some warning messages for non INFO levels', () => {
+  expect(
+    shallow(
+      <ChangeLogLevelForm infoMsg="Foo" logLevel="DEBUG" onChange={() => {}} onClose={() => {}} />
+    )
+  ).toMatchSnapshot();
+});
