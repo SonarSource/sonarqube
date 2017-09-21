e92f998223b72d585eca962315b7086d31ed96c9
Simon Brandhof <simon.brandhof@sonarsource.com>
2017-09-21 20:03:14 +0200

    Fix compatibility of build with latest Travis upgrade

--- travis.sh
+++ travis.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 function configureTravis {
-  mkdir ~/.local
+  mkdir -p ~/.local
   curl -sSL https://github.com/SonarSource/travis-utils/tarball/v31 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
