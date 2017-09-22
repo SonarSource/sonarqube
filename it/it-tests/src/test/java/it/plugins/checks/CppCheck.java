ca9cfc3437a251cd081c75971790912b081acc64
Simon Brandhof <simon.brandhof@sonarsource.com>
2017-09-22 13:48:20 +0200

    PluginsTest should not verify nb of issues in c++ files
    This number of issues is unstable. As an example parsing of Rand.cc
    breaks the analyzer and stops analysis of other c++ files. Depending
    on order of files that may generate a c++ dir without any issues.

--- it/it-tests/src/test/java/it/plugins/checks/CppCheck.java
+++ it/it-tests/src/test/java/it/plugins/checks/CppCheck.java
@@ -26,7 +26,6 @@ public class CppCheck implements Check {
   @Override
   public void validate(Validation validation) {
     validation.mustHaveNonEmptySource(DIR);
-    validation.mustHaveIssues(DIR);
     validation.mustHaveSize(DIR);
     validation.mustHaveComments(DIR);
     validation.mustHaveComplexity(DIR);
