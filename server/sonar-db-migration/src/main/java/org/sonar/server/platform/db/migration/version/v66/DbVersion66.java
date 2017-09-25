1b443053cd473188c17606ea48318ceadd19720b
Julien Lancelot <julien.lancelot@sonarsource.com>
2017-09-25 14:17:32 +0200

    SONAR-1330 Create table QPROFILE_EDIT_GROUPS

--- server/sonar-db-migration/src/main/java/org/sonar/server/platform/db/migration/version/v66/DbVersion66.java
+++ server/sonar-db-migration/src/main/java/org/sonar/server/platform/db/migration/version/v66/DbVersion66.java
@@ -38,6 +38,7 @@ public class DbVersion66 implements DbVersion {
       .add(1809, "Populate project_branches with existing main branches", PopulateMainProjectBranches.class)
       .add(1810, "Add ce_activity.error_type", AddErrorTypeColumnToCeActivityTable.class)
       .add(1811, "Create table qprofile_edit_users", CreateTableQProfileEditUsers.class)
+      .add(1812, "Create table qprofile_edit_groups", CreateTableQProfileEditGroups.class)
     ;
   }
 }
