9730d937b6465116379d183835665deec4c38297
Grégoire Aubert <gregoire.aubert@sonarsource.com>
2017-09-28 14:03:05 +0200

    Update icons list

---
+++ server/sonar-web/src/main/js/components/icons-components/AlertErrorIcon.tsx
@@ -0,0 +1,41 @@
+/*
+ * SonarQube
+ * Copyright (C) 2009-2017 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+import * as React from 'react';
+
+interface Props {
+  className?: string;
+  size?: number;
+}
+
+export default function AlertErrorIcon({ className, size = 16 }: Props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+      height={size}
+      width={size}
+      viewBox="0 0 16 16">
+      <path
+        style={{ fill: '#d4333f' }}
+        d="M11.402 10.018q0-0.232-0.17-0.402l-1.616-1.616 1.616-1.616q0.17-0.17 0.17-0.402 0-0.241-0.17-0.411l-0.804-0.804q-0.17-0.17-0.411-0.17-0.232 0-0.402 0.17l-1.616 1.616-1.616-1.616q-0.17-0.17-0.402-0.17-0.241 0-0.411 0.17l-0.804 0.804q-0.17 0.17-0.17 0.411 0 0.232 0.17 0.402l1.616 1.616-1.616 1.616q-0.17 0.17-0.17 0.402 0 0.241 0.17 0.411l0.804 0.804q0.17 0.17 0.411 0.17 0.232 0 0.402-0.17l1.616-1.616 1.616 1.616q0.17 0.17 0.402 0.17 0.241 0 0.411-0.17l0.804-0.804q0.17-0.17 0.17-0.411zM14.857 8q0 1.866-0.92 3.442t-2.496 2.496-3.442 0.92-3.442-0.92-2.496-2.496-0.92-3.442 0.92-3.442 2.496-2.496 3.442-0.92 3.442 0.92 2.496 2.496 0.92 3.442z"
+      />
+    </svg>
+  );
+}
--- server/sonar-web/src/main/js/components/icons-components/icons.ts
+++ server/sonar-web/src/main/js/components/icons-components/icons.ts
@@ -17,8 +17,9 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-// @flow
+import _AlertErrorIcon from './AlertErrorIcon';
 import _AlertWarnIcon from './AlertWarnIcon';
+import _BranchIcon from './BranchIcon';
 import _BubblesIcon from './BubblesIcon';
 import _BugIcon from './BugIcon';
 import _ChangeIcon from './ChangeIcon';
@@ -27,21 +28,28 @@ import _CheckIcon from './CheckIcon';
 import _CloseIcon from './CloseIcon';
 import _CodeSmellIcon from './CodeSmellIcon';
 import _DeleteIcon from './DeleteIcon';
+import _FavoriteIcon from './FavoriteIcon';
 import _HelpIcon from './HelpIcon';
 import _HistoryIcon from './HistoryIcon';
 import _LinkIcon from './LinkIcon';
 import _ListIcon from './ListIcon';
+import _LongLivingBranchIcon from './LongLivingBranchIcon';
 import _OpenCloseIcon from './OpenCloseIcon';
 import _OrganizationIcon from './OrganizationIcon';
+import _PendingIcon from './PendingIcon';
 import _ProjectEventIcon from './ProjectEventIcon';
+import _PullRequestIcon from './PullRequestIcon';
 import _QualifierIcon from './QualifierIcon';
+import _ShortLivingBranchIcon from './ShortLivingBranchIcon';
 import _SortAscIcon from './SortAscIcon';
 import _SortDescIcon from './SortDescIcon';
 import _TreeIcon from './TreeIcon';
 import _TreemapIcon from './TreemapIcon';
 import _VulnerabilityIcon from './VulnerabilityIcon';
 
+export const AlertErrorIcon = _AlertErrorIcon;
 export const AlertWarnIcon = _AlertWarnIcon;
+export const BranchIcon = _BranchIcon;
 export const BubblesIcon = _BubblesIcon;
 export const BugIcon = _BugIcon;
 export const ChangeIcon = _ChangeIcon;
@@ -50,14 +58,19 @@ export const CheckIcon = _CheckIcon;
 export const CloseIcon = _CloseIcon;
 export const CodeSmellIcon = _CodeSmellIcon;
 export const DeleteIcon = _DeleteIcon;
+export const FavoriteIcon = _FavoriteIcon;
 export const HelpIcon = _HelpIcon;
 export const HistoryIcon = _HistoryIcon;
 export const LinkIcon = _LinkIcon;
 export const ListIcon = _ListIcon;
+export const LongLivingBranchIcon = _LongLivingBranchIcon;
 export const OpenCloseIcon = _OpenCloseIcon;
 export const OrganizationIcon = _OrganizationIcon;
+export const PendingIcon = _PendingIcon;
 export const ProjectEventIcon = _ProjectEventIcon;
+export const PullRequestIcon = _PullRequestIcon;
 export const QualifierIcon = _QualifierIcon;
+export const ShortLivingBranchIcon = _ShortLivingBranchIcon;
 export const SortAscIcon = _SortAscIcon;
 export const SortDescIcon = _SortDescIcon;
 export const TreeIcon = _TreeIcon;
