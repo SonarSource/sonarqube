5bf382725b448d72da14794b028862c8c65e6d0e
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2017-09-29 16:50:57 +0200

    SONAR-9871 call webhook on some issue changes in UI
    on short living branches

---
+++ server/sonar-server/src/test/java/org/sonar/server/issue/webhook/IssueChangeWebhookTest.java
@@ -0,0 +1,53 @@
+/*
+ * SonarQube
+ * Copyright (C) 2009-2017 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.server.issue.webhook;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.sonar.api.rules.RuleType;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class IssueChangeWebhookTest {
+  @Rule
+  public ExpectedException expectedException = ExpectedException.none();
+
+  @Test
+  public void IssueChange_constructor_throws_IAE_if_both_args_are_null() {
+    expectedException.expect(IllegalArgumentException.class);
+    expectedException.expectMessage("At least one of ruleType and transitionKey must be non null");
+
+    new IssueChangeWebhook.IssueChange(null, null);
+  }
+
+  @Test
+  public void verify_IssueChange_getters() {
+    IssueChangeWebhook.IssueChange transitionKeyOnly = new IssueChangeWebhookImpl.IssueChange("foo");
+    assertThat(transitionKeyOnly.getTransitionKey()).contains("foo");
+    assertThat(transitionKeyOnly.getRuleType()).isEmpty();
+    IssueChangeWebhook.IssueChange ruleTypeOnly = new IssueChangeWebhookImpl.IssueChange(RuleType.BUG);
+    assertThat(ruleTypeOnly.getTransitionKey()).isEmpty();
+    assertThat(ruleTypeOnly.getRuleType()).contains(RuleType.BUG);
+    IssueChangeWebhook.IssueChange transitionKeyAndRuleType = new IssueChangeWebhookImpl.IssueChange(RuleType.VULNERABILITY, "bar");
+    assertThat(transitionKeyAndRuleType.getTransitionKey()).contains("bar");
+    assertThat(transitionKeyAndRuleType.getRuleType()).contains(RuleType.VULNERABILITY);
+  }
+}
