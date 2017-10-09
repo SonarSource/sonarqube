eb4ccd5d03e5aa18a0f5d160e0b0a6f7336b9935
Stas Vilchik <stas.vilchik@sonarsource.com>
2017-10-09 14:43:02 +0200

    do not let select date in future

--- server/sonar-web/src/main/js/components/controls/DateInput.tsx
+++ server/sonar-web/src/main/js/components/controls/DateInput.tsx
@@ -28,6 +28,8 @@ interface Props {
   className?: string;
   format?: string;
   inputClassName?: string;
+  // see http://api.jqueryui.com/datepicker/#option-maxDate for details
+  maxDate?: Date | string | number;
   name: string;
   onChange: (value?: string) => void;
   placeholder: string;
@@ -38,7 +40,8 @@ export default class DateInput extends React.PureComponent<Props> {
   input: HTMLInputElement;
 
   static defaultProps = {
-    format: 'yy-mm-dd'
+    format: 'yy-mm-dd',
+    maxDate: '+0'
   };
 
   componentDidMount() {
@@ -61,6 +64,7 @@ export default class DateInput extends React.PureComponent<Props> {
       dateFormat: this.props.format,
       changeMonth: true,
       changeYear: true,
+      maxDate: this.props.maxDate,
       onSelect: this.handleChange
     };
 
