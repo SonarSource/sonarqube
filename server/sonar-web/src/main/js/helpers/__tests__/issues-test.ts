c3ccc3103f8b03b3fc3aded6bba67082da8441a2
Stas Vilchik <stas.vilchik@sonarsource.com>
2017-10-10 11:27:50 +0200

    SONAR-9749 Order secondary locations markers by line

--- server/sonar-web/src/main/js/helpers/__tests__/issues-test.ts
+++ server/sonar-web/src/main/js/helpers/__tests__/issues-test.ts
@@ -31,7 +31,7 @@ it('should populate comments data', () => {
   const issue = {
     comments: [
       {
-        createdAt: '2017-04-11T10:38:09+0200',
+        createdAt: '2017-04-11T10:38:090200',
         htmlText: 'comment!',
         key: 'AVtcKbZkQmGLa7yW8J71',
         login: 'admin',
@@ -47,12 +47,29 @@ it('should populate comments data', () => {
       authorAvatar: 'c1244e6857f7be3dc4549d9e9d51c631',
       authorLogin: 'admin',
       authorName: 'Admin Admin',
-      createdAt: '2017-04-11T10:38:09+0200',
+      createdAt: '2017-04-11T10:38:090200',
       htmlText: 'comment!',
       key: 'AVtcKbZkQmGLa7yW8J71',
       login: undefined,
       markdown: 'comment!',
       updatable: true
     }
+  ]);
+});
+
+it('orders secondary locations', () => {
+  const issue = {
+    flows: [
+      { locations: [{ textRange: { startLine: 68, startOffset: 5, endLine: 68, endOffset: 7 } }] },
+      { locations: [{ textRange: { startLine: 43, startOffset: 8, endLine: 43, endOffset: 12 } }] },
+      { locations: [{ textRange: { startLine: 43, startOffset: 6, endLine: 43, endOffset: 8 } }] },
+      { locations: [{ textRange: { startLine: 70, startOffset: 12, endLine: 70, endOffset: 16 } }] }
+    ]
+  } as any;
+  expect(parseIssueFromResponse(issue).secondaryLocations).toEqual([
+    { textRange: { startLine: 43, startOffset: 6, endLine: 43, endOffset: 8 } },
+    { textRange: { startLine: 43, startOffset: 8, endLine: 43, endOffset: 12 } },
+    { textRange: { startLine: 68, startOffset: 5, endLine: 68, endOffset: 7 } },
+    { textRange: { startLine: 70, startOffset: 12, endLine: 70, endOffset: 16 } }
   ]);
 });
