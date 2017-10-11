bb9b0e23c7c911c7bf101e6c5e8a21f77a610ce4
Stas Vilchik <stas.vilchik@sonarsource.com>
2017-10-11 11:51:05 +0200

    SONAR-9847 Add tooltip explaining how to get branch support

--- server/sonar-web/src/main/js/app/components/nav/component/ComponentNavBranch.tsx
+++ server/sonar-web/src/main/js/app/components/nav/component/ComponentNavBranch.tsx
@@ -22,6 +22,7 @@ import * as classNames from 'classnames';
 import * as PropTypes from 'prop-types';
 import ComponentNavBranchesMenu from './ComponentNavBranchesMenu';
 import SingleBranchHelperPopup from './SingleBranchHelperPopup';
+import NoBranchSupportPopup from './NoBranchSupportPopup';
 import { Branch, Component } from '../../../types';
 import BranchIcon from '../../../../components/icons-components/BranchIcon';
 import { isShortLivingBranch } from '../../../../helpers/branches';
@@ -52,7 +53,8 @@ export default class ComponentNavBranch extends React.PureComponent<Props, State
   };
 
   static contextTypes = {
-    branchesEnabled: PropTypes.bool.isRequired
+    branchesEnabled: PropTypes.bool.isRequired,
+    onSonarCloud: PropTypes.bool
   };
 
   componentDidMount() {
@@ -165,11 +167,35 @@ export default class ComponentNavBranch extends React.PureComponent<Props, State
     </div>
   );
 
+  renderNoBranchSupportPopup = () => (
+    <div className="display-inline-block spacer-left">
+      <a className="link-no-underline" href="#" onClick={this.handleNoBranchSupportClick}>
+        <HelpIcon fill="#cdcdcd" />
+      </a>
+      <BubblePopupHelper
+        isOpen={this.state.noBranchSupportPopupOpen}
+        position="bottomleft"
+        popup={<NoBranchSupportPopup />}
+        togglePopup={this.toggleNoBranchSupportPopup}
+      />
+    </div>
+  );
+
   render() {
     const { branches, currentBranch } = this.props;
 
-    if (!this.context.branchesEnabled) {
+    if (this.context.onSonarCloud && !this.context.branchesEnabled) {
       return null;
+    }
+
+    if (!this.context.branchesEnabled) {
+      return (
+        <div className="navbar-context-branches">
+          <BranchIcon branch={currentBranch} className="little-spacer-right" color="#cdcdcd" />
+          <span className="note">{currentBranch.name}</span>
+          {this.renderNoBranchSupportPopup()}
+        </div>
+      );
     }
 
     if (branches.length < 2) {
