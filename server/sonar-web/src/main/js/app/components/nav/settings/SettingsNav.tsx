187da2515f6f478f819b9dda48183182b06991c9
Grégoire Aubert <gregoire.aubert@sonarsource.com>
2017-10-17 16:30:21 +0200

    SONAR-9936 Update marketplace pending plugins notif

--- server/sonar-web/src/main/js/app/components/nav/settings/SettingsNav.tsx
+++ server/sonar-web/src/main/js/app/components/nav/settings/SettingsNav.tsx
@@ -61,6 +61,11 @@ export default class SettingsNav extends React.PureComponent<Props> {
     return this.isSomethingActive(urls);
   }
 
+  isMarketplace() {
+    const urls = ['/admin/marketplace'];
+    return this.isSomethingActive(urls);
+  }
+
   renderExtension = ({ key, name }: Extension) => {
     return (
       <li key={key}>
@@ -81,7 +86,7 @@ export default class SettingsNav extends React.PureComponent<Props> {
     const projectsClassName = classNames('dropdown-toggle', { active: isProjects });
     const systemClassName = classNames('dropdown-toggle', { active: isSystem });
     const configurationClassNames = classNames('dropdown-toggle', {
-      active: !isSecurity && !isProjects && !isSystem
+      active: !isSecurity && !isProjects && !isSystem && !this.isMarketplace()
     });
 
     return (
