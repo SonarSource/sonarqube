0720d837bab32ecc80b9f54a404fbf72da68c73a
Duarte Meneses <duarte.meneses@sonarsource.com>
2017-10-17 09:25:40 +0200

    SONAR-9940 implementation of api/editions/apply_license

--- server/sonar-server/src/test/java/org/sonar/server/edition/ws/ApplyLicenseActionTest.java
+++ server/sonar-server/src/test/java/org/sonar/server/edition/ws/ApplyLicenseActionTest.java
@@ -22,26 +22,41 @@ package org.sonar.server.edition.ws;
 import com.tngtech.java.junit.dataprovider.DataProvider;
 import com.tngtech.java.junit.dataprovider.DataProviderRunner;
 import com.tngtech.java.junit.dataprovider.UseDataProvider;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
+import java.util.Base64;
+import java.util.Collections;
 import java.util.Optional;
+import java.util.Properties;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.sonar.api.server.ws.WebService;
 import org.sonar.server.edition.EditionManagementState;
+import org.sonar.server.edition.EditionManagementState.PendingStatus;
+import org.sonar.server.edition.License;
 import org.sonar.server.edition.MutableEditionManagementState;
 import org.sonar.server.exceptions.BadRequestException;
 import org.sonar.server.exceptions.ForbiddenException;
 import org.sonar.server.exceptions.UnauthorizedException;
 import org.sonar.server.license.LicenseCommit;
+import org.sonar.server.plugins.edition.EditionInstaller;
 import org.sonar.server.tester.UserSessionRule;
 import org.sonar.server.ws.TestRequest;
+import org.sonar.server.ws.TestResponse;
 import org.sonar.server.ws.WsActionTester;
 import org.sonar.test.JsonAssert;
+import org.sonarqube.ws.MediaTypes;
+import org.sonarqube.ws.WsEditions;
+import org.sonarqube.ws.WsEditions.StatusResponse;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.sonar.server.edition.EditionManagementState.PendingStatus.AUTOMATIC_IN_PROGRESS;
 import static org.sonar.server.edition.EditionManagementState.PendingStatus.NONE;
@@ -49,15 +64,17 @@ import static org.sonar.server.edition.EditionManagementState.PendingStatus.NONE
 @RunWith(DataProviderRunner.class)
 public class ApplyLicenseActionTest {
   private static final String PARAM_LICENSE = "license";
+  private static final String PENDING_EDITION_NAME = "developer-edition";
 
   @Rule
   public ExpectedException expectedException = ExpectedException.none();
   @Rule
   public UserSessionRule userSessionRule = UserSessionRule.standalone();
 
+  private EditionInstaller editionInstaller = mock(EditionInstaller.class);
   private MutableEditionManagementState mutableEditionManagementState = mock(MutableEditionManagementState.class);
   private LicenseCommit licenseCommit = mock(LicenseCommit.class);
-  private ApplyLicenseAction underTest = new ApplyLicenseAction(userSessionRule, mutableEditionManagementState, licenseCommit);
+  private ApplyLicenseAction underTest = new ApplyLicenseAction(userSessionRule, mutableEditionManagementState, editionInstaller, licenseCommit);
   private WsActionTester actionTester = new WsActionTester(underTest);
 
   @Test
@@ -127,25 +144,109 @@ public class ApplyLicenseActionTest {
   }
 
   @Test
-  public void verify_example() {
+  public void request_fails_with_BadRequestException_if_license_is_invalid() {
     userSessionRule.logIn().setSystemAdministrator();
-    when(mutableEditionManagementState.getCurrentEditionKey()).thenReturn(Optional.empty());
-    when(mutableEditionManagementState.getPendingEditionKey()).thenReturn(Optional.of("developer-edition"));
-    when(mutableEditionManagementState.getPendingInstallationStatus())
-        .thenReturn(NONE)
-        .thenReturn(AUTOMATIC_IN_PROGRESS);
+    TestRequest request = actionTester.newRequest()
+      .setParam(PARAM_LICENSE, "invalid");
+    when(mutableEditionManagementState.getPendingInstallationStatus()).thenReturn(NONE);
+
+    expectedException.expect(BadRequestException.class);
+    expectedException.expectMessage("The license provided is invalid");
+    request.execute();
+  }
+
+  @Test
+  public void verify_example() throws IOException {
+    userSessionRule.logIn().setSystemAdministrator();
+    setPendingLicense(AUTOMATIC_IN_PROGRESS);
 
     TestRequest request = actionTester.newRequest()
-      .setParam(PARAM_LICENSE, "foo");
+      .setParam(PARAM_LICENSE, createLicenseParam("dev", "plugin1"));
 
     JsonAssert.assertJson(request.execute().getInput()).isSimilarTo(actionTester.getDef().responseExampleAsString());
+  }
+
+  @Test
+  public void apply_without_need_to_install() throws IOException {
+    userSessionRule.logIn().setSystemAdministrator();
+    setPendingLicense(NONE);
+    when(editionInstaller.requiresInstallationChange(Collections.singleton("plugin1"))).thenReturn(false);
+
+    TestRequest request = actionTester.newRequest()
+      .setMediaType(MediaTypes.PROTOBUF)
+      .setParam(PARAM_LICENSE, createLicenseParam(PENDING_EDITION_NAME, "plugin1"));
+
+    TestResponse response = request.execute();
+    assertResponse(response, PENDING_EDITION_NAME, "", NONE);
+    verify(mutableEditionManagementState).newEditionWithoutInstall(PENDING_EDITION_NAME);
+  }
+
+  @Test
+  public void apply_offline() throws IOException {
+    userSessionRule.logIn().setSystemAdministrator();
+    setPendingLicense(PendingStatus.MANUAL_IN_PROGRESS);
+    when(editionInstaller.requiresInstallationChange(Collections.singleton("plugin1"))).thenReturn(true);
+    when(editionInstaller.install(Collections.singleton("plugin1"))).thenReturn(false);
+
+    TestRequest request = actionTester.newRequest()
+      .setMediaType(MediaTypes.PROTOBUF)
+      .setParam(PARAM_LICENSE, createLicenseParam(PENDING_EDITION_NAME, "plugin1"));
+
+    TestResponse response = request.execute();
+
+    assertResponse(response, PENDING_EDITION_NAME, "", PendingStatus.MANUAL_IN_PROGRESS);
+    verify(mutableEditionManagementState).startManualInstall(any(License.class));
+  }
+
+  @Test
+  public void apply_successfully_auto_installation() throws IOException {
+    userSessionRule.logIn().setSystemAdministrator();
+    setPendingLicense(PendingStatus.AUTOMATIC_IN_PROGRESS);
+    when(editionInstaller.requiresInstallationChange(Collections.singleton("plugin1"))).thenReturn(true);
+    when(editionInstaller.install(Collections.singleton("plugin1"))).thenReturn(true);
+
+    TestRequest request = actionTester.newRequest()
+      .setMediaType(MediaTypes.PROTOBUF)
+      .setParam(PARAM_LICENSE, createLicenseParam(PENDING_EDITION_NAME, "plugin1"));
+
+    TestResponse response = request.execute();
+
+    assertResponse(response, PENDING_EDITION_NAME, "", PendingStatus.AUTOMATIC_IN_PROGRESS);
+    verify(mutableEditionManagementState).startAutomaticInstall(any(License.class));
+  }
+
+  private void assertResponse(TestResponse response, String expectedNextEditionKey, String expectedEditionKey,
+    PendingStatus expectedPendingStatus) throws IOException {
+    StatusResponse parsedResponse = WsEditions.StatusResponse.parseFrom(response.getInputStream());
+    assertThat(parsedResponse.getCurrentEditionKey()).isEqualTo(expectedEditionKey);
+    assertThat(parsedResponse.getNextEditionKey()).isEqualTo(expectedNextEditionKey);
+    assertThat(parsedResponse.getInstallationStatus()).isEqualTo(WsEditions.InstallationStatus.valueOf(expectedPendingStatus.toString()));
+  }
+
+  private void setPendingLicense(PendingStatus pendingStatus) {
+    when(mutableEditionManagementState.getCurrentEditionKey()).thenReturn(Optional.empty());
+    when(mutableEditionManagementState.getPendingEditionKey()).thenReturn(Optional.of(PENDING_EDITION_NAME));
+    when(mutableEditionManagementState.getPendingInstallationStatus())
+      .thenReturn(NONE)
+      .thenReturn(pendingStatus);
+  }
+
+  private static String createLicenseParam(String editionKey, String... pluginKeys) throws IOException {
+    Properties props = new Properties();
+    props.setProperty("Plugins", String.join(",", pluginKeys));
+    props.setProperty("Edition", editionKey);
+    StringWriter writer = new StringWriter();
+    props.store(writer, "");
+
+    byte[] encoded = Base64.getEncoder().encode(writer.toString().getBytes());
+    return new String(encoded, StandardCharsets.UTF_8);
   }
 
   @DataProvider
   public static Object[][] notNonePendingInstallationStatuses() {
     return Arrays.stream(EditionManagementState.PendingStatus.values())
-        .filter(s -> s != NONE)
-        .map(s -> new Object[] {s})
-        .toArray(Object[][]::new);
+      .filter(s -> s != NONE)
+      .map(s -> new Object[] {s})
+      .toArray(Object[][]::new);
   }
 }
