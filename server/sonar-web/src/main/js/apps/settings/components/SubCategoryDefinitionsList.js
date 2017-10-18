de67c6ac242dfb2adca56ce9ebb932abc673970d
Stas Vilchik <stas.vilchik@sonarsource.com>
2017-10-18 15:40:11 +0200

    SONAR-9325 Load only displayed setting values on the Settings page

--- server/sonar-web/src/main/js/apps/settings/components/SubCategoryDefinitionsList.js
+++ server/sonar-web/src/main/js/apps/settings/components/SubCategoryDefinitionsList.js
@@ -20,7 +20,7 @@
 // @flow
 import React from 'react';
 import PropTypes from 'prop-types';
-import { groupBy, sortBy } from 'lodash';
+import { groupBy, isEqual, sortBy } from 'lodash';
 import DefinitionsList from './DefinitionsList';
 import EmailForm from './EmailForm';
 import { getSubCategoryName, getSubCategoryDescription } from '../utils';
@@ -28,8 +28,26 @@ import { getSubCategoryName, getSubCategoryDescription } from '../utils';
 export default class SubCategoryDefinitionsList extends React.PureComponent {
   static propTypes = {
     component: PropTypes.object,
+    fetchValues: PropTypes.func,
     settings: PropTypes.array.isRequired
   };
+
+  componentDidMount() {
+    this.fetchValues();
+  }
+
+  componentDidUpdate(prevProps /*: Object */) {
+    const prevKeys = prevProps.settings.map(setting => setting.definition.key);
+    const keys = this.props.settings.map(setting => setting.definition.key);
+    if (prevProps.component !== this.props.component || !isEqual(prevKeys, keys)) {
+      this.fetchValues();
+    }
+  }
+
+  fetchValues() {
+    const keys = this.props.settings.map(setting => setting.definition.key).join();
+    this.props.fetchValues(keys, this.props.component && this.props.component.key);
+  }
 
   renderEmailForm(subCategoryKey /*: string */) {
     const isEmailSettings = this.props.category === 'general' && subCategoryKey === 'email';
