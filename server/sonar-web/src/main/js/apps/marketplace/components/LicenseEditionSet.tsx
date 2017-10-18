fd4236c101ff329be8909bcb90c84a0ad817b0a1
Grégoire Aubert <gregoire.aubert@sonarsource.com>
2017-10-18 12:23:17 +0200

    LICENSE-72 Update set license component to work without an edition

--- server/sonar-web/src/main/js/apps/marketplace/components/LicenseEditionSet.tsx
+++ server/sonar-web/src/main/js/apps/marketplace/components/LicenseEditionSet.tsx
@@ -25,12 +25,14 @@ import { translate, translateWithParameters } from '../../../helpers/l10n';
 
 export interface Props {
   className?: string;
-  edition: Edition;
+  edition?: Edition;
+  editions: Edition[];
   updateLicense: (license?: string, status?: string) => void;
 }
 
 interface State {
   license: string;
+  licenseEdition?: Edition;
   loading: boolean;
   previewStatus?: string;
 }
@@ -41,7 +43,7 @@ export default class LicenseEditionSet extends React.PureComponent<Props, State>
   constructor(props: Props) {
     super(props);
     this.state = { license: '', loading: false };
-    this.fetchLicensePreview = debounce(this.fetchLicensePreview, 250);
+    this.fetchLicensePreview = debounce(this.fetchLicensePreview, 100);
   }
 
   componentDidMount() {
@@ -54,14 +56,18 @@ export default class LicenseEditionSet extends React.PureComponent<Props, State>
 
   fetchLicensePreview = (license: string) =>
     getLicensePreview({ license }).then(
-      r => {
+      ({ previewStatus, nextEditionKey }) => {
         if (this.mounted) {
-          this.updateLicense(license, r.previewStatus);
+          this.updateLicense(
+            license,
+            this.props.editions.find(edition => edition.key === nextEditionKey),
+            previewStatus
+          );
         }
       },
       () => {
         if (this.mounted) {
-          this.updateLicense(license, undefined);
+          this.updateLicense(license, undefined, undefined);
         }
       }
     );
@@ -72,35 +78,39 @@ export default class LicenseEditionSet extends React.PureComponent<Props, State>
       this.fetchLicensePreview(license);
       this.setState({ license });
     } else {
-      this.updateLicense(license, undefined);
+      this.updateLicense(license, undefined, undefined);
     }
   };
 
-  updateLicense = (license: string, previewStatus?: string) => {
-    this.setState({ license, previewStatus });
+  updateLicense = (license: string, licenseEdition?: Edition, previewStatus?: string) => {
+    this.setState({ license, licenseEdition, previewStatus });
     this.props.updateLicense(license, previewStatus);
   };
 
   render() {
     const { className, edition } = this.props;
-    const { license, previewStatus } = this.state;
+    const { license, licenseEdition, previewStatus } = this.state;
+
     return (
       <div className={className}>
-        <label htmlFor="set-license">
-          {translateWithParameters('marketplace.enter_license_for_x', edition.name)}
-          <em className="mandatory">*</em>
-        </label>
+        {edition && (
+          <label className="spacer-bottom" htmlFor="set-license">
+            {translateWithParameters('marketplace.enter_license_for_x', edition.name)}
+            <em className="mandatory">*</em>
+          </label>
+        )}
         <textarea
           autoFocus={true}
           id="set-license"
-          className="spacer-top display-block"
+          className="display-block"
           cols={62}
           onChange={this.handleLicenseChange}
           required={true}
           rows={6}
           value={license}
         />
-        {previewStatus && (
+        {previewStatus &&
+        licenseEdition && (
           <p
             className={classNames('alert spacer-top', {
               'alert-warning': previewStatus === 'AUTOMATIC_INSTALL',
@@ -109,14 +119,14 @@ export default class LicenseEditionSet extends React.PureComponent<Props, State>
             })}>
             {translateWithParameters(
               'marketplace.license_preview_status.' + previewStatus,
-              edition.name
+              licenseEdition.name
             )}
             {previewStatus === 'MANUAL_INSTALL' && (
               <p className="spacer-top">
                 <a
                   className="button"
-                  download={`sonarqube-${edition.name}.zip`}
-                  href={edition.download_link}
+                  download={`sonarqube-${licenseEdition.name}.zip`}
+                  href={licenseEdition.download_link}
                   target="_blank">
                   {translate('marketplace.download_package')}
                 </a>
@@ -130,12 +140,14 @@ export default class LicenseEditionSet extends React.PureComponent<Props, State>
             )}
           </p>
         )}
-        <a
-          className="display-inline-block spacer-top"
-          href={edition.request_license_link}
-          target="_blank">
-          {translate('marketplace.i_need_a_license')}
-        </a>
+        {edition && (
+          <a
+            className="display-inline-block spacer-top"
+            href={edition.request_license_link}
+            target="_blank">
+            {translate('marketplace.i_need_a_license')}
+          </a>
+        )}
       </div>
     );
   }
