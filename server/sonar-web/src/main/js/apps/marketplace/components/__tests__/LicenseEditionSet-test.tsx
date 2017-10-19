391a6d71ce7eb46203571590942ad1a81bc375d3
Grégoire Aubert <gregoire.aubert@sonarsource.com>
2017-10-19 16:00:36 +0200

    Update marketplace editions json format

--- server/sonar-web/src/main/js/apps/marketplace/components/__tests__/LicenseEditionSet-test.tsx
+++ server/sonar-web/src/main/js/apps/marketplace/components/__tests__/LicenseEditionSet-test.tsx
@@ -41,10 +41,10 @@ const getLicensePreview = require('../../../../api/marketplace').getLicensePrevi
 const DEFAULT_EDITION = {
   key: 'foo',
   name: 'Foo',
-  desc: 'Foo desc',
-  download_link: 'download_url',
-  more_link: 'more_url',
-  request_license_link: 'license_url'
+  textDescription: 'Foo desc',
+  downloadUrl: 'download_url',
+  homeUrl: 'more_url',
+  requestUrl: 'license_url'
 };
 
 beforeEach(() => {
