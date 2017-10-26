905296a41fe021e169912d74dc4392e55fb49d6d
Stas Vilchik <stas.vilchik@sonarsource.com>
2017-10-26 11:19:45 +0200

    update validate script, lint files on commit (#2756)

--- server/sonar-web/src/main/js/apps/marketplace/components/PluginAvailable.tsx
+++ server/sonar-web/src/main/js/apps/marketplace/components/PluginAvailable.tsx
@@ -24,12 +24,12 @@ import PluginLicense from './PluginLicense';
 import PluginOrganization from './PluginOrganization';
 import PluginStatus from './PluginStatus';
 import PluginUrls from './PluginUrls';
-import { PluginAvailable } from '../../../api/plugins';
+import { PluginAvailable as IPluginAvailable } from '../../../api/plugins';
 import { translateWithParameters } from '../../../helpers/l10n';
 import { Query } from '../utils';
 
 interface Props {
-  plugin: PluginAvailable;
+  plugin: IPluginAvailable;
   readOnly: boolean;
   refreshPending: () => void;
   status?: string;
--- server/sonar-web/src/main/js/apps/marketplace/components/__tests__/LicenseEditionSet-test.tsx
+++ server/sonar-web/src/main/js/apps/marketplace/components/__tests__/LicenseEditionSet-test.tsx
@@ -17,6 +17,7 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
+/* eslint-disable import/order */
 import * as React from 'react';
 import { shallow } from 'enzyme';
 import { change } from '../../../../helpers/testUtils';
