bd0039a0333eb9b0be0c76249929c6422f8b76fe
Stas Vilchik <stas.vilchik@sonarsource.com>
2017-10-31 18:07:36 +0100

    fixup! icons

--- server/sonar-web/src/main/js/apps/system/components/info-items/SysInfoItem.tsx
+++ server/sonar-web/src/main/js/apps/system/components/info-items/SysInfoItem.tsx
@@ -20,7 +20,7 @@
 import * as React from 'react';
 import { map } from 'lodash';
 import CheckIcon from '../../../../components/icons-components/CheckIcon';
-import CloseIcon from '../../../../components/icons-components/CloseIcon';
+import ClearIcon from '../../../../components/icons-components/ClearIcon';
 import HealthItem from './HealthItem';
 import { HealthType, SysValue, SysValueObject } from '../../../../api/system';
 import { HEALTH_FIELD } from '../../utils';
@@ -51,7 +51,7 @@ function BooleanItem({ value }: { value: boolean }) {
   if (value) {
     return <CheckIcon />;
   } else {
-    return <CloseIcon />;
+    return <ClearIcon />;
   }
 }
 
