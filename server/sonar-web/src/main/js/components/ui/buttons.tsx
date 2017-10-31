31892679cf369879cba7951a29fe1c07aa877657
Stas Vilchik <stas.vilchik@sonarsource.com>
2017-10-31 18:04:05 +0100

    create button components

---
+++ server/sonar-web/src/main/js/components/ui/buttons.tsx
@@ -0,0 +1,79 @@
+/*
+ * SonarQube
+ * Copyright (C) 2009-2017 SonarSource SA
+ * mailto:contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+import * as React from 'react';
+import * as classNames from 'classnames';
+import * as theme from '../../app/theme';
+import ClearIcon from '../icons-components/ClearIcon';
+import './buttons.css';
+import EditIcon from '../icons-components/EditIcon';
+
+interface ButtonIconProps {
+  children: React.ReactNode;
+  className?: string;
+  color?: string;
+  onClick?: () => void;
+  [x: string]: any;
+}
+
+export class ButtonIcon extends React.PureComponent<ButtonIconProps> {
+  handleClick = (event: React.SyntheticEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    event.currentTarget.blur();
+    event.stopPropagation();
+    if (this.props.onClick) {
+      this.props.onClick();
+    }
+  };
+
+  render() {
+    const { children, className, color = theme.darkBlue, ...props } = this.props;
+    return (
+      <button
+        className={classNames(className, 'button-icon')}
+        onClick={this.handleClick}
+        style={{ color }}
+        {...props}>
+        {children}
+      </button>
+    );
+  }
+}
+
+interface DeleteButtonProps {
+  className?: string;
+  onClick?: () => void;
+  [x: string]: any;
+}
+
+export function DeleteButton(props: DeleteButtonProps) {
+  return (
+    <ButtonIcon color={theme.red} {...props}>
+      <ClearIcon />
+    </ButtonIcon>
+  );
+}
+
+export function EditButton(props: DeleteButtonProps) {
+  return (
+    <ButtonIcon {...props}>
+      <EditIcon />
+    </ButtonIcon>
+  );
+}
