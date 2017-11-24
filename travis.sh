8fcb4a1344fb818c6c60d25e8f7c52eb12a77288
Stas Vilchik <stas.vilchik@sonarsource.com>
2017-11-24 16:28:11 +0100

    Silent cancellation of branch build on Travis if a PR is open

--- travis.sh
+++ travis.sh
@@ -105,15 +105,16 @@ function fixBuildVersion {
 #
 function configureTravis {
   mkdir -p ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v38 | tar zx --strip-components 1 -C ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v41 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
 configureTravis
 
-# When pull request exists on the branch, then the job related to the branch does not need
-# to be executed and should be canceled. It does not book slaves for nothing.
+# When a pull request is open on the branch, then the job related
+# to the branch does not need to be executed and should be canceled.
+# It does not book slaves for nothing.
 # @TravisCI please provide the feature natively, like at AppVeyor or CircleCI ;-)
-cancel_branch_build_with_pr
+cancel_branch_build_with_pr || if [[ $? -eq 1 ]]; then exit 0; fi
 
 case "$TARGET" in
 
