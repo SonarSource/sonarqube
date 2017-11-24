082ace5471dcc5b5b5f4d1c3cfaf82ddfae0df36
Daniel Schwarz <daniel.schwarz@sonarsource.com>
2017-11-24 09:25:26 +0100

    Copy generated sonar-ws sources to sonar-ws-generated, automatically

--- sonar-ws-generator/run.sh
+++ sonar-ws-generator/run.sh
@@ -7,3 +7,4 @@ set -euo pipefail
 
 mvn clean package -Prun-ws-generator
 java -Dorchestrator.configUrl=file://$* -jar target/sonar-ws-generator-*-SNAPSHOT-jar-with-dependencies.jar
+cp -R target/generated-sources/results/org/sonarqube/ws/client/* ../sonar-ws-generated/src/main/java/org/sonarqube/ws/client
