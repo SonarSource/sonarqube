f40afe30754c424d94ed14a2c41ade382169b24e
Simon Brandhof <simon.brandhof@sonarsource.com>
2017-11-24 15:27:22 +0100

    Fix Maven scanner incompatibility in UpgradeTest
    Signed-off-by: Simon Brandhof <simon.brandhof@sonarsource.com>

--- tests/src/test/java/org/sonarqube/tests/upgrade/UpgradeTest.java
+++ tests/src/test/java/org/sonarqube/tests/upgrade/UpgradeTest.java
@@ -205,7 +205,9 @@ public class UpgradeTest {
 
   private void scanProject() {
     MavenBuild build = MavenBuild.create(new File("projects/struts-1.3.9-diet/pom.xml"))
-      .setCleanPackageSonarGoals()
+      // force version of Maven scanner because recent releases do not support SonarQube versions
+      // lower than 5.6.
+      .setGoals("clean package", "org.sonarsource.scanner.maven:sonar-maven-plugin:3.3.0.603:sonar")
       // exclude pom.xml, otherwise it will be published in SQ 6.3+ and not in previous versions, resulting in a different number of
       // components
       .setProperty("sonar.exclusions", "**/pom.xml")
