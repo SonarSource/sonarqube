c572ffbd3ffb54eaa1018d1cd09f4b8c31af4086
Simon Brandhof <simon.brandhof@sonarsource.com>
2017-12-04 15:33:35 +0100

    Simplify usage of sonar-ws-generator

--- sonar-ws-generator/run.sh
+++ sonar-ws-generator/run.sh
@@ -6,5 +6,5 @@
 set -euo pipefail
 
 mvn clean package -Prun-ws-generator
-java -Dorchestrator.configUrl=file://$* -jar target/sonar-ws-generator-*-SNAPSHOT-jar-with-dependencies.jar
+java -jar target/sonar-ws-generator-*-SNAPSHOT-jar-with-dependencies.jar
 cp -R target/generated-sources/results/org/sonarqube/ws/client/* ../sonar-ws/src/main/java/org/sonarqube/ws/client
