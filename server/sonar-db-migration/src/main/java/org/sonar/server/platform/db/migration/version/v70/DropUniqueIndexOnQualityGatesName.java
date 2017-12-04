175a6c001cb165d6cf78644171c6a9b5548aabfc
Julien Lancelot <julien.lancelot@sonarsource.com>
2017-12-04 10:19:58 +0100

    SONAR-10134 Remove unique index on QUALITY_GATES.NAME
    As quality gates are now associated to organization, it should be possible to use the same name in different organizations

--- server/sonar-db-migration/src/main/java/org/sonar/server/platform/db/migration/version/v70/DropUniqueIndexOnQualityGatesName.java
+++ server/sonar-db-migration/src/main/java/org/sonar/server/platform/db/migration/version/v70/DropUniqueIndexOnQualityGatesName.java
@@ -19,23 +19,22 @@
  */
 package org.sonar.server.platform.db.migration.version.v70;
 
-import org.junit.Test;
+import java.sql.SQLException;
+import org.sonar.db.Database;
+import org.sonar.server.platform.db.migration.sql.DropIndexBuilder;
+import org.sonar.server.platform.db.migration.step.DdlChange;
 
-import static org.sonar.server.platform.db.migration.version.DbVersionTestUtils.verifyMigrationCount;
-import static org.sonar.server.platform.db.migration.version.DbVersionTestUtils.verifyMinimumMigrationNumber;
+public class DropUniqueIndexOnQualityGatesName extends DdlChange {
 
-public class DbVersion70Test {
-
-  private DbVersion70 underTest = new DbVersion70();
-
-  @Test
-  public void migrationNumber_starts_at_1900() {
-    verifyMinimumMigrationNumber(underTest, 1900);
+  public DropUniqueIndexOnQualityGatesName(Database db) {
+    super(db);
   }
 
-  @Test
-  public void verify_migration_count() {
-    verifyMigrationCount(underTest, 16);
+  @Override
+  public void execute(Context context) throws SQLException {
+    context.execute(new DropIndexBuilder(getDialect())
+      .setTable("quality_gates")
+      .setName("uniq_quality_gates")
+      .build());
   }
-
 }
