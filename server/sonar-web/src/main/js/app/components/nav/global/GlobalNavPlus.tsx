8d88cb446dbcec57de524357c6412b741a999614
Stas Vilchik <stas.vilchik@sonarsource.com>
2017-12-07 14:27:05 +0100

    SONAR-9554 Make "Analyze a project" and "Create an org" more discoverable

---
+++ server/sonar-web/src/main/js/app/components/nav/global/GlobalNavPlus.tsx
@@ -0,0 +1,101 @@
+/*
+ * SonarQube
+ * Copyright (C) 2009-2017 SonarSource SA
+ * mailto:contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+import * as React from 'react';
+import * as classNames from 'classnames';
+import * as PropTypes from 'prop-types';
+import CreateOrganizationForm from '../../../../apps/account/organizations/CreateOrganizationForm';
+import PlusIcon from '../../../../components/icons-components/PlusIcon';
+import Dropdown from '../../../../components/controls/Dropdown';
+import { translate } from '../../../../helpers/l10n';
+
+interface Props {
+  openOnboardingTutorial: () => void;
+}
+
+interface State {
+  createOrganization: boolean;
+}
+
+export default class GlobalNavPlus extends React.PureComponent<Props, State> {
+  static contextTypes = {
+    router: PropTypes.object
+  };
+
+  constructor(props: Props) {
+    super(props);
+    this.state = { createOrganization: false };
+  }
+
+  handleNewProjectClick = (event: React.SyntheticEvent<HTMLAnchorElement>) => {
+    event.preventDefault();
+    this.props.openOnboardingTutorial();
+  };
+
+  openCreateOrganizationForm = () => this.setState({ createOrganization: true });
+
+  closeCreateOrganizationForm = () => this.setState({ createOrganization: false });
+
+  handleNewOrganizationClick = (event: React.SyntheticEvent<HTMLAnchorElement>) => {
+    event.preventDefault();
+    event.currentTarget.blur();
+    this.openCreateOrganizationForm();
+  };
+
+  handleCreateOrganization = ({ key }: { key: string }) => {
+    this.closeCreateOrganizationForm();
+    this.context.router.push(`/organizations/${key}`);
+  };
+
+  render() {
+    return (
+      <Dropdown>
+        {({ onToggleClick, open }) => (
+          <li className={classNames('dropdown', { open })}>
+            <a className="navbar-plus" href="#" onClick={onToggleClick}>
+              <PlusIcon />
+            </a>
+            <ul className="dropdown-menu dropdown-menu-right">
+              <li>
+                <a className="js-new-project" href="#" onClick={this.handleNewProjectClick}>
+                  {translate('my_account.analyze_new_project')}
+                </a>
+              </li>
+              <li className="divider" />
+              <li>
+                <a
+                  className="js-new-organization"
+                  href="#"
+                  onClick={this.handleNewOrganizationClick}>
+                  {translate('my_account.create_new_organization')}
+                </a>
+              </li>
+            </ul>
+            {this.state.createOrganization && (
+              <CreateOrganizationForm
+                onClose={this.closeCreateOrganizationForm}
+                onCreate={this.handleCreateOrganization}
+              />
+            )}
+          </li>
+        )}
+      </Dropdown>
+    );
+  }
+}
---
+++ server/sonar-web/src/main/js/components/controls/Dropdown.tsx
@@ -0,0 +1,92 @@
+/*
+ * SonarQube
+ * Copyright (C) 2009-2017 SonarSource SA
+ * mailto:contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+import * as React from 'react';
+
+interface RenderProps {
+  closeDropdown: () => void;
+  onToggleClick: (event: React.SyntheticEvent<HTMLElement>) => void;
+  open: boolean;
+}
+
+interface Props {
+  children: (renderProps: RenderProps) => JSX.Element;
+  onOpen?: () => void;
+}
+
+interface State {
+  open: boolean;
+}
+
+export default class Dropdown extends React.PureComponent<Props, State> {
+  toggleNode?: HTMLElement;
+
+  constructor(props: Props) {
+    super(props);
+    this.state = { open: false };
+  }
+
+  componentDidUpdate(_: Props, prevState: State) {
+    if (!prevState.open && this.state.open) {
+      this.addClickHandler();
+      if (this.props.onOpen) {
+        this.props.onOpen();
+      }
+    }
+
+    if (prevState.open && !this.state.open) {
+      this.removeClickHandler();
+    }
+  }
+
+  componentWillUnmount() {
+    this.removeClickHandler();
+  }
+
+  addClickHandler = () => {
+    window.addEventListener('click', this.handleWindowClick);
+  };
+
+  removeClickHandler = () => {
+    window.removeEventListener('click', this.handleWindowClick);
+  };
+
+  handleWindowClick = (event: MouseEvent) => {
+    if (!this.toggleNode || !this.toggleNode.contains(event.target as Node)) {
+      this.closeDropdown();
+    }
+  };
+
+  closeDropdown = () => this.setState({ open: false });
+
+  handleToggleClick = (event: React.SyntheticEvent<HTMLElement>) => {
+    this.toggleNode = event.currentTarget;
+    event.preventDefault();
+    event.currentTarget.blur();
+    this.setState(state => ({ open: !state.open }));
+  };
+
+  render() {
+    return this.props.children({
+      closeDropdown: this.closeDropdown,
+      onToggleClick: this.handleToggleClick,
+      open: this.state.open
+    });
+  }
+}
