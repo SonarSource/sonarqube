0d3c531f4a6ef72a1f0982808543273760ea05ba
Stas Vilchik <stas.vilchik@sonarsource.com>
2017-12-11 17:59:17 +0100

    SONAR-10186 Switch between organizations from the header

--- server/sonar-web/src/main/js/apps/organizations/navigation/OrganizationNavigation.tsx
+++ server/sonar-web/src/main/js/apps/organizations/navigation/OrganizationNavigation.tsx
@@ -18,222 +18,25 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 import * as React from 'react';
-import * as classNames from 'classnames';
-import { Link } from 'react-router';
+import OrganizationNavigationHeaderContainer from './OrganizationNavigationHeaderContainer';
+import OrganizationNavigationMeta from './OrganizationNavigationMeta';
+import OrganizationNavigationMenu from './OrganizationNavigationMenu';
 import * as theme from '../../../app/theme';
-import { translate } from '../../../helpers/l10n';
 import ContextNavBar from '../../../components/nav/ContextNavBar';
-import NavBarTabs from '../../../components/nav/NavBarTabs';
-import OrganizationAvatar from '../../../components/common/OrganizationAvatar';
-import { getQualityGatesUrl } from '../../../helpers/urls';
-import { Extension, Organization } from '../../../app/types';
-
-const ADMIN_PATHS = [
-  'edit',
-  'groups',
-  'delete',
-  'permissions',
-  'permission_templates',
-  'projects_management'
-];
+import { Organization } from '../../../app/types';
+import './OrganizationNavigation.css';
 
 interface Props {
   location: { pathname: string };
   organization: Organization;
 }
 
-export default class OrganizationNavigation extends React.PureComponent<Props> {
-  renderAdministration(adminActive: boolean) {
-    const { organization } = this.props;
-
-    return (
-      <li className="dropdown">
-        <a
-          className={classNames('dropdown-toggle', { active: adminActive })}
-          data-toggle="dropdown"
-          id="organization-navigation-admin"
-          href="#">
-          {translate('layout.settings')}&nbsp;<i className="icon-dropdown" />
-        </a>
-        <ul className="dropdown-menu">
-          {this.renderAdminExtensions()}
-          <li>
-            <Link to={`/organizations/${organization.key}/groups`} activeClassName="active">
-              {translate('user_groups.page')}
-            </Link>
-          </li>
-          <li>
-            <Link to={`/organizations/${organization.key}/permissions`} activeClassName="active">
-              {translate('permissions.page')}
-            </Link>
-          </li>
-          <li>
-            <Link
-              to={`/organizations/${organization.key}/permission_templates`}
-              activeClassName="active">
-              {translate('permission_templates')}
-            </Link>
-          </li>
-          <li>
-            <Link
-              to={`/organizations/${organization.key}/projects_management`}
-              activeClassName="active">
-              {translate('projects_management')}
-            </Link>
-          </li>
-          <li>
-            <Link to={`/organizations/${organization.key}/edit`} activeClassName="active">
-              {translate('edit')}
-            </Link>
-          </li>
-          {organization.canDelete && (
-            <li>
-              <Link to={`/organizations/${organization.key}/delete`} activeClassName="active">
-                {translate('delete')}
-              </Link>
-            </li>
-          )}
-        </ul>
-      </li>
-    );
-  }
-
-  renderAdminExtensions() {
-    const extensions = this.props.organization.adminPages || [];
-    return extensions.map(this.renderExtension);
-  }
-
-  renderExtension = (extension: Extension) => {
-    const { organization } = this.props;
-    const pathname = `/organizations/${organization.key}/extension/${extension.key}`;
-    return (
-      <li key={extension.key}>
-        <Link to={pathname} activeClassName="active">
-          {extension.name}
-        </Link>
-      </li>
-    );
-  };
-
-  renderExtensions(moreActive: boolean) {
-    const extensions = this.props.organization.pages || [];
-    if (extensions.length > 0) {
-      return (
-        <li className={moreActive ? 'active' : ''}>
-          <a
-            className="dropdown-toggle"
-            id="organization-navigation-more"
-            data-toggle="dropdown"
-            href="#">
-            {translate('more')}&nbsp;<i className="icon-dropdown" />
-          </a>
-          <ul className="dropdown-menu">{extensions.map(this.renderExtension)}</ul>
-        </li>
-      );
-    } else {
-      return null;
-    }
-  }
-
-  render() {
-    const { organization, location } = this.props;
-
-    const isHomeActive =
-      location.pathname === `organizations/${organization.key}/projects` ||
-      location.pathname === `organizations/${organization.key}/projects/favorite`;
-
-    const adminPathsWithExtensions = (organization.adminPages || [])
-      .map(e => `extension/${e.key}`)
-      .concat(ADMIN_PATHS);
-
-    const adminActive = adminPathsWithExtensions.some(path =>
-      location.pathname.endsWith(`organizations/${organization.key}/${path}`)
-    );
-    const moreActive = !adminActive && location.pathname.includes('/extension/');
-
-    return (
-      <ContextNavBar id="context-navigation" height={theme.contextNavHeightRaw}>
-        <div className="navbar-context-header">
-          <h1 className="display-inline-block">
-            <OrganizationAvatar organization={organization} />
-            <Link
-              to={`/organizations/${organization.key}`}
-              className="link-base-color link-no-underline spacer-left">
-              {organization.name}
-            </Link>
-          </h1>
-          {organization.description != null && (
-            <div className="navbar-context-description">
-              <p className="text-limited text-top" title={organization.description}>
-                {organization.description}
-              </p>
-            </div>
-          )}
-        </div>
-
-        <div className="navbar-context-meta">
-          <div className="text-muted">
-            <strong>{translate('organization.key')}:</strong> {organization.key}
-          </div>
-          {organization.url != null && (
-            <div>
-              <p className="text-limited text-top">
-                <a
-                  className="link-underline"
-                  href={organization.url}
-                  title={organization.url}
-                  rel="nofollow">
-                  {organization.url}
-                </a>
-              </p>
-            </div>
-          )}
-        </div>
-
-        <NavBarTabs className="navbar-context-tabs">
-          <li>
-            <Link
-              to={`/organizations/${organization.key}/projects`}
-              className={isHomeActive ? 'active' : ''}>
-              {translate('projects.page')}
-            </Link>
-          </li>
-          <li>
-            <Link
-              to={{
-                pathname: `/organizations/${organization.key}/issues`,
-                query: { resolved: 'false' }
-              }}
-              activeClassName="active">
-              {translate('issues.page')}
-            </Link>
-          </li>
-          <li>
-            <Link
-              to={`/organizations/${organization.key}/quality_profiles`}
-              activeClassName="active">
-              {translate('quality_profiles.page')}
-            </Link>
-          </li>
-          <li>
-            <Link to={`/organizations/${organization.key}/rules`} activeClassName="active">
-              {translate('coding_rules.page')}
-            </Link>
-          </li>
-          <li>
-            <Link to={getQualityGatesUrl(organization.key)} activeClassName="active">
-              {translate('quality_gates.page')}
-            </Link>
-          </li>
-          <li>
-            <Link to={`/organizations/${organization.key}/members`} activeClassName="active">
-              {translate('organization.members.page')}
-            </Link>
-          </li>
-          {this.renderExtensions(moreActive)}
-          {organization.canAdmin && this.renderAdministration(adminActive)}
-        </NavBarTabs>
-      </ContextNavBar>
-    );
-  }
+export default function OrganizationNavigation({ organization, location }: Props) {
+  return (
+    <ContextNavBar id="context-navigation" height={theme.contextNavHeightRaw}>
+      <OrganizationNavigationHeaderContainer organization={organization} />
+      <OrganizationNavigationMeta organization={organization} />
+      <OrganizationNavigationMenu location={location} organization={organization} />
+    </ContextNavBar>
+  );
 }
