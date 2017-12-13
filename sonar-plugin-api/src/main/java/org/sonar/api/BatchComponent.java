e556bdc7e5373f9c524bd0d758b1f2ec4aa0e69f
Duarte Meneses <duarte.meneses@sonarsource.com>
2017-12-13 15:14:35 +0100

    Reintroduce SonarPlugin, BatchComponent and ServerComponent

---
+++ sonar-plugin-api/src/main/java/org/sonar/api/BatchComponent.java
@@ -0,0 +1,46 @@
+/*
+ * SonarQube
+ * Copyright (C) 2009-2017 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.api;
+
+import org.sonar.api.batch.ScannerSide;
+
+/**
+ * Marker interface for all the components available in container of batch (code analyzer). Note that
+ * injection of dependencies by constructor is used :
+ * <pre>
+ *   public class Foo implements BatchComponent {
+ *
+ *   }
+ *   public class Bar implements BatchComponent {
+ *     private final Foo foo;
+ *     public Bar(Foo f) {
+ *       this.foo = f;
+ *     }
+ *   }
+ *
+ * </pre>
+ *
+ * @since 2.2
+ * @deprecated since 5.2 use {@link ScannerSide} annotation
+ */
+@Deprecated
+@ScannerSide
+public interface BatchComponent {
+}
---
+++ sonar-plugin-api/src/main/java/org/sonar/api/SonarPlugin.java
@@ -0,0 +1,53 @@
+/*
+ * SonarQube
+ * Copyright (C) 2009-2017 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.api;
+
+import java.util.List;
+
+/**
+ * Plugin entry-point used to declare its extensions (see {@link org.sonar.api.Extension}.
+ * <br>
+ * <p>The JAR manifest must declare the name of the implementation class in the property <code>Plugin-Class</code>.
+ * This property is automatically set by sonar-packaging-maven-plugin when building plugin.
+ * 
+ * @since 2.8
+ * @deprecated replaced by {@link Plugin} in version 5.6.
+ */
+@Deprecated
+public abstract class SonarPlugin implements Plugin {
+
+  /**
+   * Classes of the implemented extensions.
+   */
+  public abstract List getExtensions();
+
+  /**
+   * Returns a string representation of the plugin, suitable for debugging purposes only.
+   */
+  @Override
+  public String toString() {
+    return getClass().getSimpleName();
+  }
+
+  @Override
+  public void define(Context context) {
+    context.addExtensions(getExtensions());
+  }
+}
