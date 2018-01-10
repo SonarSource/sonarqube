e5b66ff066b047ac810cbf251fd44e3015503c00
Julien Lancelot <julien.lancelot@sonarsource.com>
2018-01-10 15:04:43 +0100

    SONAR-10266 Create stub for api/project_badges WS

---
+++ server/sonar-server/src/main/java/org/sonar/server/badge/ws/ProjectBadgesWsModule.java
@@ -0,0 +1,45 @@
+/*
+ * SonarQube
+ * Copyright (C) 2009-2018 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.server.badge.ws;
+
+import org.sonar.api.config.Configuration;
+import org.sonar.core.config.WebConstants;
+import org.sonar.core.platform.Module;
+
+public class ProjectBadgesWsModule extends Module {
+
+  private final Configuration config;
+
+  public ProjectBadgesWsModule(Configuration config) {
+    this.config = config;
+  }
+
+  @Override
+  protected void configureModule() {
+    if (!config.getBoolean(WebConstants.SONARCLOUD_ENABLED).orElse(false)) {
+      return;
+    }
+    add(
+      ProjectBadgesWs.class,
+      QualityGateAction.class,
+      MeasureAction.class
+      );
+  }
+}
