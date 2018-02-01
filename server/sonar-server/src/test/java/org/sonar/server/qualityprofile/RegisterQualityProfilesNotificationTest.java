bb70f75c1c0d8dd2221009b7fcc779ed70f196c6
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2018-02-01 15:56:34 +0100

    fix flacky test in RegisterQualityProfilesNotificationTest

--- server/sonar-server/src/test/java/org/sonar/server/qualityprofile/RegisterQualityProfilesNotificationTest.java
+++ server/sonar-server/src/test/java/org/sonar/server/qualityprofile/RegisterQualityProfilesNotificationTest.java
@@ -229,7 +229,7 @@ public class RegisterQualityProfilesNotificationTest {
     OrganizationDto organization = db.organizations().insert();
 
     QProfileDto builtInProfile = insertProfile(organization, orgQProfile -> orgQProfile.setIsBuiltIn(true).setLanguage(language));
-    db.qualityProfiles().activateRule(builtInProfile, rule);
+    db.qualityProfiles().activateRule(builtInProfile, rule, ar -> ar.setSeverity(Severity.MINOR));
     QProfileDto childProfile = insertProfile(organization, orgQProfile -> orgQProfile.setIsBuiltIn(false).setLanguage(language).setParentKee(builtInProfile.getKee()));
     db.qualityProfiles().activateRule(childProfile, rule, ar -> ar.setInheritance(ActiveRuleDto.INHERITED).setSeverity(Severity.MINOR));
     addPluginProfile(builtInProfile, rule);
