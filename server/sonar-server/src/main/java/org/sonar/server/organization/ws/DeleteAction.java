2a9f40d54e6a78643cdeaff2d64510d861c452f5
Guillaume Jambet <guillaume.jambet@sonarsource.com>
2018-02-13 17:11:00 +0100

    SONAR-10345 Ensure that no orphaned webhooks remains when deleting an organization or a project.

--- server/sonar-server/src/main/java/org/sonar/server/organization/ws/DeleteAction.java
+++ server/sonar-server/src/main/java/org/sonar/server/organization/ws/DeleteAction.java
@@ -115,6 +115,7 @@ public class DeleteAction implements OrganizationsWsAction {
 
   private void deleteProjects(DbSession dbSession, OrganizationDto organization) {
     List<ComponentDto> roots = dbClient.componentDao().selectAllRootsByOrganization(dbSession, organization.getUuid());
+    roots.forEach(project ->  dbClient.webhookDao().cleanWebhooks(dbSession, project));
     componentCleanerService.delete(dbSession, roots);
   }
 
@@ -151,6 +152,7 @@ public class DeleteAction implements OrganizationsWsAction {
     dbClient.organizationMemberDao().deleteByOrganizationUuid(dbSession, organization.getUuid());
     dbClient.organizationDao().deleteByUuid(dbSession, organization.getUuid());
     dbClient.userDao().cleanHomepage(dbSession, organization);
+    dbClient.webhookDao().cleanWebhooks(dbSession, organization);
     userIndexer.commitAndIndexByLogins(dbSession, logins);
   }
 
