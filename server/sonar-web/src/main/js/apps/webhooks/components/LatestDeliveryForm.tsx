207a7f7a7e9a43885a4334ac0e01f00b008d8bb5
Grégoire Aubert <gregoire.aubert@sonarsource.com>
2018-02-19 15:30:33 +0100

    SONAR-10346 Add a shortcut to display the latest delivery of a webhook

---
+++ server/sonar-web/src/main/js/apps/webhooks/components/LatestDeliveryForm.tsx
@@ -0,0 +1,98 @@
+/*
+ * SonarQube
+ * Copyright (C) 2009-2018 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+import * as React from 'react';
+import DeliveryItem from './DeliveryItem';
+import Modal from '../../../components/controls/Modal';
+import { Webhook, WebhookDelivery } from '../../../app/types';
+import { translateWithParameters, translate } from '../../../helpers/l10n';
+import { getDelivery } from '../../../api/webhooks';
+
+interface Props {
+  delivery: WebhookDelivery;
+  onClose: () => void;
+  webhook: Webhook;
+}
+
+interface State {
+  loading: boolean;
+  payload?: string;
+}
+
+export default class LatestDeliveryForm extends React.PureComponent<Props, State> {
+  mounted = false;
+  state: State = { loading: true };
+
+  componentDidMount() {
+    this.mounted = true;
+    this.fetchPayload();
+  }
+
+  componentWillUnmount() {
+    this.mounted = false;
+  }
+
+  fetchPayload = ({ delivery } = this.props) => {
+    return getDelivery({ deliveryId: delivery.id }).then(
+      ({ delivery }) => {
+        if (this.mounted) {
+          this.setState({ payload: delivery.payload, loading: false });
+        }
+      },
+      () => {
+        if (this.mounted) {
+          this.setState({ loading: false });
+        }
+      }
+    );
+  };
+
+  formatPayload = (payload: string) => {
+    try {
+      return JSON.stringify(JSON.parse(payload), undefined, 2);
+    } catch (error) {
+      return payload;
+    }
+  };
+
+  render() {
+    const { delivery, webhook } = this.props;
+    const { loading, payload } = this.state;
+    const header = translateWithParameters('webhooks.latest_delivery_for_x', webhook.name);
+
+    return (
+      <Modal contentLabel={header} onRequestClose={this.props.onClose}>
+        <header className="modal-head">
+          <h2>{header}</h2>
+        </header>
+        <DeliveryItem
+          className="modal-body modal-container"
+          delivery={delivery}
+          loading={loading}
+          payload={payload}
+        />
+        <footer className="modal-foot">
+          <button className="button-link js-modal-close" onClick={this.props.onClose} type="button">
+            {translate('close')}
+          </button>
+        </footer>
+      </Modal>
+    );
+  }
+}
