9af977ca68388680c681942afc1eeeb82a8998ed
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2018-03-01 10:08:31 +0100

    GOV-322 support not creating task in CeQueue if pending task exists

---
+++ server/sonar-db-dao/src/main/java/org/sonar/db/ce/QueueCount.java
@@ -0,0 +1,34 @@
+/*
+ * SonarQube
+ * Copyright (C) 2009-2018 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.db.ce;
+
+public class QueueCount {
+  // set by reflection by MyBatis
+  private String componentUuid;
+  private int total;
+
+  public String getComponentUuid() {
+    return componentUuid;
+  }
+
+  public int getTotal() {
+    return total;
+  }
+}
