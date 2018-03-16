5997374259f173d14e7d82edc3ac0837df61d1b9
Pascal Mugnier <pascal.mugnier@sonarsource.com>
2018-03-16 09:18:02 +0100

    Add tests

---
+++ server/sonar-web/src/main/js/components/SelectList/__tests__/SelectList-test.tsx
@@ -0,0 +1,66 @@
+/*
+ * SonarQube
+ * Copyright (C) 2009-2018 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+import * as React from 'react';
+import { shallow } from 'enzyme';
+import SelectList, { Filter } from '../SelectList';
+import { waitAndUpdate } from '../../../helpers/testUtils';
+
+const selectList = (
+  <SelectList
+    elements={['foo', 'bar', 'baz']}
+    onSearch={jest.fn(() => Promise.resolve())}
+    onSelect={jest.fn(() => Promise.resolve())}
+    onUnselect={jest.fn(() => Promise.resolve())}
+    renderElement={(foo: string) => foo}
+    selectedElements={['foo']}
+  />
+);
+
+it('should display selected elements only by default', () => {
+  const wrapper = shallow(selectList);
+  expect(wrapper.state().filter).toBe(Filter.Selected);
+});
+
+it('should display a loader when searching', async () => {
+  const wrapper = shallow(selectList);
+  expect(wrapper).toMatchSnapshot();
+  expect(wrapper.state().loading).toBe(false);
+
+  (wrapper.instance() as SelectList).handleQueryChange('');
+  expect(wrapper.state().loading).toBe(true);
+  expect(wrapper).toMatchSnapshot();
+
+  await waitAndUpdate(wrapper);
+  expect(wrapper.state().loading).toBe(false);
+});
+
+it('should display a loader when updating filter', async () => {
+  const wrapper = shallow(selectList);
+  expect(wrapper).toMatchSnapshot();
+  expect(wrapper.state().loading).toBe(false);
+
+  (wrapper.instance() as SelectList).changeFilter(Filter.Unselected);
+  expect(wrapper.state().loading).toBe(true);
+  expect(wrapper).toMatchSnapshot();
+
+  await waitAndUpdate(wrapper);
+  expect(wrapper.state().filter).toBe(Filter.Unselected);
+  expect(wrapper.state().loading).toBe(false);
+});
