50ae43fae0068032d24f7cc73c9b8be2205836a7
Stas Vilchik <stas.vilchik@sonarsource.com>
2018-03-28 10:27:53 +0200

    update typescript to 2.8

--- server/sonar-web/src/main/js/app/types.ts
+++ server/sonar-web/src/main/js/app/types.ts
@@ -17,12 +17,7 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-
-// Diff / Omit taken from https://github.com/Microsoft/TypeScript/issues/12215#issuecomment-311923766
-export type Diff<T extends string, U extends string> = ({ [P in T]: P } &
-  { [P in U]: never } & { [x: string]: never })[T];
-
-export type Omit<T, K extends keyof T> = Pick<T, Diff<keyof T, K>>;
+export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 
 // Type ordered alphabetically to prevent merge conflicts
 
