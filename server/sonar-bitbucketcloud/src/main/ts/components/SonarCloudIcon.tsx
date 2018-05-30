145304519c70750a4253b5b242d7d67e725a0d7d
Grégoire Aubert <gregoire.aubert@sonarsource.com>
2018-05-30 10:11:11 +0200

    Fix bitbucket-cloud extension build

--- server/sonar-bitbucketcloud/src/main/ts/components/SonarCloudIcon.tsx
+++ server/sonar-bitbucketcloud/src/main/ts/components/SonarCloudIcon.tsx
@@ -18,7 +18,7 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 import * as React from 'react';
-import { IconProps } from '@sqcore/components/icons-components/types';
+import { IconProps } from '@sqcore/components/icons-components/Icon';
 
 export default function SonarCloudIcon({ className, fill = '#f3702a', size = 18 }: IconProps) {
   return (
