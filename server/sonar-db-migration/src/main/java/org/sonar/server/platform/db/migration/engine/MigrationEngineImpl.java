ccf2766838c80bcd45c28e2bb6594fc6a2d92fac
Simon Brandhof <simon.brandhof@sonarsource.com>
2018-06-12 23:20:55 +0200

    SONARCLOUD-75 Support DB migrations compatible with blue/green deployment

--- server/sonar-db-migration/src/main/java/org/sonar/server/platform/db/migration/engine/MigrationEngineImpl.java
+++ server/sonar-db-migration/src/main/java/org/sonar/server/platform/db/migration/engine/MigrationEngineImpl.java
@@ -19,40 +19,65 @@
  */
 package org.sonar.server.platform.db.migration.engine;
 
+import java.util.List;
 import java.util.Optional;
+import org.sonar.api.config.Configuration;
+import org.sonar.api.utils.AnnotationUtils;
 import org.sonar.core.platform.ComponentContainer;
+import org.sonar.process.ProcessProperties;
+import org.sonar.server.platform.db.migration.SupportsBlueGreen;
 import org.sonar.server.platform.db.migration.history.MigrationHistory;
 import org.sonar.server.platform.db.migration.step.MigrationSteps;
 import org.sonar.server.platform.db.migration.step.MigrationStepsExecutor;
+import org.sonar.server.platform.db.migration.step.RegisteredMigrationStep;
+
+import static java.lang.String.format;
 
 public class MigrationEngineImpl implements MigrationEngine {
   private final MigrationHistory migrationHistory;
   private final ComponentContainer serverContainer;
   private final MigrationContainerPopulator populator;
   private final MigrationSteps migrationSteps;
+  private final Configuration configuration;
 
   public MigrationEngineImpl(MigrationHistory migrationHistory, ComponentContainer serverContainer,
-    MigrationContainerPopulator populator, MigrationSteps migrationSteps) {
+    MigrationContainerPopulator populator, MigrationSteps migrationSteps, Configuration configuration) {
     this.migrationHistory = migrationHistory;
     this.serverContainer = serverContainer;
     this.populator = populator;
     this.migrationSteps = migrationSteps;
+    this.configuration = configuration;
   }
 
   @Override
   public void execute() {
     MigrationContainer migrationContainer = new MigrationContainerImpl(serverContainer, populator);
-
+    boolean blueGreen = configuration.getBoolean(ProcessProperties.Property.BLUE_GREEN_ENABLED.getKey()).orElse(false);
     try {
       MigrationStepsExecutor stepsExecutor = migrationContainer.getComponentByType(MigrationStepsExecutor.class);
       Optional<Long> lastMigrationNumber = migrationHistory.getLastMigrationNumber();
-      if (lastMigrationNumber.isPresent()) {
-        stepsExecutor.execute(migrationSteps.readFrom(lastMigrationNumber.get() + 1));
-      } else {
-        stepsExecutor.execute(migrationSteps.readAll());
+
+      List<RegisteredMigrationStep> steps = lastMigrationNumber
+        .map(i -> migrationSteps.readFrom(i + 1))
+        .orElse(migrationSteps.readAll());
+
+      if (blueGreen) {
+        ensureSupportBlueGreen(steps);
       }
+
+      stepsExecutor.execute(steps);
+
     } finally {
       migrationContainer.cleanup();
+    }
+  }
+
+  private static void ensureSupportBlueGreen(List<RegisteredMigrationStep> steps) {
+    for (RegisteredMigrationStep step : steps) {
+      if (AnnotationUtils.getAnnotation(step.getStepClass(), SupportsBlueGreen.class) == null) {
+        throw new IllegalStateException(format("All migrations canceled. #%d does not support blue/green deployment: %s",
+          step.getMigrationNumber(), step.getDescription()));
+      }
     }
   }
 }
--- server/sonar-db-migration/src/test/java/org/sonar/server/platform/db/migration/engine/MigrationEngineImplTest.java
+++ server/sonar-db-migration/src/test/java/org/sonar/server/platform/db/migration/engine/MigrationEngineImplTest.java
@@ -19,20 +19,28 @@
  */
 package org.sonar.server.platform.db.migration.engine;
 
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 import org.junit.Test;
-import org.sonar.api.config.Configuration;
+import org.sonar.api.config.internal.ConfigurationBridge;
+import org.sonar.api.config.internal.MapSettings;
 import org.sonar.core.platform.ComponentContainer;
+import org.sonar.process.ProcessProperties;
+import org.sonar.server.platform.db.migration.SupportsBlueGreen;
 import org.sonar.server.platform.db.migration.history.MigrationHistory;
 import org.sonar.server.platform.db.migration.step.MigrationStep;
 import org.sonar.server.platform.db.migration.step.MigrationSteps;
 import org.sonar.server.platform.db.migration.step.MigrationStepsExecutor;
 import org.sonar.server.platform.db.migration.step.RegisteredMigrationStep;
 
+import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.fail;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 
 public class MigrationEngineImplTest {
@@ -47,8 +55,8 @@ public class MigrationEngineImplTest {
   };
   private MigrationSteps migrationSteps = mock(MigrationSteps.class);
 
-  private Configuration configuration;
-  private MigrationEngineImpl underTest = new MigrationEngineImpl(migrationHistory, serverContainer, populator, migrationSteps, configuration);
+  private MapSettings settings = new MapSettings();
+  private MigrationEngineImpl underTest = new MigrationEngineImpl(migrationHistory, serverContainer, populator, migrationSteps, new ConfigurationBridge(settings));
 
   @Test
   public void execute_execute_all_steps_of_there_is_no_last_migration_number() {
@@ -74,4 +82,43 @@ public class MigrationEngineImplTest {
     verify(stepsExecutor).execute(steps);
   }
 
+  @Test
+  public void execute_steps_in_blue_green_mode() {
+    settings.setProperty(ProcessProperties.Property.BLUE_GREEN_ENABLED.getKey(), true);
+    when(migrationHistory.getLastMigrationNumber()).thenReturn(Optional.of(50L));
+    List<RegisteredMigrationStep> steps = singletonList(new RegisteredMigrationStep(1, "doo", TestBlueGreenMigrationStep.class));
+    when(migrationSteps.readFrom(51)).thenReturn(steps);
+
+    underTest.execute();
+
+    verify(migrationSteps).readFrom(51);
+    verify(stepsExecutor).execute(steps);
+  }
+
+  @Test
+  public void fail_blue_green_execution_if_some_migrations_are_not_compatible() {
+    settings.setProperty(ProcessProperties.Property.BLUE_GREEN_ENABLED.getKey(), true);
+    when(migrationHistory.getLastMigrationNumber()).thenReturn(Optional.of(50L));
+    List<RegisteredMigrationStep> steps = asList(
+      new RegisteredMigrationStep(1, "foo", TestBlueGreenMigrationStep.class),
+      new RegisteredMigrationStep(2, "bar", MigrationStep.class));
+    when(migrationSteps.readFrom(51)).thenReturn(steps);
+
+    try {
+      underTest.execute();
+      fail();
+    } catch (IllegalStateException e) {
+      assertThat(e).hasMessage("All migrations canceled. #2 does not support blue/green deployment: bar");
+      verifyZeroInteractions(stepsExecutor);
+    }
+  }
+
+  @SupportsBlueGreen
+  private static class TestBlueGreenMigrationStep implements MigrationStep {
+
+    @Override
+    public void execute() throws SQLException {
+
+    }
+  }
 }
