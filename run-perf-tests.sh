8c7686ec1e25d01f9b5a07077b01a27ecfe67f12
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2018-07-02 09:57:31 +0200

    move core integration test documentation to private

--- run-perf-tests.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-echo 'Run performance tests'
-
-./gradlew --no-daemon --console plain \
-  :tests:integrationTest \
-  -Dcategory=ServerPerformance  \
-  $*
