cd502674ecba9a621cc428155e079b77d3838611
Julien Lancelot <julien.lancelot@sonarsource.com>
2018-07-19 08:51:32 +0200

    Fix JDK download URL (new try)

--- travis.sh
+++ travis.sh
@@ -10,10 +10,10 @@ set -euo pipefail
 # at each build.
 #
 installJdk8() {
-  echo "Setup JDK 1.8u171"
+  echo "Setup JDK 1.8u181"
   mkdir -p ~/jvm
   pushd ~/jvm > /dev/null
-  if [ ! -d "jdk1.8.0_171" ]; then
+  if [ ! -d "jdk1.8.0_181" ]; then
     wget --quiet --continue --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u181-b13/96a7b8442fe848ef90c96a2fad6ed6d1/jdk-8u181-linux-x64.tar.gz
     tar xzf jdk-8u171-linux-x64.tar.gz
     rm jdk-8u171-linux-x64.tar.gz
