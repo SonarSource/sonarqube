2f441edda5ef3bd807bfd04d1e7d7f663d38f812
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2018-07-20 16:39:39 +0200

    remove useless abstract class BaseUserSession

--- server/sonar-server/src/main/java/org/sonar/server/user/AbstractUserSession.java
+++ server/sonar-server/src/main/java/org/sonar/server/user/AbstractUserSession.java
@@ -19,6 +19,12 @@
  */
 package org.sonar.server.user;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+import org.sonar.core.permission.ProjectPermissions;
+import org.sonar.core.util.stream.MoreCollectors;
 import org.sonar.db.component.ComponentDto;
 import org.sonar.db.organization.OrganizationDto;
 import org.sonar.db.permission.OrganizationPermission;
@@ -26,11 +32,72 @@ import org.sonar.server.exceptions.ForbiddenException;
 import org.sonar.server.exceptions.UnauthorizedException;
 
 import static java.lang.String.format;
+import static org.apache.commons.lang.StringUtils.defaultString;
 
-public abstract class AbstractUserSession extends BaseUserSession {
+public abstract class AbstractUserSession implements UserSession {
   private static final String INSUFFICIENT_PRIVILEGES_MESSAGE = "Insufficient privileges";
   private static final ForbiddenException INSUFFICIENT_PRIVILEGES_EXCEPTION = new ForbiddenException(INSUFFICIENT_PRIVILEGES_MESSAGE);
   private static final String AUTHENTICATION_IS_REQUIRED_MESSAGE = "Authentication is required";
+  @Override
+  public final boolean hasPermission(OrganizationPermission permission, OrganizationDto organization) {
+    return hasPermission(permission, organization.getUuid());
+  }
+
+  @Override
+  public final boolean hasPermission(OrganizationPermission permission, String organizationUuid) {
+    return isRoot() || hasPermissionImpl(permission, organizationUuid);
+  }
+
+  protected abstract boolean hasPermissionImpl(OrganizationPermission permission, String organizationUuid);
+
+  @Override
+  public final boolean hasComponentPermission(String permission, ComponentDto component) {
+    if (isRoot()) {
+      return true;
+    }
+    String projectUuid = defaultString(component.getMainBranchProjectUuid(), component.projectUuid());
+    return hasProjectUuidPermission(permission, projectUuid);
+  }
+
+  @Override
+  public final boolean hasComponentUuidPermission(String permission, String componentUuid) {
+    if (isRoot()) {
+      return true;
+    }
+    Optional<String> projectUuid = componentUuidToProjectUuid(componentUuid);
+    return projectUuid
+      .map(s -> hasProjectUuidPermission(permission, s))
+      .orElse(false);
+  }
+
+  protected abstract Optional<String> componentUuidToProjectUuid(String componentUuid);
+
+  protected abstract boolean hasProjectUuidPermission(String permission, String projectUuid);
+
+  @Override
+  public final boolean hasMembership(OrganizationDto organization) {
+    return isRoot() || hasMembershipImpl(organization);
+  }
+
+  protected abstract boolean hasMembershipImpl(OrganizationDto organization);
+
+  @Override
+  public final List<ComponentDto> keepAuthorizedComponents(String permission, Collection<ComponentDto> components) {
+    if (isRoot()) {
+      return new ArrayList<>(components);
+    }
+    return doKeepAuthorizedComponents(permission, components);
+  }
+
+  /**
+   * Naive implementation, to be overridden if needed
+   */
+  protected List<ComponentDto> doKeepAuthorizedComponents(String permission, Collection<ComponentDto> components) {
+    boolean allowPublicComponent = ProjectPermissions.PUBLIC_PERMISSIONS.contains(permission);
+    return components.stream()
+      .filter(c -> (allowPublicComponent && !c.isPrivate()) || hasComponentPermission(permission, c))
+      .collect(MoreCollectors.toList());
+  }
 
   @Override
   public UserSession checkIsRoot() {
