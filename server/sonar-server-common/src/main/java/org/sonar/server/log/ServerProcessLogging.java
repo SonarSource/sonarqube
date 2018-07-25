4fcf3b2397168654b79dff96b624c5d78f172014
Simon Brandhof <simon.brandhof@sonarsource.com>
2018-07-25 09:54:59 +0200

    SONAR-11077 disable some FOP and Hazelcast DEBUG logs

--- server/sonar-server-common/src/main/java/org/sonar/server/log/ServerProcessLogging.java
+++ server/sonar-server-common/src/main/java/org/sonar/server/log/ServerProcessLogging.java
@@ -53,6 +53,7 @@ public abstract class ServerProcessLogging {
     "com.microsoft.sqlserver.jdbc.ResultSet",
     "com.microsoft.sqlserver.jdbc.Statement",
     "com.microsoft.sqlserver.jdbc.Connection");
+
   private final ProcessId processId;
   private final String threadIdFieldPattern;
   private final LogbackHelper helper = new LogbackHelper();
@@ -81,7 +82,19 @@ public abstract class ServerProcessLogging {
     builder.immutableLevel("org.apache.tomcat", Level.INFO);
     builder.immutableLevel("org.postgresql.core.v3.QueryExecutorImpl", Level.INFO);
     builder.immutableLevel("org.postgresql.jdbc.PgConnection", Level.INFO);
-
+    // Apache FOP
+    builder.immutableLevel("org.apache.fop.render.ImageHandlerRegistry", Level.INFO);
+    builder.immutableLevel("org.apache.fop.render.RendererFactory", Level.INFO);
+    builder.immutableLevel("org.apache.fop.render.XMLHandlerRegistry", Level.INFO);
+    builder.immutableLevel("org.apache.fop.util.ContentHandlerFactoryRegistry", Level.INFO);
+    builder.immutableLevel("org.apache.xmlgraphics.image.loader.spi.ImageImplRegistry", Level.INFO);
+    // Hazelcast
+    builder.immutableLevel("com.hazelcast.internal.cluster.impl.ClusterHeartbeatManager", Level.INFO);
+    builder.immutableLevel("com.hazelcast.internal.cluster.impl.operations.HeartbeatOperation", Level.INFO);
+    builder.immutableLevel("com.hazelcast.internal.partition.InternalPartitionService", Level.INFO);
+    builder.immutableLevel("com.hazelcast.internal.partition.operation.PartitionStateOperation", Level.INFO);
+    builder.immutableLevel("com.hazelcast.replicatedmap.impl.operation.RequestMapDataOperation", Level.INFO);
+    builder.immutableLevel("com.hazelcast.replicatedmap.impl.operation.SyncReplicatedMapDataOperation", Level.INFO);
     extendLogLevelConfiguration(builder);
 
     return builder.build();
