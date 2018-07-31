d6052456d0555a6a8c7b4d7335f9ba7622fdd5a3
Grégoire Aubert <gregoire.aubert@sonarsource.com>
2018-07-31 11:40:51 +0200

    SONAR-11037 Update icons for alm repository items in the project creationg page

--- server/sonar-web/src/main/js/apps/projects/create/AlmRepositoryItem.tsx
+++ server/sonar-web/src/main/js/apps/projects/create/AlmRepositoryItem.tsx
@@ -18,11 +18,12 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 import * as React from 'react';
+import * as theme from '../../../app/theme';
 import Checkbox from '../../../components/controls/Checkbox';
+import CheckIcon from '../../../components/icons-components/CheckIcon';
 import { AlmRepository, IdentityProvider } from '../../../app/types';
 import { getBaseUrl } from '../../../helpers/urls';
 import { translate } from '../../../helpers/l10n';
-import CheckIcon from '../../../components/icons-components/CheckIcon';
 
 interface Props {
   identityProvider: IdentityProvider;
@@ -48,14 +49,14 @@ export default class AlmRepositoryItem extends React.PureComponent<Props> {
           alt={identityProvider.name}
           className="spacer-left"
           height={14}
-          src={getBaseUrl() + identityProvider.iconPath}
-          style={{ filter: alreadyImported ? 'invert(50%)' : 'invert(100%)' }}
+          src={`${getBaseUrl()}/images/sonarcloud/${identityProvider.key}.svg`}
+          style={{ opacity: alreadyImported ? 0.5 : 1 }}
           width={14}
         />
         <span className="spacer-left">{this.props.repository.label}</span>
         {alreadyImported && (
           <span className="big-spacer-left">
-            <CheckIcon className="little-spacer-right" />
+            <CheckIcon className="little-spacer-right" fill={theme.green} />
             {translate('onboarding.create_project.already_imported')}
           </span>
         )}
