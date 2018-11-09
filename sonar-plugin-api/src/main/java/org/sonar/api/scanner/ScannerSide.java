0348fbfef3d9268003ffda07a81b85ec9a35b97c
Julien HENRY <julien.henry@sonarsource.com>
2018-11-09 10:35:06 +0100

    SONAR-11465 Deprecate InstantiationStrategy

--- sonar-plugin-api/src/main/java/org/sonar/api/scanner/ScannerSide.java
+++ sonar-plugin-api/src/main/java/org/sonar/api/scanner/ScannerSide.java
@@ -1,6 +1,6 @@
 /*
  * SonarQube
- * Copyright (C) 2009-2019 SonarSource SA
+ * Copyright (C) 2009-2018 SonarSource SA
  * mailto:info AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
@@ -17,38 +17,36 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-package org.sonar.api.batch;
+package org.sonar.api.scanner;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Define instantiation strategy of batch IoC components. If a component is not annotated, then default value
- * is {@link org.sonar.api.batch.InstantiationStrategy#PER_PROJECT}.
- * @since 4.4
- * @deprecated since 7.6 removal of modules and tasks is planned. All extensions should progressively move to the project scope, using {@link org.sonar.api.scanner.ScannerSide}
+ * Marker annotation for all the components available in the container of the scanner (code analyzer). Note that
+ * injection of dependencies by constructor is used :
+ * <pre>
+ *   {@literal @}ScannerSide
+ *   public class Foo {
+ *
+ *   }
+ *   {@literal @}ScannerSide
+ *   public class Bar {
+ *     private final Foo foo;
+ *     public Bar(Foo f) {
+ *       this.foo = f;
+ *     }
+ *   }
+ *
+ * </pre>
+ *
+ * @since 7.6
  */
-@Deprecated
+@Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-public @interface InstantiationStrategy {
-
-  /**
-   * Shared task extension. Available in task container.
-   */
-  String PER_TASK = "PER_TASK";
-
-  /**
-   * Shared extension. Available in top level project container.
-   */
-  String PER_BATCH = "PER_BATCH";
-
-  /**
-   * Created and initialized for each project and sub-project (a project is a module in Maven terminology).
-   */
-  String PER_PROJECT = "PER_PROJECT";
-
-  String value();
+public @interface ScannerSide {
 }
