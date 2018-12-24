a9fd4eb48b2ae984bdcac4e2e8ebdd16d5ac3a76
Wouter Admiraal <wouter.admiraal@sonarsource.com>
2018-12-24 10:37:04 +0100

    Remove unused imports

--- server/sonar-web/src/main/js/apps/project-admin/store/actions.js
+++ server/sonar-web/src/main/js/apps/project-admin/store/actions.js
@@ -17,7 +17,6 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-import { getProjectLinks, createLink, deleteLink } from '../../../api/projectLinks';
 import { getTree, changeKey as changeKeyApi } from '../../../api/components';
 import throwGlobalError from '../../../app/utils/throwGlobalError';
 
