321bbc90919cc7c1c0e56d1dbcd0c526fc81d44b
Julien Lancelot <julien.lancelot@sonarsource.com>
2019-01-08 11:05:59 +0100

    SONAR-11519 Fix description of api/users/update WS

--- server/sonar-server/src/main/java/org/sonar/server/user/ws/UpdateAction.java
+++ server/sonar-server/src/main/java/org/sonar/server/user/ws/UpdateAction.java
@@ -74,7 +74,7 @@ public class UpdateAction implements UsersWsAction {
   @Override
   public void define(WebService.NewController controller) {
     WebService.NewAction action = controller.createAction(ACTION_UPDATE)
-      .setDescription("Update a user. If a deactivated user account exists with the given login, it will be reactivated. " +
+      .setDescription("Update a user.<br/>" +
         "Requires Administer System permission")
       .setSince("3.7")
       .setChangelog(new Change("5.2", "User's password can only be changed using the 'change_password' action."))
