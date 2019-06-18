cb944cf1ff0459a8b457680fcae22ce4264aba02
Wouter Admiraal <wouter.admiraal@sonarsource.com>
2019-06-18 08:53:08 +0200

    SONAR-11830 Prevent branch names from disappearing in portfolio projects

--- server/sonar-web/src/main/js/apps/code/components/ComponentName.tsx
+++ server/sonar-web/src/main/js/apps/code/components/ComponentName.tsx
@@ -19,14 +19,13 @@
  */
 import * as React from 'react';
 import { Link } from 'react-router';
-import Truncated from './Truncated';
 import * as theme from '../../../app/theme';
 import QualifierIcon from '../../../components/icons-components/QualifierIcon';
 import { getBranchLikeQuery } from '../../../helpers/branches';
 import LongLivingBranchIcon from '../../../components/icons-components/LongLivingBranchIcon';
 import { translate } from '../../../helpers/l10n';
 
-function getTooltip(component: T.ComponentMeasure) {
+export function getTooltip(component: T.ComponentMeasure) {
   const isFile = component.qualifier === 'FIL' || component.qualifier === 'UTS';
   if (isFile && component.path) {
     return component.path + '\n\n' + component.key;
@@ -35,7 +34,7 @@ function getTooltip(component: T.ComponentMeasure) {
   }
 }
 
-function mostCommitPrefix(strings: string[]) {
+export function mostCommonPrefix(strings: string[]) {
   const sortedStrings = strings.slice(0).sort();
   const firstString = sortedStrings[0];
   const firstStringLength = firstString.length;
@@ -50,7 +49,7 @@ function mostCommitPrefix(strings: string[]) {
   return prefix.substr(0, prefix.length - lastPrefixPart.length);
 }
 
-interface Props {
+export interface Props {
   branchLike?: T.BranchLike;
   canBrowse?: boolean;
   component: T.ComponentMeasure;
@@ -58,68 +57,81 @@ interface Props {
   rootComponent: T.ComponentMeasure;
 }
 
-export default class ComponentName extends React.PureComponent<Props> {
-  render() {
-    const { branchLike, component, rootComponent, previous, canBrowse = false } = this.props;
-    const areBothDirs = component.qualifier === 'DIR' && previous && previous.qualifier === 'DIR';
-    const prefix =
-      areBothDirs && previous !== undefined
-        ? mostCommitPrefix([component.name + '/', previous.name + '/'])
-        : '';
-    const name = prefix ? (
-      <span>
-        <span style={{ color: theme.secondFontColor }}>{prefix}</span>
-        <span>{component.name.substr(prefix.length)}</span>
-      </span>
-    ) : (
-      component.name
+export default function ComponentName({
+  branchLike,
+  component,
+  rootComponent,
+  previous,
+  canBrowse = false
+}: Props) {
+  const areBothDirs = component.qualifier === 'DIR' && previous && previous.qualifier === 'DIR';
+  const prefix =
+    areBothDirs && previous !== undefined
+      ? mostCommonPrefix([component.name + '/', previous.name + '/'])
+      : '';
+  const name = prefix ? (
+    <span>
+      <span style={{ color: theme.secondFontColor }}>{prefix}</span>
+      <span>{component.name.substr(prefix.length)}</span>
+    </span>
+  ) : (
+    component.name
+  );
+
+  let inner = null;
+
+  if (component.refKey && component.qualifier !== 'SVW') {
+    const branch = rootComponent.qualifier === 'APP' ? { branch: component.branch } : {};
+    inner = (
+      <Link
+        className="link-with-icon"
+        to={{ pathname: '/dashboard', query: { id: component.refKey, ...branch } }}>
+        <QualifierIcon qualifier={component.qualifier} /> <span>{name}</span>
+      </Link>
     );
-
-    let inner = null;
-
-    if (component.refKey && component.qualifier !== 'SVW') {
-      const branch = rootComponent.qualifier === 'APP' ? { branch: component.branch } : {};
-      inner = (
-        <Link
-          className="link-with-icon"
-          to={{ pathname: '/dashboard', query: { id: component.refKey, ...branch } }}>
-          <QualifierIcon qualifier={component.qualifier} /> <span>{name}</span>
-        </Link>
-      );
-    } else if (canBrowse) {
-      const query = { id: rootComponent.key, ...getBranchLikeQuery(branchLike) };
-      if (component.key !== rootComponent.key) {
-        Object.assign(query, { selected: component.key });
-      }
-      inner = (
-        <Link className="link-with-icon" to={{ pathname: '/code', query }}>
-          <QualifierIcon qualifier={component.qualifier} /> <span>{name}</span>
-        </Link>
-      );
-    } else {
-      inner = (
-        <span>
-          <QualifierIcon qualifier={component.qualifier} /> {name}
-        </span>
-      );
+  } else if (canBrowse) {
+    const query = { id: rootComponent.key, ...getBranchLikeQuery(branchLike) };
+    if (component.key !== rootComponent.key) {
+      Object.assign(query, { selected: component.key });
     }
+    inner = (
+      <Link className="link-with-icon" to={{ pathname: '/code', query }}>
+        <QualifierIcon qualifier={component.qualifier} /> <span>{name}</span>
+      </Link>
+    );
+  } else {
+    inner = (
+      <span>
+        <QualifierIcon qualifier={component.qualifier} /> {name}
+      </span>
+    );
+  }
 
-    if (rootComponent.qualifier === 'APP') {
-      inner = (
-        <>
+  if (rootComponent.qualifier === 'APP') {
+    return (
+      <span className="max-width-100 display-inline-flex-center">
+        <span className="text-ellipsis" title={getTooltip(component)}>
           {inner}
-          {component.branch ? (
-            <>
-              <LongLivingBranchIcon className="spacer-left little-spacer-right" />
-              <span className="note">{component.branch}</span>
-            </>
-          ) : (
-            <span className="spacer-left outline-badge">{translate('branches.main_branch')}</span>
-          )}
-        </>
-      );
-    }
-
-    return <Truncated title={getTooltip(component)}>{inner}</Truncated>;
+        </span>
+        {component.branch ? (
+          <span className="text-ellipsis spacer-left">
+            <LongLivingBranchIcon className="little-spacer-right" />
+            <span className="note">{component.branch}</span>
+          </span>
+        ) : (
+          <span className="spacer-left outline-badge flex-1">
+            {translate('branches.main_branch')}
+          </span>
+        )}
+      </span>
+    );
+  } else {
+    return (
+      <span
+        className="max-width-100 display-inline-block text-ellipsis"
+        title={getTooltip(component)}>
+        {inner}
+      </span>
+    );
   }
 }
---
+++ server/sonar-web/src/main/js/apps/code/components/__tests__/ComponentName-test.tsx
@@ -0,0 +1,107 @@
+/*
+ * SonarQube
+ * Copyright (C) 2009-2019 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+import * as React from 'react';
+import { shallow } from 'enzyme';
+import ComponentName, { getTooltip, mostCommonPrefix, Props } from '../ComponentName';
+import { mockMainBranch, mockComponentMeasure } from '../../../../helpers/testMocks';
+
+describe('#getTooltip', () => {
+  it('should correctly format component information', () => {
+    expect(getTooltip(mockComponentMeasure(true))).toMatchSnapshot();
+    expect(getTooltip(mockComponentMeasure(true, { qualifier: 'UTS' }))).toMatchSnapshot();
+    expect(getTooltip(mockComponentMeasure(true, { path: undefined }))).toMatchSnapshot();
+    expect(getTooltip(mockComponentMeasure(false))).toMatchSnapshot();
+  });
+});
+
+describe('#mostCommonPrefix', () => {
+  it('should correctly find the common path prefix', () => {
+    expect(mostCommonPrefix(['src/main/ts/tests', 'src/main/java/tests'])).toEqual('src/main/');
+    expect(mostCommonPrefix(['src/main/ts/app', 'src/main/ts/app'])).toEqual('src/main/ts/');
+    expect(mostCommonPrefix(['src/main/ts', 'lib/main/ts'])).toEqual('');
+  });
+});
+
+describe('#ComponentName', () => {
+  it('should render correctly for files', () => {
+    expect(shallowRender()).toMatchSnapshot();
+    expect(shallowRender({ canBrowse: true })).toMatchSnapshot();
+    expect(
+      shallowRender({ rootComponent: mockComponentMeasure(false, { qualifier: 'TRK' }) })
+    ).toMatchSnapshot();
+    expect(
+      shallowRender({ rootComponent: mockComponentMeasure(false, { qualifier: 'APP' }) })
+    ).toMatchSnapshot();
+    expect(
+      shallowRender({
+        component: mockComponentMeasure(true, { branch: 'foo' }),
+        rootComponent: mockComponentMeasure(false, { qualifier: 'APP' })
+      })
+    ).toMatchSnapshot();
+  });
+
+  it('should render correctly for dirs', () => {
+    expect(
+      shallowRender({
+        component: mockComponentMeasure(false, { name: 'src/main/ts/app', qualifier: 'DIR' }),
+        previous: mockComponentMeasure(false, { name: 'src/main/ts/tests', qualifier: 'DIR' })
+      })
+    ).toMatchSnapshot();
+    expect(
+      shallowRender({
+        component: mockComponentMeasure(false, { name: 'src', qualifier: 'DIR' }),
+        previous: mockComponentMeasure(false, { name: 'lib', qualifier: 'DIR' })
+      })
+    ).toMatchSnapshot();
+  });
+
+  it('should render correctly for refs', () => {
+    expect(
+      shallowRender({
+        component: mockComponentMeasure(false, {
+          branch: 'foo',
+          refKey: 'src/main/ts/app',
+          qualifier: 'TRK'
+        })
+      })
+    ).toMatchSnapshot();
+    expect(
+      shallowRender({
+        component: mockComponentMeasure(false, {
+          branch: 'foo',
+          refKey: 'src/main/ts/app',
+          qualifier: 'TRK'
+        }),
+        rootComponent: mockComponentMeasure(false, { qualifier: 'APP' })
+      })
+    ).toMatchSnapshot();
+  });
+});
+
+function shallowRender(props: Partial<Props> = {}) {
+  return shallow(
+    <ComponentName
+      branchLike={mockMainBranch()}
+      component={mockComponentMeasure(true)}
+      rootComponent={mockComponentMeasure()}
+      {...props}
+    />
+  );
+}
