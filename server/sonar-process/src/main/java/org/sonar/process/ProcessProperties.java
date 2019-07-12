c2d9ced3637a5aa08422427e6435aaecaf659feb
colin-mueller-sonarsource <44168128+colin-mueller-sonarsource@users.noreply.github.com>
2019-07-12 11:54:03 +0200

    SONAR-12279 Swap Elasticsearch options to match convention

--- server/sonar-process/src/main/java/org/sonar/process/ProcessProperties.java
+++ server/sonar-process/src/main/java/org/sonar/process/ProcessProperties.java
@@ -67,7 +67,7 @@ public class ProcessProperties {
     SEARCH_HOST("sonar.search.host", InetAddress.getLoopbackAddress().getHostAddress()),
     SEARCH_PORT("sonar.search.port", "9001"),
     SEARCH_HTTP_PORT("sonar.search.httpPort"),
-    SEARCH_JAVA_OPTS("sonar.search.javaOpts", "-Xms512m -Xmx512m -XX:+HeapDumpOnOutOfMemoryError"),
+    SEARCH_JAVA_OPTS("sonar.search.javaOpts", "-Xmx512m -Xms512m -XX:+HeapDumpOnOutOfMemoryError"),
     SEARCH_JAVA_ADDITIONAL_OPTS("sonar.search.javaAdditionalOpts", ""),
     SEARCH_REPLICAS("sonar.search.replicas"),
     SEARCH_MINIMUM_MASTER_NODES("sonar.search.minimumMasterNodes"),
