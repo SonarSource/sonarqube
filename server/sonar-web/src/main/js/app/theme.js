89a27af94d6afe36d70e57474827f902d86270a6
Grégoire Aubert <gregoire.aubert@sonarsource.com>
2019-10-03 16:04:06 +0200

    SONAR-12503 Update Alert error icon color

--- server/sonar-web/src/main/js/app/theme.js
+++ server/sonar-web/src/main/js/app/theme.js
@@ -79,7 +79,7 @@ module.exports = {
     alertBorderError: '#ebccd1',
     alertBackgroundError: '#f2dede',
     alertTextError: '#862422',
-    alertIconError: '#b81723',
+    alertIconError: '#a4030f',
 
     alertBorderWarning: '#faebcc',
     alertBackgroundWarning: '#fcf8e3',
