e7c8853b57a219ab992a6710591d229ae1b7963c
Jeremy Davis <jeremy.davis@sonarsource.com>
2020-01-15 12:11:31 +0100

    SONAR-12720 Fix api call

--- server/sonar-web/src/main/js/api/security-hotspots.ts
+++ server/sonar-web/src/main/js/api/security-hotspots.ts
@@ -46,7 +46,9 @@ export function setSecurityHotspotStatus(
 }
 
 export function commentSecurityHotspot(hotspotKey: string, comment: string): Promise<void> {
-  return post('api/hotspots/add_comment', { hotspot: hotspotKey, comment }).catch(throwGlobalError);
+  return post('/api/hotspots/add_comment', { hotspot: hotspotKey, comment }).catch(
+    throwGlobalError
+  );
 }
 
 export function getSecurityHotspots(
