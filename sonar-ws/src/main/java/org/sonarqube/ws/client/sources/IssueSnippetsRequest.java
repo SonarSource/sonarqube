bd02f38e9742d462f9d3bd8d3d147539cd91059c
Julien Lancelot <julien.lancelot@sonarsource.com>
2020-02-21 17:47:01 +0100

    SONAR-13117 Fix SSF-103

---
+++ sonar-ws/src/main/java/org/sonarqube/ws/client/sources/IssueSnippetsRequest.java
@@ -0,0 +1,47 @@
+/*
+ * SonarQube
+ * Copyright (C) 2009-2019 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonarqube.ws.client.sources;
+
+import javax.annotation.Generated;
+
+/**
+ * This is part of the internal API.
+ * This is a POST request.
+ * @see <a href="https://next.sonarqube.com/sonarqube/web_api/api/sources/issue_snippets">Further information about this action online (including a response example)</a>
+ * @since 7.8
+ */
+@Generated("sonar-ws-generator")
+public class IssueSnippetsRequest {
+
+  private String issueKey;
+
+  /**
+   * This is a mandatory parameter.
+   * Example value: "AU-Tpxb--iU5OvuD2FLy"
+   */
+  public IssueSnippetsRequest setIssueKey(String issueKey) {
+    this.issueKey = issueKey;
+    return this;
+  }
+
+  public String getIssueKey() {
+    return issueKey;
+  }
+}
