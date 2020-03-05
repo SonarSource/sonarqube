4e276405988f4f25d1365a8592d81de24890e604
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2020-03-05 09:37:56 +0100

    increase analysis log level

--- travis.sh
+++ travis.sh
@@ -41,7 +41,7 @@ BUILD)
   # the '-' at the end is needed when using set -u (the 'nounset' flag)
   # see https://stackoverflow.com/a/9824943/641955
   if [[ -n "${SONAR_TOKEN-}" ]]; then
-    ./gradlew jacocoTestReport sonarqube --no-daemon --console plain \
+    ./gradlew jacocoTestReport sonarqube --info --no-daemon --console plain \
       -Dsonar.projectKey=org.sonarsource.sonarqube:sonarqube \
       -Dsonar.organization=sonarsource \
       -Dsonar.host.url=https://sonarcloud.io \
