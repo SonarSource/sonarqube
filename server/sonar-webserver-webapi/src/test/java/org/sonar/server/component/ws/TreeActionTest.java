4ae5d153538617c36d738c5bd322630dbbba980a
Simon Brandhof <simon.brandhof@sonarsource.com>
2020-03-17 21:34:10 +0100

    SONAR-13155 upgrade gson from 2.8.5 to 2.8.6
    No major changes
    https://github.com/google/gson/blob/master/CHANGELOG.md

--- server/sonar-webserver-webapi/src/test/java/org/sonar/server/component/ws/TreeActionTest.java
+++ server/sonar-webserver-webapi/src/test/java/org/sonar/server/component/ws/TreeActionTest.java
@@ -541,7 +541,7 @@ public class TreeActionTest {
 
     Date now = new Date();
     JsonParser jsonParser = new JsonParser();
-    JsonElement jsonTree = jsonParser.parse(IOUtils.toString(getClass().getResource("tree-example.json"), UTF_8));
+    JsonElement jsonTree = jsonParser.parseString(IOUtils.toString(getClass().getResource("tree-example.json"), UTF_8));
     JsonArray components = jsonTree.getAsJsonObject().getAsJsonArray("components");
     for (int i = 0; i < components.size(); i++) {
       JsonElement componentAsJsonElement = components.get(i);
