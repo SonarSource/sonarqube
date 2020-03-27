281f480cfd65d85ebb27da9e77f15dbf71cb7f54
Wouter Admiraal <wouter.admiraal@sonarsource.com>
2020-03-27 13:38:20 +0100

    SONAR-13259 Fix branch analysis on Travis CI for public repo

--- travis.sh
+++ travis.sh
@@ -41,11 +41,20 @@ BUILD)
   # the '-' at the end is needed when using set -u (the 'nounset' flag)
   # see https://stackoverflow.com/a/9824943/641955
   if [[ -n "${SONAR_TOKEN-}" ]]; then
-    ./gradlew jacocoTestReport sonarqube --info --no-daemon --console plain \
-      -Dsonar.projectKey=org.sonarsource.sonarqube:sonarqube \
-      -Dsonar.organization=sonarsource \
-      -Dsonar.host.url=https://sonarcloud.io \
-      -Dsonar.login="$SONAR_TOKEN"
+    if [[ "${TRAVIS_BRANCH}" == "master" ]]; then
+      ./gradlew jacocoTestReport sonarqube --info --no-daemon --console plain \
+        -Dsonar.projectKey=org.sonarsource.sonarqube:sonarqube \
+        -Dsonar.organization=sonarsource \
+        -Dsonar.host.url=https://sonarcloud.io \
+        -Dsonar.login="$SONAR_TOKEN"
+    else
+      ./gradlew jacocoTestReport sonarqube --info --no-daemon --console plain \
+        -Dsonar.projectKey=org.sonarsource.sonarqube:sonarqube \
+        -Dsonar.organization=sonarsource \
+        -Dsonar.host.url=https://sonarcloud.io \
+        -Dsonar.login="$SONAR_TOKEN" \
+        -Dsonar.branch.name="$TRAVIS_BRANCH"
+    fi
   fi
   ;;
 
