48603acf7442237c1405740994ee112a1a622c62
Duarte Meneses <duarte.meneses@sonarsource.com>
2020-04-21 15:21:59 -0500

    SONAR-13221 PERM_TEMPLATES_USERS

---
+++ server/sonar-db-migration/src/test/java/org/sonar/server/platform/db/migration/version/v83/permtemplatesgroups/PopulatePermTemplatesGroupsUuidTest.java
@@ -0,0 +1,82 @@
+/*
+ * SonarQube
+ * Copyright (C) 2009-2020 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.server.platform.db.migration.version.v83.permtemplategroups;
+
+import java.sql.SQLException;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import org.junit.Rule;
+import org.junit.Test;
+import org.sonar.core.util.UuidFactory;
+import org.sonar.core.util.UuidFactoryFast;
+import org.sonar.db.CoreDbTester;
+import org.sonar.server.platform.db.migration.step.DataChange;
+import org.sonar.server.platform.db.migration.version.v83.permtemplatesgroups.PopulatePermTemplatesGroupsUuid;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PopulatePermTemplatesGroupsUuidTest {
+
+  @Rule
+  public CoreDbTester db = CoreDbTester.createForSchema(PopulatePermTemplatesGroupsUuidTest.class, "schema.sql");
+
+  private UuidFactory uuidFactory = UuidFactoryFast.getInstance();
+  private DataChange underTest = new PopulatePermTemplatesGroupsUuid(db.database(), uuidFactory);
+
+  @Test
+  public void populate_uuids() throws SQLException {
+    insertPermTemplatesGroups(1L);
+    insertPermTemplatesGroups(2L);
+    insertPermTemplatesGroups(3L);
+
+    underTest.execute();
+
+    verifyUuidsAreNotNull();
+  }
+
+  @Test
+  public void migration_is_reentrant() throws SQLException {
+    insertPermTemplatesGroups(1L);
+    insertPermTemplatesGroups(2L);
+    insertPermTemplatesGroups(3L);
+
+    underTest.execute();
+    // re-entrant
+    underTest.execute();
+
+    verifyUuidsAreNotNull();
+  }
+
+  private void verifyUuidsAreNotNull() {
+    assertThat(db.select("select uuid from perm_templates_groups")
+      .stream()
+      .map(row -> row.get("UUID"))
+      .filter(Objects::isNull)
+      .collect(Collectors.toList())).isEmpty();
+  }
+
+  private void insertPermTemplatesGroups(Long id) {
+    db.executeInsert("perm_templates_groups",
+      "id", id,
+      "user_id", id + 1,
+      "template_id", id + 2,
+      "permission_reference", "ref" + id);
+  }
+}
---
+++ server/sonar-db-migration/src/test/java/org/sonar/server/platform/db/migration/version/v83/permtplcharacteristics/AddUuidColumnToPermTplCharacteristicsTableTest.java
@@ -0,0 +1,64 @@
+/*
+ * SonarQube
+ * Copyright (C) 2009-2020 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.server.platform.db.migration.version.v83.permtplcharacteristics;
+
+import java.sql.SQLException;
+import java.sql.Types;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.sonar.db.CoreDbTester;
+import org.sonar.server.platform.db.migration.step.DdlChange;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AddUuidColumnToPermTplCharacteristicsTableTest {
+
+  @Rule
+  public CoreDbTester db = CoreDbTester.createForSchema(AddUuidColumnToPermTplCharacteristicsTableTest.class, "schema.sql");
+
+  private DdlChange underTest = new AddUuidColumnToPermTplCharacteristicsTable(db.database());
+
+  @Before
+  public void setup() {
+    insertPermTplCharacteristics(1L);
+    insertPermTplCharacteristics(2L);
+    insertPermTplCharacteristics(3L);
+  }
+
+  @Test
+  public void add_uuid_column() throws SQLException {
+    underTest.execute();
+
+    db.assertColumnDefinition("perm_tpl_characteristics", "uuid", Types.VARCHAR, 40, true);
+
+    assertThat(db.countRowsOfTable("perm_tpl_characteristics"))
+      .isEqualTo(3);
+  }
+
+  private void insertPermTplCharacteristics(Long id) {
+    db.executeInsert("perm_tpl_characteristics",
+      "id", id,
+      "group_id", id + 1,
+      "template_id", id + 2,
+      "permission_reference", "ref" + id);
+  }
+
+}
