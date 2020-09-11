1c53c91568bc9af10ca78ae5aff7a1e79f255074
Michal Duda <michal.duda@sonarsource.com>
2020-09-11 15:48:56 +0200

    SONAR-13867 Raise analysis warning in CE when scanner report has unanalysed C/C++ files

--- server/sonar-ce-task-projectanalysis/src/main/java/org/sonar/ce/task/projectanalysis/step/ReportComputationSteps.java
+++ server/sonar-ce-task-projectanalysis/src/main/java/org/sonar/ce/task/projectanalysis/step/ReportComputationSteps.java
@@ -38,6 +38,7 @@ public class ReportComputationSteps extends AbstractComputationSteps {
   private static final List<Class<? extends ComputationStep>> STEPS = Arrays.asList(
     ExtractReportStep.class,
     PersistScannerContextStep.class,
+    PerformNotAnalyzedFilesCheckStep.class,
     PersistAnalysisWarningsStep.class,
     GenerateAnalysisUuid.class,
 
