0f33764b532d9447347c5b64beba0db18e3922ac
Wouter Admiraal <wouter.admiraal@sonarsource.com>
2021-01-04 16:33:19 +0100

    Update license headers for 2021 [update headers]

--- server/sonar-web/config/jest/SetupTestEnvironment.ts
+++ server/sonar-web/config/jest/SetupTestEnvironment.ts
@@ -1,6 +1,6 @@
 /*
  * SonarQube
- * Copyright (C) 2009-2020 SonarSource SA
+ * Copyright (C) 2009-2021 SonarSource SA
  * mailto:info AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
@@ -17,7 +17,6 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-
 import ThemeContext from 'sonar-ui-common/components/theme';
 import SonarUiCommonInitializer, { DEFAULT_LOCALE } from 'sonar-ui-common/helpers/init';
 import * as theme from '../../src/main/js/app/theme';
--- server/sonar-web/src/main/js/apps/projectQualityGate/constants.ts
+++ server/sonar-web/src/main/js/apps/projectQualityGate/constants.ts
@@ -1,6 +1,6 @@
 /*
  * SonarQube
- * Copyright (C) 2009-2020 SonarSource SA
+ * Copyright (C) 2009-2021 SonarSource SA
  * mailto:info AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
@@ -17,5 +17,4 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-
 export const USE_SYSTEM_DEFAULT = '-1';
