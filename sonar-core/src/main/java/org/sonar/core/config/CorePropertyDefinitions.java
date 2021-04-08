0fe9175b3a6e5960f33843a06725f48eaf36193a
colin-mueller-sonarsource <colin.mueller@sonarsource.com>
2021-04-08 19:22:00 +0200

    SONAR-14677 Adjust Server base URL description to include HTTPS

--- sonar-core/src/main/java/org/sonar/core/config/CorePropertyDefinitions.java
+++ sonar-core/src/main/java/org/sonar/core/config/CorePropertyDefinitions.java
@@ -76,7 +76,7 @@ public class CorePropertyDefinitions {
         .build(),
       PropertyDefinition.builder(CoreProperties.SERVER_BASE_URL)
         .name("Server base URL")
-        .description("HTTP URL of this SonarQube server, such as <i>http://yourhost.yourdomain/sonar</i>. This value is used i.e. to create links in emails.")
+        .description("HTTP(S) URL of this SonarQube server, such as <i>https://yourhost.yourdomain/sonar</i>. This value is used i.e. to create links in emails.")
         .category(CoreProperties.CATEGORY_GENERAL)
         .build(),
 
