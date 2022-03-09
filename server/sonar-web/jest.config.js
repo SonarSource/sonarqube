5b6c0a28eac75d9dba2a3d53b2c55f61326b4b12
Philippe Perrin <philippe.perrin@sonarsource.com>
2022-03-09 14:59:40 +0100

    [NO-JIRA] Jest maxworker for ci only

--- server/sonar-web/jest.config.js
+++ server/sonar-web/jest.config.js
@@ -7,7 +7,6 @@ module.exports = {
       diagnostics: false
     }
   },
-  maxWorkers: 4,
   moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
   moduleNameMapper: {
     '^.+\\.(md|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
