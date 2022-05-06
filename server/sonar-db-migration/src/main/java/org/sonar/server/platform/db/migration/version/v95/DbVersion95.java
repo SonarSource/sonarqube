9f54cb9455149e9991e8ae780cf9a02cbeaf550b
Pierre <pierre.guillot@sonarsource.com>
2022-05-06 16:07:09 +0200

    Revert "SONAR-16303 migrate plugin file hash to force reloading rules"
    This reverts commit 7be1995f608831e9067b12da9be9706c3b04cd32.

--- server/sonar-db-migration/src/main/java/org/sonar/server/platform/db/migration/version/v95/DbVersion95.java
+++ server/sonar-db-migration/src/main/java/org/sonar/server/platform/db/migration/version/v95/DbVersion95.java
@@ -36,7 +36,6 @@ public class DbVersion95 implements DbVersion {
       .add(6408, "Drop column DESCRIPTIONS from RULES table", DropRuleDescriptionColumn.class)
       .add(6409, "Drop column CREATED_AT from RULES_METADATA table", DropRuleMetadataCreatedAtColumn.class)
       .add(6410, "Drop column UPDATED_AT from RULES_METADATA table", DropRuleMetadataUpdatedAtColumn.class)
-      .add(6411, "Overwrite plugin file hash to force reloading rules", OverwritePluginFileHash.class)
       ;
   }
 }
