565635c50c30f678edc98b79a44907aa37ce7946
Olaf Śnieżek <osniezek@gradle.com>
2022-11-17 13:31:00 +0100

    Fixed formatting in LicenseReader

--- buildSrc/src/main/groovy/org.sonar.build/LicenseReader.groovy
+++ buildSrc/src/main/groovy/org.sonar.build/LicenseReader.groovy
@@ -5,25 +5,25 @@ import groovy.json.JsonSlurper
 
 class LicenseReader extends FilterReader {
 
-    LicenseReader(Reader fileReader) {
-        super(build(fileReader))
-    }
+  LicenseReader(Reader fileReader) {
+    super(build(fileReader))
+  }
 
-    private static Reader build(Reader fileReader) {
-        def json = new JsonSlurper().parse(fileReader)
+  private static Reader build(Reader fileReader) {
+    def json = new JsonSlurper().parse(fileReader)
 
-        json.dependencies.each { dependency ->
-            if (dependency.licenses.size() > 1) {
-                def idx = dependency.licenses.findIndexOf { it.name == "Elastic License 2.0" }
-                if (idx >= 0) {
-                    dependency.licenses = [dependency.licenses[idx]]
-                }
-            }
+    json.dependencies.each { dependency ->
+      if (dependency.licenses.size() > 1) {
+        def idx = dependency.licenses.findIndexOf { it.name == "Elastic License 2.0" }
+        if (idx >= 0) {
+          dependency.licenses = [dependency.licenses[idx]]
         }
-
-        json.dependencies.sort { it.name }
-
-        def jsonText = JsonOutput.toJson(json)
-        return new StringReader(JsonOutput.prettyPrint(jsonText))
+      }
     }
+
+    json.dependencies.sort { it.name }
+
+    def jsonText = JsonOutput.toJson(json)
+    return new StringReader(JsonOutput.prettyPrint(jsonText))
+  }
 }
