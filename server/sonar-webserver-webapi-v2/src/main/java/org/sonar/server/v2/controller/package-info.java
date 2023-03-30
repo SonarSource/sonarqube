d52d1132c486145376cda30d8042448af630dfbc
Aurelien Poscia <aurelien.poscia@sonarsource.com>
2023-03-30 16:03:14 +0200

    NO-JIRA added missing package-info.java

--- server/sonar-webserver-webapi-v2/src/main/java/org/sonar/server/v2/controller/package-info.java
+++ server/sonar-webserver-webapi-v2/src/main/java/org/sonar/server/v2/controller/package-info.java
@@ -18,7 +18,6 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 @ParametersAreNonnullByDefault
-package org.sonar.server.management;
+package org.sonar.server.v2.controller;
 
 import javax.annotation.ParametersAreNonnullByDefault;
-
