d3e2410ab73229f519e3740f00479d4383bca7ea
Wouter Admiraal <wouter.admiraal@sonarsource.com>
2023-07-06 09:26:11 +0200

    [NO JIRA] Move SCIM bbts to core-extension-scim

---
+++ buildSrc/src/main/groovy/org.sonar.build/BlackBoxTest.groovy
@@ -0,0 +1,25 @@
+package org.sonar.build
+
+import org.gradle.api.tasks.testing.Test
+
+class BlackBoxTest extends Test {
+  BlackBoxTest() {
+    def branch = System.getenv('GITHUB_BRANCH')
+    if (branch != null && Set.of("branch-nightly-build", "master").contains(branch)) {
+      jvmArgs("-javaagent:" + System.getenv('ASPECTJ_WEAVER_PATH'))
+    }
+
+    systemProperty 'java.awt.headless', 'true'
+    systemProperty 'orchestrator.configUrl', System.getProperty('orchestrator.configUrl')
+
+    if (!project.version.endsWith("-SNAPSHOT")) {
+      systemProperty 'sonar.runtimeVersion', project.version
+    }
+
+    testLogging {
+      events "skipped", "failed"
+      showStandardStreams = true
+      exceptionFormat 'full'
+    }
+  }
+}
