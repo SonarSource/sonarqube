53d88b962a15bd617f4e5608db33cf7ae3c3d557
lokeswarayadav-ar <110166241+lokeswarayadav-ar@users.noreply.github.com>
2023-09-05 18:57:29 +0530

    CD-4133 Unable to see projects and issues of an organization even though projects are present if user tries to login as root user. (#342)

--- server/sonar-webserver-es/src/main/java/org/sonar/server/permission/index/WebAuthorizationTypeSupport.java
+++ server/sonar-webserver-es/src/main/java/org/sonar/server/permission/index/WebAuthorizationTypeSupport.java
@@ -49,6 +49,9 @@ public class WebAuthorizationTypeSupport {
    * user has read access.
    */
   public QueryBuilder createQueryFilter() {
+    if (userSession.isRoot()) {
+      return QueryBuilders.matchAllQuery();
+    }
     BoolQueryBuilder filter = boolQuery();
 
     // anyone
