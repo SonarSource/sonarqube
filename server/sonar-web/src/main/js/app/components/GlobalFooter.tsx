55b03321e6a362fe6ae01edbd263e8bfc3232246
stanislavh <stanislav.honcharov@sonarsource.com>
2023-11-24 15:15:09 +0100

    SONAR-20326 Fix positioning of embedded DB warning

--- server/sonar-web/src/main/js/app/components/GlobalFooter.tsx
+++ server/sonar-web/src/main/js/app/components/GlobalFooter.tsx
@@ -17,6 +17,7 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
+import { LargeCenteredLayout } from 'design-system';
 import * as React from 'react';
 import DocLink from '../../components/common/DocLink';
 import InstanceMessage from '../../components/common/InstanceMessage';
@@ -37,58 +38,60 @@ export default function GlobalFooter({ hideLoggedInInfo }: GlobalFooterProps) {
 
   return (
     <div className="page-footer page-container" id="footer">
-      {appState?.productionDatabase === false && (
-        <Alert display="inline" id="evaluation_warning" variant="warning">
-          <p className="big">{translate('footer.production_database_warning')}</p>
-          <p>
-            <InstanceMessage message={translate('footer.production_database_explanation')} />
-          </p>
-        </Alert>
-      )}
+      <LargeCenteredLayout className=" sw-flex sw-flex-col sw-items-center">
+        {appState?.productionDatabase === false && (
+          <Alert display="inline" id="evaluation_warning" variant="warning">
+            <p className="big">{translate('footer.production_database_warning')}</p>
+            <p>
+              <InstanceMessage message={translate('footer.production_database_explanation')} />
+            </p>
+          </Alert>
+        )}
 
-      <GlobalFooterBranding />
+        <GlobalFooterBranding />
 
-      <ul className="page-footer-menu">
-        {!hideLoggedInInfo && currentEdition && (
-          <li className="page-footer-menu-item">{currentEdition.name}</li>
-        )}
-        {!hideLoggedInInfo && appState?.version && (
+        <ul className="page-footer-menu">
+          {!hideLoggedInInfo && currentEdition && (
+            <li className="page-footer-menu-item">{currentEdition.name}</li>
+          )}
+          {!hideLoggedInInfo && appState?.version && (
+            <li className="page-footer-menu-item">
+              {translateWithParameters('footer.version_x', appState.version)}
+            </li>
+          )}
           <li className="page-footer-menu-item">
-            {translateWithParameters('footer.version_x', appState.version)}
+            <a
+              href="https://www.gnu.org/licenses/lgpl-3.0.txt"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              {translate('footer.license')}
+            </a>
           </li>
-        )}
-        <li className="page-footer-menu-item">
-          <a
-            href="https://www.gnu.org/licenses/lgpl-3.0.txt"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            {translate('footer.license')}
-          </a>
-        </li>
-        <li className="page-footer-menu-item">
-          <a
-            href="https://community.sonarsource.com/c/help/sq"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            {translate('footer.community')}
-          </a>
-        </li>
-        <li className="page-footer-menu-item">
-          <DocLink to="/">{translate('footer.documentation')}</DocLink>
-        </li>
-        <li className="page-footer-menu-item">
-          <DocLink to="/instance-administration/plugin-version-matrix/">
-            {translate('footer.plugins')}
-          </DocLink>
-        </li>
-        {!hideLoggedInInfo && (
           <li className="page-footer-menu-item">
-            <Link to="/web_api">{translate('footer.web_api')}</Link>
+            <a
+              href="https://community.sonarsource.com/c/help/sq"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              {translate('footer.community')}
+            </a>
           </li>
-        )}
-      </ul>
+          <li className="page-footer-menu-item">
+            <DocLink to="/">{translate('footer.documentation')}</DocLink>
+          </li>
+          <li className="page-footer-menu-item">
+            <DocLink to="/instance-administration/plugin-version-matrix/">
+              {translate('footer.plugins')}
+            </DocLink>
+          </li>
+          {!hideLoggedInInfo && (
+            <li className="page-footer-menu-item">
+              <Link to="/web_api">{translate('footer.web_api')}</Link>
+            </li>
+          )}
+        </ul>
+      </LargeCenteredLayout>
     </div>
   );
 }
