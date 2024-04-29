b7c16152a0fa0214811985a8a5279fb9bc2ba09a
Alain Kermis <alain.kermis@sonarsource.com>
2024-04-29 18:32:32 +0200

    [NO-JIRA] Fix issue related to local variable naming

--- server/sonar-auth-gitlab/src/test/java/org/sonar/auth/gitlab/GitLabIdentityProviderTest.java
+++ server/sonar-auth-gitlab/src/test/java/org/sonar/auth/gitlab/GitLabIdentityProviderTest.java
@@ -120,8 +120,6 @@ public class GitLabIdentityProviderTest {
   public void init_whenErrorWhileBuildingScribe_shouldReThrow() {
     IllegalStateException exception = new IllegalStateException("GitLab authentication is disabled");
     when(scribeFactory.newScribe(any(), any(), any())).thenThrow(exception);
-
-    OAuth2IdentityProvider.InitContext initContext = mock(OAuth2IdentityProvider.InitContext.class);
     when(initContext.getCallbackUrl()).thenReturn("http://server/callback");
 
     assertThatIllegalStateException()
