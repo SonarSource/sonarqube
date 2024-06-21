8e24b10dc39071401722ff654055f14cf0585c3e
Steve Marion <steve.marion@sonarsource.com>
2024-06-21 17:00:33 +0200

    SONAR-21207 make nightly build depend on external trigger instead of internal state detection

--- buildSrc/src/main/groovy/org.sonar.build/BlackBoxTest.groovy
+++ buildSrc/src/main/groovy/org.sonar.build/BlackBoxTest.groovy
@@ -4,11 +4,6 @@ import org.gradle.api.tasks.testing.Test
 
 class BlackBoxTest extends Test {
   BlackBoxTest() {
-    def branch = System.getenv('GITHUB_BRANCH')
-    if (branch != null && Set.of("branch-nightly-build", "master").contains(branch)) {
-      jvmArgs("-javaagent:" + System.getenv('ASPECTJ_WEAVER_PATH'))
-    }
-
     systemProperty 'java.awt.headless', 'true'
     systemProperty 'orchestrator.configUrl', System.getProperty('orchestrator.configUrl')
     systemProperty 'webdriver.chrome.driver', System.getProperty('webdriver.chrome.driver')
