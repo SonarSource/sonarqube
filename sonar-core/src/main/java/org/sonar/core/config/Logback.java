443491c44c6d33ab11bbba4d0ff1bea02b5d1ac5
Matteo Mara <matteo.mara@sonarsource.com>
2024-07-08 15:00:17 +0200

    NO-JIRA Remove use of deprecated StatusPrinter class

--- sonar-core/src/main/java/org/sonar/core/config/Logback.java
+++ sonar-core/src/main/java/org/sonar/core/config/Logback.java
@@ -22,7 +22,7 @@ package org.sonar.core.config;
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.joran.JoranConfigurator;
 import ch.qos.logback.core.joran.spi.JoranException;
-import ch.qos.logback.core.util.StatusPrinter;
+import ch.qos.logback.core.util.StatusPrinter2;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -74,7 +74,8 @@ public class Logback {
     } finally {
       IOUtils.closeQuietly(input);
     }
-    StatusPrinter.printInCaseOfErrorsOrWarnings(lc);
+    var statusPrinter2 = new StatusPrinter2();
+    statusPrinter2.printInCaseOfErrorsOrWarnings(lc);
   }
 
   private static LoggerContext configureContext(LoggerContext context, Map<String, String> substitutionVariables) {
