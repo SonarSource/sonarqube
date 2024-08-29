583e4bc1630edd9440af57856f040bf90874e364
Matteo Mara <matteo.mara@sonarsource.com>
2024-08-29 15:07:58 +0200

    NO-JIRA Override telemetry metrics URL in telemetry patch

--- scripts/patches/telemetry.sh
+++ scripts/patches/telemetry.sh
@@ -5,6 +5,7 @@ SQ_HOME=$1
 
 echo "configuring telemetry"
 set_property sonar.telemetry.url https://<your_account>.eu.ngrok.io "$SQ_HOME/conf/sonar.properties"
+set_property sonar.telemetry.metrics.url https://<your_account>.eu.ngrok.io "$SQ_HOME/conf/sonar.properties"
 set_property sonar.telemetry.enable true "$SQ_HOME/conf/sonar.properties"
 set_property sonar.telemetry.compression false "$SQ_HOME/conf/sonar.properties"
 set_property sonar.telemetry.frequencyInSeconds 10 "$SQ_HOME/conf/sonar.properties"
