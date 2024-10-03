ffd90e0f542f6b813d93c3c5d3fde4b4b9fc5b76
ritesh-ghiya-cs <149673688+ritesh-ghiya-cs@users.noreply.github.com>
2024-10-03 18:03:43 +0530

    [CD-5916] Remove google analytics and pendo scripts (#431)

--- server/sonar-web/config/indexHtmlTemplate.js
+++ server/sonar-web/config/indexHtmlTemplate.js
@@ -40,30 +40,6 @@ module.exports = (cssHash, jsHash) => `
     <meta name="msapplication-TileImage" content="%WEB_CONTEXT%/mstile-512x512.png" />
     <title>%INSTANCE%</title>
 
-    <!-- Google Tag Manager -->
-    <script>
-      if (window.location.hostname.includes('codescan.io')) {
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-TGN67LR');
-      }
-    </script>
-    <!-- End Google Tag Manager -->
-
-    <!-- Pendo.io -->
-    <script>
-      if (window.location.hostname.includes('codescan.io') || window.location.hostname.includes('autorabit.com')) {
-        (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=o._q||[];
-        v=['initialize','identify','updateOptions','pageLoad','track'];for(w=0,x=v.length;w<x;++w)(function(m){
-          o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);
-        y=e.createElement(n);y.async=!0;y.src='https://cdn.pendo.io/agent/static/6c4b3816-7b04-42d8-6025-bd01283d95a3/pendo.js';
-        z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
-      }
-    </script>
-    <!-- End Pendo.io -->
-
     <link rel="stylesheet" href="%WEB_CONTEXT%/js/out${cssHash}.css" />
 </head>
 
