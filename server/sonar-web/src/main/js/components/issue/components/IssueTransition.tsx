15993571359f929ae933042a6383f3a6f9761949
Rohit Srikar <rohit.srikar@autorabit.com>
2024-12-03 16:18:25 +0530

    CD-6246: Added background

--- server/sonar-web/src/main/js/components/issue/components/IssueTransition.tsx
+++ server/sonar-web/src/main/js/components/issue/components/IssueTransition.tsx
@@ -84,7 +84,7 @@ export default function IssueTransition(props: Readonly<Props>) {
         withClickOutHandler={!guideIsRunning}
         withFocusOutHandler={!guideIsRunning}
         overlay={
-          <div className="sw-fixed">
+          <div className="sw-fixed bg-white">
             <IssueTransitionOverlay
               issue={issue}
               onClose={handleClose}
