5f07343798c10a6e7b478f3500c23f655072fc45
Jeremy Davis <jeremy.davis@sonarsource.com>
2024-12-03 17:08:22 +0100

    SONAR-23605 Fix config and add missing files

--- .cirrus/cirrus-yarn-lint-report.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-source .cirrus/cirrus-env
-
-./gradlew yarn_lint-report-ci \
-  --parallel --configure-on-demand --console plain -Pqa
--- .cirrus/cirrus-yarn-validate-ci.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-source .cirrus/cirrus-env
-
-./gradlew yarn_validate-ci \
-  --parallel --configure-on-demand --console plain -Pqa
