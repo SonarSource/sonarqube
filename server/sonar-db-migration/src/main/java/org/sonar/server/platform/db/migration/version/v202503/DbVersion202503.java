267e2d6f0d9103b35028b824239ec4e70bf9d642
Steve Marion <steve.marion@sonarsource.com>
2025-05-02 15:17:28 +0200

    SONAR-24909 SONAR-24923 Add graph_version to architecture_graphs table.
    Signed-off-by: Steve Marion <steve.marion@sonarsource.com>

--- server/sonar-db-migration/src/main/java/org/sonar/server/platform/db/migration/version/v202503/DbVersion202503.java
+++ server/sonar-db-migration/src/main/java/org/sonar/server/platform/db/migration/version/v202503/DbVersion202503.java
@@ -74,6 +74,7 @@ public class DbVersion202503 implements DbVersion {
       .add(2025_03_035, "Rename column 'source' to 'ecosystem' on 'architecture_graphs' table", UpdateArchitectureGraphsSourceColumnRename.class)
       .add(2025_03_036, "Create unique index on 'architecture_graphs' table", CreateUniqueIndexOnArchitectureGraphs.class)
       .add(2025_03_037, "Add previous_manual_status to SCA issues releases", AddPreviousManualStatusToScaIssuesReleases.class)
+      .add(2025_03_038, "Add 'graph_version' column to 'architecture_graphs' table", AddGraphVersionOnArchitectureGraphsTable.class)
 
     ;
   }
