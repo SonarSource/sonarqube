bd107fbfa8f850806939d771e5813d3adbe3b9e3
Rajashekar-codescan <rajashekar.chekka@autorabit.com>
2025-06-11 14:36:15 +0530

    Reapply "version change 25.1.2"
    This reverts commit 00a5a13a6933deff99fd9d0e8f7d7719663f36f9.

--- server/sonar-web/src/main/js/components/embed-docs-modal/EmbedDocsPopupHelper.tsx
+++ server/sonar-web/src/main/js/components/embed-docs-modal/EmbedDocsPopupHelper.tsx
@@ -82,7 +82,7 @@ export default function EmbedDocsPopupHelper() {
       {aboutCodescanOpen &&
         renderAboutCodescan(
           'https://knowledgebase.autorabit.com/codescan/docs/codescan-release-notes',
-          'embed-doc/codescan-version-25_1_1.png',
+          'embed-doc/codescan-version-25_1_2.png',
           translate('embed_docs.codescan_version'),
         )}
     </div>
