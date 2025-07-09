dc89821daac4fdaeddae9e6b2e9c13687efa4cfc
Dhanush Terala <dhanush.terala@autorabit.com>
2025-07-09 12:25:05 +0530

    CD-6397 removed enterprise tags

--- server/sonar-web/src/main/js/apps/projectInformation/constants.ts
+++ server/sonar-web/src/main/js/apps/projectInformation/constants.ts
@@ -3,6 +3,3 @@ export const GITLAB_ALM_TAG = 'gitlab';
 export const GIT_ALM_TAG = 'git';
 export const BITBUCKET_ALM_TAG = 'bitbucket';
 export const SALESFORCE_ALM_TAG = 'salesforce';
-export const GITHUB_ENTERPRISE_ALM_TAG = 'github-enterprise';
-export const GITLAB_ENTERPRISE_ALM_TAG = 'gitlab-enterprise';
-export const BITBUCKET_ENTERPRISE_ALM_TAG = 'bitbucket-enterprise';
