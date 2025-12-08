0a672a36649382571fe878e8467adcdb8d2192de
lokeswarayadav-ar <110166241+lokeswarayadav-ar@users.noreply.github.com>
2025-12-08 11:01:21 +0530

    CodeScan Version 25.1.15 (#631)
    Co-authored-by: ritesh.ghiya <ritesh.ghiya@autorabit.com>

--- server/sonar-web/src/main/js/components/embed-docs-modal/EmbedDocsPopupHelper.tsx
+++ server/sonar-web/src/main/js/components/embed-docs-modal/EmbedDocsPopupHelper.tsx
@@ -80,7 +80,7 @@ export default function EmbedDocsPopupHelper() {
       {aboutCodescanOpen &&
         renderAboutCodescan(
           'https://knowledgebase.autorabit.com/codescan/docs/codescan-release-notes',
-          'embed-doc/codescan-version-25_1_14.png',
+          'embed-doc/codescan-version-25_1_15.png',
           translate('embed_docs.codescan_version'),
         )}
     </div>
