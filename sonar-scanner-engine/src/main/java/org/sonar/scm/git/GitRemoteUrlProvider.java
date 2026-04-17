9ec5e86425011f6c1ffa0eb2db08880e3b347271
Lukasz Jarocki <lukasz.jarocki@sonarsource.com>
2026-04-17 09:11:12 +0200

    SONAR-26720 Updated timeless license headers

--- sonar-scanner-engine/src/main/java/org/sonar/scm/git/GitRemoteUrlProvider.java
+++ sonar-scanner-engine/src/main/java/org/sonar/scm/git/GitRemoteUrlProvider.java
@@ -1,6 +1,6 @@
 /*
  * SonarQube
- * Copyright (C) 2009-2025 SonarSource Sàrl
+ * Copyright (C) SonarSource Sàrl
  * mailto:info AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
@@ -17,7 +17,6 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-
 package org.sonar.scm.git;
 
 import java.net.URI;
