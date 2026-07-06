049a8fd0a214a17bd9c7f38a0c7d6904067ed027
Julien HENRY <julien.henry@sonarsource.com>
2026-07-06 15:58:54 +0200

    SONAR-30645 Add 'origin' column migration for arch_directives

---
+++ server/sonar-db-migration/src/test/java/org/sonar/server/platform/db/migration/version/v202604/AddOriginToArchDirectivesTest.java
@@ -0,0 +1,55 @@
+/*
+ * SonarQube
+ * Copyright (C) SonarSource Sàrl
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.server.platform.db.migration.version.v202604;
+
+import java.sql.SQLException;
+import java.sql.Types;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.sonar.db.MigrationDbTester;
+
+import static org.sonar.server.platform.db.migration.version.v202604.AddOriginToArchDirectives.COLUMN_NAME;
+import static org.sonar.server.platform.db.migration.version.v202604.AddOriginToArchDirectives.COLUMN_SIZE;
+import static org.sonar.server.platform.db.migration.version.v202604.AddOriginToArchDirectives.TABLE_NAME;
+
+class AddOriginToArchDirectivesTest {
+
+  @RegisterExtension
+  public final MigrationDbTester db = MigrationDbTester.createForMigrationStep(AddOriginToArchDirectives.class);
+
+  private final AddOriginToArchDirectives underTest = new AddOriginToArchDirectives(db.database());
+
+  @Test
+  void execute_shouldAddNullableOriginColumn() throws SQLException {
+    db.assertColumnDoesNotExist(TABLE_NAME, COLUMN_NAME);
+
+    underTest.execute();
+
+    db.assertColumnDefinition(TABLE_NAME, COLUMN_NAME, Types.VARCHAR, COLUMN_SIZE, true);
+  }
+
+  @Test
+  void execute_shouldBeReentrant() throws SQLException {
+    underTest.execute();
+    underTest.execute();
+
+    db.assertColumnDefinition(TABLE_NAME, COLUMN_NAME, Types.VARCHAR, COLUMN_SIZE, true);
+  }
+}
