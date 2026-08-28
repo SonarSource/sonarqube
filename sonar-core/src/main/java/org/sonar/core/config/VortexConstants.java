0e1d5c6ba8defa5ff4eda78e61dff3e162c7466b
Léo Geoffroy <leo.geoffroy@sonarsource.com>
2026-08-28 15:38:00 +0200

    SONAR-31932 Audit-track sonar.vortex.enabled property changes

---
+++ sonar-core/src/main/java/org/sonar/core/config/VortexConstants.java
@@ -0,0 +1,32 @@
+/*
+ * SonarQube
+ * Copyright (C) SonarSource Sàrl
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.core.config;
+
+public final class VortexConstants {
+  /**
+   * Lives directly under the {@code sonar.vortex.*} namespace, as it toggles the whole feature and not only the
+   * analysis part.
+   */
+  public static final String VORTEX_ENABLED_PROPERTY = "sonar.vortex.enabled";
+
+  private VortexConstants() {
+  }
+
+}
